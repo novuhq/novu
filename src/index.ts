3413eec5df10a391d04988f49d5920ea60ad0104
Dima Grossman <dima@grossman.io>
2021-08-26 16:12:29 +0300

    Extract sendgrid provider to repository

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './lib/notifire';
 export * from './lib/notifire.interface';
 export * from './lib/template/template.interface';
-export * from './providers/sendgrid.provider';
+export * from './lib/provider/provider.interface';
