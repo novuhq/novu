7c532cdff7404516ff3c375326126438e02bb03a
ComBarnea <com.barnea@gmail.com>
2021-10-09 12:25:25 +0300

    :factory: update test to use generic names

--- providers/postmark/src/lib/postmark.provider.spec.ts
+++ providers/postmark/src/lib/postmark.provider.spec.ts
@@ -2,8 +2,8 @@ import { PostmarkEmailProvider } from './postmark.provider';
 
 test('should trigger postmark correctly', async () => {
   const provider = new PostmarkEmailProvider({
-    apiKey: '0d5d3d8c-53d6-4006-b9e2-d8f72822c852',
-    from: 'tomer@notifire.co',
+    apiKey: '<postmark-id>',
+    from: 'test@test.com',
   });
   const spy = jest
     .spyOn(provider['client'], 'sendEmail')
@@ -12,15 +12,15 @@ test('should trigger postmark correctly', async () => {
       return {} as any;
     });
   await provider.sendMessage({
-    to: 'tomer@notifire.co',
+    to: 'test2@test.com',
     subject: 'test subject',
     html: '<div> Mail Content </div>',
   });
 
   expect(spy).toHaveBeenCalled();
   expect(spy).toHaveBeenCalledWith({
-    From: 'tomer@notifire.co',
-    To: 'tomer@notifire.co',
+    From: 'test@test.com',
+    To: 'test2@test.com',
     HtmlBody: '<div> Mail Content </div>',
     TextBody: '<div> Mail Content </div>',
     Subject: 'test subject',
