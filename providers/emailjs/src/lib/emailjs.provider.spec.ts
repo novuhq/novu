43ae16642dba6eaacf8a5ead9aae025735602f67
Sudarsan Balaji <sudarsanb@sahaj.ai>
2021-10-13 13:15:16 +0530

    Add a provider for emailjs

---
+++ providers/emailjs/src/lib/emailjs.provider.spec.ts
@@ -0,0 +1,57 @@
+const sendAsyncMock = jest.fn().mockResolvedValue(undefined);
+const SMTPClientMock = jest.fn().mockImplementation(() => {
+  return { sendAsync: sendAsyncMock };
+});
+const MessageMock = jest.fn().mockImplementation((hash) => hash);
+
+import { EmailJsProvider } from './emailjs.provider';
+
+jest.mock('emailjs', () => {
+  return {
+    SMTPClient: SMTPClientMock,
+    Message: MessageMock,
+  };
+});
+
+test('should trigger emailjs correctly', async () => {
+  const provider = new EmailJsProvider({
+    from: 'test@test.com',
+    host: 'test.test.email',
+    user: 'test@test.com',
+    password: 'test123',
+    port: 587,
+    secure: false,
+  });
+
+  await provider.sendMessage({
+    to: 'test@test2.com',
+    subject: 'test subject',
+    html: '<div> Mail Content </div>',
+    text: 'Mail content',
+  });
+
+  expect(SMTPClientMock).toHaveBeenCalled();
+  expect(SMTPClientMock).toHaveBeenCalledWith({
+    host: 'test.test.email',
+    user: 'test@test.com',
+    password: 'test123',
+    port: 587,
+    ssl: false,
+  });
+  expect(MessageMock).toHaveBeenCalled();
+  expect(MessageMock).toHaveBeenCalledWith({
+    from: 'test@test.com',
+    to: 'test@test2.com',
+    subject: 'test subject',
+    text: 'Mail content',
+    attachment: { data: '<div> Mail Content </div>', alternative: true },
+  });
+  expect(sendAsyncMock).toHaveBeenCalled();
+  expect(sendAsyncMock).toHaveBeenCalledWith({
+    from: 'test@test.com',
+    to: 'test@test2.com',
+    subject: 'test subject',
+    text: 'Mail content',
+    attachment: { data: '<div> Mail Content </div>', alternative: true },
+  });
+});
---
+++ providers/emailjs/src/lib/emailjs.provider.ts
@@ -0,0 +1,39 @@
+import { ChannelTypeEnum, IEmailOptions, IEmailProvider } from '@notifire/core';
+import { Message, SMTPClient } from 'emailjs';
+import { EmailJsConfig } from './emailjs.config';
+
+export class EmailJsProvider implements IEmailProvider {
+  readonly id = 'emailjs';
+  readonly channelType = ChannelTypeEnum.EMAIL as ChannelTypeEnum.EMAIL;
+  private readonly client: SMTPClient;
+
+  constructor(private readonly config: EmailJsConfig) {
+    const { host, port, secure: ssl, user, password } = this.config;
+    this.client = new SMTPClient({
+      host,
+      port,
+      ssl,
+      user,
+      password,
+    });
+  }
+
+  async sendMessage({
+    from,
+    to,
+    subject,
+    text,
+    html,
+  }: // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  IEmailOptions): Promise<any> {
+    return await this.client.sendAsync(
+      new Message({
+        from: from || this.config.from,
+        to,
+        subject,
+        text,
+        attachment: { data: html, alternative: true },
+      })
+    );
+  }
+}
