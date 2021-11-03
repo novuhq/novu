602b9fa63bb1c629f8099e7573bfece8827e2f39
Duy Nguyen <nguyenduy010798@gmail.com>
2021-11-03 19:56:33 +0200

    add spec for sns provider

--- providers/sns/src/lib/sns.provider.spec.ts
+++ providers/sns/src/lib/sns.provider.spec.ts
@@ -1,6 +1,35 @@
+import { SNSClient } from '@aws-sdk/client-sns';
+
 import { SnsEmailProvider } from './sns.provider';
 
-const snsProvider = new SnsEmailProvider({apiKey: ''})
-
 test('should trigger sns library correctly', async () => {
+  const mockResponse = { MessageId: 'mock-message-id'  };
+  const spy = jest
+    .spyOn(SNSClient.prototype, 'send')
+    .mockImplementation(async () => mockResponse);
+
+  const mockConfig = {
+    from: '',
+    accessKeyId: 'TEST',
+    secretAccessKey: 'TEST',
+    region: 'test-1',
+  };
+  const provider = new SnsEmailProvider(mockConfig);
+
+  const mockNotifireMessage = {
+    to: '0123456789',
+    content: 'hello',
+  };
+  const response = await provider.sendMessage(mockNotifireMessage);
+
+  const publishInput = {
+    input: {
+      PhoneNumber: mockNotifireMessage.to,
+      Message: mockNotifireMessage.content
+    }
+  }
+
+  expect(spy).toHaveBeenCalled();
+  expect(spy).toHaveBeenCalledWith(expect.objectContaining(publishInput))
+  expect(response.id).toBe(mockResponse.MessageId)
 });
