ee5cea67292c36ca835fc3e228274e67b2812047
Gal Ezra <galezra@gmail.com>
2021-11-12 19:09:16 +0200

    Added sendinblue as a provider

--- providers/sendinblue/src/lib/sendinblue.provider.spec.ts
+++ providers/sendinblue/src/lib/sendinblue.provider.spec.ts
@@ -1,5 +1,29 @@
-// import { SendinblueEmailProvider } from './sendinblue.provider';
+import { SendinblueEmailProvider } from './sendinblue.provider';
 
 test('should trigger sendinblue library correctly', async () => {
-  expect(true).toBe(true);
+  const provider = new SendinblueEmailProvider({
+    apiKey:
+      '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',
+  });
+  const spy = jest
+    .spyOn(provider, 'sendMessage')
+    .mockImplementation(async () => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return {} as any;
+    });
+
+  await provider.sendMessage({
+    from: 'test@test.com',
+    to: 'test@test.com',
+    html: '<div> Mail Content </div>',
+    subject: 'Test subject',
+  });
+
+  expect(spy).toBeCalled();
+  expect(spy).toBeCalledWith({
+    from: 'test@test.com',
+    to: 'test@test.com',
+    html: '<div> Mail Content </div>',
+    subject: 'Test subject',
+  });
 });
--- providers/sendinblue/src/lib/sendinblue.provider.ts
+++ providers/sendinblue/src/lib/sendinblue.provider.ts
@@ -4,26 +4,52 @@ import {
   IEmailProvider,
   ISendMessageSuccessResponse,
 } from '@notifire/core';
-
+import {
+  SendSmtpEmail,
+  SendSmtpEmailTo,
+  TransactionalEmailsApi,
+  TransactionalEmailsApiApiKeys,
+} from '@sendinblue/client';
 export class SendinblueEmailProvider implements IEmailProvider {
   id = 'sendinblue';
   channelType = ChannelTypeEnum.EMAIL as ChannelTypeEnum.EMAIL;
+  private transactionalEmailsApi: TransactionalEmailsApi;
 
   constructor(
     private config: {
       apiKey: string;
     }
   ) {
-    console.log(this.config);
+    this.transactionalEmailsApi = new TransactionalEmailsApi();
+    this.transactionalEmailsApi.setApiKey(
+      TransactionalEmailsApiApiKeys.apiKey,
+      this.config.apiKey
+    );
   }
 
   async sendMessage(
     options: IEmailOptions
   ): Promise<ISendMessageSuccessResponse> {
-    console.log(options);
+    const email = new SendSmtpEmail();
+    email.sender = { email: options.from || options.from };
+    email.to = getFormattedTo(options.to);
+    email.subject = options.subject;
+    email.htmlContent = options.html;
+    email.textContent = options.text;
+
+    const { response, body } =
+      await this.transactionalEmailsApi.sendTransacEmail(email);
+
     return {
-      id: 'PLACEHOLDER',
-      date: 'PLACEHOLDER',
+      id: body?.messageId,
+      date: response?.headers?.date,
     };
   }
+}
+
+function getFormattedTo(to: string | string[]): SendSmtpEmailTo[] {
+  if (typeof to === 'string') {
+    return [{ email: to }];
+  }
+  return to.map((email: string) => ({ email }));
 }
