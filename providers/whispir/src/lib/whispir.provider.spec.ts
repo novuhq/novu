81c04ef472659d52e7b99003fe9f9749a2c2107f
Dima Grossman <dima@grossman.io>
2021-11-13 22:08:14 +0200

    Update whispir.provider.spec.ts

--- providers/whispir/src/lib/whispir.provider.spec.ts
+++ providers/whispir/src/lib/whispir.provider.spec.ts
@@ -1,7 +1,7 @@
-import { whispirSmsProvider } from './whispir.provider';
+import { WhispirSmsProvider } from './whispir.provider';
 
 test('should trigger whispir correctly', async () => {
-  const provider = new whispirSmsProvider({
+  const provider = new WhispirSmsProvider({
     username: "john.smith",
       password: "one2three",
       apiKey: "ds0dsf09hdfsh90df90hdfsj",
