f9a7dfc1342df74e6ca77af956be19f4ade9b0af
galezra <galezra@gmail.com>
2021-11-20 10:06:39 +0200

    test: set up a mock object for better convention

--- providers/postmark/src/lib/postmark.provider.spec.ts
+++ providers/postmark/src/lib/postmark.provider.spec.ts
@@ -1,10 +1,21 @@
 import { PostmarkEmailProvider } from './postmark.provider';
 
+const mockConfig = {
+  apiKey: '<postmark-id>',
+  from: 'test@test.com',
+};
+
+const mockNotifireMessage = {
+  to: 'test2@test.com',
+  subject: 'test subject',
+  html: '<div> Mail Content </div>',
+  attachments: [
+    { mime: 'text/plain', file: Buffer.from('test'), name: 'test.txt' },
+  ],
+};
+
 test('should trigger postmark correctly', async () => {
-  const provider = new PostmarkEmailProvider({
-    apiKey: '<postmark-id>',
-    from: 'test@test.com',
-  });
+  const provider = new PostmarkEmailProvider(mockConfig);
   const spy = jest
     // eslint-disable-next-line @typescript-eslint/dot-notation
     .spyOn(provider['client'], 'sendEmail')
@@ -12,26 +23,19 @@ test('should trigger postmark correctly', async () => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       return {} as any;
     });
-  await provider.sendMessage({
-    to: 'test2@test.com',
-    subject: 'test subject',
-    html: '<div> Mail Content </div>',
-    attachments: [
-      { mime: 'text/plain', file: Buffer.from('dGVzdA=='), name: 'test.txt' },
-    ],
-  });
+  await provider.sendMessage(mockNotifireMessage);
 
   expect(spy).toHaveBeenCalled();
   expect(spy).toHaveBeenCalledWith({
-    From: 'test@test.com',
-    To: 'test2@test.com',
-    HtmlBody: '<div> Mail Content </div>',
-    TextBody: '<div> Mail Content </div>',
-    Subject: 'test subject',
+    From: mockConfig.from,
+    To: mockNotifireMessage.to,
+    HtmlBody: mockNotifireMessage.html,
+    TextBody: mockNotifireMessage.html,
+    Subject: mockNotifireMessage.subject,
     Attachments: [
       {
         Name: 'test.txt',
-        Content: 'dGVzdA==',
+        Content: 'test',
         ContentID: null,
         ContentType: 'text/plain',
       },
