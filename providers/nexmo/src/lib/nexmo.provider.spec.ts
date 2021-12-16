117d103b22e6d3f5ab25ceb6d94bb5941c5465f4
Dima Grossman <dima@grossman.io>
2021-12-16 14:44:42 +0200

    Fix nexmo tests

--- providers/nexmo/src/lib/nexmo.provider.spec.ts
+++ providers/nexmo/src/lib/nexmo.provider.spec.ts
@@ -11,21 +11,28 @@ test('should trigger nexmo library correctly', async () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     .spyOn(provider.vonageClient.message, 'sendSms')
-    .mockImplementation(async () => {
-      return {
-        messages: [{ 'message-id': '123' }],
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } as any;
+    .mockImplementation(async (a, b, c, d, cb) => {
+      cb(null, {
+        'message-count': 1,
+        messages: [
+          {
+            'message-id': '123',
+            to: '1',
+            'message-price': '1',
+            'remaining-balance': '1',
+            status: '0' as any,
+            'account-ref': '1',
+            network: '1',
+          },
+        ],
+      });
     });
   await provider.sendMessage({
     to: '+176543',
     content: 'SMS Content',
   });
 
-  expect(spy).toHaveBeenCalled();
-  expect(spy).toHaveBeenCalledWith({
-    from: '+112345',
-    body: 'SMS Content',
-    to: '+176543',
-  });
+  expect(spy.mock.calls[0][0]).toBe('+112345');
+  expect(spy.mock.calls[0][1]).toBe('+176543');
+  expect(spy.mock.calls[0][2]).toBe('SMS Content');
 });
