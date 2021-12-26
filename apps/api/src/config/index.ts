b8b14253e6e2bfd67ad8bdd138312045e2ef7540
Hesoyam <37535505+Scrip7@users.noreply.github.com>
2021-12-26 03:47:31 +0330

    refactor(api): config path definition

--- apps/api/src/config/index.ts
+++ apps/api/src/config/index.ts
@@ -4,26 +4,15 @@ import { str, url, port, ValidatorSpec } from 'envalid';
 
 dotenv.config();
 
-let path;
-switch (process.env.NODE_ENV) {
-  case 'prod':
-    path = `${__dirname}/../.env.production`;
-    break;
-  case 'test':
-    path = `${__dirname}/../.env.test`;
-    break;
-  case 'ci':
-    path = `${__dirname}/../.env.ci`;
-    break;
-  case 'local':
-    path = `${__dirname}/../.env.local`;
-    break;
-  case 'dev':
-    path = `${__dirname}/../.env.development`;
-    break;
-  default:
-    path = `${__dirname}/../.env.local`;
-}
-//
+const path =
+  `${__dirname}/../` +
+  ({
+    prod: '.env.production',
+    test: '.env.test',
+    ci: '.env.ci',
+    local: '.env.local',
+    dev: '.env.development',
+  }[process.env.NODE_ENV] || '.env.local');
+
 const { error } = dotenv.config({ path });
 if (error && !process.env.LAMBDA_TASK_ROOT) throw error;
