5480618b24df333c466f4664d1333b20b68296b5
Hesoyam <37535505+Scrip7@users.noreply.github.com>
2021-12-26 20:06:41 +0330

    refactor(api-config): Extract Variable pattern

--- apps/api/src/config/index.ts
+++ apps/api/src/config/index.ts
@@ -4,15 +4,16 @@ import { str, url, port, ValidatorSpec } from 'envalid';
 
 dotenv.config();
 
-const path =
-  `${__dirname}/../` +
-  ({
-    prod: '.env.production',
-    test: '.env.test',
-    ci: '.env.ci',
-    local: '.env.local',
-    dev: '.env.development',
-  }[process.env.NODE_ENV] || '.env.local');
+const envFileMapper = {
+  prod: '.env.production',
+  test: '.env.test',
+  ci: '.env.ci',
+  local: '.env.local',
+  dev: '.env.development',
+};
+const selectedEnvFile = envFileMapper[process.env.NODE_ENV] || '.env.local';
+
+const path = `${__dirname}/../${selectedEnvFile}`;
 
 const { error } = dotenv.config({ path });
 if (error && !process.env.LAMBDA_TASK_ROOT) throw error;
