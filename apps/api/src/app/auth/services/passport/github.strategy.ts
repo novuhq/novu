ffb9fc00541cd8772047b96839d3af8527b0eba9
Gosha <djabarov.george@gmail.com>
2022-01-20 12:12:03 +0200

    update packege passport-github to passport-github2

--- apps/api/src/app/auth/services/passport/github.strategy.ts
+++ apps/api/src/app/auth/services/passport/github.strategy.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import * as githubPassport from 'passport-github';
+import * as githubPassport from 'passport-github2';
 import { Metadata, StateStoreStoreCallback, StateStoreVerifyCallback } from 'passport-oauth2';
 import { AuthProviderEnum } from '@notifire/shared';
 import { AuthService } from '../auth.service';
@@ -12,6 +12,7 @@ export class GithubStrategy extends PassportStrategy(githubPassport.Strategy, 'g
       clientID: process.env.GITHUB_OAUTH_CLIENT_ID,
       clientSecret: process.env.GITHUB_OAUTH_CLIENT_SECRET,
       callbackURL: process.env.GITHUB_OAUTH_REDIRECT,
+      scope: ['user:email'],
       passReqToCallback: true,
       store: {
         verify(req, state: string, meta: Metadata, callback: StateStoreVerifyCallback) {
@@ -26,11 +27,12 @@ export class GithubStrategy extends PassportStrategy(githubPassport.Strategy, 'g
 
   async validate(req, accessToken: string, refreshToken: string, profile, done: (err, data) => void) {
     try {
+      const updatedProfile = { ...profile._json, email: profile.emails[0].value };
       const response = await this.authService.authenticate(
         AuthProviderEnum.GITHUB,
         accessToken,
         refreshToken,
-        profile._json,
+        updatedProfile,
         req.query.state
       );
 
