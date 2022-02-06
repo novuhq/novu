8e1971d66ccb37a0b105be650dd7e1a28250096b
ainouzgali <ainouzgali@gmail.com>
2022-02-06 10:21:44 +0200

    Create shadow styles and add to Button

--- apps/web/src/design-system/button/Button.stories.tsx
+++ apps/web/src/design-system/button/Button.stories.tsx
@@ -3,16 +3,12 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Group } from '@mantine/core';
 import { Button } from './Button';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Components/Button',
   component: Button,
-
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = ({ ...args }) => <Button {...args}>Click Me</Button>;
 
 export const Default = Template.bind({});
--- apps/web/src/design-system/button/Button.styles.ts
+++ apps/web/src/design-system/button/Button.styles.ts
@@ -7,9 +7,14 @@ const getLabelStyles = (theme) => ({
   backgroundClip: 'text',
   fontWeight: 'bold',
 });
-const getFilledStyles = (theme) => ({
+const getFilledDisabledStyles = (theme) => ({
   backgroundImage: theme.colorScheme === 'dark' ? getGradient(theme.colors.dark[4]) : getGradient(theme.colors.gray[2]),
 });
+const getFilledStyles = (theme) => ({
+  border: 'transparent',
+  boxShadow: theme.colorScheme === 'dark' ? theme.shadows.lg : theme.shadows.xl,
+});
+
 const getOutlineStyles = (theme, disabled) => {
   const dark = theme.colorScheme === 'dark';
   const backgroundColor = getGradient(dark ? theme.black : theme.white);
@@ -20,13 +25,20 @@ const getOutlineStyles = (theme, disabled) => {
     backgroundClip: 'padding-box, border-box',
     backgroundOrigin: 'border-box',
     color: dark ? theme.white : 'transparent',
+    boxShadow: dark ? theme.shadows.lg : theme.shadows.md,
   };
 };
 
-export default createStyles((theme: MantineTheme, disabled: boolean) => {
+export default createStyles((theme: MantineTheme, disabled: boolean, getRef) => {
+  const loading = getRef('loading');
   return {
     label: disabled ? {} : getLabelStyles(theme),
-    filled: disabled ? getFilledStyles(theme) : { border: 'transparent' },
+    filled: disabled ? getFilledDisabledStyles(theme) : getFilledStyles(theme),
     outline: getOutlineStyles(theme, disabled),
+    root: {
+      [`&:not(.${loading}):disabled`]: {
+        boxShadow: 'none',
+      },
+    },
   };
 });
