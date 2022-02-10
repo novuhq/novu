5fc75033d873b84257e3fb7991c8fc608bce2118
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2022-02-10 12:29:08 +0000

    removed indeterminate and sizes

--- apps/web/src/design-system/checkbox/Checkbox.stories.tsx
+++ apps/web/src/design-system/checkbox/Checkbox.stories.tsx
@@ -18,20 +18,8 @@ Default.args = {
 
 export const States = () => (
   <Group style={{ display: 'flex', flexDirection: 'column', alignItems: 'unset' }}>
-    <Checkbox label="Indeterminate Checkbox" indeterminate />
     <Checkbox label="Checked checkbox" checked />
     <Checkbox label="Disabled checkbox" disabled />
     <Checkbox label="Disabled checked checkbox" checked disabled />
-    <Checkbox label="Disabled indeterminate checkbox" disabled indeterminate />
-  </Group>
-);
-
-export const Sizes = () => (
-  <Group style={{ display: 'flex', flexDirection: 'column', alignItems: 'unset' }}>
-    <Checkbox label="xs" size="xs" />
-    <Checkbox label="sm" size="sm" />
-    <Checkbox label="md" size="md" />
-    <Checkbox label="lg" size="lg" />
-    <Checkbox label="xl" size="xl" />
   </Group>
 );
--- apps/web/src/design-system/checkbox/Checkbox.tsx
+++ apps/web/src/design-system/checkbox/Checkbox.tsx
@@ -3,10 +3,8 @@ import { Checkbox as MantineCheckbox } from '@mantine/core';
 import useStyles from './Checkbox.styles';
 
 interface CheckboxProps {
-  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
   checked?: boolean;
   disabled?: boolean;
-  indeterminate?: boolean;
   label?: string;
 }
 
@@ -14,24 +12,9 @@ interface CheckboxProps {
  * Checkbox Component
  *
  */
-export function Checkbox({
-  label = 'Default Checkbox',
-  size = 'md',
-  checked,
-  disabled = false,
-  indeterminate = false,
-  ...props
-}: CheckboxProps) {
+export function Checkbox({ label = 'Default Checkbox', checked, disabled = false, ...props }: CheckboxProps) {
   const { classes } = useStyles();
   return (
-    <MantineCheckbox
-      classNames={classes}
-      label={label}
-      disabled={disabled}
-      size={size}
-      checked={checked}
-      indeterminate={indeterminate}
-      {...props}
-    />
+    <MantineCheckbox classNames={classes} label={label} disabled={disabled} size="sm" checked={checked} {...props} />
   );
 }
