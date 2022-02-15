4f5d6860bd2ab2fa2bb86c44a85977e7c857cee3
Gosha <djabarov.george@gmail.com>
2022-02-15 16:17:09 +0200

    :white_check_mark: Update pr comments

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -1,20 +1,17 @@
 import * as open from 'open';
-import { Answers } from 'inquirer';
 import { prompt } from '../client';
 import { promptIntroArray } from './init.consts';
 import { HttpServer } from '../server';
 import { SERVER_PORT, SERVER_HOST, REDIRECT_ROUTH, API_OAUTH_URL } from '../constants';
 
-let answers: Answers;
-
 export async function initCommand() {
   try {
-    answers = await prompt(promptIntroArray);
+    await prompt(promptIntroArray);
 
     await gitHubOAuth();
   } catch (error) {
     // eslint-disable-next-line no-console
-    console.log(error);
+    console.error(error);
   }
 }
 
@@ -34,15 +31,15 @@ async function gitHubOAuth(): Promise<string> {
   } finally {
     httpServer.close();
   }
+}
 
-  function serverResponse(server: HttpServer): Promise<string> {
-    return new Promise((resolve) => {
-      const interval = setInterval(() => {
-        if (server.token) {
-          clearInterval(interval);
-          resolve(server.token);
-        }
-      }, 300);
-    });
-  }
+function serverResponse(server: HttpServer): Promise<string> {
+  return new Promise((resolve) => {
+    const interval = setInterval(() => {
+      if (server.token) {
+        clearInterval(interval);
+        resolve(server.token);
+      }
+    }, 300);
+  });
 }
