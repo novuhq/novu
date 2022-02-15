732490f71f187746b6fcdfd649c430391e49d00e
Dima Grossman <dima@grossman.io>
2022-02-15 14:09:48 +0200

    🚨 Variable naming conventions for camel and pascal casing

--- apps/ws/src/shared/framework/user.decorator.ts
+++ apps/ws/src/shared/framework/user.decorator.ts
@@ -1,6 +1,7 @@
 import { createParamDecorator, UnauthorizedException } from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export const UserSession = createParamDecorator((data, ctx) => {
   let req;
   if (ctx.getType() === 'graphql') {
@@ -25,6 +26,7 @@ export const UserSession = createParamDecorator((data, ctx) => {
   return null;
 });
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export const SubscriberSession = createParamDecorator((data, ctx) => {
   let req;
   if (ctx.getType() === 'graphql') {
@@ -34,7 +36,6 @@ export const SubscriberSession = createParamDecorator((data, ctx) => {
   }
 
   if (req.user) return req.user;
-
   if (req.headers) {
     if (req.headers.authorization) {
       const tokenParts = req.headers.authorization.split(' ');
