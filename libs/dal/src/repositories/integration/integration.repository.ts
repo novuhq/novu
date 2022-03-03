afbce2f953719046ecb8a9498d3891d356083569
Gosha <djabarov.george@gmail.com>
2022-03-03 19:06:18 +0200

    :hammer: Update pr comments resolve

--- libs/dal/src/repositories/integration/integration.repository.ts
+++ libs/dal/src/repositories/integration/integration.repository.ts
@@ -1,4 +1,4 @@
-import { UnprocessableEntityException } from '@nestjs/common';
+import { HttpException, HttpStatusCode } from '@notifire/shared';
 import { BaseRepository } from '../base-repository';
 import { IntegrationEntity } from './integration.entity';
 import { Integration } from './integration.schema';
@@ -15,16 +15,17 @@ export class IntegrationRepository extends BaseRepository<IntegrationEntity> {
   }
 
   async create(data: Partial<IntegrationEntity>): Promise<IntegrationEntity> {
-    const test = await this.find({
+    const existingIntegration = await this.findOne({
       _applicationId: data._applicationId,
       providerId: data.providerId,
       channel: data.channel,
     });
-    if (test.length === 0) {
+    if (!existingIntegration) {
       return await super.create(data);
     }
 
-    throw new UnprocessableEntityException(
+    throw new HttpException(
+      HttpStatusCode.UNPROCESSABLE_ENTITY,
       'Duplicate key - One application may not have two providers of the same channel type'
     );
   }
