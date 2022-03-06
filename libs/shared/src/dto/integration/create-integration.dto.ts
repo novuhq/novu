f8f8ecaeb3b0154007026d252d8e85dcd1fa7eff
Gosha <djabarov.george@gmail.com>
2022-03-06 14:18:40 +0200

    :hammer: Update shared create dto

--- libs/shared/src/dto/integration/create-integration.dto.ts
+++ libs/shared/src/dto/integration/create-integration.dto.ts
@@ -1,3 +1,8 @@
 import { IConstructIntegrationDto } from './shared';
+import { ChannelTypeEnum } from '../../entities/message-template';
 
-export type ICreateIntegrationBodyDto = IConstructIntegrationDto;
+export interface ICreateIntegrationBodyDto extends IConstructIntegrationDto {
+  providerId: string;
+
+  channel: ChannelTypeEnum;
+}
