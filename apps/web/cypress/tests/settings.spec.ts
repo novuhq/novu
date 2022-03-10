4993fc35280c1ce50cc1220631b8d48d3620df39
ninjadev101 <sanchzxel@gmail.com>
2022-03-10 00:07:35 -0400

    🔥  removed unnecessary function

--- apps/web/cypress/tests/settings.spec.ts
+++ apps/web/cypress/tests/settings.spec.ts
@@ -101,23 +101,3 @@ describe('Settings Screen', function () {
     cy.getByTestId('font-family-selector').should('have.value', 'Lato');
   });
 });
-
-function b64toBlob(b64Data, contentType = '', sliceSize = 512) {
-  const byteCharacters = atob(b64Data);
-  const byteArrays: any[] = [];
-
-  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-    const slice = byteCharacters.slice(offset, offset + sliceSize);
-
-    const byteNumbers = new Array(slice.length);
-    for (let i = 0; i < slice.length; i++) {
-      byteNumbers[i] = slice.charCodeAt(i);
-    }
-
-    const byteArray = new Uint8Array(byteNumbers);
-    byteArrays.push(byteArray);
-  }
-
-  const blob = new Blob(byteArrays, { type: contentType });
-  return blob;
-}
