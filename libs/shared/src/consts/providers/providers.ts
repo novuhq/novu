85eee8c364b2b9b6fefe37721cf419f0433c79c9
Gosha <djabarov.george@gmail.com>
2022-03-15 20:28:35 +0200

    :recycle: Add shared provider index

---
+++ libs/shared/src/consts/providers/providers.ts
@@ -0,0 +1,80 @@
+import { ISharedProvider } from './provider.interface';
+import { ChannelTypeEnum } from '../../entities/message-template';
+
+export const providers: ISharedProvider[] = [
+  {
+    id: 'emailjs',
+    displayName: 'EMail.js',
+    channel: ChannelTypeEnum.EMAIL,
+  },
+  {
+    id: 'mailgun',
+    displayName: 'Mailgun',
+    channel: ChannelTypeEnum.EMAIL,
+  },
+  {
+    id: 'mailjet',
+    displayName: 'Mailjet',
+    channel: ChannelTypeEnum.EMAIL,
+  },
+  {
+    id: 'mandrill',
+    displayName: 'Mandrill',
+    channel: ChannelTypeEnum.EMAIL,
+  },
+  {
+    id: 'nexmo',
+    displayName: 'Nexmo',
+    channel: ChannelTypeEnum.SMS,
+  },
+  {
+    id: 'nodemailer',
+    displayName: 'Nodemailer',
+    channel: ChannelTypeEnum.EMAIL,
+  },
+  {
+    id: 'plivo',
+    displayName: 'Plivo',
+    channel: ChannelTypeEnum.SMS,
+  },
+  {
+    id: 'postmark',
+    displayName: 'Postmark',
+    channel: ChannelTypeEnum.EMAIL,
+  },
+  {
+    id: 'sendgrid',
+    displayName: 'SendGrid',
+    channel: ChannelTypeEnum.EMAIL,
+  },
+  {
+    id: 'sendinblue',
+    displayName: 'Sendinblue',
+    channel: ChannelTypeEnum.EMAIL,
+  },
+  {
+    id: 'ses',
+    displayName: 'SES',
+    channel: ChannelTypeEnum.EMAIL,
+  },
+  {
+    id: 'sms77',
+    displayName: 'sms77',
+    channel: ChannelTypeEnum.SMS,
+  },
+  {
+    id: 'sns',
+    displayName: 'SNS',
+    channel: ChannelTypeEnum.SMS,
+  },
+  {
+    id: 'telnyx',
+    displayName: 'Telnyx',
+    channel: ChannelTypeEnum.SMS,
+  },
+  {
+    id: 'twilio',
+    displayName: 'Twilio',
+    channel: ChannelTypeEnum.SMS,
+  },
+];
