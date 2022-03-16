e93929acef5b552fa16b19d6c6b24ca85fe17fd4
ainouzgali <ainouzgali@gmail.com>
2022-03-16 19:04:37 +0200

    :lipstick: new icons

--- apps/web/src/design-system/icons/index.ts
+++ apps/web/src/design-system/icons/index.ts
@@ -4,6 +4,7 @@ export { Settings } from './general/Settings';
 export { Bolt } from './general/Bolt';
 export { Moon } from './general/Moon';
 export { DotsHorizontal } from './general/DotsHorizontal';
+export { DotsHorizontalOutlined } from './general/DotsHorizontalOutlined';
 export { EyeShield } from './general/EyeShield';
 export { MusicNote } from './general/MusicNote';
 export { Sun } from './general/Sun';
@@ -13,15 +14,18 @@ export { Bell } from './general/Bell';
 export { Mail } from './general/Mail';
 export { Trash } from './general/Trash';
 export { Check } from './general/Check';
+export { TextAlignment } from './general/TextAlignment';
 
 export { Copy } from './actions/Copy';
 export { CloseCircle } from './actions/CloseCircle';
 export { PlusCircle } from './actions/PlusCircle';
+export { PlusCircleOutlined } from './actions/PlusCircleOutlined';
 export { Search } from './actions/Search';
 export { Edit } from './actions/Edit';
 export { Upload } from './actions/Upload';
 
 export { ArrowDown } from './arrows/ArrowDown';
+export { DoubleArrowRight } from './arrows/DoubleArrowRight';
 
 export { BellGradient } from './gradient/BellGradient';
 export { MailGradient } from './gradient/MailGradient';
