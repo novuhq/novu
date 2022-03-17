56c0c6897087ea1be94dcaa18e79f27d73e2b621
ninjadev101 <sanchzxel@gmail.com>
2022-03-16 22:51:37 -0300

    ⬆️  updated faker and normalized email address

--- libs/testing/src/notification-template.service.ts
+++ libs/testing/src/notification-template.service.ts
@@ -1,4 +1,4 @@
-import * as faker from 'faker';
+import { faker } from '@faker-js/faker';
 import { ChannelCTATypeEnum, ChannelTypeEnum } from '@notifire/shared';
 import {
   MessageTemplateRepository,
