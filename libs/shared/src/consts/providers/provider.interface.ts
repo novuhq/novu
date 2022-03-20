f838e7d12f24f425155ff5ee8e017feabda79a8a
Gosha <djabarov.george@gmail.com>
2022-03-20 13:31:29 +0200

    :hammer: Update provider config credentials

--- libs/shared/src/consts/providers/provider.interface.ts
+++ libs/shared/src/consts/providers/provider.interface.ts
@@ -4,223 +4,268 @@ export interface IProviderConfig {
   id: string;
   displayName: string;
   channel: ChannelTypeEnum;
-  credentials: {
-    key: string;
-    type: string;
-  }[];
+  credentials: IConfigCredentials[];
   docReference: string;
   comingSoon?: boolean;
 }
 
-export const mailJsConfig = [
+export interface IConfigCredentials {
+  key: string;
+  value: string;
+  type: string;
+}
+
+export const mailJsConfig: IConfigCredentials[] = [
   {
     key: 'apiKey',
+    value: 'API Key',
     type: 'string',
   },
   {
-    key: 'secret',
+    key: 'secretKey',
+    value: 'Secret key',
     type: 'string',
   },
 ];
 
-export const mailgunConfig = [
+export const mailgunConfig: IConfigCredentials[] = [
   {
     key: 'apiKey',
+    value: 'API Key',
     type: 'string',
   },
   {
     key: 'username',
+    value: 'User name',
     type: 'string',
   },
   {
     key: 'domain',
+    value: 'Domain',
     type: 'string',
   },
   {
     key: 'from',
+    value: 'From',
     type: 'string',
   },
 ];
 
-export const mailjetConfig = [
+export const mailjetConfig: IConfigCredentials[] = [
   {
     key: 'apiKey',
+    value: 'API Key',
     type: 'string',
   },
   {
     key: 'apiSecret',
+    value: 'API Secret',
     type: 'string',
   },
   {
     key: 'from',
+    value: 'From',
     type: 'string',
   },
 ];
 
-export const nexmoConfig = [
+export const nexmoConfig: IConfigCredentials[] = [
   {
     key: 'apiKey',
+    value: 'API Key',
     type: 'string',
   },
   {
     key: 'apiSecret',
+    value: 'API secret',
     type: 'string',
   },
   {
     key: 'from',
+    value: 'From',
     type: 'string',
   },
 ];
 
-export const mandrillConfig = [
+export const mandrillConfig: IConfigCredentials[] = [
   {
     key: 'apiKey',
+    value: 'API Key',
     type: 'string',
   },
   {
     key: 'from',
+    value: 'From',
     type: 'string',
   },
 ];
 
-export const nodemailerConfig = [
-  {
-    key: 'from',
-    type: 'string',
-  },
-  {
-    key: 'host',
-    type: 'string',
-  },
-  {
-    key: 'port',
-    type: 'number',
-  },
-  {
-    key: 'secure',
-    type: 'boolean',
-  },
+export const nodemailerConfig: IConfigCredentials[] = [
   {
     key: 'user',
+    value: 'User',
     type: 'string',
   },
   {
     key: 'password',
+    value: 'Password',
+    type: 'string',
+  },
+  {
+    key: 'host',
+    value: 'Host',
+    type: 'string',
+  },
+  {
+    key: 'port',
+    value: 'Port',
+    type: 'number',
+  },
+  {
+    key: 'secure',
+    value: 'Secure',
+    type: 'boolean',
+  },
+  {
+    key: 'from',
+    value: 'From',
     type: 'string',
   },
 ];
 
-export const postmarkConfig = [
+export const postmarkConfig: IConfigCredentials[] = [
   {
     key: 'apiKey',
+    value: 'API Key',
     type: 'string',
   },
   {
     key: 'from',
+    value: 'From',
     type: 'string',
   },
 ];
 
-export const sendgridConfig = [
+export const sendgridConfig: IConfigCredentials[] = [
   {
     key: 'apiKey',
+    value: 'API Key',
     type: 'string',
   },
   {
     key: 'from',
+    value: 'From',
     type: 'string',
   },
 ];
 
-export const sendinblueConfig = [
+export const sendinblueConfig: IConfigCredentials[] = [
   {
     key: 'apiKey',
+    value: 'API Key',
     type: 'string',
   },
 ];
 
-export const sesConfig = [
-  {
-    key: 'region',
-    type: 'string',
-  },
+export const sesConfig: IConfigCredentials[] = [
   {
     key: 'accessKeyId',
+    value: 'Access key ID',
     type: 'string',
   },
   {
     key: 'secretAccessKey',
+    value: 'Secret access key',
+    type: 'string',
+  },
+  {
+    key: 'region',
+    value: 'Region',
     type: 'string',
   },
   {
     key: 'from',
+    value: 'From',
     type: 'string',
   },
 ];
 
-export const plivoConfig = [
+export const plivoConfig: IConfigCredentials[] = [
   {
     key: 'accountSid',
+    value: 'Account SID',
     type: 'string',
   },
   {
     key: 'authToken',
+    value: 'Auth token',
     type: 'string',
   },
   {
     key: 'from',
+    value: 'From',
     type: 'string',
   },
 ];
 
-export const sms77Config = [
+export const sms77Config: IConfigCredentials[] = [
   {
     key: 'apiKey',
+    value: 'API Key',
     type: 'string',
   },
   {
     key: 'from',
+    value: 'From',
     type: 'string',
   },
 ];
 
-export const snsConfig = [
+export const snsConfig: IConfigCredentials[] = [
   {
     key: 'accessKeyId',
+    value: 'Access key ID',
     type: 'string',
   },
   {
     key: 'secretAccessKey',
+    value: 'Secret access key',
     type: 'string',
   },
 ];
 
-export const telnyxConfig = [
+export const telnyxConfig: IConfigCredentials[] = [
   {
     key: 'apiKey',
-    type: 'string',
-  },
-  {
-    key: 'from',
+    value: 'API Key',
     type: 'string',
   },
   {
     key: 'messageProfileId',
-    type: 'string',
-  },
-];
-
-export const twilioConfig = [
-  {
-    key: 'accountSid',
-    type: 'string',
-  },
-  {
-    key: 'authToken',
+    value: 'Message profile ID',
     type: 'string',
   },
   {
     key: 'from',
+    value: 'From',
+    type: 'string',
+  },
+];
+
+export const twilioConfig: IConfigCredentials[] = [
+  {
+    key: 'accountSid',
+    value: 'Account SID',
+    type: 'string',
+  },
+  {
+    key: 'authToken',
+    value: 'Auth token',
+    type: 'string',
+  },
+  {
+    key: 'from',
+    value: 'From',
     type: 'string',
   },
 ];
