3b239dd702067afa5293eddbdd176fac468d3247
ainouzgali <ainouzgali@gmail.com>
2022-03-25 01:40:12 +0200

    :sparkles: Invite form and changes to authcontainer

--- apps/web/src/pages/auth/InvitationPage.tsx
+++ apps/web/src/pages/auth/InvitationPage.tsx
@@ -1,11 +1,12 @@
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
-import { LoadingOverlay } from '@mantine/core';
+import { Center, LoadingOverlay } from '@mantine/core';
 import { IGetInviteResponseDto } from '@notifire/shared';
 import { getInviteTokenData } from '../../api/invitation';
 import AuthLayout from '../../components/layout/components/AuthLayout';
 import AuthContainer from '../../components/layout/components/AuthContainer';
 import { SignUpForm } from '../../components/auth/SignUpForm';
+import { colors, Text } from '../../design-system';
 
 export default function InvitationPage() {
   const { token } = useParams<{ token: string }>();
@@ -21,8 +22,35 @@ export default function InvitationPage() {
     <AuthLayout>
       <AuthContainer
         title="Get Started"
-        description={`You've been invited by <b>{data?.inviter?.firstName}</b> to join <b>{data?.organization.name}</b>`}>
-        {isLoading ? <LoadingOverlay visible={isLoading} /> : <SignUpForm email={data?.email} token={token} />}
+        customDescription={
+          <Center inline mb={60} mt={20}>
+            <Text size="lg" mr={4} color={colors.B60}>
+              You've been invited by
+            </Text>
+            <Text size="lg" weight="bold" mr={4}>
+              {data?.inviter?.firstName || ''}
+            </Text>
+            <Text size="lg" mr={4} color={colors.B60}>
+              to join
+            </Text>
+            <Text size="lg" weight="bold">
+              {data?.organization.name || ''}
+            </Text>
+            <Text size="lg" color={colors.B60}>
+              .
+            </Text>
+          </Center>
+        }>
+        <div style={{ position: 'relative', minHeight: 'inherit' }}>
+          <LoadingOverlay
+            visible={isLoading}
+            overlayColor={colors.B30}
+            loaderProps={{
+              color: colors.error,
+            }}
+          />
+          {!isLoading && <SignUpForm email={data?.email} token={token} />}
+        </div>
       </AuthContainer>
     </AuthLayout>
   );
