60162e17343468b9bec432c68d1cb688e6cb116e
Gosha <djabarov.george@gmail.com>
2022-03-30 15:23:57 +0300

    Revert ":fire: Remove integration init because it initialized in userSession"
    This reverts commit faa502b60192c6f27e6c42fe96a6bdbc53f900c6.

--- apps/web/cypress/plugins/index.ts
+++ apps/web/cypress/plugins/index.ts
@@ -1,3 +1,5 @@
+import { IntegrationService } from '@notifire/testing/src/integraion.service';
+
 /**
  * @type {Cypress.PluginConfig}
  */
@@ -10,6 +12,11 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   on('task', {
+    async createIntegration({ organizationId, applicationId }) {
+      const service = new IntegrationService();
+      await service.createIntegration(applicationId, organizationId);
+      return 'ok';
+    },
     async createNotifications({ identifier, token, count = 1, applicationId, organizationId }) {
       const subscriberService = new SubscribersService(organizationId, applicationId);
       const subscriber = await subscriberService.createSubscriber();
@@ -36,7 +43,7 @@ module.exports = (on, config) => {
       const dal = new DalService();
       await dal.connect('mongodb://localhost:27017/notifire-test');
 
-      new UserSession('http://localhost:1336');
+      const session = new UserSession('http://localhost:1336');
 
       return true;
     },
