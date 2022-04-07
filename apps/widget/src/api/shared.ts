072a0ebe9827c25e61d946503305d96bafe9ac2c
davidsoderberg <david@novu.co>
2022-04-07 10:56:18 +0200

    ✅ Fix so widget can be inside of an iframe in cypress tests

--- apps/widget/src/api/shared.ts
+++ apps/widget/src/api/shared.ts
@@ -6,11 +6,11 @@ declare global {
 }
 
 export const API_URL =
-  isBrowser() && (window as any).Cypress
+  isBrowser() && ((window as any).Cypress || (window as any).parent.Cypress)
     ? window._env_.REACT_APP_API_URL || 'http://localhost:1336'
     : window._env_.REACT_APP_API_URL || 'http://localhost:3000';
 export const WS_URL =
-  isBrowser() && (window as any).Cypress
+  isBrowser() && ((window as any).Cypress || (window as any).parent.Cypress)
     ? window._env_.REACT_APP_WS_URL || 'http://localhost:1340'
     : window._env_.REACT_APP_WS_URL || 'http://localhost:3002';
 
