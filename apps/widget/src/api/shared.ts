f515640af742c6ccf5ac297cbf3738188464fcd3
davidsoderberg <david@novu.co>
2022-04-07 09:34:18 +0200

    💚 FIx logic for config vars

--- apps/widget/src/api/shared.ts
+++ apps/widget/src/api/shared.ts
@@ -1,9 +1,17 @@
+import { isBrowser } from '@novu/shared';
 declare global {
   interface Window {
     _env_: any;
   }
 }
 
-export const API_URL = window._env_.REACT_APP_API_URL || 'http://localhost:3000';
-export const WS_URL = window._env_.REACT_APP_WS_URL || 'http://localhost:3002';
+export const API_URL =
+  isBrowser() && (window as any).Cypress
+    ? window._env_.REACT_APP_API_URL || 'http://localhost:1336'
+    : window._env_.REACT_APP_API_URL || 'http://localhost:3000';
+export const WS_URL =
+  isBrowser() && (window as any).Cypress
+    ? window._env_.REACT_APP_WS_URL || 'http://localhost:1340'
+    : window._env_.REACT_APP_WS_URL || 'http://localhost:3002';
+
 export const ENV = window._env_.REACT_APP_ENVIRONMENT;
