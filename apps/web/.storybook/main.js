b49423b02c1787ca6812524db558c550d47dc531
ninjadev101 <sanchzxel@gmail.com>
2022-04-12 11:11:04 -0300

    🚧  upgrade dependencies, fixed deprecated imports

--- apps/web/.storybook/main.js
+++ apps/web/.storybook/main.js
@@ -1,6 +1,11 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-preset-craco', 'storybook-dark-mode'],
+  addons: [
+    // '@storybook/preset-create-react-app',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-dark-mode',
+  ],
   framework: '@storybook/react',
   features: {
     emotionAlias: false,
