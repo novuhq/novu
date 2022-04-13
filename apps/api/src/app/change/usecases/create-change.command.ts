7c251072f8ed3c8052900f9874273c3ae7c4083d
davidsoderberg <granskog_@hotmail.com>
2022-04-13 13:21:11 +0200

    ✅ Add tests for create change usecase

--- apps/api/src/app/change/usecases/create-change.command.ts
+++ apps/api/src/app/change/usecases/create-change.command.ts
@@ -4,13 +4,17 @@ import { Document } from 'mongoose';
 import { CommandHelper } from '../../shared/commands/command.helper';
 import { EnvironmentWithUserCommand } from '../../shared/commands/project.command';
 
+interface IItem extends Pick<Document, '_id'> {
+  [key: string]: any;
+}
+
 export class CreateChangeCommand extends EnvironmentWithUserCommand {
   static create(data: CreateChangeCommand) {
     return CommandHelper.create(CreateChangeCommand, data);
   }
 
   @IsDefined()
-  item: Partial<Document>;
+  item: IItem;
 
   @IsDefined()
   @IsString()
---
+++ apps/api/src/app/change/usecases/create-change.spec.ts
@@ -0,0 +1,75 @@
+import { Test } from '@nestjs/testing';
+import { UserSession } from '@novu/testing';
+import { CreateChange } from './create-change.usecase';
+import { ChangeModule } from '../change.module';
+import { CreateChangeCommand } from './create-change.command';
+import { ChangeEntityType } from '@novu/dal';
+import { SharedModule } from '../../shared/shared.module';
+import { expect } from 'chai';
+
+describe('Create Change', function () {
+  let useCase: CreateChange;
+  let session: UserSession;
+  let _id = '6256ade0099f90172d1cc435';
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [SharedModule, ChangeModule],
+      providers: [],
+    }).compile();
+
+    session = new UserSession();
+    await session.initialize();
+
+    useCase = moduleRef.get<CreateChange>(CreateChange);
+  });
+
+  it('should create a change', async function () {
+    const result = await useCase.execute(
+      CreateChangeCommand.create({
+        organizationId: session.organization._id,
+        environmentId: session.environment._id,
+        type: ChangeEntityType.NotificationTemplate,
+        userId: session.user._id,
+        item: {
+          _id,
+        },
+      })
+    );
+    expect(result.enabled).to.be.eq(true);
+    expect(result._entityId).to.be.eq(_id);
+    expect(result._creatorId).to.be.eq(session.user._id);
+    expect(result._environmentId).to.be.eq(session.environment._id);
+    expect(result._organizationId).to.be.eq(session.organization._id);
+    expect(result.type).to.be.eq(ChangeEntityType.NotificationTemplate);
+  });
+
+  it('should find diff for item', async function () {
+    const oldChange = await useCase.execute(
+      CreateChangeCommand.create({
+        organizationId: session.organization._id,
+        environmentId: session.environment._id,
+        type: ChangeEntityType.NotificationTemplate,
+        userId: session.user._id,
+        item: {
+          _id,
+        },
+      })
+    );
+    const change = await useCase.execute(
+      CreateChangeCommand.create({
+        organizationId: session.organization._id,
+        environmentId: session.environment._id,
+        type: ChangeEntityType.NotificationTemplate,
+        userId: session.user._id,
+        item: {
+          _id,
+          name: 'test',
+        },
+      })
+    );
+    expect(change.change[0].op).to.eq('add');
+    expect(change.change[0].value).to.eq('test');
+    expect(change.change[0].path).to.eql(['name']);
+  });
+});
