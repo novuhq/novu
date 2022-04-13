39ac78341d58b214632766a9ce97eed73b4c558b
ninjadev101 <sanchzxel@gmail.com>
2022-04-13 00:06:27 -0300

    🐛  fixed a bug

--- apps/web/.storybook/main.js
+++ apps/web/.storybook/main.js
@@ -2,4 +2,7 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-preset-craco', 'storybook-dark-mode'],
   framework: '@storybook/react',
+  features: {
+    emotionAlias: false,
+  },
 };
