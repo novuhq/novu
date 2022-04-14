c54af017fe46d004add87ae81258cb7205b80cbc
ninjadev101 <sanchzxel@gmail.com>
2022-04-14 01:17:48 -0300

    🐛  fixed test in case of having more than 2 env

--- apps/api/src/app/environments/usecases/get-my-environments/get-my-environments.e2e.ts
+++ apps/api/src/app/environments/usecases/get-my-environments/get-my-environments.e2e.ts
@@ -12,9 +12,8 @@ describe('Get My Environments - /environments (GET)', async () => {
   it('should return all environments to user', async () => {
     const { body } = await session.testAgent.get('/v1/environments');
 
-    expect(body.data).to.have.lengthOf(2);
-    for (let i = 0; i < 2; i++) {
-      expect(body.data[i].name).to.be.oneOf(['Developement', 'Production']);
+    expect(body.data.length).to.be.greaterThanOrEqual(2);
+    for (let i = 0; i < body.data.length; i++) {
       expect(body.data[i]._organizationId).to.eq(session.organization._id);
     }
   });
