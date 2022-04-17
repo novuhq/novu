7b66b48e30a61022fef3cf8f6f839ddad6123a45
Gosha <djabarov.george@gmail.com>
2022-04-17 09:28:28 +0300

    :wrench: Add popover component

---
+++ packages/bell/src/components/popover-notification-center/PopoverNotificationCenter.tsx
@@ -0,0 +1,61 @@
+import React, { useContext, useState } from 'react';
+import { IMessage } from '@novu/shared';
+import styled from 'styled-components';
+import { Popover } from '@mantine/core';
+import { NotificationCenter } from '../notification-center';
+import { NovuContext } from '../../store/novu-provider.context';
+import { colors } from '../../shared/config/colors';
+
+interface IPopoverNotificationCenterProps {
+  onUrlChange?: (url: string) => void;
+  onNotificationClick?: (notification: IMessage) => void;
+  onUnseenCountChanged?: (unseenCount: number) => void;
+  bell: JSX.Element | Element;
+}
+
+export function PopoverNotificationCenter(props: IPopoverNotificationCenterProps) {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const { colorScheme } = useContext(NovuContext);
+
+  function handlerBellClick() {
+    setIsVisible(!isVisible);
+  }
+
+  return (
+    <>
+      <StyledPopover
+        opened={isVisible}
+        onClose={() => setIsVisible(false)}
+        target={<BellContainer onClick={handlerBellClick}>{props.bell}</BellContainer>}
+        position={'bottom'}
+        placement={'end'}
+        styles={{
+          inner: { margin: 0, padding: 0 },
+          body: { border: 0 },
+          popover: { background: `${colorScheme === 'dark' ? colors.B15 : colors.white}` },
+          arrow: {
+            background: `${colorScheme === 'dark' ? colors.B15 : colors.white}`,
+            backgroundColor: `${colorScheme === 'dark' ? colors.B15 : colors.white}`,
+            borderColor: `${colorScheme === 'dark' ? colors.B15 : colors.white}`,
+          },
+        }}
+        withArrow>
+        <NotificationCenter
+          onNotificationClick={props.onNotificationClick}
+          onUnseenCountChanged={props.onUnseenCountChanged}
+          onUrlChange={props.onUrlChange}
+          bell={props.bell}
+        />
+      </StyledPopover>
+    </>
+  );
+}
+
+const StyledPopover = styled(Popover)`
+  & .your-inner-class {
+    background: green;
+  }
+`;
+
+const BellContainer = styled.span``;
