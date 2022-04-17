f71f4abb9ada63d62bc0a7890e00eb605c222dc0
Gosha <djabarov.george@gmail.com>
2022-04-17 21:41:49 +0300

    :hammer: Updated bell unseen callback

--- apps/web/src/components/widget/NotificationCenterWidget.tsx
+++ apps/web/src/components/widget/NotificationCenterWidget.tsx
@@ -2,17 +2,25 @@ import { NotificationBell, NovuProvider, PopoverNotificationCenter } from '@novu
 import { useApplication } from '../../api/hooks/use-application';
 import { IUserEntity } from '@novu/shared';
 import { useMantineColorScheme } from '@mantine/core';
+import { useState } from 'react';
 
 export function NotificationCenterWidget({ user }: { user: IUserEntity | undefined }) {
   const { application } = useApplication();
   const { colorScheme } = useMantineColorScheme();
+  const [unseenCount, setUnseenCount] = useState<number>(0);
+
+  function unseenChanged(count: number) {
+    setUnseenCount(count);
+  }
 
   return (
     <NovuProvider
       subscriberId={user?._id as string}
       applicationIdentifier={application?.identifier as string}
       colorScheme={colorScheme}>
-      <PopoverNotificationCenter bell={<NotificationBell />} />
+      <PopoverNotificationCenter onUnseenCountChanged={unseenChanged} unseenCount={unseenCount}>
+        {(props) => <NotificationBell {...props} />}
+      </PopoverNotificationCenter>
     </NovuProvider>
   );
 }
--- packages/bell/src/components/notification-bell/NotificationBell.tsx
+++ packages/bell/src/components/notification-bell/NotificationBell.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { colors } from '../../shared/config/colors';
-import { Bell as BellIcon } from '../../shared/icons/Bell';
+import { Bell as BellIcon, GradientDot } from '../../shared/icons';
 import { ActionIcon } from '@mantine/core';
+import { NovuContext } from '../../store/novu-provider.context';
+import styled from 'styled-components';
 
 const headerIconsSettings = { color: colors.B60, width: 30, height: 30 };
 
-export function NotificationBell() {
+export interface INotificationBellProps {
+  unseenCount: number;
+}
+
+export function NotificationBell({ unseenCount }: INotificationBellProps) {
   return (
     <ActionIcon variant="transparent">
       <BellIcon {...headerIconsSettings} />
+      {unseenCount > 0 ? <StyledGradientDot /> : null}
     </ActionIcon>
   );
 }
+
+export function GradientDotWrap(props: any) {
+  const { colorScheme } = useContext(NovuContext);
+  const borderColor = colorScheme === 'dark' ? colors.B15 : colors.white;
+
+  return <GradientDot {...props} color={borderColor} />;
+}
+
+const StyledGradientDot = styled(GradientDotWrap)`
+  position: absolute;
+  top: -3%;
+  right: 10%;
+  width: 12px;
+  height: 12px;
+`;
