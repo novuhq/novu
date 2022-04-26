e90efd0da67320b9ba4aac5a9c6b4b874e79bccc
Dima Grossman <dima@grossman.io>
2022-04-26 10:42:19 +0300

    Remove un-needed API_URL

--- packages/notification-center/src/api/shared.ts
+++
@@ -1,4 +0,0 @@
-import { isBrowser } from '@novu/shared';
-
-export const API_URL = isBrowser() && (window as any).Cypress ? 'http://localhost:1336' : 'http://localhost:3000';
-export const WS_URL = isBrowser() && (window as any).Cypress ? 'http://localhost:1340' : 'http://localhost:3002';
