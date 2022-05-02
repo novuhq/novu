91d2dcef340779294ff169c2f84c14dde6588464
ainouzgali <ainouzgali@gmail.com>
2022-05-02 10:01:02 +0300

    :lipstick: Fix disabled styles - light mode

--- apps/web/src/components/templates/in-app-editor/InAppEditorBlock.tsx
+++ apps/web/src/components/templates/in-app-editor/InAppEditorBlock.tsx
@@ -48,7 +48,7 @@ export function InAppEditorBlock({
   }
 
   return (
-    <InAppWidgetPreview readonly>
+    <InAppWidgetPreview readonly={readonly}>
       <div style={{ position: 'relative' }}>
         <div
           ref={ref}
