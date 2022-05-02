17f9406b1636bbc80f9887768af663836188ec3c
davidsoderberg <granskog_@hotmail.com>
2022-05-02 09:55:22 +0200

    ✅ Fix test

--- apps/web/cypress/tests/notifications-editor.spec.ts
+++ apps/web/cypress/tests/notifications-editor.spec.ts
@@ -191,8 +191,8 @@ describe('Notifications Creator', function () {
 
   it('should validate form inputs', function () {
     cy.visit('/templates/create');
-    addChannel('inApp');
     cy.getByTestId('description').type('this is a notification template description');
+    addChannel('inApp');
 
     cy.getByTestId('submit-btn').click();
     cy.getByTestId('title').should('have.class', 'mantine-TextInput-invalid');
