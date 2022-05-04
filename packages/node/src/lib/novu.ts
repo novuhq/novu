8faca6f3d2447400de46d4e965199b9a971a9bf2
Gosha <djabarov.george@gmail.com>
2022-05-04 18:18:49 +0300

    🔧 Add dynamic backendUrl

--- packages/node/src/lib/novu.ts
+++ packages/node/src/lib/novu.ts
@@ -1,23 +1,24 @@
 import { EventEmitter } from 'events';
 import axios, { AxiosInstance } from 'axios';
 import { Subscribers } from './subscribers/subscribers';
-import {
-  TriggerRecipientsType,
-  ITriggerPayloadOptions,
-} from './subscribers/subscriber.interface';
+import { ITriggerPayloadOptions } from './subscribers/subscriber.interface';
 
 export class Novu extends EventEmitter {
   private readonly apiKey?: string;
   private readonly http: AxiosInstance;
   readonly subscribers: Subscribers;
 
-  constructor(apiKey: string) {
+  constructor(apiKey: string, config?: INovuConfiguration) {
     super();
+
+    const backendUrl = config?.backendUrl
+      ? config?.backendUrl
+      : 'https://api.novu.co/v1';
 
     this.apiKey = apiKey;
 
     this.http = axios.create({
-      baseURL: 'https://api.novu.co/v1',
+      baseURL: backendUrl,
       headers: {
         Authorization: `ApiKey ${this.apiKey}`,
       },
---
+++ packages/node/src/types/example.d.ts
@@ -0,0 +1,3 @@
+interface INovuConfiguration {
+  backendUrl?: string;
+}
