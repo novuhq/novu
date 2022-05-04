5ec3cb57ed12174294185497254bd75d76178f9b
ainouzgali <ainouzgali@gmail.com>
2022-05-04 15:30:10 +0300

    :sparkles: api changes

--- apps/api/src/app/user/user.controller.ts
+++ apps/api/src/app/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { ClassSerializerInterceptor, Controller, Get, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Get, Put, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { IJwtPayload } from '@novu/shared';
 import { UserSession } from '../shared/framework/user.decorator';
@@ -6,12 +6,17 @@ import { GetMyProfileUsecase } from './usecases/get-my-profile/get-my-profile.us
 import { GetMyProfileCommand } from './usecases/get-my-profile/get-my-profile.dto';
 import { IGetMyProfileDto } from './dtos/get-my-profile';
 import { JwtAuthGuard } from '../auth/framework/auth.guard';
+import { UpdateOnBoardingCommand } from './usecases/update-on-boarding/update-on-boarding.command';
+import { UpdateOnBoardingUsecase } from './usecases/update-on-boarding/update-on-boarding.usecase';
 
 @Controller('/users')
 @UseInterceptors(ClassSerializerInterceptor)
 @UseGuards(JwtAuthGuard)
 export class UsersController {
-  constructor(private getMyProfileUsecase: GetMyProfileUsecase) {}
+  constructor(
+    private getMyProfileUsecase: GetMyProfileUsecase,
+    private updateOnBoardingUsecase: UpdateOnBoardingUsecase
+  ) {}
 
   @Get('/me')
   async getMyProfile(@UserSession() user: IJwtPayload): Promise<IGetMyProfileDto> {
@@ -20,5 +25,15 @@ export class UsersController {
     });
 
     return await this.getMyProfileUsecase.execute(command);
+  }
+
+  @Put('/onboarding')
+  async updateOnBoarding(@UserSession() user: IJwtPayload, @Body() body: { onBoarding: boolean }) {
+    return await this.updateOnBoardingUsecase.execute(
+      UpdateOnBoardingCommand.create({
+        userId: user._id,
+        onBoarding: body.onBoarding,
+      })
+    );
   }
 }
--- apps/web/src/api/user.ts
+++ apps/web/src/api/user.ts
@@ -3,3 +3,7 @@ import { api } from './api.client';
 export async function getUser() {
   return api.get('/v1/users/me');
 }
+
+export async function updateUserOnBoarding(onBoarding: boolean) {
+  return api.put('/v1/users/onboarding', { onBoarding });
+}
