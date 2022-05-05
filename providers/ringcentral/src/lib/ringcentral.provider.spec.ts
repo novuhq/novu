c0be1147d78a07531a0a1caefbe746cbfea89aaf
ninjadev101 <sanchzxel@gmail.com>
2022-05-05 09:48:55 -0300

    ✨  added ringcentral

--- providers/ringcentral/src/lib/ringcentral.provider.spec.ts
+++ providers/ringcentral/src/lib/ringcentral.provider.spec.ts
@@ -1,24 +1,19 @@
-import { RingcentralSmsProvider } from './ringcentral.provider';
+import { RingCentralSmsProvider } from './ringcentral.provider';
 
 test('should trigger ringcentral library correctly', async () => {
-    const provider = new RingcentralSmsProvider({
-        clientId: '<ringcentral-client-id>',
-        clientSecret: '<ringcentral-client-secret>',
+    const provider = new RingCentralSmsProvider({
+        username: '<ringcentral-username>',
+        password: '<ringcentral-password>',
         from: '+112345',
     });
-    
-    const spy = jest
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    .spyOn(provider.ringCentralClient, 'post');
 
-    await provider.sendMessage({
+    const result = await provider.sendMessage({
         to: '+123456789',
         content: 'Hello World',
     });
     
     expect(result).toEqual({
-        id: expect.any(String),
+        id: expect.any(Number),
         date: expect.any(String),
     });
 });
--- providers/ringcentral/src/lib/ringcentral.provider.ts
+++ providers/ringcentral/src/lib/ringcentral.provider.ts
@@ -7,26 +7,43 @@ import {
 
 import '../config';
 import { SDK as RingCentralSDK } from '@ringcentral/sdk';
+import Platform from '@ringcentral/sdk/lib/platform/Platform';
 
 export class RingCentralSmsProvider implements ISmsProvider {
   id = 'ringcentral';
   channelType = ChannelTypeEnum.SMS as ChannelTypeEnum.SMS;
-  private ringCentralClient: RingCentralSDK;
+  private rcsdk: RingCentralSDK;
+  private platform: Platform;
 
   constructor(
     private config: {
-      clientId: string;
-      clientSecret: string;
+      username: string;
+      password: string;
       from: string;
     }
   ) {
-    this.ringCentralClient = new RingCentralSDK({ clientId: config.clientId, clientSecret: config.clientSecret });
+    this.rcsdk = new RingCentralSDK({ server: process.env.RC_SERVER_URL, clientSecret: process.env.RC_CLIENT_SECRET, clientId: process.env.RC_CLIENT_ID });
+    this.platform = this.rcsdk.platform();
   }
 
   async sendMessage(
     options: ISmsOptions
   ): Promise<ISendMessageSuccessResponse> {
-    const result = await this.ringCentralClient.post('/restapi/v1.0/account/~/extension/~/sms', {
+    const isLoggedIn = await this.platform.loggedIn();
+    if (!isLoggedIn) {
+      await new Promise((resolve, reject) => {
+
+        this.platform.login({ username: this.config.username, password: this.config.password, extension: process.env.RC_EXTENSION_ID });
+        this.platform.on(this.platform.events.loginSuccess, function(e) {
+          resolve(e);
+        });
+        this.platform.on(this.platform.events.loginError, function(e) {
+          throw new Error(e.message);
+        })
+      });
+    }
+
+    const result = await this.platform.post('/account/~/extension/~/sms', {
       text: options.content,
       to: [ { phoneNumber: options.to }],
       from: { phoneNumber: this.config.from },
