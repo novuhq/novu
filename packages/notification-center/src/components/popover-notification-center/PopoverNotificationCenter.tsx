45f73e8aae9961e6dfb850feab0838864456aa85
Gosha <djabarov.george@gmail.com>
2022-06-06 10:48:34 +0300

    :hammer:  Add onNotificationClick as mandatory

--- packages/notification-center/src/components/popover-notification-center/PopoverNotificationCenter.tsx
+++ packages/notification-center/src/components/popover-notification-center/PopoverNotificationCenter.tsx
@@ -7,8 +7,8 @@ import { UnseenCountContext } from '../../store/unseen-count.context';
 import { ColorScheme } from '../../index';
 
 interface IPopoverNotificationCenterProps {
-  onUrlChange: (url: string) => void;
-  onNotificationClick?: (notification: IMessage) => void;
+  onUrlChange?: (url: string) => void;
+  onNotificationClick: (notification: IMessage) => void;
   onUnseenCountChanged?: (unseenCount: number) => void;
   children: (props: INotificationBellProps) => JSX.Element;
   header?: () => JSX.Element;
