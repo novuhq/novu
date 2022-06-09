8685ac12889a0d248a37af69995bea1254860622
Biswajeet Das <biswajeetdas18@gmail.com>
2022-06-09 21:49:11 +0530

    Added env flag for github oauth option

--- apps/web/src/config/index.ts
+++ apps/web/src/config/index.ts
@@ -23,3 +23,5 @@ export const ENV = process.env.REACT_APP_ENVIRONMENT;
 export const APP_ID = process.env.REACT_APP_NOVU_APP_ID;
 
 export const WIDGET_EMEBED_PATH = process.env.REACT_APP_WIDGET_EMBED_PATH || 'http://localhost:4701/embed.umd.min.js';
+
+export const GITHUB_AUTH_OPTION = process.env.REACT_APP_GITHUB_AUTH_OPTION === 'show';
