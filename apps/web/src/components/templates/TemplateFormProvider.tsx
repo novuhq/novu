8129cf37403bb79942d81b4ce05fa2925096e156
ainouzgali <ainouzgali@gmail.com>
2022-06-14 14:04:23 +0300

    fix: add errors to steps in form provider

--- apps/web/src/components/templates/TemplateFormProvider.tsx
+++ apps/web/src/components/templates/TemplateFormProvider.tsx
@@ -1,6 +1,7 @@
 import { FormProvider, useFieldArray, useForm } from 'react-hook-form';
 import { FieldArrayProvider } from './FieldArrayProvider';
 import { IForm } from './use-template-controller.hook';
+import { ChannelTypeEnum } from '@novu/shared';
 
 export const TemplateFormProvider = ({ children }) => {
   const methods = useForm<IForm>({
@@ -14,6 +15,18 @@ export const TemplateFormProvider = ({ children }) => {
       if (!data.notificationGroup) {
         errors.notificationGroup = 'Required field notification group';
       }
+
+      data.steps.map((step, index) => {
+        if (
+          (step.template.type === ChannelTypeEnum.SMS || step.template.type === ChannelTypeEnum.IN_APP) &&
+          step.template.content.length === 0
+        ) {
+          errors[`steps.${index}.template.content`] = 'Required field content';
+        }
+        if (step.template.type === ChannelTypeEnum.EMAIL && !step.template.subject) {
+          errors[`steps.${index}.template.subject`] = 'Required field subject';
+        }
+      });
 
       if (Object.keys(errors).length) {
         values = {} as any;
