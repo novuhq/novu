d210f8cd176a3ca207fc950f94defc4fdf4f9351
davidsoderberg <granskog_@hotmail.com>
2022-06-14 13:14:55 +0200

    fix: so worflow are disabled when production env is selected

--- apps/web/src/pages/templates/workflow/StepActiveSwitch.tsx
+++ apps/web/src/pages/templates/workflow/StepActiveSwitch.tsx
@@ -1,16 +1,28 @@
 import styled from '@emotion/styled';
 import { Controller } from 'react-hook-form';
 import { Switch } from '../../../design-system';
+import { useEnvController } from '../../../store/use-env-controller';
 
-export const StepActiveSwitch = ({ control, index }) => (
-  <Controller
-    control={control}
-    name={`steps.${index}.active`}
-    render={({ field: { value, ...field } }) => {
-      return <StyledSwitch {...field} checked={value} label={`Step is ${value ? 'Active' : 'Disabled'}`} />;
-    }}
-  />
-);
+export const StepActiveSwitch = ({ control, index }) => {
+  const { readonly } = useEnvController();
+
+  return (
+    <Controller
+      control={control}
+      name={`steps.${index}.active`}
+      render={({ field: { value, ...field } }) => {
+        return (
+          <StyledSwitch
+            {...field}
+            disabled={readonly}
+            checked={value}
+            label={`Step is ${value ? 'Active' : 'Disabled'}`}
+          />
+        );
+      }}
+    />
+  );
+};
 
 const StyledSwitch = styled(Switch)`
   max-width: 150px !important;
