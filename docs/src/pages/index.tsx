9db94613731e8e361fe1cb35b083bd95fd9f6a29
Thi Van Le <vannnyle@gmail.com>
2022-06-23 00:05:37 +0300

    fix: add redirect / to /docs

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,41 +1,6 @@
 import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs">
-            Get Started
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import { Redirect } from '@docusaurus/router';
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-
-  return (
-    <Layout
-      title={`Novu developer docs`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
-      {/*    <main>
-        <HomepageFeatures />
-      </main> */}
-    </Layout>
-  );
+  return <Redirect to="/docs/" />;
 }
