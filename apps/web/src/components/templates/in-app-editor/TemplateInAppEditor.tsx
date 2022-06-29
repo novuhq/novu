34da7e4ad497f4e2869f5e671e72b2b4d2a98f96
ainouzgali <ainouzgali@gmail.com>
2022-06-29 16:18:49 +0300

    feat(web):  add web support for feed

--- apps/web/src/components/templates/in-app-editor/TemplateInAppEditor.tsx
+++ apps/web/src/components/templates/in-app-editor/TemplateInAppEditor.tsx
@@ -1,12 +1,57 @@
-import { Control, Controller } from 'react-hook-form';
+import { Control, Controller, useFormContext } from 'react-hook-form';
 import { Container, Group } from '@mantine/core';
 import { IForm } from '../use-template-controller.hook';
 import { InAppEditorBlock } from './InAppEditorBlock';
-import { Input } from '../../../design-system';
+import { Input, Select } from '../../../design-system';
 import { useEnvController } from '../../../store/use-env-controller';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { createFeed, getFeeds } from '../../../api/feeds';
+import { useEffect } from 'react';
+import { QueryKeys } from '../../../api/query.keys';
 
 export function TemplateInAppEditor({ control, index }: { control: Control<IForm>; index: number; errors: any }) {
+  const queryClient = useQueryClient();
   const { readonly } = useEnvController();
+  const {
+    formState: { errors },
+    setValue,
+    getValues,
+  } = useFormContext();
+  const { data: feeds, isLoading: loadingFeeds } = useQuery(QueryKeys.getFeeds, getFeeds);
+  const { mutateAsync: createNewFeed, isLoading: loadingCreateFeed } = useMutation<
+    { name: string; _id: string },
+    { error: string; message: string; statusCode: number },
+    { name: string }
+  >(createFeed, {
+    onSuccess: (data) => {
+      queryClient.setQueryData(QueryKeys.getFeeds, [...feeds, data]);
+    },
+  });
+
+  useEffect(() => {
+    const feed = getValues(`steps.${index}.template.feedId`);
+    if (feeds?.length && !feed) {
+      selectDefaultFeed();
+    }
+  }, [feeds]);
+
+  function selectDefaultFeed() {
+    setTimeout(() => {
+      setValue(`steps.${index}.template.feedId`, feeds[0]._id);
+    }, 0);
+  }
+
+  async function addNewFeed(newFeed) {
+    if (newFeed) {
+      const response = await createNewFeed({
+        name: newFeed,
+      });
+
+      setTimeout(() => {
+        setValue(`steps.${index}.template.feedId`, response._id);
+      }, 0);
+    }
+  }
 
   return (
     <>
@@ -26,6 +71,34 @@ export function TemplateInAppEditor({ control, index }: { control: Control<IForm
                 placeholder="i.e /tasks/{{taskId}}"
               />
             )}
+          />
+          <Controller
+            name={`steps.${index}.template.feedId` as any}
+            control={control}
+            render={({ field }) => {
+              return (
+                <>
+                  <Select
+                    {...field}
+                    label="Feeds"
+                    data-test-id="feedSelector"
+                    loading={loadingFeeds || loadingCreateFeed}
+                    disabled={readonly}
+                    creatable
+                    searchable
+                    required
+                    description="Choose a feed category"
+                    error={errors?.steps ? errors.steps[index]?.template?.feedId?.message : undefined}
+                    getCreateLabel={(newGroup) => (
+                      <div data-test-id="submit-category-btn">+ Create Feed {newGroup}</div>
+                    )}
+                    onCreate={addNewFeed}
+                    placeholder="Attach message to feed"
+                    data={(feeds || []).map((item) => ({ label: item.name, value: item._id }))}
+                  />
+                </>
+              );
+            }}
           />
           <Controller
             name={`steps.${index}.template.content` as any}
