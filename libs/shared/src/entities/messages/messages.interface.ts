6dda12cf8f2277c9664fe181f62af067b8cd1f8a
Gosha <djabarov.george@gmail.com>
2022-06-30 11:06:19 +0300

    feat: add message action interface

--- libs/shared/src/entities/messages/messages.interface.ts
+++ libs/shared/src/entities/messages/messages.interface.ts
@@ -1,5 +1,6 @@
 import { ChannelCTATypeEnum, ChannelTypeEnum, IEmailBlock } from '../message-template';
 import { INotificationTemplate } from '../notification-template';
+import { ButtonTypeEnum } from './action.enum';
 
 export interface IMessage {
   _id: string;
@@ -19,6 +20,12 @@ export interface IMessage {
     data: {
       url?: string;
     };
+    actions?: IMessageAction[];
   };
   payload: Record<string, unknown>;
+}
+
+export interface IMessageAction {
+  type: ButtonTypeEnum;
+  content: { text: string };
 }
