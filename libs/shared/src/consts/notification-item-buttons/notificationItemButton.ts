c841c6374ab6ae85ddaf85261a4028bf4a3ae406
Gosha <djabarov.george@gmail.com>
2022-07-05 18:10:27 +0300

    feat: add notification item buttons

--- libs/shared/src/consts/notification-item-buttons/notificationItemButton.ts
+++ libs/shared/src/consts/notification-item-buttons/notificationItemButton.ts
@@ -1,26 +1,32 @@
-import { ButtonTypeEnum } from '@novu/shared';
+import { ButtonTypeEnum } from '../../entities/messages';
 
 export interface INotificationButtonStyles {
   key: ButtonTypeEnum;
   value: IButtonStyles;
+  displayName: string;
 }
 
 export interface IButtonStyles {
   color: string;
+  backGround: string;
 }
 
 const primaryButton: INotificationButtonStyles = {
   key: ButtonTypeEnum.PRIMARY,
   value: {
-    color: 'blue',
+    color: 'white',
+    backGround: 'linear-gradient(99deg,#DD2476 0% 0%, #FF512F 100% 100%)', //colors.horizontal
   },
+  displayName: 'Primary',
 };
 
 const secondaryButton: INotificationButtonStyles = {
   key: ButtonTypeEnum.SECONDARY,
   value: {
-    color: 'red',
+    color: 'white',
+    backGround: '#3D3D4D', //colors.B30
   },
+  displayName: 'Secondary',
 };
 
 export const notificationItemButtons: INotificationButtonStyles[] = [primaryButton, secondaryButton];
