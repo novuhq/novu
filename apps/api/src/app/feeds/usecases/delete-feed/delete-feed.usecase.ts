ef98ceb2d817fafaa08d33f0e692170cd5713b94
ainouzgali <ainouzgali@gmail.com>
2022-07-06 18:01:53 +0300

    feat(api):  delete feed endpoint

---
+++ apps/api/src/app/feeds/usecases/delete-feed/delete-feed.usecase.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { FeedRepository, FeedEntity, DalException } from '@novu/dal';
+import { DeleteFeedCommand } from './delete-feed.command';
+import { ApiException } from '../../../shared/exceptions/api.exception';
+
+@Injectable()
+export class DeleteFeed {
+  constructor(private feedRepository: FeedRepository) {}
+
+  async execute(command: DeleteFeedCommand): Promise<FeedEntity[]> {
+    try {
+      await this.feedRepository.delete({
+        _environmentId: command.environmentId,
+        _organizationId: command.organizationId,
+        _id: command.feedId,
+      });
+    } catch (e) {
+      if (e instanceof DalException) {
+        throw new ApiException(e.message);
+      }
+      throw e;
+    }
+
+    return await this.feedRepository.find({
+      _environmentId: command.environmentId,
+      _organizationId: command.organizationId,
+    });
+  }
+}
