1e9edcd00a4035adb0cb1c69d80fe9e20c26c02b
Shahriar Shojib <shahriar_shojib@hotmail.com>
2022-07-14 01:54:37 +0600

    feat: added nest module async provider option

--- packages/nest/src/interfaces/novu-options.interface.ts
+++ packages/nest/src/interfaces/novu-options.interface.ts
@@ -1,3 +1,4 @@
+import { ModuleMetadata, Type } from '@nestjs/common';
 import {
   IDirectProvider,
   IEmailProvider,
@@ -5,11 +6,26 @@ import {
   ITemplate,
 } from '@novu/stateless';
 
-export interface NovuOptions {
-  //
-  // This interface describes the options you want to pass to
-  // NovuModule.
-  //
+export interface INovuOptions {
+  /*
+   *
+   * This interface describes the options you want to pass to
+   * NovuModule.
+   *
+   */
   providers: (IEmailProvider | ISmsProvider | IDirectProvider)[];
   templates: ITemplate[];
+}
+
+export interface INovuOptionsFactory {
+  createNovuOptions(): Promise<INovuOptions> | INovuOptions;
+}
+
+export interface INovuModuleAsyncOptions
+  extends Pick<ModuleMetadata, 'imports'> {
+  connectionName?: string;
+  useExisting?: Type<INovuOptionsFactory>;
+  useClass?: Type<INovuOptionsFactory>;
+  useFactory?: (...args: any[]) => Promise<INovuOptions> | INovuOptions;
+  inject?: any[];
 }
--- packages/nest/src/module/novu.module.ts
+++ packages/nest/src/module/novu.module.ts
@@ -1,16 +1,26 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
-import { NovuOptions } from '../interfaces';
-import { createNovuProviders } from '../providers';
+import { INovuModuleAsyncOptions, INovuOptions } from '../interfaces';
+import { createAsyncNovuProviders, createNovuProviders } from '../providers';
 
 @Global()
 @Module({})
 export class NovuModule {
-  public static forRoot(options: NovuOptions): DynamicModule {
+  public static forRoot(options: INovuOptions): DynamicModule {
     const providers = createNovuProviders(options);
 
     return {
       module: NovuModule,
       providers,
+      exports: providers,
+    };
+  }
+
+  public static forRootAsync(options: INovuModuleAsyncOptions): DynamicModule {
+    const providers = createAsyncNovuProviders(options);
+
+    return {
+      module: NovuModule,
+      providers: [],
       exports: providers,
     };
   }
