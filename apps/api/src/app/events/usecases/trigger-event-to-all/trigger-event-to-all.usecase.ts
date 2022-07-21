5cdadd3f82ac7f8685e7c1940c696f949d648495
davidsoderberg <granskog_@hotmail.com>
2022-07-21 10:52:56 +0200

    fix: batching of subscribers

--- apps/api/src/app/events/usecases/trigger-event-to-all/trigger-event-to-all.usecase.ts
+++ apps/api/src/app/events/usecases/trigger-event-to-all/trigger-event-to-all.usecase.ts
@@ -1,35 +1,50 @@
 import { Injectable } from '@nestjs/common';
-import { SubscriberRepository } from '@novu/dal';
-import { ISubscribersDefine } from '@novu/node';
+import { SubscriberEntity, SubscriberRepository } from '@novu/dal';
 import { TriggerEventCommand } from '../trigger-event/trigger-event.command';
 import { TriggerEvent } from '../trigger-event/trigger-event.usecase';
 import { TriggerEventToAllCommand } from './trigger-event-to-all.command';
+import * as _ from 'lodash';
 
 @Injectable()
 export class TriggerEventToAll {
   constructor(private triggerEvent: TriggerEvent, private subscriberRepository: SubscriberRepository) {}
 
   public async execute(command: TriggerEventToAllCommand) {
-    const subscribers = await this.subscriberRepository.find(
+    const batchSize = 500;
+    let list = [];
+
+    for await (const subscriber of this.subscriberRepository.findBatch(
       {
         _environmentId: command.environmentId,
         _organizationId: command.organizationId,
       },
-      'subscriberId'
-    );
+      'subscriberId',
+      {},
+      batchSize
+    )) {
+      list.push(subscriber);
+      if (list.length === batchSize) {
+        await this.trigger(command, list);
+        list = [];
+      }
+    }
 
-    return this.triggerEvent.execute(
+    if (list.length > 0) {
+      await this.trigger(command, list);
+    }
+  }
+
+  private async trigger(command: TriggerEventToAllCommand, list: SubscriberEntity[]) {
+    await this.triggerEvent.execute(
       TriggerEventCommand.create({
         userId: command.userId,
         environmentId: command.environmentId,
         organizationId: command.organizationId,
         identifier: command.identifier,
         payload: command.payload,
-        to: subscribers.map(
-          (subscriber): ISubscribersDefine => ({
-            subscriberId: subscriber.subscriberId,
-          })
-        ),
+        to: list.map((item) => ({
+          subscriberId: item.subscriberId,
+        })),
         transactionId: command.transactionId,
       })
     );
