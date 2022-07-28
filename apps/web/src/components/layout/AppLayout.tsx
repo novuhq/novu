493714a247165c171cabd53fdc27dfc94e8ed7fc
Dima Grossman <dima@grossman.io>
2022-07-28 11:36:30 +0300

    Feedback collection support (#912)

--- apps/web/src/components/layout/AppLayout.tsx
+++ apps/web/src/components/layout/AppLayout.tsx
@@ -5,37 +5,49 @@ import { ThemeProvider } from '../../design-system/ThemeProvider';
 import { HeaderNav } from './components/HeaderNav';
 import { SideNav } from './components/SideNav';
 import { colors } from '../../design-system';
+import { IntercomProvider } from 'react-use-intercom';
+import { INTERCOM_APP_ID } from '../../config';
 
 export function AppLayout() {
   return (
     <ThemeProvider>
-      <AppShell
-        padding="lg"
-        navbar={<SideNav />}
-        header={<HeaderNav />}
-        styles={(theme) => ({
-          main: { backgroundColor: theme.colorScheme === 'dark' ? colors.BGDark : colors.BGLight },
-        })}
-      >
-        <Sentry.ErrorBoundary
-          fallback={({ error, resetError, eventId }) => (
-            <>
-              Sorry, but something went wrong. <br />
-              Our team been notified about it and we will look at it asap.
-              <br />
-              <code>
-                <small style={{ color: 'lightGrey' }}>
-                  Event Id: {eventId}.
-                  <br />
-                  {error.toString()}
-                </small>
-              </code>
-            </>
-          )}
+      <SupportChatProvider>
+        <AppShell
+          padding="lg"
+          navbar={<SideNav />}
+          header={<HeaderNav />}
+          styles={(theme) => ({
+            main: { backgroundColor: theme.colorScheme === 'dark' ? colors.BGDark : colors.BGLight },
+          })}
         >
-          <Outlet />
-        </Sentry.ErrorBoundary>
-      </AppShell>
+          <Sentry.ErrorBoundary
+            fallback={({ error, resetError, eventId }) => (
+              <>
+                Sorry, but something went wrong. <br />
+                Our team been notified about it and we will look at it asap.
+                <br />
+                <code>
+                  <small style={{ color: 'lightGrey' }}>
+                    Event Id: {eventId}.
+                    <br />
+                    {error.toString()}
+                  </small>
+                </code>
+              </>
+            )}
+          >
+            <Outlet />
+          </Sentry.ErrorBoundary>
+        </AppShell>
+      </SupportChatProvider>
     </ThemeProvider>
   );
+}
+
+function SupportChatProvider({ children }) {
+  if (INTERCOM_APP_ID) {
+    return <IntercomProvider appId={INTERCOM_APP_ID}>{children}</IntercomProvider>;
+  }
+
+  return children;
 }
