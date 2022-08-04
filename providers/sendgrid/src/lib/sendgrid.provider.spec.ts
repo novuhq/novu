64a5dd68df48ddae80df4567a6e868c00fb1262f
Dima Grossman <dima@grossman.io>
2022-08-04 13:49:21 +0300

    ci: check if failing

--- providers/sendgrid/src/lib/sendgrid.provider.spec.ts
+++ providers/sendgrid/src/lib/sendgrid.provider.spec.ts
@@ -36,9 +36,9 @@ test('should trigger sendgrid correctly', async () => {
     substitutions: {},
     attachments: [
       {
-        type: 'text/plain',
+        type: 'text/דplain',
         content: Buffer.from('ZEdWemRBPT0=').toString(),
-        filename: 'test.txt',
+        filename: 'tesדt.txt',
       },
     ],
   });
