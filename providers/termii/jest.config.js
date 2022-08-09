0c07bfb0db8e59222d61fbd3f7bbd301c1a4b62d
Longfeng Lian <lianlongfeng@gmail.com>
2022-08-09 10:14:28 -0400

    fix: upgrade node-fetch fix: f fix: f

--- providers/termii/jest.config.js
+++ providers/termii/jest.config.js
@@ -2,4 +2,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
