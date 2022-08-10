263244bdc64fe295b2edc19d44c0beb4b58fd1d6
raikasdev <roni.aikas@gmail.com>
2022-08-10 14:59:00 +0300

    fix: notification center build

--- packages/notification-center/rollup.config.js
+++ packages/notification-center/rollup.config.js
@@ -33,7 +33,7 @@ export default [
     ],
   },
   {
-    input: 'dist/esm/types/index.d.ts',
+    input: 'dist/esm/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
   },
