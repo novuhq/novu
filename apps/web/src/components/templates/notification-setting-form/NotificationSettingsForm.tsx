4f9ccca0976f62483911e95dee7bd69d4701ecfc
Gosha <djabarov.george@gmail.com>
2022-08-10 15:37:59 +0300

    refactor: after design update

--- apps/web/src/components/templates/notification-setting-form/NotificationSettingsForm.tsx
+++ apps/web/src/components/templates/notification-setting-form/NotificationSettingsForm.tsx
@@ -6,7 +6,7 @@ import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { INotificationTrigger } from '@novu/shared';
 import { getNotificationGroups } from '../../../api/notifications';
 import { api } from '../../../api/api.client';
-import { Checkbox, Input, Select, Tooltip } from '../../../design-system';
+import { Input, Select, Tooltip } from '../../../design-system';
 import { Check, Copy } from '../../../design-system/icons';
 import { useEnvController } from '../../../store/use-env-controller';
 import { TemplatePreference } from './TemplatePreference';
@@ -67,109 +67,94 @@ export const NotificationSettingsForm = ({
   }
 
   return (
-    <Grid gutter={30} grow>
-      <Grid.Col md={6} sm={12}>
-        <Controller
-          control={control}
-          name="name"
-          render={({ field }) => (
-            <Input
-              {...field}
-              mb={30}
-              data-test-id="title"
-              disabled={readonly}
-              required
-              value={field.value || ''}
-              error={errors.name?.message}
-              label="Notification Name"
-              description="This will be used to identify the notification in the dashboard."
-              placeholder="Notification name goes here..."
-            />
-          )}
-        />
-        <Controller
-          name="description"
-          control={control}
-          render={({ field }) => (
-            <Input
-              {...field}
-              value={field.value || ''}
-              disabled={readonly}
-              mb={30}
-              data-test-id="description"
-              description="Write an internal description of when and how this notification will be used."
-              label="Notification Description"
-              placeholder="Describe your notification..."
-            />
-          )}
-        />
-        <Controller
-          name="preferenceSettings"
-          control={control}
-          render={({ field }) => {
-            return <TemplatePreference {...field} />;
-          }}
-        />
-      </Grid.Col>
-      <Grid.Col md={6} sm={12}>
-        {trigger && (
-          <Input
-            mb={30}
-            data-test-id="trigger-id"
-            disabled={true}
-            value={trigger.identifier || ''}
-            error={errors.name?.message}
-            label="Notification Identifier"
-            description="This will be used to identify the notification template using the API."
-            rightSection={
-              <Tooltip data-test-id={'Tooltip'} label={idClipboard.copied ? 'Copied!' : 'Copy Key'}>
-                <ActionIcon variant="transparent" onClick={() => idClipboard.copy(trigger.identifier)}>
-                  {idClipboard.copied ? <Check /> : <Copy />}
-                </ActionIcon>
-              </Tooltip>
-            }
+    <>
+      <Grid gutter={30} grow>
+        <Grid.Col md={6} sm={12}>
+          <Controller
+            control={control}
+            name="name"
+            render={({ field }) => (
+              <Input
+                {...field}
+                mb={30}
+                data-test-id="title"
+                disabled={readonly}
+                required
+                value={field.value || ''}
+                error={errors.name?.message}
+                label="Notification Name"
+                description="This will be used to identify the notification in the dashboard."
+                placeholder="Notification name goes here..."
+              />
+            )}
           />
-        )}
-        <Controller
-          name="notificationGroup"
-          control={control}
-          render={({ field }) => {
-            return (
-              <>
-                <Select
-                  {...field}
-                  label="Notification Group"
-                  data-test-id="groupSelector"
-                  loading={loadingGroups || loadingCreateGroup}
-                  disabled={readonly}
-                  creatable
-                  searchable
-                  required
-                  description="Categorize notifications into groups for unified settings control"
-                  error={errors.notificationGroup?.message}
-                  getCreateLabel={(newGroup) => <div data-test-id="submit-category-btn">+ Create Group {newGroup}</div>}
-                  onCreate={addGroupItem}
-                  placeholder="Attach notification to group"
-                  data={(groups || []).map((item) => ({ label: item.name, value: item._id }))}
-                />
-              </>
-            );
-          }}
-        />
-        <Controller
-          name="critical"
-          control={control}
-          render={({ field }) => (
-            <Checkbox
-              {...field}
-              checked={field.value || false}
-              disabled={readonly}
-              data-test-id="critical"
-              label="Critical Workflow"
+          <Controller
+            name="description"
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...field}
+                value={field.value || ''}
+                disabled={readonly}
+                mb={30}
+                data-test-id="description"
+                description="Write an internal description of when and how this notification will be used."
+                label="Notification Description"
+                placeholder="Describe your notification..."
+              />
+            )}
+          />
+        </Grid.Col>
+        <Grid.Col md={6} sm={12}>
+          {trigger && (
+            <Input
+              mb={30}
+              data-test-id="trigger-id"
+              disabled={true}
+              value={trigger.identifier || ''}
+              error={errors.name?.message}
+              label="Notification Identifier"
+              description="This will be used to identify the notification template using the API."
+              rightSection={
+                <Tooltip data-test-id={'Tooltip'} label={idClipboard.copied ? 'Copied!' : 'Copy Key'}>
+                  <ActionIcon variant="transparent" onClick={() => idClipboard.copy(trigger.identifier)}>
+                    {idClipboard.copied ? <Check /> : <Copy />}
+                  </ActionIcon>
+                </Tooltip>
+              }
             />
           )}
-        />
-      </Grid.Col>
-    </Grid>
+          <Controller
+            name="notificationGroup"
+            control={control}
+            render={({ field }) => {
+              return (
+                <>
+                  <Select
+                    {...field}
+                    label="Notification Group"
+                    data-test-id="groupSelector"
+                    loading={loadingGroups || loadingCreateGroup}
+                    disabled={readonly}
+                    creatable
+                    searchable
+                    required
+                    description="Categorize notifications into groups for unified settings control"
+                    error={errors.notificationGroup?.message}
+                    getCreateLabel={(newGroup) => (
+                      <div data-test-id="submit-category-btn">+ Create Group {newGroup}</div>
+                    )}
+                    onCreate={addGroupItem}
+                    placeholder="Attach notification to group"
+                    data={(groups || []).map((item) => ({ label: item.name, value: item._id }))}
+                  />
+                </>
+              );
+            }}
+          />
+        </Grid.Col>
+      </Grid>
+      <TemplatePreference />
+    </>
   );
 };
