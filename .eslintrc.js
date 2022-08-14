95582a8a8ec7da1c8280fe190b41a114458a68aa
Dima Grossman <dima@grossman.io>
2022-08-14 20:29:48 +0300

    fix: add jest.config.js to ignore patterns

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:promise/recommended',
   ],
-  ignorePatterns: ['.eslintrc.js', '*.json'],
+  ignorePatterns: ['.eslintrc.js', '*.json', 'jest.config.js'],
   plugins: ['import', 'promise', '@typescript-eslint', 'prettier'],
   parser: '@typescript-eslint/parser',
   settings: {
