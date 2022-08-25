e022886b015a71cb0c8ae3acb26b56d2d663e94c
Gosha <djabarov.george@gmail.com>
2022-08-25 10:46:20 +0300

    feat: add store index

---
+++ packages/notification-center/src/store/index.ts
@@ -0,0 +1,14 @@
+export * from './notifications-provider.context';
+export * from './auth-provider.context';
+export * from './novu-provider.context';
+export * from './screens-provider.context';
+export * from './novu-theme-provider.context';
+
+export * from './unseen-count.context';
+export * from './screens.context';
+export * from './novu-theme.context';
+export * from './i18n.context';
+export * from './api.context';
+export * from './auth.context';
+export * from './notification-center.context';
+export * from './notifications.context';
