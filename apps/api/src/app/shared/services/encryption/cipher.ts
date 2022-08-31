80d2d05102dcb866128a0e8d59824a15044dd46b
Gosha <djabarov.george@gmail.com>
2022-08-31 11:47:25 +0300

    feat: add encryption service

---
+++ apps/api/src/app/shared/services/encryption/cipher.ts
@@ -0,0 +1,26 @@
+import { createCipheriv, createDecipheriv, randomBytes } from 'crypto';
+
+const ENCRYPTION_KEY = process.env.P_ENCRYPTION_KEY;
+const IV_LENGTH = 16;
+
+export function encrypt(text) {
+  const iv = randomBytes(IV_LENGTH);
+  const cipher = createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+  let encrypted = cipher.update(text);
+
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+  return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+
+export function decrypt(text) {
+  const textParts = text.split(':');
+  const iv = Buffer.from(textParts.shift(), 'hex');
+  const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+  const decipher = createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+  let decrypted = decipher.update(encryptedText);
+
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+  return decrypted.toString();
+}
---
+++ apps/api/src/app/shared/services/encryption/encrypt-provider.ts
@@ -0,0 +1,46 @@
+import { decrypt, encrypt } from './cipher';
+import { ICredentialsDto, secureCredentials } from '@novu/shared';
+
+const novuSubMask = 'nvsk.';
+
+export function encryptProviderSecret(text) {
+  const encrypted = encrypt(text);
+
+  return novuSubMask + encrypted;
+}
+
+export function decryptProviderSecret(text) {
+  if (text.startsWith(novuSubMask)) {
+    text = text.slice(novuSubMask.length);
+  }
+
+  return decrypt(text);
+}
+
+export function encryptCredentials(credentials: ICredentialsDto): ICredentialsDto {
+  const encryptedCredentials: ICredentialsDto = {};
+
+  Object.keys(credentials).forEach((key) => {
+    if (secureCredentials.some((secureCred) => secureCred === key)) {
+      encryptedCredentials[key] = encryptProviderSecret(credentials[key]);
+    } else {
+      encryptedCredentials[key] = credentials[key];
+    }
+  });
+
+  return encryptedCredentials;
+}
+
+export function decryptCredentials(credentials: ICredentialsDto): ICredentialsDto {
+  const decryptedCredentials: ICredentialsDto = {};
+
+  Object.keys(credentials).forEach((key) => {
+    if (credentials[key].startsWith(novuSubMask)) {
+      decryptedCredentials[key] = decryptProviderSecret(credentials[key]);
+    } else {
+      decryptedCredentials[key] = credentials[key];
+    }
+  });
+
+  return decryptedCredentials;
+}
