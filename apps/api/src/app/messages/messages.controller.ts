a1b46d4bc9e26ab7ad9e9e195f610c56ef1a9561
David Söderberg <granskog_@hotmail.com>
2022-09-03 08:53:55 +0200

    feat: add endpoint to delete message

---
+++ apps/api/src/app/messages/messages.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Delete, Param, UseGuards } from '@nestjs/common';
+import { RemoveMessage, RemoveMessageCommand } from './usecases/remove-message';
+import { JwtAuthGuard } from '../auth/framework/auth.guard';
+import { ExternalApiAccessible } from '../auth/framework/external-api.decorator';
+import { UserSession } from '../shared/framework/user.decorator';
+import { IJwtPayload } from '@novu/shared';
+import { ApiTags, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { DeleteMessageResponseDto } from './dtos/delete-message-response.dto';
+
+@Controller('/messages')
+@ApiTags('Messages')
+export class MessagesController {
+  constructor(private removeMessage: RemoveMessage) {}
+
+  @Delete('/:messageId')
+  @ExternalApiAccessible()
+  @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({
+    type: DeleteMessageResponseDto,
+  })
+  @ApiOperation({
+    summary: 'Delete message',
+    description: 'Deletes a message entity from the Novu platform',
+  })
+  async removeSubscriber(
+    @UserSession() user: IJwtPayload,
+    @Param('messageId') messageId: string
+  ): Promise<DeleteMessageResponseDto> {
+    return await this.removeMessage.execute(
+      RemoveMessageCommand.create({
+        environmentId: user.environmentId,
+        organizationId: user.organizationId,
+        messageId,
+      })
+    );
+  }
+}
---
+++ apps/api/src/app/messages/usecases/index.ts
@@ -0,0 +1,3 @@
+import { RemoveMessage } from './remove-message';
+
+export const USE_CASES = [RemoveMessage];
