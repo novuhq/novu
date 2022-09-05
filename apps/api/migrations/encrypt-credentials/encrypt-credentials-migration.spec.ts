807a1ac3bac20c8bf4d01978d36a59329603819c
Gosha <djabarov.george@gmail.com>
2022-09-05 14:52:42 +0300

    refactor: update after pr comments

--- apps/api/migrations/encrypt-credentials/encrypt-credentials-migration.spec.ts
+++ apps/api/migrations/encrypt-credentials/encrypt-credentials-migration.spec.ts
@@ -28,7 +28,25 @@ describe('Encrypt Old Credentials', function () {
         _organizationId: session.organization._id,
         providerId: data.providerId + i,
         channel: data.channel,
-        credentials: { user: 'smith' + i, apiKey: '123' + i, secretKey: 'abc' + i },
+        credentials: {
+          apiKey: '123',
+          user: 'smith' + i,
+          secretKey: '123',
+          domain: 'domain',
+          password: '123',
+          host: 'host',
+          port: 'port',
+          secure: true,
+          region: 'region',
+          accountSid: 'accountSid',
+          messageProfileId: 'messageProfileId',
+          token: '123',
+          from: 'from',
+          senderName: 'senderName',
+          applicationId: 'applicationId',
+          clientId: 'clientId',
+          projectName: 'projectName',
+        },
         active: data.active,
       });
     }
@@ -44,9 +62,22 @@ describe('Encrypt Old Credentials', function () {
     for (const integrationKey in encryptIntegration) {
       const integration = encryptIntegration[integrationKey];
 
-      expect(integration.credentials.user).to.equal('smith' + integrationKey);
       expect(integration.credentials.apiKey).to.contains('nvsk.');
+      expect(integration.credentials.user).to.equal('smith' + integrationKey);
       expect(integration.credentials.secretKey).to.contains('nvsk.');
+      expect(integration.credentials.domain).to.equal('domain');
+      expect(integration.credentials.password).to.contains('nvsk.');
+      expect(integration.credentials.host).to.equal('host');
+      expect(integration.credentials.secure).to.contains('nvsk.');
+      expect(integration.credentials.region).to.equal('region');
+      expect(integration.credentials.accountSid).to.equal('accountSid');
+      expect(integration.credentials.messageProfileId).to.equal('messageProfileId');
+      expect(integration.credentials.token).to.contains('nvsk.');
+      expect(integration.credentials.from).to.equal('from');
+      expect(integration.credentials.senderName).to.equal('senderName');
+      expect(integration.credentials.applicationId).to.equal('applicationId');
+      expect(integration.credentials.clientId).to.equal('clientId');
+      expect(integration.credentials.projectName).to.equal('projectName');
     }
   });
 });
