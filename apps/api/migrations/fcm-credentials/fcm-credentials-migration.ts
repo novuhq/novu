17d0d6694dad82e53cbf753ea9bc42dba792a8ab
Gosha <djabarov.george@gmail.com>
2022-09-05 15:28:28 +0300

    refactor: update after pr comment

--- apps/api/migrations/fcm-credentials/fcm-credentials-migration.ts
+++ apps/api/migrations/fcm-credentials/fcm-credentials-migration.ts
@@ -1,4 +1,4 @@
-import { IntegrationRepository, IntegrationEntity } from '@novu/dal';
+import { IntegrationRepository } from '@novu/dal';
 import { ChannelTypeEnum } from '@novu/stateless';
 
 export async function updateFcmCredentials() {
@@ -7,37 +7,18 @@ export async function updateFcmCredentials() {
 
   const integrationRepository = new IntegrationRepository();
 
-  const integrations = await integrationRepository.find({
-    provider: 'fcm',
-    channel: ChannelTypeEnum.PUSH,
-    'credentials.user': { $exists: true },
-  });
+  // eslint-disable-next-line no-console
+  console.log('rename all credentials.user credentials.serviceAccount - channel push, provider fcm');
 
-  for (const integration of integrations) {
-    // eslint-disable-next-line no-console
-    console.log(`integration ${integration._id}`);
+  await integrationRepository.update(
+    {
+      provider: 'fcm',
+      channel: ChannelTypeEnum.PUSH,
+      'credentials.user': { $exists: true },
+    },
+    { $rename: { 'credentials.user': 'credentials.serviceAccount' } }
+  );
 
-    const updatePayload: Partial<IntegrationEntity> = {};
-
-    if (!integration.credentials?.user) {
-      // eslint-disable-next-line no-console
-      console.log(`integration ${integration._id} - is not contains credential.user, skipping..`);
-      continue;
-    }
-
-    // fcmConfig at the moment contains only serviceAccount therefore no need in adding other params
-    updatePayload.credentials = {};
-    updatePayload.credentials.serviceAccount = integration.credentials.user;
-
-    await integrationRepository.update(
-      { _id: integration._id },
-      {
-        $set: updatePayload,
-      }
-    );
-    // eslint-disable-next-line no-console
-    console.log(`integration ${integration._id} - credentials updated`);
-  }
   // eslint-disable-next-line no-console
   console.log('end migration');
 }
