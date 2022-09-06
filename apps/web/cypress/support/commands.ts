d5689d7e9637a4af0bb87b29699193cd998ef924
venarius <tim.lange@titanhq.de>
2022-09-06 03:21:41 +0200

    feat: ability to invite existing user

--- apps/web/cypress/support/commands.ts
+++ apps/web/cypress/support/commands.ts
@@ -1,6 +1,8 @@
 // load the global Cypress types
 /// <reference types="cypress" />
 
+import { MemberRoleEnum, MemberStatusEnum } from '@novu/shared';
+
 Cypress.Commands.add('getByTestId', (selector, ...args) => {
   return cy.get(`[data-test-id=${selector}]`, ...args);
 });
@@ -33,6 +35,42 @@ Cypress.Commands.add(
     });
   }
 );
+
+Cypress.Commands.add('inviteUser', (email: string) => {
+  return cy
+    .initializeSession()
+    .then((session) => {
+      cy.request({
+        method: 'POST',
+        url: `${Cypress.env('apiUrl')}/v1/invites`,
+        body: {
+          email,
+          role: MemberRoleEnum.ADMIN,
+        },
+        auth: {
+          bearer: session.token,
+        },
+      });
+
+      cy.request({
+        method: 'GET',
+        url: `${Cypress.env('apiUrl')}/v1/organizations/members`,
+        auth: {
+          bearer: session.token,
+        },
+      })
+        .then((response) => {
+          const member = response.body.data.find((i) => i.memberStatus === MemberStatusEnum.INVITED);
+          return member.invite.token;
+        })
+        .as('token');
+
+      cy.logout().then(() => {
+        return session.organization;
+      });
+    })
+    .as('organization');
+});
 
 Cypress.Commands.add('logout', (settings = {}) => {
   return window.localStorage.removeItem('auth_token');
