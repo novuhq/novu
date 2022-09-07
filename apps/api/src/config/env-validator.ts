8ad6dd38e1624c5a9e548a8a7ec99f64c10b0f7d
Gosha <djabarov.george@gmail.com>
2022-09-07 11:40:28 +0300

    feat: add str32 validator

--- apps/api/src/config/env-validator.ts
+++ apps/api/src/config/env-validator.ts
@@ -1,6 +1,13 @@
-import { port, str, url, ValidatorSpec } from 'envalid';
+import { makeValidator, port, str, url, ValidatorSpec } from 'envalid';
 import * as envalid from 'envalid';
 
+const str32 = makeValidator((variable) => {
+  if (!(typeof variable === 'string') || variable.length != 32) {
+    throw new Error('Expected to be string 32 char long');
+  }
+
+  return variable;
+});
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const validators: { [K in keyof any]: ValidatorSpec<any[K]> } = {
   NODE_ENV: str({
@@ -30,7 +37,7 @@ const validators: { [K in keyof any]: ValidatorSpec<any[K]> } = {
   NOVU_API_KEY: str({
     default: '',
   }),
-  STORE_ENCRYPTION_KEY: str(),
+  STORE_ENCRYPTION_KEY: str32(),
 };
 
 if (process.env.NODE_ENV !== 'local' && process.env.NODE_ENV !== 'test') {
