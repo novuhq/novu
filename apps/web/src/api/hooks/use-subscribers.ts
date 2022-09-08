8c9c5f44b5e2478d7a80ffb3c2c3108da486de9c
Pawan Jain <jainpawan211199@gmail.com>
2022-09-08 10:58:58 +0530

    fix: add limit query params in api

--- apps/web/src/api/hooks/use-subscribers.ts
+++ apps/web/src/api/hooks/use-subscribers.ts
@@ -6,8 +6,11 @@ import { ISubscriber } from '@novu/shared';
 export function useSubscribers(page = 0, limit = 10) {
   const { environment } = useEnvController();
   const { data, isLoading } = useQuery<{ data: ISubscriber[]; totalCount: number; pageSize: number }>(
-    ['subscribersList', environment?._id],
-    () => getSubscribersList(page, limit)
+    ['subscribersList', environment?._id, page, limit],
+    () => getSubscribersList(page, limit),
+    {
+      keepPreviousData: true,
+    }
   );
 
   return {
--- apps/web/src/pages/subscribers/SubscribersListPage.tsx
+++ apps/web/src/pages/subscribers/SubscribersListPage.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import { Badge, ActionIcon, useMantineTheme } from '@mantine/core';
-import { Link, useNavigate } from 'react-router-dom';
 import { ColumnWithStrictAccessor } from 'react-table';
 import styled from '@emotion/styled';
 import { format } from 'date-fns';
@@ -11,80 +9,76 @@ import PageContainer from '../../components/layout/components/PageContainer';
 import { Table, Text } from '../../design-system';
 import { Tooltip } from '../../design-system';
 import { Data } from '../../design-system/table/Table';
-import { useEnvController } from '../../store/use-env-controller';
+
+const columns: ColumnWithStrictAccessor<Data>[] = [
+  {
+    accessor: '_id',
+    Header: 'Subscriber Id',
+    Cell: ({ _id }: any) => (
+      <Tooltip label={_id}>
+        <Text rows={1}>{_id}</Text>
+      </Tooltip>
+    ),
+  },
+  {
+    accessor: 'firstName',
+    Header: 'First Name',
+    Cell: ({ firstName }: any) => (
+      <Tooltip label={firstName}>
+        <Text rows={1}>{firstName}</Text>
+      </Tooltip>
+    ),
+  },
+  {
+    accessor: 'lastName',
+    Header: 'Last Name',
+    Cell: ({ lastName }: any) => (
+      <Tooltip label={lastName}>
+        <Text rows={1}>{lastName}</Text>
+      </Tooltip>
+    ),
+  },
+  {
+    accessor: 'deleted',
+    Header: 'Deleted',
+    Cell: ({ deleted }: any) => (
+      <Tooltip label={deleted ? 'true' : 'false'}>
+        <Text rows={1}>{deleted ? 'true' : 'false'}</Text>
+      </Tooltip>
+    ),
+  },
+  {
+    accessor: 'email',
+    Header: 'Email',
+    Cell: ({ email }: any) => (
+      <Tooltip label={email}>
+        <Text rows={1}>{email}</Text>
+      </Tooltip>
+    ),
+  },
+  {
+    accessor: 'phone',
+    Header: 'Phone',
+    Cell: ({ phone }: any) => (
+      <Tooltip label={phone}>
+        <Text rows={1}>{phone}</Text>
+      </Tooltip>
+    ),
+  },
+  {
+    accessor: 'createdAt',
+    Header: 'Created At',
+    Cell: ({ createdAt }: any) => format(new Date(createdAt), 'dd/MM/yyyy HH:mm'),
+  },
+];
 
 function SubscribersList() {
-  const { readonly } = useEnvController();
   const [page, setPage] = useState<number>(0);
   const { subscibers, loading: isLoading, totalCount, pageSize } = useSubscribers(page);
-  const theme = useMantineTheme();
-  const navigate = useNavigate();
 
   function handleTableChange(pageIndex) {
     setPage(pageIndex);
   }
-
-  const columns: ColumnWithStrictAccessor<Data>[] = [
-    {
-      accessor: '_id',
-      Header: 'Subscriber Id',
-      Cell: ({ _id }: any) => (
-        <Tooltip label={_id}>
-          <Text rows={1}>{_id}</Text>
-        </Tooltip>
-      ),
-    },
-    {
-      accessor: 'firstName',
-      Header: 'First Name',
-      Cell: ({ firstName }: any) => (
-        <Tooltip label={firstName}>
-          <Text rows={1}>{firstName}</Text>
-        </Tooltip>
-      ),
-    },
-    {
-      accessor: 'lastName',
-      Header: 'Last Name',
-      Cell: ({ lastName }: any) => (
-        <Tooltip label={lastName}>
-          <Text rows={1}>{lastName}</Text>
-        </Tooltip>
-      ),
-    },
-    {
-      accessor: 'deleted',
-      Header: 'Deleted',
-      Cell: ({ deleted }: any) => (
-        <Tooltip label={deleted ? 'true' : 'false'}>
-          <Text rows={1}>{deleted ? 'true' : 'false'}</Text>
-        </Tooltip>
-      ),
-    },
-    {
-      accessor: 'email',
-      Header: 'Email',
-      Cell: ({ email }: any) => (
-        <Tooltip label={email}>
-          <Text rows={1}>{email}</Text>
-        </Tooltip>
-      ),
-    },
-    {
-      accessor: 'phone',
-      Header: 'Phone',
-      Cell: ({ phone }: any) => (
-        <Tooltip label={phone}>
-          <Text rows={1}>{phone}</Text>
-        </Tooltip>
-      ),
-    },
-    {
-      accessor: 'createdAt',
-      Header: 'Created At',
-      Cell: ({ createdAt }: any) => format(new Date(createdAt), 'dd/MM/yyyy HH:mm'),
-    },
-  ];
 
   return (
     <PageContainer>
@@ -102,9 +96,7 @@ function SubscribersList() {
             total: totalCount,
             onPageChange: handleTableChange,
           }}
-        >
-          {' '}
-        </Table>
+        />
       </SubscibersListTableWrapper>
     </PageContainer>
   );
