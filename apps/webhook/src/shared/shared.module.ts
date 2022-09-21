1025ad6cd3670baf63b0de6a9fbf14908e6ccd84
David Söderberg <granskog_@hotmail.com>
2022-09-21 10:59:39 +0200

    feat: create new webhook service

---
+++ apps/webhook/src/shared/shared.module.ts
@@ -0,0 +1,52 @@
+import { Module } from '@nestjs/common';
+import {
+  DalService,
+  UserRepository,
+  OrganizationRepository,
+  EnvironmentRepository,
+  NotificationTemplateRepository,
+  SubscriberRepository,
+  NotificationRepository,
+  MessageRepository,
+  MemberRepository,
+} from '@novu/dal';
+import { JwtModule } from '@nestjs/jwt';
+
+const DAL_MODELS = [
+  UserRepository,
+  OrganizationRepository,
+  EnvironmentRepository,
+  NotificationTemplateRepository,
+  SubscriberRepository,
+  NotificationRepository,
+  MessageRepository,
+  MemberRepository,
+];
+
+const dalService = new DalService();
+
+const PROVIDERS = [
+  {
+    provide: DalService,
+    useFactory: async () => {
+      await dalService.connect(process.env.MONGO_URL);
+
+      return dalService;
+    },
+  },
+  ...DAL_MODELS,
+];
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secretOrKeyProvider: () => process.env.JWT_SECRET as string,
+      signOptions: {
+        expiresIn: 360000,
+      },
+    }),
+  ],
+  providers: [...PROVIDERS],
+  exports: [...PROVIDERS, JwtModule],
+})
+export class SharedModule {}
