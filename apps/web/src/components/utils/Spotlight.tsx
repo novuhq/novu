1faa3818045e6fe8ce5ecf01141dc67192040cb6
David Söderberg <granskog_@hotmail.com>
2022-09-22 12:15:06 +0200

    fix: remove some navigation

--- apps/web/src/components/utils/Spotlight.tsx
+++ apps/web/src/components/utils/Spotlight.tsx
@@ -1,7 +1,7 @@
 import { SpotlightProvider } from '@mantine/spotlight';
 import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Activity, Bolt, Box, Settings, Team, Repeat } from '../../design-system/icons';
+import { Bolt } from '../../design-system/icons';
 import { SpotlightContext } from '../../store/spotlightContext';
 
 export const SpotLight = ({ children }) => {
@@ -15,42 +15,6 @@ export const SpotLight = ({ children }) => {
         title: 'Go to Notification Template',
         onTrigger: () => navigate('/templates'),
         icon: <Bolt />,
-      },
-      {
-        id: 'navigate-subscribers',
-        title: 'Go to Subscribers',
-        onTrigger: () => navigate('/subscribers'),
-        icon: <Team />,
-      },
-      {
-        id: 'navigate-activity-feed',
-        title: 'Go to Activity feed',
-        onTrigger: () => navigate('/activities'),
-        icon: <Activity />,
-      },
-      {
-        id: 'navigate-integrations',
-        title: 'Go to Integrations',
-        onTrigger: () => navigate('/integrations'),
-        icon: <Box />,
-      },
-      {
-        id: 'navigate-settings',
-        title: 'Go to Settings',
-        onTrigger: () => navigate('/settings'),
-        icon: <Settings />,
-      },
-      {
-        id: 'navigate-team-members',
-        title: 'Go to Team members',
-        onTrigger: () => navigate('/tema'),
-        icon: <Team />,
-      },
-      {
-        id: 'navigate-changes',
-        title: 'Go to Changes',
-        onTrigger: () => navigate('/changes'),
-        icon: <Repeat />,
       },
       {
         id: 'navigate-docs',
