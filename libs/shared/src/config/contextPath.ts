3fadf30df593927b7a63552420884942e742bafb
309528 <zaclifton@pplweb.com>
2022-09-22 22:37:22 -0400

    feat(change contextpath to context_path and remove embed context path env variable): fix contextpath
    Change CONTEXTPATH to CONTEXT_PATH and remove embed context path env variable

--- libs/shared/src/config/contextPath.ts
+++ libs/shared/src/config/contextPath.ts
@@ -3,41 +3,34 @@ export enum NovuComponentEnum {
   API,
   WIDGET,
   WS,
-  EMBEDED,
 }
 
 export function getContextPath(component: NovuComponentEnum) {
   let contextPath = '';
 
-  if (process.env.GLOBAL_CONTEXTPATH) {
-    contextPath += process.env.GLOBAL_CONTEXTPATH + '/';
+  if (process.env.GLOBAL_CONTEXT_PATH) {
+    contextPath += process.env.GLOBAL_CONTEXT_PATH + '/';
   }
 
   switch (component) {
     case NovuComponentEnum.API:
-      if (process.env.API_CONTEXTPATH) {
-        contextPath += process.env.API_CONTEXTPATH + '/';
+      if (process.env.API_CONTEXT_PATH) {
+        contextPath += process.env.API_CONTEXT_PATH + '/';
       }
       break;
     case NovuComponentEnum.WEB:
-      if (process.env.FRONT_BASE_CONTEXTPATH) {
-        contextPath += process.env.FRONT_BASE_CONTEXTPATH + '/';
+      if (process.env.FRONT_BASE_CONTEXT_PATH) {
+        contextPath += process.env.FRONT_BASE_CONTEXT_PATH + '/';
       }
       break;
     case NovuComponentEnum.WIDGET:
-      if (process.env.WIDGET_CONTEXTPATH) {
-        contextPath += process.env.WIDGET_CONTEXTPATH + '/';
+      if (process.env.WIDGET_CONTEXT_PATH) {
+        contextPath += process.env.WIDGET_CONTEXT_PATH + '/';
       }
       break;
-    case NovuComponentEnum.EMBEDED:
-      if (process.env.WIDGET_EMBED_CONTEXTPATH) {
-        contextPath += process.env.WIDGET_EMBED_CONTEXTPATH + '/';
-      }
-      break;
-
     case NovuComponentEnum.WS:
-      if (process.env.REACT_APP_WS_CONTEXTPATH) {
-        contextPath += process.env.REACT_APP_WS_CONTEXTPATH + '/';
+      if (process.env.WS_CONTEXT_PATH) {
+        contextPath += process.env.WS_CONTEXT_PATH + '/';
       }
       break;
   }
