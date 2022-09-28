78d0971b3e96f6a3f49734f7e2f1fdca5e3f23a5
Biswajeet Das <biswajeetdas18@gmail.com>
2022-09-28 16:56:40 +0530

    refactor: updated the imports order

--- apps/api/src/app/vercel-integration/vercel-integration.module.ts
+++ apps/api/src/app/vercel-integration/vercel-integration.module.ts
@@ -1,8 +1,8 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { USE_CASES } from './usecases';
 import { VercelIntegrationController } from './vercel-integration.controller';
 import { SharedModule } from '../shared/shared.module';
-import { HttpModule } from '@nestjs/axios';
 import { EnvironmentsModule } from '../environments/environments.module';
 
 @Module({
