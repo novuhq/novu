a897c3f3e2b7c60ce5d78bec9384099e32e1c24d
chavda-bhavik <bhavikvchavda@gmail.com>
2022-10-01 07:02:44 +0530

    test: Added test-cases for storage-helper-service for Azure and GoogleCloud

--- apps/api/src/app/events/services/storage-helper-service/storage-helper.service.spec.ts
+++ apps/api/src/app/events/services/storage-helper-service/storage-helper.service.spec.ts
@@ -1,8 +1,15 @@
+import * as sinon from 'sinon';
 import { expect } from 'chai';
+import { File } from '@google-cloud/storage';
 import { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
 import { mockClient } from 'aws-sdk-client-mock';
-import { S3StorageService } from '../../../shared/services/storage/storage.service';
+import {
+  S3StorageService,
+  GCSStorageService,
+  AzureBlobStorageService,
+} from '../../../shared/services/storage/storage.service';
 import { StorageHelperService } from './storage-helper.service';
+import { BlockBlobClient } from '@azure/storage-blob';
 
 const s3Mock = mockClient(S3Client);
 
@@ -53,6 +60,80 @@ describe('Storage-Helper service', function () {
       s3Mock.on(GetObjectCommand).rejects('The specified key does not exist.');
       await S3StorageHelperService.getAttachments(resultAttachments);
       expect(resultAttachments[0].file).to.be.null;
+    });
+  });
+
+  // mocking the storage service with sinon
+  describe('Google Cloud', function () {
+    afterEach(() => {
+      sinon.restore();
+    });
+    let GCSStorageHelperService = new StorageHelperService(new GCSStorageService());
+    let gcAttachments = [
+      {
+        name: 'test.png',
+        file: Buffer.from('test'),
+        mime: 'image/png',
+      },
+    ];
+
+    it('should upload file', async function () {
+      // resolve upload
+      const bucketStub = sinon.stub(File.prototype, 'save').resolves(gcAttachments);
+      await GCSStorageHelperService.uploadAttachments(gcAttachments);
+      sinon.assert.calledOnce(bucketStub);
+    });
+
+    it('should delete file', async function () {
+      // resolve delete
+      const bucketStub = sinon.stub(File.prototype, 'delete').resolves(gcAttachments);
+      await GCSStorageHelperService.deleteAttachments(gcAttachments);
+      sinon.assert.calledOnce(bucketStub);
+    });
+
+    it('should get file', async function () {
+      // resolve get
+      const bucketStub = sinon.stub(File.prototype, 'download').resolves(gcAttachments);
+      await GCSStorageHelperService.getAttachments(gcAttachments);
+      sinon.assert.calledOnce(bucketStub);
+    });
+  });
+
+  // mocking the azure storage service with sinon
+  describe('Azure', function () {
+    afterEach(() => {
+      sinon.restore();
+    });
+    let AzureStorageHelperService = new StorageHelperService(new AzureBlobStorageService());
+    let azureAttachments = [
+      {
+        name: 'test.png',
+        file: Buffer.from('test'),
+        mime: 'image/png',
+      },
+    ];
+
+    it('should upload file', async function () {
+      // @ts-ignore
+      const uploadStub = sinon.stub(BlockBlobClient.prototype, 'upload').resolves({ _response: { status: 201 } });
+      await AzureStorageHelperService.uploadAttachments(azureAttachments);
+      sinon.assert.calledOnce(uploadStub);
+    });
+
+    it('should delete file', async function () {
+      // @ts-ignore
+      const deleteStub = sinon.stub(BlockBlobClient.prototype, 'delete').resolves({ _response: { status: 202 } });
+      await AzureStorageHelperService.deleteAttachments(azureAttachments);
+      sinon.assert.calledOnce(deleteStub);
+    });
+
+    it('should get file', async function () {
+      const downloadStub = sinon
+        .stub(BlockBlobClient.prototype, 'downloadToBuffer')
+        // @ts-ignore
+        .resolves({ _response: { status: 200 } });
+      await AzureStorageHelperService.getAttachments(azureAttachments);
+      sinon.assert.calledOnce(downloadStub);
     });
   });
 });
