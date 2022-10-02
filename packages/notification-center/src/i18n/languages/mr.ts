99775934b62f12049934be0378886bdf0202a704
Shrinath Pawar <97937070+ShrinathPawar@users.noreply.github.com>
2022-10-02 20:48:45 +0530

    mr.ts created

---
+++ packages/notification-center/src/i18n/languages/mr.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const HR: ITranslationEntry = {
+  translations: {
+    notifications: 'सुचना',
+    markAllAsRead: 'सर्व वाचल्याप्रमाणे चिन्हांकित करा',
+    poweredBy: 'द्वारा समर्थित',
+    settings: 'सेटिंग्स',
+  },
+  lang: 'mr',
+};
