547b0a5d6cfd5ffc6b8469508bca9f4f02762129
ABHISHEK FADAKE <31249309+fadkeabhi@users.noreply.github.com>
2022-10-04 11:19:30 +0530

    Create sd.ts

---
+++ packages/notification-center/src/i18n/languages/sd.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const EN: ITranslationEntry = {
+  translations: {
+    notifications: 'نوٽيفڪيشن',
+    markAllAsRead: 'سڀني کي پڙهيل طور نشان لڳايو',
+    poweredBy: 'پاران تيار ڪيل',
+    settings: 'سيٽنگون',
+  },
+  lang: 'sd',
+};
