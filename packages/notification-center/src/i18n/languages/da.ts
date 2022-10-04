82b5255ed7c8e2d4c3c5f46fb603f57d8a2c0dfe
Sahil Sunil Phadtare <97933138+IamsahilPhadtare@users.noreply.github.com>
2022-10-04 19:15:03 +0530

    Create da.ts
    Added Danish Language

---
+++ packages/notification-center/src/i18n/languages/da.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const EN: ITranslationEntry = {
+  translations: {
+    notifications: 'Meddelelser',
+    markAllAsRead: 'Markér alle som læst',
+    poweredBy: 'Drevet af',
+    settings: 'Indstillinger',
+  },
+  lang: 'da',
+};
