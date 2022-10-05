81693c8fcbcfb6e2dee60630344f18e7872c6567
Sahil Sunil Phadtare <97933138+IamsahilPhadtare@users.noreply.github.com>
2022-10-05 11:20:48 +0530

    Create gk.ts
    Added Greek Support

---
+++ packages/notification-center/src/i18n/languages/gk.ts
@@ -0,0 +1,10 @@
+import { ITranslationEntry } from '../lang';
+
+export const GK: ITranslationEntry = {
+  translations: {
+    notifications: 'ειδοποιήσεις',
+    markAllAsRead: 'επισημάνετε όλα ως αναγνωσμένα',
+    poweredBy: 'τροφοδοτείται από',
+  },
+  lang: 'gk',
+};
