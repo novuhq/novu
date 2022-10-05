b2066a665b898b1a1a39609704fb375ff10a28b5
Pere Montpeo <pere.montpeo@decathlon.com>
2022-10-05 11:48:50 +0200

    fix typo

--- packages/notification-center/src/i18n/languages/es.ts
+++ packages/notification-center/src/i18n/languages/es.ts
@@ -3,7 +3,7 @@ import { ITranslationEntry } from '../lang';
 export const ES: ITranslationEntry = {
   translations: {
     notifications: 'Notificaciones',
-    markAllAsRead: 'marcar todo como leido',
+    markAllAsRead: 'marcar todo como leído',
     poweredBy: 'Con tecnología de',
     settings: 'Configuración',
   },
