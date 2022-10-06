76bbfd5416f2c492d579b6a37ab60de2c55b2abc
David Söderberg <granskog_@hotmail.com>
2022-10-06 10:45:16 +0200

    test: add test for get execution details by notification id

--- apps/api/src/app/execution-details/execution-details.module.ts
+++ apps/api/src/app/execution-details/execution-details.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { USE_CASES } from './usecases';
 import { ExecutionDetailsController } from './execution-details.controller';
 import { SharedModule } from '../shared/shared.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [SharedModule],
+  imports: [SharedModule, AuthModule],
   providers: [...USE_CASES],
   exports: [...USE_CASES],
   controllers: [ExecutionDetailsController],
