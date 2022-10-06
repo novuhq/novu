aac6d2c8674409e27ef0e55ce01815bf75335546
dilshan-h <77499497+Dilshan-H@users.noreply.github.com>
2022-10-07 00:10:36 +0530

    Add Sinhala language support

--- packages/notification-center/src/i18n/lang.ts
+++ packages/notification-center/src/i18n/lang.ts
@@ -33,6 +33,7 @@ import { JA } from './languages/ja';
 import { HU } from './languages/hu';
 import { DA } from './languages/da';
 import { AS } from './languages/as';
+import { SI } from './languages/si';
 
 export interface ITranslationContent {
   readonly notifications: string;
@@ -82,6 +83,7 @@ export const TRANSLATIONS: Record<I18NLanguage, ITranslationEntry> = {
   hu: HU,
   da: DA,
   as: AS,
+  si: SI,
 };
 
 /**
@@ -130,4 +132,5 @@ export type I18NLanguage =
   | 'as'
   | 'ja'
   | 'hu'
-  | 'da';
+  | 'da'
+  | 'si';
