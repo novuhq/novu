d7fe2d1e95a332cabd1a94a645cdc9dac56abfdf
Susmita-Dey <susmitadey475@gmail.com>
2022-10-06 22:15:32 +0530

    telugu

---
+++ packages/notification-center/src/i18n/languages/te.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const TE: ITranslationEntry = {
+  translations: {
+    notifications: 'నోటిఫికేషన్‌లు',
+    markAllAsRead: 'మార్క్ అన్ని ',
+    poweredBy: 'ద్వారా ఆధారితం',
+    settings: 'సెట్టింగ్‌లు',
+  },
+  lang: 'te',
+}; 
