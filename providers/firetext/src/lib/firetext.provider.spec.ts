ea2036cc98c98d578bc125378c7d20ec9da3e3ac
Nick Prinsloo <nick@ultramed.co>
2022-10-06 22:29:32 +0100

    feat: Implement FiretextSmsProvider

--- providers/firetext/src/lib/firetext.provider.spec.ts
+++ providers/firetext/src/lib/firetext.provider.spec.ts
@@ -1,3 +1,76 @@
 import { FiretextSmsProvider } from './firetext.provider';
+import fetchMock from 'fetch-mock';
 
-test('should trigger firetext library correctly', async () => {});
+describe('FiretextSmsProvider', () => {
+  const date = new Date();
+
+  const provider = new FiretextSmsProvider({
+    apiKey: 'apiKey',
+    from: 'testFrom',
+  });
+
+  beforeAll(() => {
+    jest.useFakeTimers('modern').setSystemTime(date);
+  });
+
+  afterAll(() => {
+    jest.clearAllTimers();
+  });
+
+  afterEach(() => {
+    fetchMock.reset();
+  });
+
+  test('should trigger firetext library correctly', async () => {
+    fetchMock.mock('*', {
+      headers: {
+        'X-Message': 'ID',
+        'Content-Type': 'text/plain',
+      },
+      body: '0:12 SMS successfully queued',
+    });
+
+    const result = await provider.sendMessage({
+      content: 'content',
+      to: '+44123456789',
+    });
+
+    expect(result).toMatchObject({ id: 'ID', date: date.toISOString() });
+  });
+
+  test('should call fetch correctly', async () => {
+    fetchMock.mock('*', {
+      headers: {
+        'X-Message': 'ID',
+        'Content-Type': 'text/plain',
+      },
+      body: '0:12 SMS successfully queued',
+    });
+
+    const result = await provider.sendMessage({
+      content: 'content',
+      to: '+44123456789',
+    });
+
+    expect(fetchMock.lastUrl()).toBe(
+      'https://www.firetext.co.uk/api/sendsms?apiKey=apiKey&to=%2B44123456789&from=testFrom&message=content'
+    );
+  });
+
+  test('should throw error', async () => {
+    fetchMock.mock('*', {
+      headers: {
+        'X-Message': 'ID',
+        'Content-Type': 'text/plain',
+      },
+      body: '1:0 Authentication error',
+    });
+
+    const result = provider.sendMessage({
+      content: 'content',
+      to: '+44123456789',
+    });
+
+    await expect(result).rejects.toThrowError('1: Authentication error');
+  });
+});
--- providers/firetext/src/lib/firetext.provider.ts
+++ providers/firetext/src/lib/firetext.provider.ts
@@ -5,9 +5,15 @@ import {
   ISmsProvider,
 } from '@novu/stateless';
 
+if (!globalThis.fetch) {
+  // eslint-disable-next-line global-require
+  globalThis.fetch = require('node-fetch');
+}
+
 export class FiretextSmsProvider implements ISmsProvider {
   id = 'firetext';
   channelType = ChannelTypeEnum.SMS as ChannelTypeEnum.SMS;
+  private BASE_URL = 'https://www.firetext.co.uk/api/sendsms';
 
   constructor(
     private config: {
@@ -16,12 +22,46 @@ export class FiretextSmsProvider implements ISmsProvider {
     }
   ) {}
 
+  private parseResponse(body: string) {
+    const re = /^(\d+):(\d+)\s(.*)$/i;
+    const found = body.match(re);
+    const code = found[1] ?? 'Unknown status code';
+    const message = found[3] ?? 'Unknown status message';
+
+    return [code, message];
+  }
+
+  private getMessageId(headers: Headers) {
+    return headers.get('X-Message');
+  }
+
   async sendMessage(
     options: ISmsOptions
   ): Promise<ISendMessageSuccessResponse> {
+    const baseMessage = {
+      apiKey: this.config.apiKey,
+      to: options.to,
+      from: this.config.from,
+      message: options.content,
+    };
+
+    const urlSearchParams = new URLSearchParams(baseMessage);
+    const url = new URL(this.BASE_URL);
+    url.search = urlSearchParams.toString();
+
+    const response = await fetch(url.toString());
+    const body = await response.text();
+    const [code, message] = this.parseResponse(body);
+
+    if (code !== '0') {
+      throw new Error(`${code}: ${message}`);
+    }
+
+    const messageId = this.getMessageId(response.headers);
+
     return {
-      id: 'PLACEHOLDER',
-      date: 'PLACEHOLDER',
+      id: messageId,
+      date: new Date().toISOString(),
     };
   }
 }
