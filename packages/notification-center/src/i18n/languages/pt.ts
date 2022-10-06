fa6ed55b160a249fc575d8403262141bb4965a47
Manish <111450082+ManishModak@users.noreply.github.com>
2022-10-06 20:01:07 +0530

    Create pt.ts
    Added Portuguese Support
    Couldn't find proper translation for Powered by and Settings
    So instead used translation for  Support By and System respectively.

---
+++ packages/notification-center/src/i18n/languages/pt.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const PT: ITranslationEntry = {
+  translations: {
+    notifications: 'notificações',
+    markAllAsRead: 'marcar tudo como lido',
+    poweredBy: 'Apoiado por',
+    settings: 'Sistema',
+  },
+  lang: 'pt',
+};
