eeb7f1f17ecacf04514e78945ec0224d73090444
Manish <111450082+ManishModak@users.noreply.github.com>
2022-10-07 14:23:38 +0530

    Update packages/notification-center/src/i18n/languages/pt.ts
    Co-authored-by: Pablo Fernández <pablo@novu.co>

--- packages/notification-center/src/i18n/languages/pt.ts
+++ packages/notification-center/src/i18n/languages/pt.ts
@@ -2,7 +2,7 @@ import { ITranslationEntry } from '../lang';
 
 export const PT: ITranslationEntry = {
   translations: {
-    notifications: 'notificações',
+    notifications: 'Notificações',
     markAllAsRead: 'Marcar tudo como lido',
     poweredBy: 'Desenvolvido por',
     settings: 'Configurações',
