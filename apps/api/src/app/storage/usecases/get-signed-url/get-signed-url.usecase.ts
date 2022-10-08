7a23e66d027e7bf9789f169aacdeda7acee8abc1
devblin <dhruwxl@gmail.com>
2022-10-08 15:43:35 +0530

    feat(api): added logic to check for existing brand-image-key and reuse it

--- apps/api/src/app/storage/usecases/get-signed-url/get-signed-url.usecase.ts
+++ apps/api/src/app/storage/usecases/get-signed-url/get-signed-url.usecase.ts
@@ -3,18 +3,34 @@ import * as hat from 'hat';
 import { StorageService } from '../../../shared/services/storage/storage.service';
 import { UploadUrlResponse } from '../../dtos/upload-url-response.dto';
 import { GetSignedUrlCommand } from './get-signed-url.command';
+import { GetOrganization } from '../../../organization/usecases/get-organization/get-organization.usecase';
 
 const mimeTypes = {
   jpeg: 'image/jpeg',
   png: 'image/png',
+  jpg: 'image/jpeg',
 };
 
 @Injectable()
 export class GetSignedUrl {
-  constructor(private storageService: StorageService) {}
+  constructor(private storageService: StorageService, private getOrganization: GetOrganization) {}
 
   async execute(command: GetSignedUrlCommand): Promise<UploadUrlResponse> {
-    const path = `${command.organizationId}/${command.environmentId}/${hat()}.${command.extension}`;
+    const organization = await this.getOrganization.execute({ id: command.organizationId, userId: command.userId });
+
+    let path = `${command.organizationId}/${command.environmentId}/${hat()}.${command.extension}`;
+
+    if (organization.branding.logo) {
+      const currentImageName = organization.branding.logo.split('/').pop().split('.');
+      const currentImagePrefix = currentImageName.shift();
+      const currentImageExt = currentImageName.pop();
+
+      if (currentImageExt === command.extension) {
+        path = `${command.organizationId}/${command.environmentId}/${currentImagePrefix}.${currentImageExt}`;
+      } else {
+        path = `${command.organizationId}/${command.environmentId}/${currentImagePrefix}.${command.extension}`;
+      }
+    }
 
     const response = await this.storageService.getSignedUrl(path, mimeTypes[command.extension]);
 
--- apps/api/src/app/storage/usecases/index.ts
+++ apps/api/src/app/storage/usecases/index.ts
@@ -1,6 +1,4 @@
+import { GetOrganization } from '../../organization/usecases/get-organization/get-organization.usecase';
 import { GetSignedUrl } from './get-signed-url/get-signed-url.usecase';
 
-export const USE_CASES = [
-  GetSignedUrl,
-  //
-];
+export const USE_CASES = [GetSignedUrl, GetOrganization];
