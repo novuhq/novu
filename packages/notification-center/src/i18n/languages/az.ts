76bb9702cd2e760fd7455eabe3b856f8074c283d
Utsav Paul <91927689+Smartmind12@users.noreply.github.com>
2022-10-09 21:01:49 +0530

    Create az.ts

---
+++ packages/notification-center/src/i18n/languages/az.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const AZ: ITranslationEntry = {
+  translations: {
+    notifications: 'bildirişlər',
+    markAllAsRead: 'Hamısını oxunmuş olaraq işarələ',
+    poweredBy: 'tərəfindən idarə olunur',
+    settings: 'parametrlər',
+  },
+  lang: 'az',
+};
