1458e3e47c459be81f542925c1c167438557e18b
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2022-10-09 15:54:37 +0530

    Create sv.ts

---
+++ packages/notification-center/src/i18n/languages/sv.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const SV: ITranslationEntry = {
+  translations: {
+    notifications: 'meddelanden',
+    markAllAsRead: 'Markera allt som läst',
+    poweredBy: 'poweredBy',
+    settings: 'inställningar',
+  },
+  lang: 'sv',
+};
