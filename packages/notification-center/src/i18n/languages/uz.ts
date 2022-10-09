66ca28a05b7ca2b69b47bd3a2a85b8df21479635
Utsav Paul <91927689+Smartmind12@users.noreply.github.com>
2022-10-09 21:43:33 +0530

    Create uz.ts

---
+++ packages/notification-center/src/i18n/languages/uz.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const UZ: ITranslationEntry = {
+  translations: {
+    notifications: 'bildirishnomalar',
+    markAllAsRead: 'Hammasini o'qing deb belgilang',
+    poweredBy: 'Tomonidan qo'llab-quvvatlanadi',
+    settings: 'sozlamalar',
+  },
+  lang: 'uz',
+};
