6235d24e8d01f6ac3f09a13bdc3e930c4356c652
PRATHAMESH NAGAWADE <100768328+prathameshsnagawade@users.noreply.github.com>
2022-10-09 15:12:33 +0530

    Create bh.ts

---
+++ packages/notification-center/src/i18n/languages/bh.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const BH: ITranslationEntry = {
+  translations: {
+    notifications: 'सूचनाा',
+    markAllAsRead: 'सब के पढ़ल चिन्हित करीं',
+    poweredBy: 'पावर बाय',
+    settings: 'सेटिंग्स',
+  },
+  lang: 'bh',
+};
