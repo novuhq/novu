e8feeb76f608c695fe07378081b10b49374f30f2
Dima Grossman <dima@grossman.io>
2022-10-11 16:38:50 +0300

    Update Spotlight.tsx

--- apps/web/src/components/utils/Spotlight.tsx
+++ apps/web/src/components/utils/Spotlight.tsx
@@ -1,7 +1,6 @@
 import { SpotlightProvider } from '@mantine/spotlight';
 import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Bolt, Box, Settings, Repeat } from '../../design-system/icons';
 import { Activity, Bolt, Box, Settings, Repeat } from '../../design-system/icons';
 import { SpotlightContext } from '../../store/spotlightContext';
 
