74fc816385082752f3160ddbe0f13592e7dc70c6
Palash Dhabale <100768184+PalashDhabale@users.noreply.github.com>
2022-10-11 18:42:04 +0530

    Create kok.ts

---
+++ packages/notification-center/src/i18n/languages/kok.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const KOK: ITranslationEntry = {
+  translations: {
+    notifications: 'अधिसुचोवण्यो',
+    markAllAsRead: 'सगळें वाचलां अशें चिन्न करचें',
+    poweredBy: 'पॉवर केल्लें बाय',
+    settings: 'मांडावळी',
+  },
+  lang: 'kok',
+};
