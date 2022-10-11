24f066e30868ad440bfa079d19892a54bf403918
ShaneMaglangit <shanedmaglangit@gmail.com>
2022-10-11 13:24:37 +0800

    feat: Add webhook parser for Sendinblue

--- providers/sendinblue/src/lib/sendinblue.provider.ts
+++ providers/sendinblue/src/lib/sendinblue.provider.ts
@@ -1,10 +1,12 @@
 import {
   ChannelTypeEnum,
+  CheckIntegrationResponseEnum,
+  EmailEventStatusEnum,
+  ICheckIntegrationResponse,
+  IEmailEventBody,
   IEmailOptions,
   IEmailProvider,
   ISendMessageSuccessResponse,
-  ICheckIntegrationResponse,
-  CheckIntegrationResponseEnum,
 } from '@novu/stateless';
 import {
   SendSmtpEmail,
@@ -52,6 +54,75 @@ export class SendinblueEmailProvider implements IEmailProvider {
       id: body?.messageId,
       date: response?.headers?.date,
     };
+  }
+
+  getMessageId(body: any | any[]): string[] {
+    if (Array.isArray(body)) {
+      return body.map((item) => item['message-id']);
+    }
+
+    return [body['message-id']];
+  }
+
+  parseEventBody(
+    body: any | any[],
+    identifier: string
+  ): IEmailEventBody | undefined {
+    if (Array.isArray(body)) {
+      body = body.find((item) => item['message-id'] === identifier);
+    }
+
+    if (!body) {
+      return undefined;
+    }
+
+    const status = this.getStatus(body.event);
+
+    if (status === undefined) {
+      return undefined;
+    }
+
+    return {
+      status: status,
+      date: body.date,
+      externalId: body.id,
+      row: body,
+    };
+  }
+
+  private getStatus(event: string): EmailEventStatusEnum | undefined {
+    switch (event) {
+      case 'sent':
+        return EmailEventStatusEnum.SENT;
+      case 'request':
+        return EmailEventStatusEnum.REQUEST;
+      case 'delivered':
+        return EmailEventStatusEnum.DELIVERED;
+      case 'hardBounce':
+        return EmailEventStatusEnum.HARD_BOUNCE;
+      case 'softBounce':
+        return EmailEventStatusEnum.SOFT_BOUNCE;
+      case 'blocked':
+        return EmailEventStatusEnum.BLOCKED;
+      case 'spam':
+        return EmailEventStatusEnum.SPAM;
+      case 'invalid':
+        return EmailEventStatusEnum.INVALID;
+      case 'deferred':
+        return EmailEventStatusEnum.DEFERRED;
+      case 'click':
+        return EmailEventStatusEnum.CLICKED;
+      case 'opened':
+        return EmailEventStatusEnum.OPENED;
+      case 'uniqueOpened':
+        return EmailEventStatusEnum.UNIQUE_OPENED;
+      case 'unsubscribed':
+        return EmailEventStatusEnum.UNSUBSCRIBED;
+      case 'listAddition':
+        return EmailEventStatusEnum.LIST_ADDITION;
+      case 'inboundEmailProcessed':
+        return EmailEventStatusEnum.INBOUND_EMAIL_PROCESSED;
+    }
   }
 
   async checkIntegration(
