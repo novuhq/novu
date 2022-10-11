d129323d4f599ef2241e62bd0f2e516035a44434
Palash Dhabale <100768184+PalashDhabale@users.noreply.github.com>
2022-10-11 19:00:53 +0530

    Create ga.ts

---
+++ packages/notification-center/src/i18n/languages/ga.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const EN: ITranslationEntry = {
+  translations: {
+    notifications: 'Fógraí',
+    markAllAsRead: 'Marcáil go léir mar a léitear',
+    poweredBy: 'Cumhachtaithe ag',
+    settings: 'Socruithe',
+  },
+  lang: 'en',
+};
