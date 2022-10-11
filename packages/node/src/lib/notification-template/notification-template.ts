22aec3bbf99a3ec6bc37f58a971be3149da14f3e
Kartik <kjmickey003@gmail.com>
2022-10-12 03:08:29 +0530

    ♻️ refactor: remove return type from methods - notification-templates - add check on page and limit in getAll templates

--- packages/node/src/lib/notification-template/notification-template.ts
+++ packages/node/src/lib/notification-template/notification-template.ts
@@ -1,4 +1,4 @@
-import { AxiosInstance, AxiosResponse } from 'axios';
+import axios from 'axios';
 
 import { WithHttp } from '../novu.interface';
 
@@ -14,28 +14,37 @@ export class NotificationTemplates
   /**
    * @param {number} page - Page number to fetch
    * @param {number} limit - Number of results to fetch in one page
-   * @returns {Promise<AxiosResponse>} - Fetches all the notification-templates with the given parameters
    */
-  async getAll(page?: number, limit?: number): Promise<AxiosResponse> {
-    return await this.http.get(`/notification-templates`, {
-      params: {
-        page,
-        limit,
-      },
-    });
+  async getAll(page?: number, limit?: number) {
+    if (page === undefined && limit === undefined) {
+      return await this.http.get(`/notification-templates`);
+    } else if (page === undefined) {
+      return await this.http.get(`/notification-templates`, {
+        params: {
+          limit,
+        },
+      });
+    } else if (limit === undefined) {
+      return await this.http.get(`/notification-templates`, {
+        params: { page },
+      });
+    } else {
+      return await this.http.get(`/notification-templates`, {
+        params: { page, limit },
+      });
+    }
   }
 
   /**
    * @param {string} name - Name of the notification-template to create
    * @param {string} notificationGroupId - Id of the group that the notification-template belongs
    * @param {INotificationTemplatePayload} data - All the additional parameters to create a notification-template
-   * @returns {Promise<AxiosResponse>} - Creates the notification-template with the given parameters
    */
   async create(
     name: string,
     notificationGroupId: string,
     data?: INotificationTemplatePayload
-  ): Promise<AxiosResponse> {
+  ) {
     return await this.http.post(`/notification-templates`, {
       name,
       notificationGroupId,
@@ -47,14 +56,13 @@ export class NotificationTemplates
    * @param {string} templateId - templateId of the notification-template to update
    * @param {string} name - Name of the notification-template to update
    * @param {INotificationTemplatePayload} data - All the additional parameters to create a notification-template
-   * @returns {Promise<AxiosResponse>} - Creates the notification-template with the given parameters
    */
   async update(
     templateId: string,
     name: string,
     data?: INotificationTemplatePayload
-  ): Promise<AxiosResponse> {
-    return await this.http.put(`notification-templates/${templateId}`, {
+  ) {
+    return await this.http.put(`/notification-templates/${templateId}`, {
       name,
       ...data,
     });
@@ -62,29 +70,25 @@ export class NotificationTemplates
 
   /**
    * @param {string} templateId - templateId of the notification-template to delete
-   * @returns {Promise<AxiosResponse>} - Creates the notification-template with the given parameters
    */
-  async delete(templateId: string): Promise<AxiosResponse> {
-    return await this.http.delete(`notification-templates/${templateId}`);
+  async delete(templateId: string) {
+    return await this.http.delete(`/notification-templates/${templateId}`);
   }
 
   /**
    * @param {string} templateId - templateId of the notification-template to get details of
-   * @returns {Promise<AxiosResponse>} - Creates the notification-template with the given parameters
    */
-  async getOne(templateId: string): Promise<AxiosResponse> {
-    return await this.http.get(`notification-templates/${templateId}`);
+  async getOne(templateId: string) {
+    return await this.http.get(`/notification-templates/${templateId}`);
   }
 
   /**
    * @param {string} templateId - templateId of the notification-template to update status
    * @param {boolean} active - status of the notification-template
-   * @returns {Promise<AxiosResponse>} - Creates the notification-template with the given parameters
    */
   async updateStatus(templateId: string, active: boolean) {
-    return await this.http.put(
-      `notification-templates/${templateId}/status`,
-      active
-    );
+    return await this.http.put(`/notification-templates/${templateId}/status`, {
+      active,
+    });
   }
 }
