4d1e5ef47aa6db082554744d857a39b2088901d9
Utsav Paul <91927689+Smartmind12@users.noreply.github.com>
2022-10-11 22:32:54 +0530

    Create hm.ts

---
+++ packages/notification-center/src/i18n/languages/hm.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const HM: ITranslationEntry = {
+  translations: {
+    notifications: 'ծանուցումներ',
+    markAllAsRead: 'նշել բոլորը որպես կարդացված',
+    poweredBy: 'աջակցվում է',
+    settings: 'կարգավորումներ',
+  },
+  lang: 'hm',
+};
