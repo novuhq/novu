0cdd3aa2a613132defd52f1cedadb53248bcc50e
Donnacha Heffernan <Donnacha.Heffernan@bd.com>
2022-10-11 15:49:49 +0100

    Feature: i18n - Basque Support

---
+++ packages/notification-center/src/i18n/languages/eu.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const EU: ITranslationEntry = {
+  translations: {
+    notifications: 'Notificacionespenak',
+    markAllAsRead: 'Markatu guztiak irakurrita bezala',
+    poweredBy: 'Bultzatuta',
+    settings: 'Ezarpenak',
+  },
+  lang: 'eu',
+};
