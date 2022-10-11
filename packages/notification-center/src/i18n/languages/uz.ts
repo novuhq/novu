c134668da1c23b3181334f13b61c44c555711da5
Pablo Fernández <pablo@novu.co>
2022-10-11 09:18:33 +0100

    Update packages/notification-center/src/i18n/languages/uz.ts

--- packages/notification-center/src/i18n/languages/uz.ts
+++ packages/notification-center/src/i18n/languages/uz.ts
@@ -3,8 +3,8 @@ import { ITranslationEntry } from '../lang';
 export const UZ: ITranslationEntry = {
   translations: {
     notifications: 'Bildirishnomalar',
-    markAllAsRead: 'Hammasini o'qing deb belgilang',
-    poweredBy: 'Tomonidan qo'llab-quvvatlanadi',
+    markAllAsRead: `Hammasini o'qing deb belgilang`,
+    poweredBy: `Tomonidan qo'llab-quvvatlanadi`,
     settings: 'Sozlamalar',
   },
   lang: 'uz',
