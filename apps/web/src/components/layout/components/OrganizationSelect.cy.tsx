24a8ca60b4a15bbf0148407302ee8f5b3b70d5fb
ludovico.besana <ludovico.besana@aidexa.it>
2022-10-12 03:48:21 +0100

    wip: Added initial test file

---
+++ apps/web/src/components/layout/components/OrganizationSelect.cy.tsx
@@ -0,0 +1,28 @@
+// import { Select } from '../../../design-system';
+// import { TestWrapper } from '../../../testing';
+
+// it('should display the organization select when passed as children', () => {
+//   cy.mount(
+//     <TestWrapper>
+//       <>
+//       <SelectWrapper>
+//         <Select/>
+//       </SelectWrapper>
+//     </>
+//     </TestWrapper>
+//   );
+
+//   cy.get('button').should('have.text', 'Test Button');
+// });
+
+// it('should display loading state of a button', function () {
+//   cy.mount(
+//     <TestWrapper>
+//       <Button loading={true}>Test Button</Button>
+//     </TestWrapper>
+//   );
+
+
+//   cy.get('button').should('have.attr', 'disabled');
+//   cy.get('button').should('have.class', 'mantine-Button-loading');
+// });
