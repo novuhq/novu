35272348ed3fe94dfef71217756e111c6c9db1f3
Pablo Fernández <pablo@novu.co>
2022-10-12 12:10:29 +0100

    feat(web): replace discord for intercom

--- apps/web/src/components/utils/GotAQuestionButton.tsx
+++ apps/web/src/components/utils/GotAQuestionButton.tsx
@@ -1,12 +1,17 @@
+import { useIntercom } from 'react-use-intercom';
+
+import { INTERCOM_APP_ID } from '../../config';
 import { Button, Size } from '../../design-system';
 
-function onClick() {
-  window?.open('https://discord.novu.co', '_blank')?.focus();
-}
-
 export function GotAQuestionButton({ mt, size }: { mt: number; size: Size }) {
+  if (!INTERCOM_APP_ID) {
+    return null;
+  }
+
+  const { boot } = useIntercom();
+
   return (
-    <Button mt={mt} size={size} onClick={onClick}>
+    <Button mt={mt} size={size} onClick={boot}>
       Got a question?
     </Button>
   );
