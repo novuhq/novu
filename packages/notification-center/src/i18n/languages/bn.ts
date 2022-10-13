ee67d81e5159ace66dad6132324109bc2170cc53
HitBlast <67194380+hitblast@users.noreply.github.com>
2022-10-13 13:55:33 +0600

    simplified `markAllAsRead` key

--- packages/notification-center/src/i18n/languages/bn.ts
+++ packages/notification-center/src/i18n/languages/bn.ts
@@ -3,7 +3,7 @@ import { ITranslationEntry } from '../lang';
 export const BN: ITranslationEntry = {
   translations: {
     notifications: 'নোটিফিকেশন',
-    markAllAsRead: 'সবগুলো \'পড়া হয়েছে\' চিহ্নিত করুন',
+    markAllAsRead: 'সবগুলো পড়া হয়েছে',
     poweredBy: 'পাওয়ার্ড বাই',
     settings: 'সেটিংস',
   },
