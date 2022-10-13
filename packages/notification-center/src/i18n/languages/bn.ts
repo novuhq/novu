866b01d2855ab7bce605e4cedc05d9263e396e5e
HitBlast <67194380+hitblast@users.noreply.github.com>
2022-10-13 13:51:04 +0600

    Englishified Bangla

--- packages/notification-center/src/i18n/languages/bn.ts
+++ packages/notification-center/src/i18n/languages/bn.ts
@@ -3,8 +3,8 @@ import { ITranslationEntry } from '../lang';
 export const BN: ITranslationEntry = {
   translations: {
     notifications: 'নোটিফিকেশন',
-    markAllAsRead: 'সবগুলো পঠিত হিসেবে চিহ্নিত করুন',
-    poweredBy: 'দ্বারা চালিত',
+    markAllAsRead: 'সবগুলো \'পড়া হয়েছে\' চিহ্নিত করুন',
+    poweredBy: 'পাওয়ার্ড বাই',
     settings: 'সেটিংস',
   },
   lang: 'bn',
