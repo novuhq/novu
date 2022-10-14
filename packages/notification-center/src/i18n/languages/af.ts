2e20dbf2d00d2c4684a1b0f30a4d4bbd8380dc11
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2022-10-14 18:27:19 +0530

    Create af.ts
    Signed-off-by: Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>

---
+++ packages/notification-center/src/i18n/languages/af.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const AF: ITranslationEntry = {
+  translations: {
+    notifications: 'Kennisgewing',
+    markAllAsRead: 'Merk alles as gelees',
+    poweredBy: 'Aangedryf deur',
+    settings: 'Instellings',
+  },
+  lang: 'af',
+};
