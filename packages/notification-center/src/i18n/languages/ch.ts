e653a281e2b91a4f1c29ac81f6c30f6f7cb16cc5
Anupesh Kumar Verma <77433959+anupeshverma@users.noreply.github.com>
2022-10-18 11:53:55 +0530

    Chinese translation

---
+++ packages/notification-center/src/i18n/languages/ch.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const AF: ITranslationEntry = {
+  translations: {
+    notifications: '通知',
+    markAllAsRead: '标记为已读',
+    poweredBy: '供电',
+    settings: '设置',
+  },
+  lang: 'ch',
+};
