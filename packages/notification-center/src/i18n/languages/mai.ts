998b74f3d61072a7f8b64bde6df03e3f36f84ba3
Gaurav Shah <62633971+shahGnp@users.noreply.github.com>
2022-10-28 09:52:39 +0545

    Update mai.ts

--- packages/notification-center/src/i18n/languages/mai.ts
+++ packages/notification-center/src/i18n/languages/mai.ts
@@ -3,8 +3,8 @@ import { ITranslationEntry } from '../lang';
 
 export const MAI: ITranslationEntry = {
   translations: {
-    notifications: 'सूचनाएँ',
-    markAllAsRead: 'सब पढ़ल गेल चिन्हित करू',
+    notifications: 'सूचना',
+    markAllAsRead: 'सब पढ़ल गेल, चिन्हित करू',
     poweredBy: 'द्वारा संचालित',
     settings: 'सेटिंग्स',
   },
