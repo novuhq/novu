e36871bf774028f638889d4bd0aa5540db351641
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2022-10-28 16:07:02 +0530

    Update sm.ts

--- packages/notification-center/src/i18n/languages/sm.ts
+++ packages/notification-center/src/i18n/languages/sm.ts
@@ -2,7 +2,7 @@ import { ITranslationEntry } from '../lang';
 
 export const AM: ITranslationEntry = {
   translations: {
-    notifications: 'fa'amatalaga',
+    notifications: 'faasilasilaga',
     markAllAsRead: 'faailoga Mea uma e pei ona Faitau',
     poweredBy: 'fa'amalosia e',
     settings: 'faatulagaga',
