8f0d7083fc70766eb128aef5d78db9d6e32c6c63
Gaurav Shah <62633971+shahGnp@users.noreply.github.com>
2022-10-28 09:30:36 +0545

    Create mai.ts
    Maithili language translation

---
+++ packages/notification-center/src/i18n/languages/mai.ts
@@ -0,0 +1,12 @@
+
+import { ITranslationEntry } from '../lang';
+
+export const MAI: ITranslationEntry = {
+  translations: {
+    notifications: 'सूचनाएँ',
+    markAllAsRead: 'सब पढ़ल गेल चिन्हित करू',
+    poweredBy: 'द्वारा संचालित',
+    settings: 'सेटिंग्स',
+  },
+  lang: 'mai',
+};
