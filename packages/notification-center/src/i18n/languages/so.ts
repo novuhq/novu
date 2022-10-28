f7e8689b54387b46efc3e44ab564ad55f476075f
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2022-10-28 15:57:40 +0530

    Create so.ts

---
+++ packages/notification-center/src/i18n/languages/so.ts
@@ -0,0 +1,11 @@
+import { ITranslationEntry } from '../lang';
+
+export const AM: ITranslationEntry = {
+  translations: {
+    notifications: 'fa'amatalaga',
+    markAllAsRead: 'faailoga Mea uma e pei ona Faitau',
+    poweredBy: 'fa'amalosia e',
+    settings: 'faatulagaga',
+  },
+  lang: 'so',
+};
