39736ae37b601d49eeeb5f7b8a20b17d6dff231e
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2022-10-29 00:13:44 +0530

    Update lang.ts

--- packages/notification-center/src/i18n/lang.ts
+++ packages/notification-center/src/i18n/lang.ts
@@ -7,6 +7,7 @@ import { BE } from './languages/be';
 import { BG } from './languages/bg';
 import { BH } from './languages/bh';
 import { BN } from './languages/bn';
+import { BS } from './languages/bs';
 import { CA } from './languages/ca';
 import { CS } from './languages/cs';
 import { DA } from './languages/da';
@@ -89,6 +90,7 @@ export const TRANSLATIONS: Record<I18NLanguage, ITranslationEntry> = {
   bg: BG,
   bh: BH,
   bn: BN,
+  bs: BS,
   ca: CA,
   cs: CS,
   da: DA,
@@ -171,6 +173,7 @@ export type I18NLanguage =
   | 'bg'
   | 'bh'
   | 'bn'
+  | 'bs'
   | 'ca'
   | 'cs'
   | 'da'
