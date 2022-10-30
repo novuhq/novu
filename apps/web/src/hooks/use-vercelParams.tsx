17b436886cc98a0e62751a4ffb72ccd90bb7d964
Biswajeet Das <biswajeetdas18@gmail.com>
2022-10-30 21:17:18 +0530

    feat: vercel project link integration

--- apps/web/src/hooks/use-vercelParams.tsx
+++ apps/web/src/hooks/use-vercelParams.tsx
@@ -4,11 +4,13 @@ export function useVercelParams() {
   const [params] = useSearchParams();
   const code = params.get('code');
   const next = params.get('next');
+  const configurationId = params.get('configurationId');
   const isFromVercel = !!(code && next);
 
   return {
     code,
     next,
+    configurationId,
     isFromVercel,
   };
 }
