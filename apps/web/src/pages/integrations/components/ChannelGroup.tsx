3b820a575babe840ffb6c2de7ee9ded2290fd989
maxim <fedaraumaxim@gmail.com>
2022-11-18 22:21:24 +0300

    fix: implement fix using @mantine/core grid system

--- apps/web/src/pages/integrations/components/ChannelGroup.tsx
+++ apps/web/src/pages/integrations/components/ChannelGroup.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from '@emotion/styled';
+import { Grid } from '@mantine/core';
 import { ProviderCard } from './ProviderCard';
 import { Title } from '../../../design-system';
 import { IIntegratedProvider } from '../IntegrationsStorePage';
@@ -18,51 +18,15 @@ export function ChannelGroup({
   }
 
   return (
-    <Wrapper>
-      <div data-test-id={`integration-group-${title.toLowerCase()}`}>
+    <Grid mb={50}>
+      <Grid.Col span={12} data-test-id={`integration-group-${title.toLowerCase()}`}>
         <Title size={2}>{title}</Title>
-      </div>
-      <ContentWrapper>
-        {providers.map((provider) => (
-          <div key={provider.providerId}>
-            <ProviderCard provider={provider} onConnectClick={handlerOnConnectClick} />
-          </div>
-        ))}
-      </ContentWrapper>
-    </Wrapper>
+      </Grid.Col>
+      {providers.map((provider) => (
+        <Grid.Col sm={12} xs={6} md={4} lg={3} key={provider.providerId}>
+          <ProviderCard provider={provider} onConnectClick={handlerOnConnectClick} />
+        </Grid.Col>
+      ))}
+    </Grid>
   );
 }
-
-const LAYOUT_GAP = 25;
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: ${LAYOUT_GAP}px;
-  margin-bottom: 50px;
-`;
-
-const ContentWrapper = styled.div`
-  --grid-layout-gap: ${LAYOUT_GAP}px;
-  --grid-column-count: 4;
-  --grid-item--min-width: 205px; /* Provider Card min-width */
-
-  --gap-count: calc(var(--grid-column-count) - 1);
-  --total-gap-width: calc(var(--gap-count) * var(--grid-layout-gap));
-  --grid-item--max-width: calc((100% - var(--total-gap-width)) / var(--grid-column-count));
-
-  display: grid;
-  grid-gap: var(--grid-layout-gap);
-
-  /* 1400px - theme.breakpoints.xs */
-  @media screen and (min-width: 1400px) {
-    grid-template-columns: repeat(
-      auto-fill,
-      minmax(max(var(--grid-item--min-width), var(--grid-item--max-width)), 1fr)
-    );
-  }
-
-  @media screen and (max-width: 1400px) {
-    grid-template-columns: repeat(auto-fill, minmax(calc(var(--grid-item--min-width) + var(--grid-layout-gap)), 3fr));
-  }
-`;
