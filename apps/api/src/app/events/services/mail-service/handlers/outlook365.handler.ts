9ce3564127f24be1006fb2db0073a474f7650b8e
Hodges, William D <wdhodges@carilionclinic.org>
2022-11-22 15:02:36 -0500

    refactor: Removed old git merge messages

--- apps/api/src/app/events/services/mail-service/handlers/outlook365.handler.ts
+++ apps/api/src/app/events/services/mail-service/handlers/outlook365.handler.ts
@@ -8,8 +8,6 @@ export class Outlook365Handler extends BaseHandler {
     super('outlook365', ChannelTypeEnum.EMAIL);
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   buildProvider(credentials: ICredentials) {
     const config: {
       from: string;
@@ -19,28 +17,7 @@ export class Outlook365Handler extends BaseHandler {
       from: credentials.from,
       senderName: credentials.senderName,
       password: credentials.password,
-=======
-  buildProvider(credentials: ICredentials, from: string) {
-=======
-  buildProvider(credentials: ICredentials) {
->>>>>>> 623a888d8 (feat: Updated docs, updated logo, updated config)
-    const config: {
-      user: string;
-      password: string;
-    } = {
-      user: credentials.user,
-      password: credentials.password,
-<<<<<<< HEAD
-      dkim: {
-        domainName: credentials.domain,
-        keySelector: credentials.accountSid,
-        privateKey: credentials.secretKey,
-      },
->>>>>>> df77c37be (feat: New Office365 provider)
-=======
->>>>>>> 623a888d8 (feat: Updated docs, updated logo, updated config)
     };
-
     this.provider = new Outlook365Provider(config);
   }
 }
