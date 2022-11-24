d2d6448ed447eed56abba733e1333e1253c8f48c
Vu Duc Dung <ducdung8491@gmail.com>
2022-11-24 12:34:03 +0700

    support card message for ms-team

--- providers/ms-teams/src/lib/msTeams.provider.ts
+++ providers/ms-teams/src/lib/msTeams.provider.ts
@@ -14,13 +14,22 @@ export class MsTeamsProvider implements IChatProvider {
   constructor(private config) {}
 
   async sendMessage(data: IChatOptions): Promise<ISendMessageSuccessResponse> {
-    const response = await this.axiosInstance.post(
-      data.webhookUrl,
-      {
-        text: data.content
-      }
-    );
-
+    let response
+    
+    try {
+      response = await this.axiosInstance.post(
+        data.webhookUrl,
+        JSON.parse(data.content)
+      );
+    } catch (err) {
+      response = await this.axiosInstance.post(
+        data.webhookUrl,
+        {
+          text: data.content
+        }
+      );
+    }
+    
     return {
       id: response.headers['request-id'],
       date: new Date().toISOString(),
