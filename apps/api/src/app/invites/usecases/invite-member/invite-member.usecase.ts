c77ad7f0aaef869f7f865ceb32c624427d539108
Dima Grossman <dima@grossman.io>
2022-11-24 11:32:40 +0700

    refactor: upgrade spelling

--- apps/api/src/app/invites/usecases/invite-member/invite-member.usecase.ts
+++ apps/api/src/app/invites/usecases/invite-member/invite-member.usecase.ts
@@ -37,6 +37,7 @@ export class InviteMember {
     if (process.env.NOVU_API_KEY && (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'prod')) {
       const novu = new Novu(process.env.NOVU_API_KEY);
 
+      // eslint-disable-next-line @cspell/spellchecker
       await novu.trigger(process.env.NOVU_TEMPLATEID_INVITE_TO_ORGANISATION || 'invite-to-organization-wBnO8NpDn', {
         to: {
           subscriberId: command.email,
--- apps/api/src/app/notifications/notification.controller.ts
+++ apps/api/src/app/notifications/notification.controller.ts
@@ -11,7 +11,7 @@ import { GetActivityGraphStatsCommand } from './usecases/get-activity-graph-stat
 import { ApiOkResponse, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { ActivityStatsResponseDto } from './dtos/activity-stats-response.dto';
 import { ActivitiesResponseDto, ActivityNotificationResponseDto } from './dtos/activities-response.dto';
-import { ActivityGraphqStatesResponse } from './dtos/activity-graph-states-response.dto';
+import { ActivityGraphStatesResponse } from './dtos/activity-graph-states-response.dto';
 import { ActivitiesRequestDto } from './dtos/activities-request.dto';
 import { ExternalApiAccessible } from '../auth/framework/external-api.decorator';
 import { GetActivity } from './usecases/get-activity/get-activity.usecase';
@@ -94,7 +94,7 @@ export class NotificationsController {
   @UseGuards(JwtAuthGuard)
   @ExternalApiAccessible()
   @ApiOkResponse({
-    type: [ActivityGraphqStatesResponse],
+    type: [ActivityGraphStatesResponse],
   })
   @ApiOperation({
     summary: 'Get notification graph statistics',
@@ -107,7 +107,7 @@ export class NotificationsController {
   getActivityGraphStats(
     @UserSession() user: IJwtPayload,
     @Query('days') days = 32
-  ): Promise<ActivityGraphqStatesResponse[]> {
+  ): Promise<ActivityGraphStatesResponse[]> {
     return this.getActivityGraphStatsUsecase.execute(
       GetActivityGraphStatsCommand.create({
         days: days ? Number(days) : 32,
