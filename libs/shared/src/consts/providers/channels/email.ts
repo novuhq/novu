6fc32f069ea0dd7f886175bb33368c04ee880b36
Mike Elsmore <mike.elsmore@infobip.com>
2022-11-25 13:44:10 +0000

    fix: Missing const

--- libs/shared/src/consts/providers/channels/email.ts
+++ libs/shared/src/consts/providers/channels/email.ts
@@ -9,6 +9,7 @@ import {
   sendgridConfig,
   sendinblueConfig,
   sesConfig,
+  infobipConfig,
 } from '../credentials';
 import { IProviderConfig } from '../provider.interface';
 import { ChannelTypeEnum } from '../../../entities/message-template';
@@ -94,5 +95,13 @@ export const emailProviders: IProviderConfig[] = [
     credentials: netCoreConfig,
     docReference: 'https://netcorecloud.com/email/email-api/',
     logoFileName: { light: 'netcore.png', dark: 'netcore.png' },
+  },
+  {
+    id: EmailProviderIdEnum.Infobip,
+    displayName: 'Infobip',
+    channel: ChannelTypeEnum.EMAIL,
+    credentials: infobipConfig,
+    docReference: 'https://www.infobip.com/docs',
+    logoFileName: { light: 'infobip.png', dark: 'infobip.png' },
   },
 ];
