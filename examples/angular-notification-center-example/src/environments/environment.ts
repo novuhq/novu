65cc4b8d52a44e1c7eb79b6714407ddbd8721ba9
Paweł <pawel.tymczuk@gmail.com>
2022-11-25 12:09:55 +0100

    chore(notification-center): updated the versions in package.json files

--- examples/angular-notification-center-example/src/environments/environment.ts
+++ examples/angular-notification-center-example/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  applicationIdentifier: '',
-  subscriberId: '',
+  applicationIdentifier: '3RB-pKkUG2uu',
+  subscriberId: '63497a94e2a2de81df1e8dac',
 };
