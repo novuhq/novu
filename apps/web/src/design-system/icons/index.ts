1dc8b31539f1a2b89a05acdcbd54d86159c7326f
Dima Grossman <dima@grossman.io>
2022-11-29 17:34:30 +0700

    Revert "fix: fix bad import"
    This reverts commit bc48b8dcb8aabdb736e52286b28dbf65856d3290.

--- apps/web/src/design-system/icons/index.ts
+++ apps/web/src/design-system/icons/index.ts
@@ -61,4 +61,5 @@ export { ConnectGradient } from './gradient/ConnectGradient';
 export { ChatGradient } from './gradient/ChatGradient';
 export { LevelsGradient } from './gradient/LevelsGradient';
 export { TimerGradient } from './gradient/TimerGradient';
-export { GitHub } from './social/Github';
+
+export { GitHub } from './social/GitHub';
