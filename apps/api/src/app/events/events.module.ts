424d9ad079a9ba0ffef366d3ff1edb98513ec72a
venarius <tim.lange@titanhq.de>
2022-12-03 00:51:32 +0000

    fix: fix failing api build

--- apps/api/src/app/events/events.module.ts
+++ apps/api/src/app/events/events.module.ts
@@ -8,9 +8,14 @@ import { LogsModule } from '../logs/logs.module';
 import { ContentTemplatesModule } from '../content-templates/content-templates.module';
 import { IntegrationModule } from '../integrations/integrations.module';
 import { ExecutionDetailsModule } from '../execution-details/execution-details.module';
-import { WorkflowQueueService } from './services/workflow.queue.service';
 import { StorageHelperService } from './services/storage-helper-service/storage-helper.service';
+/*
+ * Import on USE_CASES needs to be above the import of WorkflowQueueService
+ * as they depend on it
+ */
+// eslint-disable-next-line import/order
 import { USE_CASES } from './usecases';
+import { WorkflowQueueService } from './services/workflow.queue.service';
 import { EventsController } from './events.controller';
 
 @Module({
