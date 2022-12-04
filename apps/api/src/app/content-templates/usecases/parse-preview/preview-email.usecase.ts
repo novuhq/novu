88262ab49f70632a08e3ee2730dddc9baae7d133
David Söderberg <granskog_@hotmail.com>
2022-12-04 09:04:40 +0100

    fix: move where trim is used on the rendered content

--- apps/api/src/app/content-templates/usecases/parse-preview/preview-email.usecase.ts
+++ apps/api/src/app/content-templates/usecases/parse-preview/preview-email.usecase.ts
@@ -48,7 +48,6 @@ export class PreviewEmail {
       content = [...content] as IEmailBlock[];
       for (const block of content) {
         block.content = await this.renderContent(block.content, payload);
-        block.content = block.content.trim();
         block.url = await this.renderContent(block.url || '', payload);
       }
 
@@ -59,7 +58,7 @@ export class PreviewEmail {
   }
 
   private async renderContent(content: string, payload: any) {
-    return await this.compileTemplate.execute(
+    const renderedContent = await this.compileTemplate.execute(
       CompileTemplateCommand.create({
         templateId: 'custom',
         customTemplate: content,
@@ -68,5 +67,7 @@ export class PreviewEmail {
         },
       })
     );
+
+    return renderedContent.trim();
   }
 }
