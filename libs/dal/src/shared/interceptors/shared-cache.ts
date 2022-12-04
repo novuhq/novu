95c0f985f77704d9a729e13911721fce23612ab3
Gosha <djabarov.george@gmail.com>
2022-12-04 19:34:11 +0200

    feat: add shared funcs tests

--- libs/dal/src/shared/interceptors/shared-cache.ts
+++ libs/dal/src/shared/interceptors/shared-cache.ts
@@ -10,28 +10,48 @@ export function validateCredentials(keyPrefix: string, credentials: string) {
  * @param key
  * @param keyConfig
  */
-function getIdentifier(key: string, keyConfig: Record<string, undefined>) {
-  const subscriberPreferred = keyConfig._subscriberId ?? keyConfig.subscriberId ?? keyConfig._id ?? keyConfig.id;
-  const idPreferred = keyConfig._id ?? keyConfig.id ?? keyConfig._subscriberId ?? keyConfig.subscriberId;
+export function getIdentifier(key: string, keyConfig: Record<string, unknown>): [string, string] {
+  const subscriberPreferredKeys = ['_subscriberId', 'subscriberId', '_id', 'id'];
+  const idPreferredKeys = ['_id', 'id', '_subscriberId', 'subscriberId'];
+
+  const subscriberPrefKey = subscriberPreferredKeys.find((prefKey) => keyConfig[prefKey]);
+  const subscriberPreferred = [subscriberPrefKey, keyConfig[subscriberPrefKey] as string] as [string, string];
+
+  const idPrefKey = idPreferredKeys.find((prefKey) => keyConfig[prefKey]);
+  const idPreferred = [idPrefKey, keyConfig[idPrefKey] as string] as [string, string];
 
   return key.startsWith('Message') ? subscriberPreferred : idPreferred;
 }
 
-export function buildCredentialsKeyPart(key: string, keyConfig: Record<string, undefined>) {
+export function getEnvironment(keyConfig: Record<string, unknown>): [string, string] {
+  return keyConfig._environmentId
+    ? (['_environmentId', keyConfig._environmentId] as [string, string])
+    : keyConfig.environmentId
+    ? (['environmentId', keyConfig.environmentId] as [string, string])
+    : [undefined, undefined];
+}
+
+export function buildCredentialsKeyPart(key: string, keyConfig: Record<string, unknown>): string {
   let credentialsResult = '';
   const identifier = getIdentifier(key, keyConfig);
 
-  if (identifier) {
-    credentialsResult += ':' + identifier;
+  if (identifier[0]) {
+    credentialsResult += ':' + getCredentialWithContext(identifier[0], identifier[1]);
   }
 
-  const environment = keyConfig._environmentId ?? keyConfig.environmentId;
+  const environment = getEnvironment(keyConfig);
 
-  if (environment) {
-    credentialsResult += ':' + environment;
+  if (environment[0]) {
+    credentialsResult += ':' + getCredentialWithContext(environment[0], environment[1]);
   }
 
   return credentialsResult;
+}
+
+export function getCredentialWithContext(credentialKey: string, credentialValue: string): string {
+  const context = credentialKey.replace('_', '')[0];
+
+  return context + '=' + credentialValue;
 }
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -42,19 +62,19 @@ export enum CacheInterceptorTypeEnum {
 
 export function buildKey(
   prefix: string,
-  keyConfig: Record<undefined, string>,
+  keyConfig: Record<string, unknown>,
   interceptorType: CacheInterceptorTypeEnum
 ): string {
   let cacheKey = prefix;
 
-  cacheKey = cacheKey + buildQueryKeyPart(cacheKey, interceptorType, keyConfig);
+  cacheKey = cacheKey + buildQueryKeyPart(interceptorType, keyConfig);
 
   const credentials = buildCredentialsKeyPart(cacheKey, keyConfig);
 
   return validateCredentials(prefix, credentials) ? cacheKey + credentials : '';
 }
 
-function getQueryParams(keysConfig: any): string {
+export function getQueryParams(keysConfig: unknown): string {
   let result = '';
 
   const keysToExclude = [...getCredentialsKeys()];
@@ -80,7 +100,7 @@ function getQueryParams(keysConfig: any): string {
   return result;
 }
 
-function getCredentialsKeys() {
+export function getCredentialsKeys() {
   return ['id', 'subscriberId', 'environmentId', 'organizationId'].map((cred) => [cred, `_${cred}`]).flat();
 }
 
@@ -89,24 +109,29 @@ function getCredentialsKeys() {
  * that occurs only in method 'findById'
  * @param args
  */
-export function buildCachedQuery(args: any[]) {
-  return typeof args[0] === 'string'
-    ? { id: args[0], environmentId: args[1] }
-    : args.reduce((obj, item) => Object.assign(obj, item), {});
+export function buildCachedQuery(args: unknown[]): Record<string, unknown> {
+  const fromStringArray = { id: args[0], environmentId: args[1] };
+  const fromObjectArray = args.reduce((obj, item) => Object.assign(obj, item), {});
+
+  return (typeof args[0] === 'string' ? fromStringArray : fromObjectArray) as Record<string, unknown>;
 }
 
 /**
  * on create request the _id is available after collection creation,
  * therefore we need to build it from the storage response
- * @param key
+ * @param methodName
  * @param res
  * @param args
  */
-export function getInvalidateQuery(key: string, res, args: any[]) {
-  return key === 'create' ? res : args[0];
+export function getInvalidateQuery(
+  methodName: string,
+  res: Record<string, unknown>,
+  args: Record<string, unknown>[]
+): Record<string, unknown> {
+  return methodName === 'create' ? res : args[0];
 }
 
-function buildQueryKeyPart(cacheKey: string, interceptorType: CacheInterceptorTypeEnum, keyConfig: any) {
+export function buildQueryKeyPart(interceptorType: CacheInterceptorTypeEnum, keyConfig: Record<string, unknown>) {
   const WILD_CARD = '*';
 
   return interceptorType === CacheInterceptorTypeEnum.INVALIDATE ? WILD_CARD : getQueryParams(keyConfig);
