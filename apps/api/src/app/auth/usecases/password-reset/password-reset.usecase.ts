63030f9f66de54f7ce97d6e0afe6446e743b2d4d
Biswajeet Das <biswajeetdas18@gmail.com>
2022-12-13 19:02:02 +0530

    feat: Validate password in API

--- apps/api/src/app/auth/usecases/password-reset/password-reset.usecase.ts
+++ apps/api/src/app/auth/usecases/password-reset/password-reset.usecase.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { UserRepository } from '@novu/dal';
 import * as bcrypt from 'bcrypt';
 import { isBefore, subDays } from 'date-fns';
+import { passwordConstraints } from '@novu/shared';
 import { PasswordResetCommand } from './password-reset.command';
 import { ApiException } from '../../../shared/exceptions/api.exception';
 import { AuthService } from '../../services/auth.service';
@@ -23,6 +24,13 @@ export class PasswordReset {
 
     if (isBefore(new Date(user.resetTokenDate), subDays(new Date(), 7))) {
       throw new ApiException('Token has expired');
+    }
+    const isValidPassword = this.validatePassword(command.password);
+    if (!isValidPassword) {
+      throw new ApiException(
+        // eslint-disable-next-line max-len
+        'The password must contain minimum 8 and maximum 64 characters, at least one uppercase letter, one lowercase letter, one number and one special character'
+      );
     }
 
     const passwordHash = await bcrypt.hash(command.password, 10);
@@ -53,5 +61,13 @@ export class PasswordReset {
     return {
       token: await this.authService.generateUserToken(user),
     };
+  }
+
+  private validatePassword(password: string) {
+    const passwordRegex = passwordConstraints.pattern;
+
+    const isValidPassword = passwordRegex.test(password);
+
+    return isValidPassword;
   }
 }
