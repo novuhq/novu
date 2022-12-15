4c4d6408c03d21afaaba7419fb95b9a97bd929dc
Pablo Fernández <pablo@novu.co>
2022-12-15 12:02:57 +0000

    Update libs/dal/src/repositories/subscriber/subscriber.schema.ts
    Co-authored-by: Dima Grossman <dima@grossman.io>

--- libs/dal/src/repositories/subscriber/subscriber.schema.ts
+++ libs/dal/src/repositories/subscriber/subscriber.schema.ts
@@ -28,7 +28,6 @@ const subscriberSchema = new Schema(
 );
 
 subscriberSchema.index({ _environmentId: 1, subscriberId: 1 });
-subscriberSchema.index({ _environmentId: 1, _organizationId: 1, subscriberId: 1 });
 
 subscriberSchema.plugin(mongooseDelete, { deletedAt: true, deletedBy: true, overrideMethods: 'all' });
 
