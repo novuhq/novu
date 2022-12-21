ca1a7e38b34b019dcd7c7419f16af1556b2f1dfd
Gosha <djabarov.george@gmail.com>
2022-12-21 20:57:58 +0200

    feat: add webhook filter support

--- apps/api/src/app/events/usecases/trigger-event/message-filter.matcher.ts
+++ apps/api/src/app/events/usecases/trigger-event/message-filter.matcher.ts
@@ -1,24 +1,33 @@
 import { NotificationStepEntity, SubscriberEntity } from '@novu/dal';
 import { ITriggerPayload } from '@novu/node';
 import * as _ from 'lodash';
+import axios from 'axios';
 
 export interface IFilterVariables {
   payload: ITriggerPayload;
   subscriber?: SubscriberEntity;
+  webhook?: Record<string, unknown>;
 }
 
-export function matchMessageWithFilters(step: NotificationStepEntity, variables: IFilterVariables): boolean {
+export async function matchMessageWithFilters(
+  step: NotificationStepEntity,
+  variables: IFilterVariables
+): Promise<boolean> {
   if (!step?.filters || !Array.isArray(step?.filters)) {
     return true;
   }
   if (step.filters?.length) {
-    const foundFilter = step.filters.find((filter) => {
+    const foundFilter = await findAsync(step.filters, async (filter) => {
       const children = filter.children;
       if (!children || (Array.isArray(children) && children.length === 0)) {
         return true;
       }
 
-      return handleGroupFilters(filter, variables);
+      if (!children || (Array.isArray(children) && children.length === 1)) {
+        return await processFilter(filter.children[0], variables);
+      }
+
+      return await handleGroupFilters(filter, variables);
     });
 
     return foundFilter !== undefined;
@@ -27,29 +36,31 @@ export function matchMessageWithFilters(step: NotificationStepEntity, variables:
   return true;
 }
 
-function handleGroupFilters(filter, variables: IFilterVariables) {
+async function handleGroupFilters(filter, variables: IFilterVariables) {
   if (filter.value === 'OR') {
-    return handleOrFilters(filter, variables);
+    return await handleOrFilters(filter, variables);
   }
 
   if (filter.value === 'AND') {
-    return handleAndFilters(filter, variables);
+    return await handleAndFilters(filter, variables);
   }
 
   return false;
 }
 
-function handleAndFilters(filter, variables: IFilterVariables) {
-  const foundFilterMatches = filter.children.filter((i) => processFilterEquality(i, variables));
+async function handleAndFilters(filter, variables: IFilterVariables) {
+  // todo priority here - first sync operation then async
+  const foundFilterMatches = await filterAsync(filter.children, (i) => processFilter(i, variables));
 
   return foundFilterMatches.length === filter.children.length;
 }
 
-function handleOrFilters(filter, variables: IFilterVariables) {
-  return filter.children.find((i) => processFilterEquality(i, variables));
+async function handleOrFilters(filter, variables: IFilterVariables) {
+  // todo priority here - first sync operation then async
+  return await findAsync(filter.children, (i) => processFilter(i, variables));
 }
 
-function processFilterEquality(i, variables: IFilterVariables) {
+function processFilterEquality(variables: IFilterVariables, i) {
   const payloadVariable = _.get(variables, [i.on, i.field]);
   const value = parseValue(payloadVariable, i.value);
   if (i.operator === 'EQUAL') {
@@ -80,6 +91,35 @@ function processFilterEquality(i, variables: IFilterVariables) {
   return false;
 }
 
+async function getWebhookResponse(i, variables: IFilterVariables): Promise<Record<string, unknown>> {
+  try {
+    return await axios
+      .post(i.webhookUrl, variables)
+      .then((response) => {
+        return response.data as Record<string, unknown>;
+      })
+      .catch((error) => {
+        // eslint-disable-next-line promise/no-return-wrap
+        return Promise.reject(error?.response?.data || error?.response || error);
+      });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+
+    return undefined;
+  }
+}
+
+async function processFilter(i, variables: IFilterVariables) {
+  if (i.on === 'webhook') {
+    const res = await getWebhookResponse(i, variables);
+
+    return processFilterEquality({ payload: undefined, webhook: res }, i);
+  }
+
+  return processFilterEquality(variables, i);
+}
+
 function parseValue(originValue, parsingValue) {
   switch (typeof originValue) {
     case 'number':
@@ -93,4 +133,22 @@ function parseValue(originValue, parsingValue) {
     default:
       return parsingValue;
   }
+}
+
+async function findAsync<T>(array: T[], predicate: (t: T) => Promise<boolean>): Promise<T | undefined> {
+  for (const t of array) {
+    if (await predicate(t)) {
+      return t;
+    }
+  }
+
+  return undefined;
+}
+
+async function filterAsync<T>(arr: T[], callback: (item: T) => Promise<boolean>): Promise<T[]> {
+  const fail = Symbol();
+
+  return (await Promise.all(arr.map(async (item) => ((await callback(item)) ? item : fail)))).filter(
+    (i) => i !== fail
+  ) as T[];
 }
