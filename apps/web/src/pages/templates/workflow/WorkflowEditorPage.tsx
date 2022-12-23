76f0d0742d2c7e66232c8601373757d0e0ff7d45
Biswajeet Das <biswajeetdas18@gmail.com>
2022-12-23 14:25:39 +0530

    fix: disabled add filter and delete step btns in readonly mode

--- apps/web/src/pages/templates/workflow/WorkflowEditorPage.tsx
+++ apps/web/src/pages/templates/workflow/WorkflowEditorPage.tsx
@@ -296,6 +296,7 @@ const WorkflowEditorPage = ({
                     onClick={() => {
                       onDelete(selectedNodeId);
                     }}
+                    disabled={readonly}
                   >
                     <Trash
                       style={{
