78705b464c9e79ce8102176c8b7b227535078ebd
Gosha <djabarov.george@gmail.com>
2023-01-02 19:17:52 +0200

    refactor: remove default attempts

--- apps/api/src/app/events/services/workflow-queue/workflow.queue.service.ts
+++ apps/api/src/app/events/services/workflow-queue/workflow.queue.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { Queue, Worker, QueueBaseOptions, JobsOptions, QueueScheduler } from 'bullmq';
+import { JobsOptions, Queue, QueueBaseOptions, QueueScheduler, Worker } from 'bullmq';
 import { JobEntity, JobRepository, JobStatusEnum } from '@novu/dal';
 import { RunJob } from '../../usecases/run-job/run-job.usecase';
 import { RunJobCommand } from '../../usecases/run-job/run-job.command';
@@ -15,7 +15,6 @@ import { QueueNextJob } from '../../usecases/queue-next-job/queue-next-job.useca
 
 @Injectable()
 export class WorkflowQueueService {
-  private readonly FAIL_JOB = -1;
   private bullConfig: QueueBaseOptions = {
     connection: {
       db: Number(process.env.REDIS_DB_INDEX),
@@ -42,7 +41,6 @@ export class WorkflowQueueService {
       ...this.bullConfig,
       defaultJobOptions: {
         removeOnComplete: true,
-        attempts: this.DEFAULT_ATTEMPTS,
       },
     });
 
@@ -126,21 +124,31 @@ export class WorkflowQueueService {
       removeOnComplete: true,
       removeOnFail: true,
       delay,
-      backoff: {
-        type: 'webhookFilterBackoff',
-      },
     };
 
+    const stepContainsWebhookFilter = this.stepContainsFilter(data, 'webhook');
+
+    if (stepContainsWebhookFilter) {
+      options.backoff = {
+        type: 'webhookFilterBackoff',
+      };
+      options.attempts = this.DEFAULT_ATTEMPTS;
+    }
+
     await this.queue.add(id, data, options);
+  }
+
+  private stepContainsFilter(data: JobEntity, onFilter: string) {
+    return data.step.filters.some((filter) => {
+      return filter.children.some((child) => {
+        return child.on === onFilter;
+      });
+    });
   }
 
   private getBackoffStrategies = () => {
     return {
       webhookFilterBackoff: async (attemptsMade, err, job) => {
-        if (!shouldBackoff(err)) {
-          return this.FAIL_JOB;
-        }
-
         await this.createExecutionDetails.execute(
           CreateExecutionDetailsCommand.create({
             ...CreateExecutionDetailsCommand.getDetailsFromJob(job.data),
