9c7ede0fb057cfa72af37e6e3cec82f1a5fd5b96
Dima Grossman <dima@grossman.io>
2023-01-08 17:14:04 +0200

    fix: uuid mapping

--- providers/nexmo/jest.config.js
+++ providers/nexmo/jest.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
+    uuid: require.resolve('uuid'),
+  },
 };
