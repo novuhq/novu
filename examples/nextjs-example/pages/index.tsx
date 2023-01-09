a145cc46acf4a1f070250733beb4d4417a53053a
ShaneMaglangit <shanedmaglangit@gmail.com>
2023-01-09 10:31:09 +0800

    feat(nextjs-example): Add trigger button

--- examples/nextjs-example/pages/index.tsx
+++ examples/nextjs-example/pages/index.tsx
@@ -18,6 +18,8 @@ export default function Home({ appID, subscriberID }: HomeProps) {
     }
   };
 
+  const sayHello = () => fetch('/api/hello');
+
   return (
     <>
       <Head>
@@ -43,10 +45,8 @@ export default function Home({ appID, subscriberID }: HomeProps) {
         </div>
 
         <div className={styles.center}>
-          <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
-          <div className={styles.thirteen}>
-            <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-          </div>
+          <p>A slight push, a grand beginning.</p>
+          <button onClick={sayHello}>Trigger a new notification! 👋</button>
         </div>
 
         <div className={styles.grid}>
