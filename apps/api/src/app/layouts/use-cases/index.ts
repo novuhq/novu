dbd7167691a6e3122b095e309704534ae63fdc9f
p-fernandez <pablo.fernandez.otero@gmail.com>
2023-01-11 18:20:51 +0000

    feat(api): filter layouts endpoint

--- apps/api/src/app/layouts/use-cases/index.ts
+++ apps/api/src/app/layouts/use-cases/index.ts
@@ -1,7 +1,9 @@
 import { CreateLayoutUseCase } from './create-layout/create-layout.use-case';
+import { FilterLayoutsUseCase } from './filter-layouts/filter-layouts.use-case';
 import { GetLayoutUseCase } from './get-layout/get-layout.use-case';
 
 export * from './create-layout';
+export * from './filter-layouts';
 export * from './get-layout';
 
-export const USE_CASES = [CreateLayoutUseCase, GetLayoutUseCase];
+export const USE_CASES = [CreateLayoutUseCase, FilterLayoutsUseCase, GetLayoutUseCase];
