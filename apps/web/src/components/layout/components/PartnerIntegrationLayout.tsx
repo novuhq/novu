5f327db8c92b99fc61d3f5678ec81e23587dec7f
Biswajeet Das <biswajeetdas18@gmail.com>
2023-01-15 19:16:47 +0530

    fix: removed the useeffect

--- apps/web/src/components/layout/components/PartnerIntegrationLayout.tsx
+++ apps/web/src/components/layout/components/PartnerIntegrationLayout.tsx
@@ -1,14 +1,7 @@
-import { useEffect, useLayoutEffect } from 'react';
 import { Box, useMantineColorScheme } from '@mantine/core';
 
 export function PartnerIntegrationLayout({ children }: { children: React.ReactNode }) {
-  const { toggleColorScheme, colorScheme } = useMantineColorScheme();
-
-  useEffect(() => {
-    if (colorScheme === 'light') {
-      toggleColorScheme('dark');
-    }
-  }, [colorScheme]);
+  const { colorScheme } = useMantineColorScheme();
 
   const isDark = colorScheme === 'dark';
   const logoUrl = `/static/images/logo-formerly-${isDark ? 'dark' : 'light'}-bg.png`;
