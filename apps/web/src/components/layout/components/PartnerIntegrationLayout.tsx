92fd645fb87432b32c908a0cfb4d1084c51e4194
Biswajeet Das <biswajeetdas18@gmail.com>
2023-01-15 19:12:59 +0530

    fix: page flickering issue while using light theme on system

--- apps/web/src/components/layout/components/PartnerIntegrationLayout.tsx
+++ apps/web/src/components/layout/components/PartnerIntegrationLayout.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { useMantineColorScheme } from '@mantine/core';
+import { useEffect, useLayoutEffect } from 'react';
+import { Box, useMantineColorScheme } from '@mantine/core';
 
 export function PartnerIntegrationLayout({ children }: { children: React.ReactNode }) {
   const { toggleColorScheme, colorScheme } = useMantineColorScheme();
@@ -8,7 +8,17 @@ export function PartnerIntegrationLayout({ children }: { children: React.ReactNo
     if (colorScheme === 'light') {
       toggleColorScheme('dark');
     }
-  }, [colorScheme, toggleColorScheme]);
+  }, [colorScheme]);
 
-  return <div>{children}</div>;
+  const isDark = colorScheme === 'dark';
+  const logoUrl = `/static/images/logo-formerly-${isDark ? 'dark' : 'light'}-bg.png`;
+
+  return (
+    <div>
+      <Box>
+        <img src={logoUrl} alt="logo" style={{ maxWidth: 150, marginTop: 10, marginLeft: 10 }} />
+      </Box>
+      {children}
+    </div>
+  );
 }
