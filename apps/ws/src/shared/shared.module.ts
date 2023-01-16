937b9a92dccba284db22e3ab4197575c9b0b925f
Paweł <pawel.tymczuk@gmail.com>
2023-01-16 11:01:39 +0100

    chore(ws): put the dal service back for is online filters functionality

--- apps/ws/src/shared/shared.module.ts
+++ apps/ws/src/shared/shared.module.ts
@@ -1,7 +1,32 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import {
+  DalService,
+  UserRepository,
+  OrganizationRepository,
+  EnvironmentRepository,
+  NotificationTemplateRepository,
+  SubscriberRepository,
+  NotificationRepository,
+  MessageRepository,
+  MemberRepository,
+} from '@novu/dal';
+
 import { QueueService } from './queue';
 import { SubscriberOnlineService } from './subscriber-online';
+
+const DAL_MODELS = [
+  UserRepository,
+  OrganizationRepository,
+  EnvironmentRepository,
+  NotificationTemplateRepository,
+  SubscriberRepository,
+  NotificationRepository,
+  MessageRepository,
+  MemberRepository,
+];
+
+const dalService = new DalService();
 
 const PROVIDERS = [
   {
@@ -10,6 +35,15 @@ const PROVIDERS = [
       return new QueueService();
     },
   },
+  {
+    provide: DalService,
+    useFactory: async () => {
+      await dalService.connect(process.env.MONGO_URL as string);
+
+      return dalService;
+    },
+  },
+  ...DAL_MODELS,
   SubscriberOnlineService,
 ];
 
