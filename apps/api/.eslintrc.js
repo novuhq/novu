68c0b4cd81923837b422a9ad2c49b4b41b65dad7
Dima Grossman <dima@grossman.io>
2023-01-17 18:03:48 +0200

    fix: add mongoose to banned imports

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -2,6 +2,12 @@ module.exports = {
   extends: ['../../.eslintrc.js'],
   rules: {
     'func-names': 'off',
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['@novu/shared/*', '@novu/dal/*', '!import2/good', 'mongoose'],
+      },
+    ],
   },
   ignorePatterns: '*.spec.ts',
 };
