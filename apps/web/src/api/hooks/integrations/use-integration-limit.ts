1d359230b58cbba48a2b35deb5ee0d7a52a9c249
David Söderberg <granskog_@hotmail.com>
2023-01-19 12:32:24 +0100

    fix: after pr comments

--- apps/web/src/api/hooks/integrations/use-integration-limit.ts
+++ apps/web/src/api/hooks/integrations/use-integration-limit.ts
@@ -2,17 +2,22 @@ import { useQuery } from '@tanstack/react-query';
 import { ChannelTypeEnum } from '@novu/shared';
 import { getIntegrationLimit } from '../../integration';
 import { useActiveIntegrations } from '.';
+import { useMemo } from 'react';
 
 export function useIntegrationLimit(type: ChannelTypeEnum) {
   const selfHosted = process.env.REACT_APP_DOCKER_HOSTED_ENV === 'true';
   const { integrations = [] } = useActiveIntegrations();
+
+  const enabled = useMemo<boolean>(() => {
+    return selfHosted && integrations.filter((integration) => integration.channel === type).length === 0;
+  }, [integrations, type, selfHosted]);
 
   const {
     data = { limit: 0, count: 0 },
     isLoading,
     refetch,
   } = useQuery(['integrationLimit', type], () => getIntegrationLimit(type), {
-    enabled: selfHosted && integrations.filter((integration) => integration.channel === type).length === 0,
+    enabled,
     refetchInterval: 10000,
   });
 
@@ -20,6 +25,6 @@ export function useIntegrationLimit(type: ChannelTypeEnum) {
     limit: data,
     loading: isLoading,
     refetch,
-    enabled: selfHosted && integrations.filter((integration) => integration.channel === type).length === 0,
+    enabled,
   };
 }
