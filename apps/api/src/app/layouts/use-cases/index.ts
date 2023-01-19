b2eb9ebbfc6dbf70c532dbac7a0e132e060d5a7c
p-fernandez <pablo.fernandez.otero@gmail.com>
2023-01-19 20:06:13 +0000

    feat(api): disallow delete of assigned

--- apps/api/src/app/layouts/use-cases/index.ts
+++ apps/api/src/app/layouts/use-cases/index.ts
@@ -1,3 +1,4 @@
+import { CheckLayoutIsUsedUseCase } from './check-layout-is-used/check-layout-is-used.use-case';
 import { CreateLayoutUseCase } from './create-layout/create-layout.use-case';
 import { DeleteLayoutUseCase } from './delete-layout/delete-layout.use-case';
 import { FilterLayoutsUseCase } from './filter-layouts/filter-layouts.use-case';
@@ -5,6 +6,7 @@ import { GetLayoutUseCase } from './get-layout/get-layout.use-case';
 import { SetDefaultLayoutUseCase } from './set-default-layout/set-default-layout.use-case';
 import { UpdateLayoutUseCase } from './update-layout/update-layout.use-case';
 
+export * from './check-layout-is-used';
 export * from './create-layout';
 export * from './delete-layout';
 export * from './filter-layouts';
@@ -13,6 +15,7 @@ export * from './set-default-layout';
 export * from './update-layout';
 
 export const USE_CASES = [
+  CheckLayoutIsUsedUseCase,
   CreateLayoutUseCase,
   DeleteLayoutUseCase,
   FilterLayoutsUseCase,
