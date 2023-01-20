f73710bc045520741bc498a32ea86717eea4eaea
Biswajeet Das <biswajeetdas18@gmail.com>
2023-01-20 14:15:49 +0530

    fix: added missing key

--- apps/web/src/components/activity/ExecutionDetailsConditions.tsx
+++ apps/web/src/components/activity/ExecutionDetailsConditions.tsx
@@ -5,8 +5,10 @@ import { ExecutionDetailsConditionItem } from './ExecutionDetailsConditionItem';
 export function ExecutionDetailsConditions({ conditions }: { conditions: ICondition[] }) {
   return (
     <Stack spacing={5}>
-      {conditions.map((condition) => {
-        return <ExecutionDetailsConditionItem condition={condition} />;
+      {conditions.map((condition, idx) => {
+        return (
+          <ExecutionDetailsConditionItem key={`${condition.filter}-${condition.field}-${idx}`} condition={condition} />
+        );
       })}
     </Stack>
   );
