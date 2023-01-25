27ab6f332e5b0cd18c998cb1b57f1c1d2b1e5788
Biswajeet Das <biswajeetdas18@gmail.com>
2023-01-25 13:24:57 +0530

    fix: UseIntercom usage to be only when INTERCOM_APP_ID is available

--- apps/web/src/components/utils/GotAQuestionButton.tsx
+++ apps/web/src/components/utils/GotAQuestionButton.tsx
@@ -4,23 +4,24 @@ import { IntercomContextValues, useIntercom } from 'react-use-intercom';
 import { INTERCOM_APP_ID } from '../../config';
 import { Button, Size } from '../../design-system';
 
-const useShowIntercom = () => {
-  const { show } = useIntercom();
-  const [isIntercomEnabled] = useState<boolean>(!!INTERCOM_APP_ID);
-
-  return useCallback(() => isIntercomEnabled && show(), [isIntercomEnabled, show]);
-};
+const isIntercomEnabled = !!INTERCOM_APP_ID;
 
 export function GotAQuestionButton({ mt, size }: { mt: number; size: Size }) {
-  const showIntercom = useShowIntercom();
+  if (!isIntercomEnabled) {
+    return null;
+  }
+
+  return <QuestionButton mt={mt} size={size} />;
+}
+
+function QuestionButton({ mt, size }: { mt: number; size: Size }) {
+  const { show } = useIntercom();
 
   const text = 'Got a question?';
 
   return (
-    showIntercom && (
-      <Button mt={mt} size={size} onClick={showIntercom}>
-        {text}
-      </Button>
-    )
+    <Button mt={mt} size={size} onClick={show}>
+      {text}
+    </Button>
   );
 }
