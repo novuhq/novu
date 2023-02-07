d7b7e7997d27708aa4dc28f2b8a95c259535a774
Paweł <pawel.tymczuk@gmail.com>
2023-02-07 10:37:28 +0100

    chore(api,web): fixed failing e2e tests in the pr 2585

--- apps/api/src/app/invites/e2e/accept-invite.e2e.ts
+++ apps/api/src/app/invites/e2e/accept-invite.e2e.ts
@@ -63,12 +63,13 @@ describe('Accept invite - /invites/:inviteToken/accept (POST)', async () => {
       });
 
       const members = await memberRepository.getOrganizationMembers(thirdUserSession.organization._id);
-      const newInvitee = members.find((i) => i._userId === invitedUserSession.user._id);
+      const newInvitee = members.find(
+        (member) => member.invite && member.invite.email === invitedUserSession.user.email
+      );
       expect(newInvitee).to.exist;
 
       const { body } = await invitedUserSession.testAgent.get(`/v1/invites/${newInvitee.invite.token}`).expect(200);
-
-      expect(body.data._userId).to.eq(newInvitee.user._id);
+      expect(body.data.email).to.eq(invitedUserSession.user.email);
 
       await invitedUserSession.testAgent.post(`/v1/invites/${newInvitee.invite.token}/accept`).expect(201);
 
