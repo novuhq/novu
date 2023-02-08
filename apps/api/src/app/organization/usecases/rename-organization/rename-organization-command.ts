fddd2e3c0b41baf7292bc17728c24433597f4cd4
Dima Grossman <dima@grossman.io>
2023-02-08 13:30:28 +0200

    fix: remove max length validator

--- apps/api/src/app/organization/usecases/rename-organization/rename-organization-command.ts
+++ apps/api/src/app/organization/usecases/rename-organization/rename-organization-command.ts
@@ -1,10 +1,11 @@
-import { IsDefined, MaxLength } from 'class-validator';
+import { IsDefined, IsNotEmpty } from 'class-validator';
 import { AuthenticatedCommand } from '../../../shared/commands/authenticated.command';
 
 export class RenameOrganizationCommand extends AuthenticatedCommand {
   @IsDefined()
   public readonly id: string;
 
-  @MaxLength(50)
+  @IsDefined()
+  @IsNotEmpty()
   name: string;
 }
