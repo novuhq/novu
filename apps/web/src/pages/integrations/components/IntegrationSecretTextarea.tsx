cf87c82b4572876aed00f7a4a78a2b603588f5e7
David Söderberg <granskog_@hotmail.com>
2023-02-17 08:31:37 +0100

    feat: hide content of textarea with multiline keys

---
+++ apps/web/src/pages/integrations/components/IntegrationSecretTextarea.tsx
@@ -0,0 +1,69 @@
+import { EyeInvisibleOutlined, EyeOutlined } from '@ant-design/icons';
+import { Grid, UnstyledButton } from '@mantine/core';
+import { IConfigCredentials } from '@novu/shared';
+import { useState } from 'react';
+import { When } from '../../../components/utils/When';
+import { Textarea } from '../../../design-system';
+
+export const IntegrationSecretTextarea = ({
+  credential,
+  errors,
+  field,
+  register,
+}: {
+  credential: IConfigCredentials;
+  errors: any;
+  field: any;
+  register: any;
+}) => {
+  const [hidden, setHidden] = useState(true);
+
+  return (
+    <Grid align="center">
+      <Grid.Col span={11}>
+        <When truthy={hidden}>
+          <Textarea
+            label={credential.displayName}
+            required={credential.required}
+            placeholder="*****************"
+            description={credential.description ?? ''}
+            data-test-id={credential.key}
+            error={errors[credential.key]?.message}
+            disabled
+          />
+        </When>
+        <When truthy={!hidden}>
+          <Textarea
+            label={credential.displayName}
+            required={credential.required}
+            placeholder={credential.displayName}
+            description={credential.description ?? ''}
+            data-test-id={credential.key}
+            error={errors[credential.key]?.message}
+            {...field}
+            {...register(credential.key, {
+              required: credential.required && `Please enter a ${credential.displayName.toLowerCase()}`,
+            })}
+          />
+        </When>
+      </Grid.Col>
+      <Grid.Col span={1}>
+        <UnstyledButton
+          sx={{
+            fontSize: '20px',
+          }}
+          onClick={() => {
+            setHidden(!hidden);
+          }}
+        >
+          <When truthy={hidden}>
+            <EyeOutlined size={20} />
+          </When>
+          <When truthy={!hidden}>
+            <EyeInvisibleOutlined size={20} />
+          </When>
+        </UnstyledButton>
+      </Grid.Col>
+    </Grid>
+  );
+};
