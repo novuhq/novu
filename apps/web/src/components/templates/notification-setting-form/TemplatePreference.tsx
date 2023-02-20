3432883df40d052944e76faf9600db6430aac5ca
Dima Grossman <dima@grossman.io>
2023-02-20 09:07:48 +0200

    Update TemplatePreference.tsx

--- apps/web/src/components/templates/notification-setting-form/TemplatePreference.tsx
+++ apps/web/src/components/templates/notification-setting-form/TemplatePreference.tsx
@@ -107,7 +107,7 @@ function CriticalDescription({ field }) {
 
   return (
     <DescriptionWrapper>
-      {"When on, the template will not show in the user preferences , meaning they wouldn't be able to opt out."}
+      {"When on, the template will not show in the user preferences, meaning they wouldn't be able to opt out."}
       <Switch {...field} checked={field.value || false} disabled={readonly} data-test-id="critical" />
     </DescriptionWrapper>
   );
