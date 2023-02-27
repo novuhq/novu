e0f21b4335bf773b6d18704114944b9246f30ac9
Paweł <pawel.tymczuk@gmail.com>
2023-02-27 22:21:19 +0100

    fix(api): fixed a new ts issue

--- apps/api/src/app/notification-template/usecases/create-blueprint-notification-template/create-blueprint-notification-template.usecase.ts
+++ apps/api/src/app/notification-template/usecases/create-blueprint-notification-template/create-blueprint-notification-template.usecase.ts
@@ -139,7 +139,7 @@ export class CreateBlueprintNotificationTemplate {
     return group;
   }
 
-  private getBlueprintOrganizationId() {
-    return NotificationTemplateRepository.getBlueprintOrganizationId();
+  private getBlueprintOrganizationId(): string {
+    return NotificationTemplateRepository.getBlueprintOrganizationId() as string;
   }
 }
