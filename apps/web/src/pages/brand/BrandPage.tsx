ebe295e3447166836749ef98dc4e404ac84dcbde
Biswajeet Das <biswajeetdas18@gmail.com>
2023-02-27 17:10:53 +0530

    feat: Layout analytics UI

--- apps/web/src/pages/brand/BrandPage.tsx
+++ apps/web/src/pages/brand/BrandPage.tsx
@@ -1,4 +1,4 @@
-import { Container } from '@mantine/core';
+import { Container, TabsValue } from '@mantine/core';
 
 import PageMeta from '../../components/layout/components/PageMeta';
 import PageHeader from '../../components/layout/components/PageHeader';
@@ -6,12 +6,14 @@ import PageContainer from '../../components/layout/components/PageContainer';
 import { Tabs } from '../../design-system';
 import { useAuthContext } from '../../components/providers/AuthProvider';
 import { BrandingForm, LayoutsListPage } from './tabs';
+import { useSegment } from '../../components/providers/SegmentProvider';
 
 const BRANDING = 'Assets';
 const LAYOUT = 'Layouts';
 
 export function BrandPage() {
   const { currentOrganization } = useAuthContext();
+  const { isSegmentEnabled, track } = useSegment();
 
   const menuTabs = [
     {
@@ -24,12 +26,23 @@ export function BrandPage() {
     },
   ];
 
+  const trackLayoutFocus = (value: TabsValue) => {
+    if (value === LAYOUT && isSegmentEnabled()) {
+      track('[Layout] - Tab Focus');
+    }
+  };
+
   return (
     <PageContainer>
       <PageMeta title="Brand" />
       <PageHeader title="Brand" />
       <Container fluid mt={15} ml={5}>
-        <Tabs loading={!currentOrganization} menuTabs={menuTabs} defaultValue={BRANDING} />
+        <Tabs
+          loading={!currentOrganization}
+          menuTabs={menuTabs}
+          defaultValue={BRANDING}
+          onTabChange={trackLayoutFocus}
+        />
       </Container>
     </PageContainer>
   );
