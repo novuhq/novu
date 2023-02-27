7e4739ffa4583562628762a010ac3617b349d105
Biswajeet Das <biswajeetdas18@gmail.com>
2023-02-27 21:54:10 +0530

    feat: added more analytics

--- apps/web/src/pages/brand/BrandPage.tsx
+++ apps/web/src/pages/brand/BrandPage.tsx
@@ -7,13 +7,30 @@ import { Tabs } from '../../design-system';
 import { useAuthContext } from '../../components/providers/AuthProvider';
 import { BrandingForm, LayoutsListPage } from './tabs';
 import { useSegment } from '../../components/providers/SegmentProvider';
+import { useEnvController } from '../../hooks';
 
 const BRANDING = 'Assets';
 const LAYOUT = 'Layouts';
 
 export function BrandPage() {
-  const { currentOrganization } = useAuthContext();
+  const { currentOrganization, currentUser } = useAuthContext();
+  const { environment } = useEnvController();
   const { isSegmentEnabled, track } = useSegment();
+
+  const handleLayoutAnalytics = (event: string, data?: Record<string, unknown>) => {
+    track(`[Layout] - ${event}`, {
+      _organizationId: currentOrganization?._id,
+      _environmentId: environment?._id,
+      userId: currentUser?._id,
+      ...data,
+    });
+  };
+
+  const trackLayoutFocus = (value: TabsValue) => {
+    if (value === LAYOUT && isSegmentEnabled()) {
+      handleLayoutAnalytics('Layout tab clicked');
+    }
+  };
 
   const menuTabs = [
     {
@@ -22,15 +39,9 @@ export function BrandPage() {
     },
     {
       value: LAYOUT,
-      content: <LayoutsListPage />,
+      content: <LayoutsListPage handleLayoutAnalytics={handleLayoutAnalytics} />,
     },
   ];
-
-  const trackLayoutFocus = (value: TabsValue) => {
-    if (value === LAYOUT && isSegmentEnabled()) {
-      track('[Layout] - Tab Focus');
-    }
-  };
 
   return (
     <PageContainer>
