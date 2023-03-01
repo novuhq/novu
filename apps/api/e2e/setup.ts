099715dc3309bf92767902497dfb1e6e3f879891
p-fernandez <pablo.fernandez.otero@gmail.com>
2023-03-01 17:36:33 +0000

    feat(api): skeleton for regression testing

--- apps/api/e2e/setup.ts
+++ apps/api/e2e/setup.ts
@@ -1,28 +1,4 @@
-import { DalService } from '@novu/dal';
-import { testServer } from '@novu/testing';
-import * as sinon from 'sinon';
-
-import { bootstrap } from '../src/bootstrap';
-
-const dalService = new DalService();
-
-before(async () => {
-  await testServer.create(await bootstrap());
-  await dalService.connect(process.env.MONGO_URL);
-});
-
-after(async () => {
-  await testServer.teardown();
-
-  try {
-    await dalService.destroy();
-  } catch (e) {
-    if (e.code !== 12586) {
-      throw e;
-    }
-  }
-});
-
-afterEach(() => {
-  sinon.restore();
-});
+/*
+ * Seems `mocha` needs this file to exist even if it is empty.
+ * So we need it to have different configs for the different test suites.
+ */
