e9aca3bf36be5a599162bf423cfab3b59bc63415
p-fernandez <pablo.fernandez.otero@gmail.com>
2023-03-01 17:36:33 +0000

    feat(api): skeleton for regression testing

---
+++ apps/api/e2e/regression-setup.ts
@@ -0,0 +1,30 @@
+import { DalService } from '@novu/dal';
+import { testServer } from '@novu/testing';
+import * as sinon from 'sinon';
+
+import { bootstrap } from '../src/bootstrap';
+
+const database = process.env.MONGO_URL || 'mongodb://localhost:27017/novu-regression';
+
+const dalService = new DalService();
+
+before(async () => {
+  await testServer.create(await bootstrap());
+  await dalService.connect(database);
+});
+
+after(async () => {
+  await testServer.teardown();
+
+  try {
+    await dalService.destroy();
+  } catch (e) {
+    if (e.code !== 12586) {
+      throw e;
+    }
+  }
+});
+
+afterEach(() => {
+  sinon.restore();
+});
