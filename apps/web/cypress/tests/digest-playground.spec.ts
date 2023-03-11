cd5208be2aacc8c45b4297982daba4b19d27076c
Paweł <pawel.tymczuk@gmail.com>
2023-03-11 16:22:01 +0100

    feat(web): digest workflow tour - hints ui

--- apps/web/cypress/tests/digest-playground.spec.ts
+++ apps/web/cypress/tests/digest-playground.spec.ts
@@ -32,4 +32,103 @@ describe('Digest Playground Workflow Page', function () {
 
     cy.url().should('include', '/templates/edit');
   });
+
+  it('should show the digest workflow hints', () => {
+    cy.intercept('**/notification-templates**').as('notificationTemplates');
+    cy.visit('/templates');
+    cy.wait('@notificationTemplates');
+
+    // click try digest playground
+    cy.getByTestId('no-workflow-templates-placeholder').should('be.visible');
+    cy.getByTestId('try-digest-playground-tile').click();
+
+    cy.url().should('include', '/templates/digest-playground');
+    cy.contains('Digest Workflow Playground');
+
+    // click set up digest workflow
+    cy.get('button').contains('Set Up Digest Workflow').click();
+
+    // in the template workflow editor
+    cy.url().should('include', '/templates/edit');
+    cy.url().should('include', '?tour=digest');
+
+    // check the digest hint
+    cy.getByTestId('digest-workflow-tooltip').contains('Set-up time interval');
+    cy.getByTestId('digest-workflow-tooltip').contains(
+      'Specify for how long the digest should collect events before sending a digested event to the next step step in the workflow.'
+    );
+    cy.getByTestId('digest-workflow-tooltip-primary-button').contains('Next');
+    cy.getByTestId('digest-workflow-tooltip-skip-button').contains('Skip tour');
+    cy.getByTestId('digest-workflow-tooltip-dots-navigation').should('be.visible');
+
+    // check if has digest step
+    cy.getByTestId('node-digestSelector').should('be.visible');
+    // check if digest step settings opened
+    cy.getByTestId('step-properties-side-menu').should('be.visible');
+    cy.getByTestId('step-properties-side-menu').contains('Digest Properties');
+
+    // click next on hint
+    cy.getByTestId('digest-workflow-tooltip-primary-button').contains('Next').click();
+
+    // check the email hint
+    cy.getByTestId('digest-workflow-tooltip').contains('Set-up email content');
+    cy.getByTestId('digest-workflow-tooltip').contains(
+      'Use custom HTML or our visual editor to define how the email will look like when sent to the subscriber.'
+    );
+    cy.getByTestId('digest-workflow-tooltip-primary-button').contains('Next');
+    cy.getByTestId('digest-workflow-tooltip-skip-button').contains('Skip tour');
+    cy.getByTestId('digest-workflow-tooltip-dots-navigation').should('be.visible');
+
+    // check if has email step
+    cy.getByTestId('node-digestSelector').should('be.visible');
+    // check if email step settings opened
+    cy.getByTestId('step-properties-side-menu').should('be.visible');
+    cy.getByTestId('step-properties-side-menu').contains('Email Properties');
+
+    // click next on hint
+    cy.getByTestId('digest-workflow-tooltip-primary-button').contains('Next').click();
+
+    // check the email hint
+    cy.getByTestId('digest-workflow-tooltip').contains('Test your workflow');
+    cy.getByTestId('digest-workflow-tooltip').contains(
+      'We will trigger the workflow multiple times to represent how it aggregates notifications.'
+    );
+    cy.getByTestId('digest-workflow-tooltip-primary-button').contains('Got it');
+    cy.getByTestId('digest-workflow-tooltip-skip-button').should('not.exist');
+    cy.getByTestId('digest-workflow-tooltip-dots-navigation').should('be.visible');
+
+    // the step settings should be hidden
+    cy.getByTestId('drag-side-menu').contains('Steps to add');
+
+    // click got it should hide the hint
+    cy.getByTestId('digest-workflow-tooltip-primary-button').contains('Got it').click();
+
+    cy.getByTestId('digest-workflow-tooltip').should('not.exist');
+  });
+
+  it('should hide the digest workflow hints when clicking on skip tour button', () => {
+    cy.intercept('**/notification-templates**').as('notificationTemplates');
+    cy.visit('/templates');
+    cy.wait('@notificationTemplates');
+
+    // click try digest playground
+    cy.getByTestId('no-workflow-templates-placeholder').should('be.visible');
+    cy.getByTestId('try-digest-playground-tile').click();
+
+    cy.url().should('include', '/templates/digest-playground');
+    cy.contains('Digest Workflow Playground');
+
+    // click set up digest workflow
+    cy.get('button').contains('Set Up Digest Workflow').click();
+
+    // in the template workflow editor
+    cy.url().should('include', '/templates/edit');
+    cy.url().should('include', '?tour=digest');
+
+    // check the digest hint
+    cy.getByTestId('digest-workflow-tooltip').contains('Set-up time interval');
+    cy.getByTestId('digest-workflow-tooltip-skip-button').contains('Skip tour').click();
+
+    cy.getByTestId('digest-workflow-tooltip').should('not.exist');
+  });
 });
