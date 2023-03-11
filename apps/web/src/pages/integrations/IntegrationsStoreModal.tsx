3f3eceb05ab73cbb052b4f45fb357554ffebe0f5
ainouzgali <ainouzgali@gmail.com>
2023-03-11 13:40:32 +0100

    feat: initial work on integration modal

---
+++ apps/web/src/pages/integrations/IntegrationsStoreModal.tsx
@@ -0,0 +1,339 @@
+import { useEffect, useState } from 'react';
+import styled from '@emotion/styled';
+import {
+  ChannelTypeEnum,
+  IConfigCredentials,
+  ILogoFileName,
+  providers,
+  PushProviderIdEnum,
+  EmailProviderIdEnum,
+  InAppProviderIdEnum,
+} from '@novu/shared';
+import { Grid, Group, Modal, ActionIcon, useMantineColorScheme, createStyles, MantineTheme } from '@mantine/core';
+import * as cloneDeep from 'lodash.clonedeep';
+import { useIntegrations } from '../../hooks';
+import { When } from '../../components/utils/When';
+import { NovuEmailProviderModal } from './components/NovuEmailProviderModal';
+import { NovuInAppProviderModal } from './components/NovuInAppProviderModal';
+import { ChannelGroup } from './components/Modal/ChannelGroup';
+import { colors, shadows, Title } from '../../design-system';
+import { ConnectIntegrationForm } from './components/Modal/ConnectIntegrationForm';
+import { Close } from '../../design-system/icons/actions/Close';
+
+export function IntegrationsStoreModal({
+  openIntegration,
+  closeIntegration,
+}: {
+  openIntegration: boolean;
+  closeIntegration: () => void;
+}) {
+  const { integrations, loading: isLoading, refetch } = useIntegrations();
+  const { colorScheme } = useMantineColorScheme();
+  const [emailProviders, setEmailProviders] = useState<IIntegratedProvider[]>([]);
+  const [smsProvider, setSmsProvider] = useState<IIntegratedProvider[]>([]);
+  const [chatProvider, setChatProvider] = useState<IIntegratedProvider[]>([]);
+  const [pushProvider, setPushProvider] = useState<IIntegratedProvider[]>([]);
+  const [isFormOpened, setFormIsOpened] = useState(false);
+  const [isCreateIntegrationModal, setIsCreateIntegrationModal] = useState(false);
+  const [provider, setProvider] = useState<IIntegratedProvider | null>(null);
+  const [selectedProvider, setSelectedProvider] = useState<string>('');
+
+  const { classes } = useModalStyles();
+
+  async function handlerVisible(
+    visible: boolean,
+    createIntegrationModal: boolean,
+    providerConfig: IIntegratedProvider
+  ) {
+    setFormIsOpened(visible);
+    setProvider(providerConfig);
+    setSelectedProvider(providerConfig.providerId);
+    setIsCreateIntegrationModal(createIntegrationModal);
+  }
+
+  async function handlerShowForm(showForm: boolean) {
+    await setFormIsOpened(showForm);
+    if (!showForm) {
+      await refetch();
+    }
+  }
+
+  function handleCloseForm() {
+    setSelectedProvider('');
+    setFormIsOpened(false);
+  }
+
+  useEffect(() => {
+    if (integrations) {
+      const initializedProviders = initializeProviders(integrations);
+
+      setEmailProviders(
+        sortProviders(initializedProviders.filter((providerItem) => providerItem.channel === ChannelTypeEnum.EMAIL))
+      );
+      setSmsProvider(
+        sortProviders(initializedProviders.filter((providerItem) => providerItem.channel === ChannelTypeEnum.SMS))
+      );
+
+      setChatProvider(
+        sortProviders(initializedProviders.filter((providerItem) => providerItem.channel === ChannelTypeEnum.CHAT))
+      );
+
+      setPushProvider(
+        sortProviders(initializedProviders.filter((providerItem) => providerItem.channel === ChannelTypeEnum.PUSH))
+      );
+    }
+  }, [integrations]);
+
+  function handleKeyDown(e) {
+    if (e.key === 'Escape') {
+      handleCloseForm();
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => window.removeEventListener('keydown', handleKeyDown);
+
+    return undefined;
+  }, []);
+
+  return (
+    <Modal
+      withCloseButton={false}
+      closeOnEscape={!isFormOpened}
+      title={
+        <Group style={{ width: '100%' }} position={'apart'}>
+          <Title>Integration Store</Title>
+          <ActionIcon variant={'transparent'} onClick={closeIntegration}>
+            <Close />
+          </ActionIcon>
+        </Group>
+      }
+      classNames={classes}
+      fullScreen
+      opened={openIntegration}
+      onClose={closeIntegration}
+    >
+      <Grid gutter={24}>
+        <Grid.Col lg={isFormOpened ? 8 : 12}>
+          {!isLoading ? (
+            <>
+              <ChannelGroup
+                selectedProvider={selectedProvider}
+                channel={ChannelTypeEnum.EMAIL}
+                providers={emailProviders}
+                title="Email"
+                onProviderClick={handlerVisible}
+              />
+              <ChannelGroup
+                selectedProvider={selectedProvider}
+                channel={ChannelTypeEnum.SMS}
+                providers={smsProvider}
+                title="SMS"
+                onProviderClick={handlerVisible}
+              />
+              <ChannelGroup
+                selectedProvider={selectedProvider}
+                channel={ChannelTypeEnum.CHAT}
+                providers={chatProvider}
+                title="Chat"
+                onProviderClick={handlerVisible}
+              />
+              <ChannelGroup
+                selectedProvider={selectedProvider}
+                channel={ChannelTypeEnum.PUSH}
+                providers={pushProvider}
+                title="Push"
+                onProviderClick={handlerVisible}
+              />
+            </>
+          ) : null}
+        </Grid.Col>
+        <When truthy={isFormOpened}>
+          <Grid.Col lg={4} mt={50}>
+            <IntegrationCardWrapper dark={colorScheme === 'dark'} onKeyPress={handleKeyDown}>
+              <When truthy={!provider?.novu}>
+                <ConnectIntegrationForm
+                  onClose={handleCloseForm}
+                  provider={provider}
+                  showForm={handlerShowForm}
+                  createModel={isCreateIntegrationModal}
+                  closeIntegration={closeIntegration}
+                />
+              </When>
+              <When truthy={provider?.providerId === EmailProviderIdEnum.Novu}>
+                <div style={{ padding: '30px' }}>
+                  <NovuEmailProviderModal onClose={handleCloseForm} />
+                </div>
+              </When>
+              <When truthy={provider?.providerId === InAppProviderIdEnum.Novu}>
+                <div style={{ padding: '30px' }}>
+                  <NovuInAppProviderModal onClose={handleCloseForm} />
+                </div>
+              </When>
+            </IntegrationCardWrapper>
+          </Grid.Col>
+        </When>
+      </Grid>
+    </Modal>
+  );
+}
+
+const IntegrationCardWrapper = styled.div<{ dark: boolean }>`
+  position: sticky;
+  top: 10rem;
+  box-sizing: border-box;
+  padding: 0;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  height: 80vh;
+  background-color: ${({ dark }) => (dark ? colors.B20 : colors.BGLight)};
+  box-shadow: ${({ dark }) => (dark ? shadows.dark : shadows.medium)};
+  border-radius: 7px;
+`;
+
+const useModalStyles = createStyles((theme: MantineTheme) => {
+  const dark = theme.colorScheme === 'dark';
+
+  return {
+    header: {
+      position: 'sticky',
+      top: 0,
+      padding: '30px',
+      marginLeft: '-30px',
+      marginRight: '-30px',
+      zIndex: 9,
+      boxShadow: dark ? shadows.dark : shadows.medium,
+      backgroundColor: dark ? colors.BGDark : colors.white,
+    },
+    title: { width: '100%' },
+    modal: {
+      backdropFilter: 'blur(15px)',
+      padding: '0px 30px !important',
+      backgroundColor: dark ? theme.fn.rgba(colors.BGDark, 0.8) : theme.fn.rgba(colors.white, 0.7),
+    },
+  };
+});
+
+const sortProviders = (unsortedProviders: IIntegratedProvider[]) => {
+  return unsortedProviders
+    .sort((a, b) => Number(b.active) - Number(a.active))
+    .sort((a, b) => Number(isConnected(b)) - Number(isConnected(a)));
+};
+
+function isConnected(provider: IIntegratedProvider) {
+  if (!provider.credentials.length) return false;
+
+  return provider.credentials?.some((cred) => {
+    return cred.value;
+  });
+}
+
+export interface IIntegratedProvider {
+  providerId: string;
+  integrationId: string;
+  displayName: string;
+  channel: ChannelTypeEnum;
+  credentials: IConfigCredentials[];
+  docReference: string;
+  comingSoon: boolean;
+  active: boolean;
+  connected: boolean;
+  logoFileName: ILogoFileName;
+  betaVersion: boolean;
+  novu?: boolean;
+}
+
+export interface ICredentials {
+  apiKey?: string;
+  user?: string;
+  secretKey?: string;
+  domain?: string;
+  password?: string;
+  host?: string;
+  port?: string;
+  secure?: boolean;
+  region?: string;
+  accountSid?: string;
+  messageProfileId?: string;
+  token?: string;
+  from?: string;
+  senderName?: string;
+  applicationId?: string;
+  clientId?: string;
+  projectName?: string;
+  serviceAccount?: string;
+}
+
+export interface IntegrationEntity {
+  _id?: string;
+
+  _environmentId: string;
+
+  _organizationId: string;
+
+  providerId: string;
+
+  channel: ChannelTypeEnum;
+
+  credentials: ICredentials;
+
+  active: boolean;
+
+  deleted: boolean;
+
+  deletedAt: string;
+
+  deletedBy: string;
+}
+
+function initializeProviders(integrations: IntegrationEntity[]): IIntegratedProvider[] {
+  return providers.map((providerItem) => {
+    const integration = integrations.find((integrationItem) => integrationItem.providerId === providerItem.id);
+
+    const clonedCredentials = cloneDeep(providerItem.credentials);
+
+    if (integration?.credentials && Object.keys(clonedCredentials).length !== 0) {
+      clonedCredentials.forEach((credential) => {
+        // eslint-disable-next-line no-param-reassign
+        credential.value = integration.credentials[credential.key]?.toString();
+      });
+    }
+
+    // Remove this like after the run of the fcm-credentials-migration script
+    fcmFallback(integration, clonedCredentials);
+
+    return {
+      providerId: providerItem.id,
+      integrationId: integration?._id ? integration._id : '',
+      displayName: providerItem.displayName,
+      channel: providerItem.channel,
+      credentials: integration?.credentials ? clonedCredentials : providerItem.credentials,
+      docReference: providerItem.docReference,
+      comingSoon: !!providerItem.comingSoon,
+      betaVersion: !!providerItem.betaVersion,
+      active: integration?.active ?? false,
+      connected: !!integration,
+      logoFileName: providerItem.logoFileName,
+    };
+  });
+}
+
+/*
+ * temporary patch before migration script
+ */
+function fcmFallback(integration: IntegrationEntity | undefined, clonedCredentials) {
+  if (integration?.providerId === PushProviderIdEnum.FCM) {
+    const serviceAccount = integration?.credentials.serviceAccount
+      ? integration?.credentials.serviceAccount
+      : integration?.credentials.user;
+
+    clonedCredentials?.forEach((cred) => {
+      if (cred.key === 'serviceAccount') {
+        cred.value = serviceAccount;
+      }
+    });
+  }
+}
---
+++ apps/web/src/pages/integrations/components/Modal/ConnectIntegrationForm.tsx
@@ -0,0 +1,379 @@
+import { useEffect, useState, useReducer } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import styled from '@emotion/styled/macro';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useClipboard } from '@mantine/hooks';
+import { Image, useMantineColorScheme, Stack, Alert, ActionIcon, Center } from '@mantine/core';
+import { WarningOutlined } from '@ant-design/icons';
+import { ChannelTypeEnum, ICredentialsDto, IConfigCredentials } from '@novu/shared';
+
+import { Button, colors, Input, shadows, Switch, Text } from '../../../../design-system';
+import { IIntegratedProvider } from '../../IntegrationsStorePage';
+import { createIntegration, getWebhookSupportStatus, updateIntegration } from '../../../../api/integration';
+import { Close } from '../../../../design-system/icons/actions/Close';
+import { IntegrationInput } from '../IntegrationInput';
+import { API_ROOT } from '../../../../config';
+import { useEnvController, useAuthController } from '../../../../hooks';
+import { Check, Copy } from '../../../../design-system/icons';
+import { CONTEXT_PATH } from '../../../../config';
+import { successMessage } from '../../../../utils/notifications';
+
+enum ACTION_TYPE_ENUM {
+  HANDLE_SHOW_SWITCH = 'handle_show_switch',
+  HANDLE_ALERT_AND_ERROR_MSG = 'handle_alert_and_error_msg',
+  TOGGLE_CHECK = 'toggle_check',
+}
+
+type ActionType =
+  | { type: ACTION_TYPE_ENUM.TOGGLE_CHECK }
+  | {
+      type: ACTION_TYPE_ENUM.HANDLE_SHOW_SWITCH;
+      payload: boolean;
+    }
+  | {
+      type: ACTION_TYPE_ENUM.HANDLE_ALERT_AND_ERROR_MSG;
+      payload: {
+        isShowAlert: boolean;
+        errorMsg: string;
+      };
+    };
+
+const checkIntegrationInitialState = {
+  check: true,
+  isShowSwitch: false,
+  isShowAlert: false,
+  errorMsg: '',
+};
+
+const checkIntegrationReducer = (state: typeof checkIntegrationInitialState, action: ActionType) => {
+  switch (action.type) {
+    case ACTION_TYPE_ENUM.HANDLE_SHOW_SWITCH:
+      return {
+        ...state,
+        isShowSwitch: action.payload,
+      };
+
+    case ACTION_TYPE_ENUM.HANDLE_ALERT_AND_ERROR_MSG:
+      return {
+        ...state,
+        isShowAlert: action.payload.isShowAlert,
+        errorMsg: action.payload.errorMsg,
+      };
+
+    case ACTION_TYPE_ENUM.TOGGLE_CHECK:
+      return {
+        ...state,
+        check: !state.check,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export function ConnectIntegrationForm({
+  provider,
+  showForm,
+  createModel,
+  onClose,
+  closeIntegration,
+}: {
+  provider: IIntegratedProvider | null;
+  showForm: (visible: boolean) => void;
+  createModel: boolean;
+  onClose: () => void;
+  closeIntegration: () => void;
+}) {
+  const {
+    register,
+    handleSubmit: handleSubmitIntegration,
+    setValue,
+    formState: { errors },
+    control,
+  } = useForm({ shouldUseNativeValidation: false });
+
+  const { colorScheme } = useMantineColorScheme();
+  const [isActive, setIsActive] = useState<boolean>(!!provider?.active);
+  const { environment } = useEnvController();
+  const { organization } = useAuthController();
+  const webhookUrlClipboard = useClipboard({ timeout: 1000 });
+  const [checkIntegrationState, dispatch] = useReducer(checkIntegrationReducer, checkIntegrationInitialState);
+
+  const { mutateAsync: createIntegrationApi, isLoading: isLoadingCreate } = useMutation<
+    { res: string },
+    { error: string; message: string; statusCode: number },
+    {
+      providerId: string;
+      channel: ChannelTypeEnum | null;
+      credentials: ICredentialsDto;
+      active: boolean;
+      check: boolean;
+    }
+  >(createIntegration);
+
+  const { mutateAsync: updateIntegrationApi, isLoading: isLoadingUpdate } = useMutation<
+    { res: string },
+    { error: string; message: string; statusCode: number },
+    {
+      integrationId: string;
+      data: { credentials: ICredentialsDto; active: boolean; check: boolean };
+    }
+  >(({ integrationId, data }) => updateIntegration(integrationId, data));
+
+  const { data: webhookSupportStatus } = useQuery(
+    ['webhookSupportStatus', provider?.providerId],
+    () => getWebhookSupportStatus(provider?.providerId as string),
+    {
+      enabled: Boolean(
+        provider?.providerId && [ChannelTypeEnum.EMAIL, ChannelTypeEnum.SMS].includes(provider.channel) && !createModel
+      ),
+    }
+  );
+
+  useEffect(() => {
+    if (provider?.credentials) {
+      for (const credential of provider.credentials) {
+        setValue(credential.key, credential.value);
+      }
+    }
+  }, [provider]);
+
+  async function onCreateIntegration(credentials: ICredentialsDto) {
+    try {
+      if (checkIntegrationState.isShowAlert) {
+        dispatch({
+          type: ACTION_TYPE_ENUM.HANDLE_ALERT_AND_ERROR_MSG,
+          payload: {
+            isShowAlert: false,
+            errorMsg: '',
+          },
+        });
+      }
+      if (createModel) {
+        await createIntegrationApi({
+          providerId: provider?.providerId ? provider?.providerId : '',
+          channel: provider?.channel ? provider?.channel : null,
+          credentials,
+          active: isActive,
+          check: checkIntegrationState.check,
+        });
+      } else {
+        await updateIntegrationApi({
+          integrationId: provider?.integrationId ? provider?.integrationId : '',
+          data: { credentials, active: isActive, check: checkIntegrationState.check },
+        });
+      }
+    } catch (e: any) {
+      dispatch({
+        type: ACTION_TYPE_ENUM.HANDLE_SHOW_SWITCH,
+        payload: true,
+      });
+      dispatch({
+        type: ACTION_TYPE_ENUM.HANDLE_ALERT_AND_ERROR_MSG,
+        payload: {
+          isShowAlert: true,
+          errorMsg: e?.message,
+        },
+      });
+
+      return;
+    }
+
+    successMessage(`Successfully ${createModel ? 'added' : 'updated'} integration`);
+
+    onClose();
+    closeIntegration();
+    showForm(false);
+  }
+
+  function handlerSwitchChange() {
+    setIsActive((prev) => !prev);
+  }
+
+  function handlerCheckIntegrationChange() {
+    dispatch({
+      type: ACTION_TYPE_ENUM.TOGGLE_CHECK,
+    });
+  }
+
+  const logoSrc = provider
+    ? `${CONTEXT_PATH}/static/images/providers/${colorScheme}/${provider.logoFileName[`${colorScheme}`]}`
+    : '';
+
+  // eslint-disable-next-line max-len
+  const webhookUrl = `${API_ROOT}/v1/webhooks/organizations/${organization?._id}/environments/${environment?._id}/${provider?.channel}/${provider?.providerId}`;
+
+  return (
+    <form noValidate onSubmit={handleSubmitIntegration(onCreateIntegration)}>
+      <Header dark={colorScheme === 'dark'}>
+        <Stack spacing={10}>
+          <Image
+            radius="md"
+            height={50}
+            width={140}
+            fit={'contain'}
+            src={logoSrc}
+            alt={`${provider?.providerId} image`}
+          />
+          <Center inline>
+            <Text>
+              Read our guide on where to get the credentials{' '}
+              <a href={provider?.docReference} target="_blank" rel="noreferrer" style={{ color: '#DD2476 ' }}>
+                here.
+              </a>
+            </Text>
+          </Center>
+        </Stack>
+        <ActionIcon mt={-50} variant={'transparent'} onClick={onClose} data-test-id="connection-integration-form-close">
+          <Close />
+        </ActionIcon>
+      </Header>
+
+      <CenterDiv>
+        {provider?.credentials.map((credential: IConfigCredentials) => (
+          <InputWrapper key={credential.key}>
+            <Controller
+              name={credential.key}
+              control={control}
+              render={({ field }) => (
+                <IntegrationInput credential={credential} errors={errors} field={field} register={register} />
+              )}
+            />
+          </InputWrapper>
+        ))}
+        {webhookSupportStatus &&
+          provider?.channel &&
+          [ChannelTypeEnum.EMAIL, ChannelTypeEnum.SMS].includes(provider?.channel) && (
+            <InputWrapper>
+              <Input
+                label="Webhook URL"
+                value={webhookUrl}
+                readOnly
+                rightSection={
+                  <CopyWrapper onClick={() => webhookUrlClipboard.copy(webhookUrl)}>
+                    {webhookUrlClipboard.copied ? <Check /> : <Copy />}
+                  </CopyWrapper>
+                }
+              />
+            </InputWrapper>
+          )}
+        <Stack my={20}>
+          <ActiveWrapper active={isActive}>
+            <Controller
+              control={control}
+              name="isActive"
+              render={({ field }) => (
+                <Switch checked={isActive} data-test-id="is_active_id" {...field} onChange={handlerSwitchChange} />
+              )}
+            />
+            <StyledText data-test-id="connect-integration-form-active-text">
+              {isActive ? 'Active' : 'Disabled'}
+            </StyledText>
+          </ActiveWrapper>
+          {provider?.channel === ChannelTypeEnum.EMAIL && checkIntegrationState.isShowSwitch && (
+            <CheckIntegrationWrapper>
+              <Controller
+                control={control}
+                name="check"
+                render={({ field }) => (
+                  <Switch
+                    checked={checkIntegrationState.check}
+                    data-test-id="is_check_integration_id"
+                    {...field}
+                    onChange={handlerCheckIntegrationChange}
+                  />
+                )}
+              />
+              <StyledText>Verify provider credentials</StyledText>
+            </CheckIntegrationWrapper>
+          )}
+
+          {checkIntegrationState.isShowAlert && (
+            <Alert icon={<WarningOutlined size={16} />} title="An error occurred!" color="red" mb={30}>
+              {checkIntegrationState.errorMsg}
+            </Alert>
+          )}
+        </Stack>
+      </CenterDiv>
+      <Footer dark={colorScheme === 'dark'}>
+        <Button onClick={onClose} variant={'outline'}>
+          Cancel
+        </Button>
+        <Button submit loading={isLoadingUpdate || isLoadingCreate}>
+          {createModel ? 'Connect' : 'Update'}
+        </Button>
+      </Footer>
+    </form>
+  );
+}
+
+const StyledText = styled(Text)`
+  display: inline-block;
+  word-break: normal;
+  margin: 0 10px;
+`;
+
+const SideElementBase = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  & > :first-of-type {
+    width: auto;
+  }
+`;
+
+const ActiveWrapper = styled(SideElementBase)<{ active: boolean }>`
+  align-items: center;
+
+  ${StyledText} {
+    color: ${({ active }) => (active ? colors.success : colors.error)};
+  }
+`;
+
+const Footer = styled.div<{ dark: boolean }>`
+  padding: 15px;
+  height: 80px;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  gap: 20px;
+  box-shadow: ${({ dark }) => (dark ? shadows.dark : shadows.medium)};
+`;
+
+const Header = styled.div<{ dark: boolean }>`
+  padding: 30px;
+  height: 120px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: ${({ dark }) => (dark ? shadows.dark : shadows.medium)};
+`;
+
+const InputWrapper = styled.div`
+  padding-bottom: 10px;
+  label {
+    font-weight: bold;
+    margin-bottom: 10px;
+    font-size: 14px;
+  }
+`;
+
+const CheckIntegrationWrapper = styled(SideElementBase)`
+  width: 100%;
+  align-items: center;
+  ${StyledText} {
+    flex-grow: 1;
+  }
+`;
+
+const CopyWrapper = styled.div`
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+const CenterDiv = styled.div`
+  height: calc(80vh - 200px);
+  overflow: auto;
+  padding: 30px;
+`;
