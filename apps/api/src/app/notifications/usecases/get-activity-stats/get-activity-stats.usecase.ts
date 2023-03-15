019245aa2bce4a78ff2639d8444d36f191d0f532
p-fernandez <pablo.fernandez.otero@gmail.com>
2023-03-15 22:52:42 +0000

    fix(api): reduce activity stats queries and improve performance

--- apps/api/src/app/notifications/usecases/get-activity-stats/get-activity-stats.usecase.ts
+++ apps/api/src/app/notifications/usecases/get-activity-stats/get-activity-stats.usecase.ts
@@ -1,42 +1,61 @@
 import { Injectable } from '@nestjs/common';
 import { NotificationRepository } from '@novu/dal';
-import { subMonths, subWeeks, subYears } from 'date-fns';
-import { ActivityStatsResponseDto } from '../../dtos/activity-stats-response.dto';
+import { isBefore, parseISO, subMonths, subWeeks, subYears } from 'date-fns';
+
 import { GetActivityStatsCommand } from './get-activity-stats.command';
+
+import { ActivityStatsResponseDto } from '../../dtos/activity-stats-response.dto';
 
 @Injectable()
 export class GetActivityStats {
   constructor(private notificationRepository: NotificationRepository) {}
 
   async execute(command: GetActivityStatsCommand): Promise<ActivityStatsResponseDto> {
-    const yearly = await this.notificationRepository.count({
-      _environmentId: command.environmentId,
-      _organizationId: command.organizationId,
-      createdAt: {
-        $gte: subYears(new Date(), 1),
-      },
-    });
+    const now = new Date();
 
-    const monthly = await this.notificationRepository.count({
-      _environmentId: command.environmentId,
-      _organizationId: command.organizationId,
-      createdAt: {
-        $gte: subMonths(new Date(), 1),
+    const yearlyNotifications = await this.notificationRepository.find(
+      {
+        _environmentId: command.environmentId,
+        _organizationId: command.organizationId,
+        createdAt: {
+          $gte: subYears(now, 1),
+        },
       },
-    });
+      {
+        _id: 1,
+        createdAt: 1,
+      }
+    );
 
-    const weekly = await this.notificationRepository.count({
-      _environmentId: command.environmentId,
-      _organizationId: command.organizationId,
-      createdAt: {
-        $gte: subWeeks(new Date(), 1),
-      },
-    });
+    let weekly = 0;
+    let monthly = 0;
+    const oneMonthAgo = subMonths(now, 1);
+    const oneWeekAgo = subWeeks(now, 1);
+
+    for (const notification of yearlyNotifications) {
+      const { createdAt } = notification;
+      if (this.isAfterDateInPast(oneWeekAgo, createdAt)) {
+        weekly += 1;
+      }
+      if (this.isAfterDateInPast(oneMonthAgo, createdAt)) {
+        monthly += 1;
+      }
+    }
 
     return {
       weeklySent: weekly,
       monthlySent: monthly,
-      yearlySent: yearly,
+      yearlySent: yearlyNotifications.length,
     };
+  }
+
+  private isAfterDateInPast(dateInPast: Date, dateToCheck?: string): boolean {
+    if (!dateToCheck) {
+      return false;
+    }
+
+    const isoDate = parseISO(dateToCheck);
+
+    return isBefore(dateInPast, isoDate);
   }
 }
