ce9ac1f5ba6b10fc32375861c8605a546df2d79e
p-fernandez <pablo.fernandez.otero@gmail.com>
2023-03-30 17:11:54 +0100

    feat(api): in-memory provider service

--- apps/api/src/app/shared/shared.module.ts
+++ apps/api/src/app/shared/shared.module.ts
@@ -26,6 +26,7 @@ import { AnalyticsService, createNestLoggingModuleOptions, LoggerModule } from '
 import { ConnectionOptions } from 'tls';
 
 import { DistributedLockService } from './services/distributed-lock';
+import { InMemoryProviderService } from './services/in-memory-provider';
 import { PerformanceService } from './services/performance';
 import { QueueService } from './services/queue';
 import {
@@ -75,24 +76,24 @@ const dalService = new DalService();
 
 export const ANALYTICS_SERVICE = 'AnalyticsService';
 
+const inMemoryProviderService = {
+  provide: InMemoryProviderService,
+  useFactory: () => {
+    return new InMemoryProviderService();
+  },
+};
+
 const cacheService = {
   provide: CacheService,
-  useFactory: async () => {
-    return new CacheService({
-      host: process.env.REDIS_CACHE_SERVICE_HOST,
-      port: process.env.REDIS_CACHE_SERVICE_PORT || '6379',
-      ttl: process.env.REDIS_CACHE_TTL,
-      password: process.env.REDIS_CACHE_PASSWORD,
-      connectTimeout: process.env.REDIS_CACHE_CONNECTION_TIMEOUT,
-      keepAlive: process.env.REDIS_CACHE_KEEP_ALIVE,
-      family: process.env.REDIS_CACHE_FAMILY,
-      keyPrefix: process.env.REDIS_CACHE_KEY_PREFIX,
-      tls: process.env.REDIS_CACHE_SERVICE_TLS as ConnectionOptions,
-    });
+  useFactory: () => {
+    const factoryInMemoryProviderService = inMemoryProviderService.useFactory();
+
+    return new CacheService(factoryInMemoryProviderService);
   },
 };
 
 const PROVIDERS = [
+  inMemoryProviderService,
   {
     provide: DistributedLockService,
     useFactory: () => {
