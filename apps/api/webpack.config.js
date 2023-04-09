b3c5e8be49ca660476bc7e20eebdcf0465edcad0
Dima Grossman <dima@grossman.io>
2023-04-09 15:47:01 +0300

    fix: revert un needed webpack config

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -1,8 +1,4 @@
 module.exports = function (options) {
-  options.externals.push({
-    '@taskforcesh/bullmq-pro': '@taskforcesh/bullmq-pro',
-  });
-
   return {
     ...options,
     devtool: 'source-map',
