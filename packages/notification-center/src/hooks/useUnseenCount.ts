dcc64bc519d2ca5c357a48c070f8a3143e588119
Dima Grossman <dima@grossman.io>
2023-04-10 10:05:38 +0300

    fix: add temporary fix for flaky testing

--- packages/notification-center/src/hooks/useUnseenCount.ts
+++ packages/notification-center/src/hooks/useUnseenCount.ts
@@ -10,6 +10,15 @@ const dispatchUnseenCountEvent = (count: number) => {
   document.dispatchEvent(new CustomEvent('novu:unseen_count_changed', { detail: count }));
 };
 
+/**
+ * TODO: This is a temporary fix
+ * Cypress is slow, so we need to increase the debounce time
+ * This is happening in a very high thruput of updates in testing env.
+ *
+ * Can also happen in real scenarios, so we need to review how we handle concurrency in the future
+ */
+const DEBOUNCE_TIME = typeof window !== 'undefined' && (window as any)?.Cypress ? 500 : 100;
+
 export const useUnseenCount = ({ onSuccess, ...restOptions }: UseQueryOptions<ICountData, Error, ICountData> = {}) => {
   const { apiService, socket, isSessionInitialized, fetchingStrategy } = useNovuContext();
   const queryClient = useQueryClient();
@@ -21,26 +30,20 @@ export const useUnseenCount = ({ onSuccess, ...restOptions }: UseQueryOptions<IC
 
     socket.on(
       'unseen_count_changed',
-      debounce(
-        (data?: { unseenCount: number }) => {
-          if (Number.isInteger(data?.unseenCount)) {
-            queryClient.setQueryData<{ count: number }>(UNSEEN_COUNT_QUERY_KEY, (oldData) => ({
-              count: data?.unseenCount ?? oldData.count,
-            }));
-            queryClient.refetchQueries(INFINITE_NOTIFICATIONS_QUERY_KEY, {
-              exact: false,
-            });
-            queryClient.refetchQueries(FEED_UNSEEN_COUNT_QUERY_KEY, {
-              exact: false,
-            });
-            dispatchUnseenCountEvent(data.unseenCount);
-          }
-        },
-        300,
-        {
-          leading: true,
+      debounce((data?: { unseenCount: number }) => {
+        if (Number.isInteger(data?.unseenCount)) {
+          queryClient.setQueryData<{ count: number }>(UNSEEN_COUNT_QUERY_KEY, (oldData) => ({
+            count: data?.unseenCount ?? oldData.count,
+          }));
+          queryClient.refetchQueries(INFINITE_NOTIFICATIONS_QUERY_KEY, {
+            exact: false,
+          });
+          queryClient.refetchQueries(FEED_UNSEEN_COUNT_QUERY_KEY, {
+            exact: false,
+          });
+          dispatchUnseenCountEvent(data.unseenCount);
         }
-      )
+      }, DEBOUNCE_TIME)
     );
 
     return () => {
