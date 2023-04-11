e01e89b17836f636eb7d835e9f3e2248988e1d23
p-fernandez <pablo.fernandez.otero@gmail.com>
2023-04-11 17:24:50 +0100

    fix(cluster): add `node error` listener

--- apps/api/src/app/shared/services/in-memory-provider/in-memory-provider.service.ts
+++ apps/api/src/app/shared/services/in-memory-provider/in-memory-provider.service.ts
@@ -132,6 +132,10 @@ export class InMemoryProviderService {
         Logger.warn('In-memory cluster end', LOG_CONTEXT);
       });
 
+      inMemoryProviderClient.on('node error', (error) => {
+        Logger.error(error, LOG_CONTEXT);
+      });
+
       inMemoryProviderClient.on('error', (error) => {
         Logger.error(error, LOG_CONTEXT);
       });
