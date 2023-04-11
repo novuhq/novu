239b866b51fad24fe97e75c93353a71ca55e6dd0
Gosha <djabarov.george@gmail.com>
2023-04-11 10:46:41 +0300

    feat: refactor notification index

--- libs/dal/src/repositories/notification/notification.schema.ts
+++ libs/dal/src/repositories/notification/notification.schema.ts
@@ -82,6 +82,78 @@ notificationSchema.virtual('jobs', {
   foreignField: '_notificationId',
 });
 
+/*
+ *
+ * Path: libs/dal/src/repositories/notification/notification.repository.ts
+ *    Context: findBySubscriberId()
+ *        Query: find({_environmentId: environmentId,
+ *                    _subscriberId: subscriberId,});
+ *
+ */
+notificationSchema.index({
+  _subscriberId: 1,
+  _environmentId: 1,
+});
+
+/*
+ * Path: libs/dal/src/repositories/notification/notification.repository.ts
+ *    Context: getFeed()
+ *        Query: find({
+ *               transactionId: subscriberId,
+ *               _environmentId: environmentId,
+ *               _templateId = {$in: query.templates};
+ *               _subscriberId = {$in: query._subscriberIds};
+ *               channels = {$in: query.channels};
+ *              .sort('-createdAt')});
+ *
+ * Path: libs/dal/src/repositories/notification/notification.repository.ts
+ *     Context: getFeed()
+ *         Query: MongooseModel.countDocuments({
+ *                 transactionId: subscriberId,
+ *                 _environmentId: environmentId,
+ *                 _templateId = {$in: query.templates};
+ *                 _subscriberId = {$in: query._subscriberIds};
+ *                 channels = {$in: query.channels}});
+ *
+ *  * Path: libs/dal/src/repositories/notification/notification.repository.ts
+ *     Context: getFeed()
+ *         Query: MongooseModel.countDocuments({
+ *                 transactionId: subscriberId,
+ *                 _environmentId: environmentId,
+ *                 _templateId = {$in: query.templates};
+ *                 _subscriberId = {$in: query._subscriberIds};
+ *                 channels = {$in: query.channels}});
+ */
+notificationSchema.index({
+  transactionId: 1,
+  _environmentId: 1,
+  _subscriberId: 1,
+  _templateId: 1,
+  channels: 1,
+  createdAt: -1,
+});
+
+/*
+ *
+ * Path: libs/dal/src/repositories/notification/notification.repository.ts
+ *    Context: getActivityGraphStats()
+ *        Query: aggregate(
+ *                {createdAt: { $gte: date }_environmentId: new Types.ObjectId(environmentId),
+ *                { $sort: { _id: -1 } }})
+ *
+ * Path: libs/dal/src/repositories/notification/notification.repository.ts
+ *    Context: getStats()
+ *        Query: aggregate({
+ *           _environmentId: this.convertStringToObjectId(environmentId),
+ *           createdAt: {$gte: monthBefore}
+ *           weekly: { $sum: { $cond: [{ $gte: ['$createdAt', weekBefore] }, 1, 0] } },
+ */
+notificationSchema.index({
+  _environmentId: 1,
+  createdAt: 1,
+  _id: -1,
+});
+
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const Notification =
   (mongoose.models.Notification as mongoose.Model<NotificationDBModel>) ||
