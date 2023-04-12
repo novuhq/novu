b88c7be931198900d7b3670b6659a0783e0667a1
David Söderberg <granskog_@hotmail.com>
2023-04-12 10:36:23 +0200

    fix: so sidebar is hidden in prod env

--- apps/web/src/pages/templates/workflow/SideBar/Sidebar.tsx
+++ apps/web/src/pages/templates/workflow/SideBar/Sidebar.tsx
@@ -12,6 +12,8 @@ import { IForm } from '../../components/formTypes';
 import { useDidUpdate, useTimeout } from '@mantine/hooks';
 import { useState } from 'react';
 import { UpdateButton } from '../../components/UpdateButton';
+import { useEnvController } from '../../../../hooks';
+import { When } from '../../../../components/utils/When';
 
 export const Sidebar = () => {
   const { colorScheme } = useMantineColorScheme();
@@ -20,6 +22,7 @@ export const Sidebar = () => {
     event.dataTransfer.effectAllowed = 'move';
   };
   const { setDragging }: any = useOutletContext();
+  const { readonly } = useEnvController();
   const navigate = useNavigate();
   const basePath = useBasePath();
   const {
@@ -65,17 +68,29 @@ export const Sidebar = () => {
           </Group>
         </Stack>
       </Container>
-      <div style={{ position: 'relative', padding: '12px', paddingLeft: 0 }}>
-        <SideBarWrapper dark={colorScheme === 'dark'}>
-          <AddStepMenu setDragging={setDragging} onDragStart={onDragStart} />
-        </SideBarWrapper>
-        <Background
-          size={1}
-          gap={10}
-          variant={BackgroundVariant.Dots}
-          color={colorScheme === 'dark' ? colors.BGDark : colors.BGLight}
-        />
-      </div>
+      <When truthy={!readonly}>
+        <div style={{ position: 'relative', padding: '12px', paddingLeft: 0 }}>
+          <SideBarWrapper dark={colorScheme === 'dark'}>
+            <AddStepMenu setDragging={setDragging} onDragStart={onDragStart} />
+          </SideBarWrapper>
+          <Background
+            size={1}
+            gap={10}
+            variant={BackgroundVariant.Dots}
+            color={colorScheme === 'dark' ? colors.BGDark : colors.BGLight}
+          />
+        </div>
+      </When>
+      <When truthy={readonly}>
+        <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+          <Background
+            size={1}
+            gap={10}
+            variant={BackgroundVariant.Dots}
+            color={colorScheme === 'dark' ? colors.BGDark : colors.BGLight}
+          />
+        </div>
+      </When>
     </>
   );
 };
