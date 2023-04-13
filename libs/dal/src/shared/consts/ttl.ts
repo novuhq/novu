47b5a7bf7ec09b0cf2badc1c4968859d43994f85
ainouzgali <ainouzgali@gmail.com>
2023-04-13 16:47:28 +0300

    feat: add ttl only if not managed and disabled by env param

--- libs/dal/src/shared/consts/ttl.ts
+++ libs/dal/src/shared/consts/ttl.ts
@@ -1,1 +1,9 @@
 export const TTL_EXPIRE_AFTER_AMOUNT = '48h';
+
+export const TTL_INDEX_DISABLED = process.env.NOVU_MANAGED_SERVICE === 'true' || process.env.DISABLE_TTL === 'true';
+
+export function getTTLOptions() {
+  if (!TTL_INDEX_DISABLED) {
+    return { expires: TTL_EXPIRE_AFTER_AMOUNT };
+  }
+}
