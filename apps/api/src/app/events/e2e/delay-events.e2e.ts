7e532b1e15ffffbacdd9a78f0fb9295d054e7d18
ainouzgali <ainouzgali@gmail.com>
2023-04-16 18:38:09 +0300

    test: fix to seconds change in test

--- apps/api/src/app/events/e2e/delay-events.e2e.ts
+++ apps/api/src/app/events/e2e/delay-events.e2e.ts
@@ -94,7 +94,7 @@ describe('Trigger event - Delay triggered events - /v1/events/trigger (POST)', f
     const subExpireMonths = subMonths(expireAt, 1);
     const diff = differenceInMilliseconds(subExpireMonths, createdAt);
 
-    expect(diff).to.approximately(300000, 100);
+    expect(diff).to.approximately(100, 100);
 
     const messages = await messageRepository.find({
       _environmentId: session.environment._id,
