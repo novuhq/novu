87b59314f81314c568faabbc7978ab711e443755
Pawan Jain <jainpawan211199@gmail.com>
2023-04-18 15:19:31 +0530

    fix: remove example folder

--- examples/vue-notification-center-example/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import NotificationCenterPlugin from '@novu/notification-center-vue';
-import '@novu/notification-center-vue/dist/style.css';
-
-import App from './App.vue';
-
-import './assets/main.css';
-
-createApp(App).use(NotificationCenterPlugin).mount('#app');
