75836bd8f5bee0fe4188af36727a504b6157bccc
Dima Grossman <dima@grossman.io>
2023-04-20 09:37:59 +0300

    feat: speed up eslint parser timing

--- packages/nest/.eslintrc.js
+++ packages/nest/.eslintrc.js
@@ -1,13 +1,9 @@
 module.exports = {
   extends: ['../../.eslintrc.js'],
-  rules: {
-    'func-names': 'off',
-  },
   parserOptions: {
     project: './tsconfig.json',
     ecmaVersion: 2020,
     sourceType: 'module',
     tsconfigRootDir: __dirname,
-  },
-  ignorePatterns: '*.spec.ts',
+  }
 };
