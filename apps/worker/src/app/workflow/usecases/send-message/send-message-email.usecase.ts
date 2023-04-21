a99aba6d13f682cee675b1d9b5b63724bb1de111
Dima Grossman <dima@grossman.io>
2023-04-21 13:50:15 +0300

    fix: subject tests

--- apps/worker/src/app/workflow/usecases/send-message/send-message-email.usecase.ts
+++ apps/worker/src/app/workflow/usecases/send-message/send-message-email.usecase.ts
@@ -202,6 +202,7 @@ export class SendMessageEmail extends SendMessageBase {
           },
           {
             $set: {
+              subject,
               content,
             },
           }
