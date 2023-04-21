cc3e2f0f3521d80a08222b40759b35ec2bfb81a8
Biswajeet Das <biswajeetdas18@gmail.com>
2023-04-21 21:19:09 +0530

    feat: framework selector screen

--- apps/web/src/pages/quick-start/steps/FrameworkSetup.tsx
+++ apps/web/src/pages/quick-start/steps/FrameworkSetup.tsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
-import { Stack } from '@mantine/core';
+import { Group, Stack } from '@mantine/core';
 
 import { QuickStartWrapper } from '../components/QuickStartWrapper';
-import { OnBoardingAnalyticsEnum, welcomeDescription } from '../consts';
+import { OnBoardingAnalyticsEnum, frameworkSetupTitle } from '../consts';
 import { Cards } from '../../../design-system';
 import { useSegment } from '../../../components/providers/SegmentProvider';
 import { ROUTES } from '../../../constants/routes.enum';
@@ -15,41 +15,35 @@ export function FrameworkSetup() {
   }, []);
 
   return (
-    <QuickStartWrapper
-      title={welcomeDescription}
-      secondaryTitle={<ImplementationDescription />}
-      faq={true}
-      goBackPath={ROUTES.QUICK_START_NOTIFICATION_CENTER}
-    >
-      <Cards
-        cells={[
-          {
-            imagePath: `/static/images/frameworks/react.png`,
-            navigateTo: '/quickstart/notification-center/set-up/react',
-          },
-          {
-            imagePath: `/static/images/frameworks/angular.png`,
-            navigateTo: '/quickstart/notification-center/set-up/angular',
-          },
-          {
-            imagePath: `/static/images/frameworks/vue.png`,
-            navigateTo: '/quickstart/notification-center/set-up/vue',
-          },
-          {
-            imagePath: `/static/images/frameworks/js.png`,
-            navigateTo: '/quickstart/notification-center/set-up/js',
-          },
-        ]}
-      />
+    <QuickStartWrapper title={frameworkSetupTitle} goBackPath={ROUTES.QUICK_START_NOTIFICATION_CENTER}>
+      <Stack>
+        <Cards
+          cells={[
+            {
+              imagePath: `/static/images/frameworks/react.png`,
+              navigateTo: '/quickstart/notification-center/set-up/react',
+            },
+            {
+              imagePath: `/static/images/frameworks/angular.png`,
+              navigateTo: '/quickstart/notification-center/set-up/angular',
+            },
+            {
+              imagePath: `/static/images/frameworks/vue.png`,
+              navigateTo: '/quickstart/notification-center/set-up/vue',
+            },
+          ]}
+        />
+        <Group position="center">
+          <Cards
+            cells={[
+              {
+                imagePath: `/static/images/frameworks/js.png`,
+                navigateTo: '/quickstart/notification-center/set-up/js',
+              },
+            ]}
+          />
+        </Group>
+      </Stack>
     </QuickStartWrapper>
-  );
-}
-
-export function ImplementationDescription() {
-  return (
-    <Stack align="center" sx={{ gap: '20px' }}>
-      <span>A fully functional notification center is now at your fingertips.</span>
-      <span>What's your go-to frontend framework?</span>
-    </Stack>
   );
 }
