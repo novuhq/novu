1d722f9341fa089492fae705b397e8f298d44705
ainouzgali <ainouzgali@gmail.com>
2023-04-25 14:59:02 +0300

    feat: migration script for expire at

--- apps/api/migrations/expire-at/expire-at.migration.spec.ts
+++ apps/api/migrations/expire-at/expire-at.migration.spec.ts
@@ -9,9 +9,8 @@ import {
 } from '@novu/dal';
 import { UserSession } from '@novu/testing';
 import { sendTrigger } from '../../src/app/events/e2e/trigger-event.e2e';
-import { DelayTypeEnum, DigestUnitEnum, JobStatusEnum, StepTypeEnum } from '@novu/shared';
-// import { inAppAsSeen, notInAppAsUnseen, seenToRead } from './seen-read-support.migration';
-import { createExpireAt, messagesExpireAt } from './expire-at.migration';
+import { StepTypeEnum } from '@novu/shared';
+import { createExpireAt, messagesSetExpireAt } from './expire-at.migration';
 
 describe('Create expireAt - TTL support', function () {
   const messageRepository = new MessageRepository();
@@ -21,161 +20,71 @@ describe('Create expireAt - TTL support', function () {
 
   let session: UserSession;
   let template: NotificationTemplateEntity;
-  let template2: NotificationTemplateEntity;
+  let query;
 
   beforeEach(async () => {
     session = new UserSession();
     await session.initialize();
     template = await createTemplate(session);
-    template2 = await createTemplate2(session);
+    query = {
+      _environmentId: session.environment._id,
+      _organizationId: session.organization._id,
+      expireAt: { $exists: false },
+    };
 
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 5; i++) {
       const newSubscriberIdInAppNotification = SubscriberRepository.createObjectId();
-      await sendTrigger(session, template, newSubscriberIdInAppNotification, { custVar: '1' });
-      await sendTrigger(session, template2, newSubscriberIdInAppNotification, { custVar: '1' });
-      // await new Promise((r) => setTimeout(r, 1000));
-      // await sendTrigger(session, template, newSubscriberIdInAppNotification, { custVar: '2' });
+      await sendTrigger(session, template, newSubscriberIdInAppNotification);
     }
     await new Promise((r) => setTimeout(r, 1000));
-    console.log(session.environment.name);
+
     await messageRepository.update({ _environmentId: session.environment._id }, { $unset: { expireAt: 1 } });
     await notificationRepository.update({ _environmentId: session.environment._id }, { $unset: { expireAt: 1 } });
     await jobRepository.update({ _environmentId: session.environment._id }, { $unset: { expireAt: 1 } });
+    await executionDetailsRepository.update({ _environmentId: session.environment._id }, { $unset: { expireAt: 1 } });
 
-    // for (let i = 0; i < 1; i++) {
-    //   const newSubscriberIdInAppNotification = SubscriberRepository.createObjectId();
-    //   await sendTrigger(session, template, newSubscriberIdInAppNotification);
-    //   await sendTrigger(session, template, newSubscriberIdInAppNotification);
-    // }
+    for (let i = 0; i < 5; i++) {
+      const newSubscriberIdInAppNotification = SubscriberRepository.createObjectId();
+      await sendTrigger(session, template, newSubscriberIdInAppNotification);
+    }
     await new Promise((r) => setTimeout(r, 1000));
   });
 
-  it('should update all seen to read', async function () {
-    // console.log(session.environment._id);
-    const messagesBefore = await messageRepository.find({
-      _environmentId: session.environment._id,
-      _organizationId: session.organization._id,
-    });
-    console.log('bef len', messagesBefore.length);
-    // console.log('bef', messagesBefore);
+  it('should set expireAt for messages', async function () {
+    await messagesSetExpireAt(query);
 
-    // await session.awaitRunningJobs(template?._id, false, 0);
-
-    await createExpireAt();
-    console.log('---------------------------------------------');
     const messages = await messageRepository.find({ _environmentId: session.environment._id });
-    console.log('aft len', messages.length);
-    // console.log('aft', messages);
+
     messages.forEach((msg) => {
       expect(msg.expireAt).to.exist;
     });
   });
-  it('should notifications all seen to read', async function () {
-    // console.log(session.environment._id);
-    const messagesBefore = await notificationRepository.find({
-      _environmentId: session.environment._id,
-      _organizationId: session.organization._id,
-    });
-    console.log('bef len', messagesBefore.length);
-    // console.log('bef', messagesBefore);
 
-    // await session.awaitRunningJobs(template?._id, false, 0);
-
+  it('should set expireAt for notification and its jobs and execution details', async function () {
     await createExpireAt();
-    console.log('---------------------------------------------');
-    const messages = await notificationRepository.find({ _environmentId: session.environment._id });
-    console.log('aft len', messages.length);
-    // console.log('aft', messages);
-    messages.forEach((msg) => {
+    const notifications = await notificationRepository.find({ _environmentId: session.environment._id });
+    const jobs = await jobRepository.find({ _environmentId: session.environment._id });
+    const executionDetails = await executionDetailsRepository.find({ _environmentId: session.environment._id });
+
+    notifications.forEach((msg) => {
+      expect(msg.expireAt).to.exist;
+    });
+    jobs.forEach((msg) => {
+      expect(msg.expireAt).to.exist;
+    });
+    executionDetails.forEach((msg) => {
       expect(msg.expireAt).to.exist;
     });
   });
-
-  it('should digest', async function () {
-    await session.awaitRunningJobs(template?._id, false, 1);
-
-    await createExpireAt();
-    console.log('---------------------------------------------');
-    const jobs = await jobRepository.find({
-      _environmentId: session.environment._id,
-      // status: { $ne: JobStatusEnum.COMPLETED },
-    });
-    console.log('aft len', jobs.length);
-    console.log('aft', jobs);
-    const messages = await messageRepository.find({
-      _environmentId: session.environment._id,
-      // status: { $ne: JobStatusEnum.COMPLETED },
-    });
-    console.log('aft len', messages.length);
-    console.log('aft', messages);
-    messages.forEach((msg) => {
-      expect(msg.expireAt).to.exist;
-    });
-  });
-
-  // it('should add not in app seen as false', async function () {
-  //   // await seenToRead();
-  //
-  //   // await inAppAsSeen();
-  //
-  //   // await notInAppAsUnseen();
-  //
-  //   const messages = await messageRepository.find({ channel: { $ne: 'in_app' } });
-  //
-  //   messages.forEach((msg) => {
-  //     expect(msg.seen).to.equal(false);
-  //   });
-  // });
 });
 
 async function createTemplate(session) {
   return await session.createTemplate({
     steps: [
-      // {
-      //   type: StepTypeEnum.SMS,
-      //   content: 'Welcome to {{organizationName}}' as string,
-      // },
       {
-        type: StepTypeEnum.DELAY,
-        content: '',
-        metadata: {
-          unit: DigestUnitEnum.MINUTES,
-          amount: 2,
-          type: DelayTypeEnum.REGULAR,
-        },
+        type: StepTypeEnum.SMS,
+        content: 'Welcome to {{organizationName}}' as string,
       },
-      {
-        type: StepTypeEnum.DIGEST,
-        content: '',
-        metadata: {
-          unit: DigestUnitEnum.SECONDS,
-          amount: 5,
-          type: DelayTypeEnum.REGULAR,
-        },
-      },
-      {
-        type: StepTypeEnum.IN_APP,
-        content: 'Hello {{step.events.length}}, Welcome to {{organizationName}}' as string,
-      },
-      // {
-      //   type: StepTypeEnum.EMAIL,
-      //   content: [
-      //     {
-      //       type: 'text',
-      //       content: 'Hello {{subscriber.lastName}}, Welcome to {{organizationName}}' as string,
-      //     },
-      //   ],
-      // },
-    ],
-  });
-}
-async function createTemplate2(session) {
-  return await session.createTemplate({
-    steps: [
-      // {
-      //   type: StepTypeEnum.SMS,
-      //   content: 'Welcome to {{organizationName}}' as string,
-      // },
       {
         type: StepTypeEnum.IN_APP,
         content: 'Hello {{subscriber.lastName}}, Welcome to {{organizationName}}' as string,
--- apps/api/migrations/expire-at/expire-at.migration.ts
+++ apps/api/migrations/expire-at/expire-at.migration.ts
@@ -7,9 +7,7 @@ import {
   EnvironmentRepository,
   JobStatusEnum,
 } from '@novu/dal';
-import { addMilliseconds, addMonths } from 'date-fns';
-import { StepTypeEnum } from '@novu/shared';
-import { CalculateDelayService } from '@novu/application-generic';
+import { addMonths } from 'date-fns';
 
 const messageRepository = new MessageRepository();
 const notificationRepository = new NotificationRepository();
@@ -20,7 +18,6 @@ const environmentRepository = new EnvironmentRepository();
 const now = Date.now();
 const expireAtOneMonth = addMonths(now, 1);
 const expireAtSixMonths = addMonths(now, 6);
-const calculateDelayService = new CalculateDelayService();
 
 export async function createExpireAt() {
   // eslint-disable-next-line no-console
@@ -33,32 +30,21 @@ export async function createExpireAt() {
   for (const organization of organizations) {
     const environments = await environmentRepository.findOrganizationEnvironments(organization._id);
     for (const environment of environments) {
-      console.log(environment.name);
       const query = {
         _organizationId: organization._id,
         _environmentId: environment._id,
         expireAt: { $exists: false },
       };
-      await messagesExpireAt(query);
+      await messagesSetExpireAt(query);
       await notificationExpireAt(query);
     }
   }
 
   // eslint-disable-next-line no-console
-  console.log('add in_app messages as seen');
-
-  await inAppAsSeen();
-
-  // eslint-disable-next-line no-console
-  console.log('add not in_app messages as unseen (due the missing feature seen/unseen on other channels)');
-
-  await notInAppAsUnseen();
-
-  // eslint-disable-next-line no-console
   console.log('end migration');
 }
 
-export async function messagesExpireAt(query) {
+export async function messagesSetExpireAt(query) {
   await messageRepository.update(
     {
       ...query,
@@ -66,6 +52,7 @@ export async function messagesExpireAt(query) {
     },
     { $set: { expireAt: expireAtOneMonth } }
   );
+
   await messageRepository.update(
     {
       ...query,
@@ -74,146 +61,34 @@ export async function messagesExpireAt(query) {
     { $set: { expireAt: expireAtSixMonths } }
   );
 }
-// const notifications = notificationRepository.find({ expireAt: { $exists: false } });
 
-// await notificationRepository.update({ expireAt: { $exists: false } }, { $set: { expireAt: expireAt } });
-function getCount() {
-  return 1;
-}
 export async function notificationExpireAt(query) {
-  // const notification = await notificationRepository.findOne(query);
+  const excludedIds = await getExcludedNotificationIds(query);
 
-  /*
-   * const pop = await notificationRepository._model.findOne(query).populate({
-   *   path: 'jobs',
-   *   match: { type: { $in: [StepTypeEnum.DELAY, StepTypeEnum.DIGEST] }, status: JobStatusEnum.DELAYED },
-   * });
-   * console.log(notification);
-   */
+  await notificationRepository.update(
+    { ...query, _id: { $nin: excludedIds } },
+    { $set: { expireAt: expireAtOneMonth } }
+  );
+  await jobRepository.update(
+    { ...query, _notificationId: { $nin: excludedIds } },
+    { $set: { expireAt: expireAtOneMonth } }
+  );
+  await executionDetailsRepository.update(
+    { ...query, _notificationId: { $nin: excludedIds } },
+    { $set: { expireAt: expireAtOneMonth } }
+  );
+}
 
-  // console.log(pop);
-
-  /*
-   * const jobsnnn = await jobRepository.find(
-   *   {
-   *     ...query,
-   *     type: { $in: [StepTypeEnum.DELAY, StepTypeEnum.DIGEST] },
-   *     status: { $in: [JobStatusEnum.DELAYED, JobStatusEnum.PENDING] },
-   *   },
-   *   function (err, job) {
-   *     if (err) {
-   *       console.log('lll');
-   *     }
-   *     console.log('kk', job._id);
-   *   }
-   * );
-   * console.log(jobsnnn);
-   */
-  const jobs = await jobRepository.find({
+export async function getExcludedNotificationIds(query) {
+  const pendingNotifications = await jobRepository._model.distinct('_notificationId', {
     ...query,
-    type: { $in: [StepTypeEnum.DELAY, StepTypeEnum.DIGEST] },
-    status: { $in: [JobStatusEnum.DELAYED, JobStatusEnum.PENDING] },
+    status: JobStatusEnum.PENDING,
   });
-  console.log(jobs);
 
-  /*
-   * if (job) {
-   *   // console.log(calculateDelayService.calculateDelay(job.step, job.payload, job.overrides));
-   * }
-   */
-
-  const acc = jobs.map((job) => {
-    return {
-      notificationId: job._notificationId,
-      delay: calculateDelayService.calculateDelay(job.step, job.payload, job.overrides),
-    };
+  // digested events stays pending, leaving the notification and deleting the actually digested could cause errors
+  return await notificationRepository._model.distinct('_id', {
+    ...query,
+    _id: { $in: pendingNotifications },
+    _digestedNotificationId: { $exists: false, $eq: null },
   });
-  console.log(acc);
-  const red = acc.reduce<{ notificationId: string; delay: number }[]>((res, rec) => {
-    const found = res.findIndex((temp) => temp?.notificationId === rec.notificationId);
-    console.log(found);
-    if (found >= 0) {
-      res[found] = { notificationId: rec.notificationId, delay: res[found].delay + rec.delay };
-      console.log(res);
-
-      return res;
-    }
-
-    return [...res, rec];
-  }, []);
-
-  console.log('red', red);
-
-  const red2 = red.reduce((res, rec) => {
-    return { ...res, [rec.notificationId]: rec.delay };
-  }, {});
-
-  console.log('2', red2);
-
-  // console.log(acc);
-  const count = await jobRepository.aggregate([
-    {
-      $match: {
-        expireAt: { $exists: false },
-        type: { $in: [StepTypeEnum.DELAY, StepTypeEnum.DIGEST] },
-        status: { $in: [JobStatusEnum.DELAYED, JobStatusEnum.PENDING] },
-        // status: JobStatusEnum.DELAYED,
-      },
-    },
-    /*
-     * {
-     *   $group: {
-     *     _id: '$_notificationId',
-     *     // step: '$step',
-     *
-     *     // delay: { $sum: calculateDelayService.calculateDelay('$digest', '$payload', '$overrides') },
-     *   },
-     * },
-     */
-    { $group: { _id: '$_notificationId', delay: { $sum: '$digest.amount' } } },
-  ]);
-
-  // console.log(count);
-  const agg = await jobRepository.aggregate([
-    { $match: { ...query, type: { $in: [StepTypeEnum.DELAY, StepTypeEnum.DIGEST] }, status: JobStatusEnum.DELAYED } },
-    { $group: { _id: null, delay: { $sum: 1 } } },
-  ]);
-
-  console.log('aggggg', count);
-
-  const arr = red.map((noti) => noti.notificationId);
-
-  // await notificationRepository.update({ ...query, _id: { $in: arr } }, { $set: { expireAt: addMilliseconds(now) } });
-  await notificationRepository.update({ ...query, _id: { $in: arr } }, { $set: { expireAt: expireAtOneMonth } });
-
-  await jobRepository.update(query, { $set: { expireAt: expireAtOneMonth } });
-  await executionDetailsRepository.update(query, { $set: { expireAt: expireAtOneMonth } });
-}
-
-// const notifications = notificationRepository.find({ expireAt: { $exists: false } });
-
-// await notificationRepository.update({ expireAt: { $exists: false } }, { $set: { expireAt: expireAt } });
-
-export async function inAppAsSeen() {
-  /*
-   * await messageRepository.update(
-   *   {
-   *     channel: 'in_app',
-   *     seen: { $exists: false },
-   *   },
-   *   { $set: { seen: true } }
-   * );
-   */
-}
-
-export async function notInAppAsUnseen() {
-  /*
-   * await messageRepository.update(
-   *   {
-   *     channel: { $ne: 'in_app' },
-   *     seen: { $exists: false },
-   *   },
-   *   { $set: { seen: false } }
-   * );
-   */
 }
