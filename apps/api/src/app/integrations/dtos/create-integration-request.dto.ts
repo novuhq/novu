c534495ffc695529d3229d78729635757b9d9ad3
Michał Dziuba <mail@michaldziuba.dev>
2023-04-28 17:46:24 +0200

    fix(api): add more strict validation for integrations

--- apps/api/src/app/integrations/dtos/create-integration-request.dto.ts
+++ apps/api/src/app/integrations/dtos/create-integration-request.dto.ts
@@ -1,4 +1,4 @@
-import { IsDefined } from 'class-validator';
+import { IsBoolean, IsDefined, IsEnum, IsString, ValidateNested } from 'class-validator';
 import { ChannelTypeEnum, ICreateIntegrationBodyDto } from '@novu/shared';
 import { CredentialsDto } from './credentials.dto';
 import { ApiProperty } from '@nestjs/swagger';
@@ -6,25 +6,30 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CreateIntegrationRequestDto implements ICreateIntegrationBodyDto {
   @ApiProperty()
   @IsDefined()
+  @IsString()
   providerId: string;
 
   @ApiProperty({
     enum: ChannelTypeEnum,
   })
   @IsDefined()
+  @IsEnum(ChannelTypeEnum)
   channel: ChannelTypeEnum;
 
   @ApiProperty({
     type: CredentialsDto,
   })
   @IsDefined()
+  @ValidateNested()
   credentials: CredentialsDto;
 
   @ApiProperty()
   @IsDefined()
+  @IsBoolean()
   active: boolean;
 
   @ApiProperty()
   @IsDefined()
+  @IsBoolean()
   check: boolean;
 }
--- apps/api/src/app/integrations/dtos/credentials.dto.ts
+++ apps/api/src/app/integrations/dtos/credentials.dto.ts
@@ -1,44 +1,84 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsBoolean, IsObject, IsString } from 'class-validator';
 
 export class CredentialsDto {
   @ApiPropertyOptional()
+  @IsString()
   apiKey?: string;
+
   @ApiPropertyOptional()
+  @IsString()
   user?: string;
+
   @ApiPropertyOptional()
+  @IsString()
   secretKey?: string;
+
   @ApiPropertyOptional()
+  @IsString()
   domain?: string;
+
   @ApiPropertyOptional()
+  @IsString()
   password?: string;
+
   @ApiPropertyOptional()
+  @IsString()
   host?: string;
+
   @ApiPropertyOptional()
+  @IsString()
   port?: string;
+
   @ApiPropertyOptional()
+  @IsBoolean()
   secure?: boolean;
+
   @ApiPropertyOptional()
+  @IsString()
   region?: string;
+
   @ApiPropertyOptional()
+  @IsString()
   accountSid?: string;
+
   @ApiPropertyOptional()
+  @IsString()
   messageProfileId?: string;
+
   @ApiPropertyOptional()
+  @IsString()
   token?: string;
+
   @ApiPropertyOptional()
+  @IsString()
   from?: string;
+
   @ApiPropertyOptional()
+  @IsString()
   senderName?: string;
+
   @ApiPropertyOptional()
+  @IsString()
   projectName?: string;
+
   @ApiPropertyOptional()
+  @IsString()
   applicationId?: string;
+
   @ApiPropertyOptional()
+  @IsString()
   clientId?: string;
+
   @ApiPropertyOptional()
+  @IsBoolean()
   requireTls?: boolean;
+
   @ApiPropertyOptional()
+  @IsBoolean()
   ignoreTls?: boolean;
+
   @ApiPropertyOptional()
+  @IsObject()
   tlsOptions?: Record<string, unknown>;
 }
