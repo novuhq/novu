db4682982185d07074c8e30b7af48dcc87fb302f
Michał Dziuba <mail@michaldziuba.dev>
2023-04-30 21:26:24 +0200

    fix: add min check for limit

--- apps/api/src/app/change/dtos/change-request.dto.ts
+++ apps/api/src/app/change/dtos/change-request.dto.ts
@@ -1,29 +1,8 @@
-import { ApiPropertyOptional, ApiProperty } from '@nestjs/swagger';
-import { IsDefined, IsInt, IsOptional, IsString, Min, Max } from 'class-validator';
-import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDefined, IsString } from 'class-validator';
+import { PaginationRequestDto } from '../../shared/dtos/pagination-request';
 
-export class ChangesRequestDto {
-  @ApiPropertyOptional({
-    type: Number,
-    required: false,
-  })
-  @IsOptional()
-  @Type(() => Number)
-  @IsInt()
-  page?: number = 0;
-
-  @ApiPropertyOptional({
-    type: Number,
-    required: false,
-    default: 10,
-    maximum: 100,
-  })
-  @IsOptional()
-  @Type(() => Number)
-  @IsInt()
-  @Max(100)
-  limit?: number = 10;
-
+export class ChangesRequestDto extends PaginationRequestDto(10, 100) {
   @ApiProperty({
     type: String,
     required: true,
--- apps/api/src/app/notification-template/dto/notification-templates-request.dto.ts
+++ apps/api/src/app/notification-template/dto/notification-templates-request.dto.ts
@@ -1,24 +1,3 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
-import { IsInt, Max } from 'class-validator';
+import { PaginationRequestDto } from '../../shared/dtos/pagination-request';
 
-export class NotificationTemplatesRequestDto {
-  @ApiPropertyOptional({
-    type: Number,
-    required: false,
-  })
-  @Type(() => Number)
-  @IsInt()
-  page?: number = 0;
-
-  @ApiPropertyOptional({
-    type: Number,
-    required: false,
-    default: 10,
-    maximum: 100,
-  })
-  @Type(() => Number)
-  @IsInt()
-  @Max(100)
-  limit?: number = 10;
-}
+export class NotificationTemplatesRequestDto extends PaginationRequestDto(10, 100) {}
