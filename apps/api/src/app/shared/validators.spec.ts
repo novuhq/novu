d828b8a7e7e4440e1108f0e69ea3979ed247f885
Michał Dziuba <mail@michaldziuba.dev>
2023-05-03 14:25:28 +0200

    test: IsBooleanAny

---
+++ apps/api/src/app/shared/validators.spec.ts
@@ -0,0 +1,47 @@
+import { IsBooleanAny } from './validators';
+import { validateSync } from 'class-validator';
+import { expect } from 'chai';
+
+function isValid(input: object) {
+  const errors = validateSync(input);
+  return errors.length === 0;
+}
+
+describe('IsBooleanAny', () => {
+  class TestDto {
+    constructor(isSomething: any) {
+      this.isSomething = isSomething;
+    }
+
+    @IsBooleanAny()
+    isSomething: boolean;
+  }
+
+  it('should accept boolean value', () => {
+    const trueBool = new TestDto(true);
+    const falseBool = new TestDto(false);
+
+    expect(isValid(trueBool)).to.true;
+    expect(isValid(falseBool)).to.true;
+  });
+
+  it('should accept string boolean value', () => {
+    const trueBool = new TestDto('true');
+    const falseBool = new TestDto('false');
+
+    expect(isValid(trueBool)).to.true;
+    expect(isValid(falseBool)).to.true;
+  });
+
+  it('should NOT accept any other values', () => {
+    const withNumber = new TestDto(0);
+    const withAnyString = new TestDto('truezz');
+    const withObj = new TestDto({ true: 'true' });
+    const withNull = new TestDto(null);
+
+    expect(isValid(withNumber)).to.false;
+    expect(isValid(withAnyString)).to.false;
+    expect(isValid(withObj)).to.false;
+    expect(isValid(withNull)).to.false;
+  });
+});
