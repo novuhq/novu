4bf5f412d41cc0711405bf6907d04455c67edb75
p-fernandez <pablo.fernandez.otero@gmail.com>
2023-05-09 16:13:25 +0100

    feat(infra): add indicators to worker health

--- apps/worker/src/app/health/health.controller.ts
+++ apps/worker/src/app/health/health.controller.ts
@@ -1,18 +1,29 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthCheck, HealthCheckResult, HealthCheckService } from '@nestjs/terminus';
-import { DalServiceHealthIndicator } from '@novu/application-generic';
+import {
+  DalServiceHealthIndicator,
+  QueueServiceHealthIndicator,
+  TriggerQueueServiceHealthIndicator,
+} from '@novu/application-generic';
 
 import { version } from '../../../package.json';
 
 @Controller('health-check')
 export class HealthController {
-  constructor(private healthCheckService: HealthCheckService, private dalHealthIndicator: DalServiceHealthIndicator) {}
+  constructor(
+    private healthCheckService: HealthCheckService,
+    private dalHealthIndicator: DalServiceHealthIndicator,
+    private queueHealthIndicator: QueueServiceHealthIndicator,
+    private triggerQueueHealthIndicator: TriggerQueueServiceHealthIndicator
+  ) {}
 
   @Get()
   @HealthCheck()
   healthCheck(): Promise<HealthCheckResult> {
     return this.healthCheckService.check([
       () => this.dalHealthIndicator.isHealthy(),
+      () => this.queueHealthIndicator.isHealthy(),
+      () => this.triggerQueueHealthIndicator.isHealthy(),
       async () => {
         return {
           apiVersion: {
--- apps/ws/src/health/health.controller.ts
+++ apps/ws/src/health/health.controller.ts
@@ -1,18 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthCheck, HealthCheckResult, HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
-import { DalServiceHealthIndicator } from '@novu/application-generic';
+import { DalServiceHealthIndicator, WsQueueServiceHealthIndicator } from '@novu/application-generic';
 
 import { version } from '../../package.json';
 
 @Controller('v1/health-check')
 export class HealthController {
-  constructor(private healthCheckService: HealthCheckService, private dalHealthIndicator: DalServiceHealthIndicator) {}
+  constructor(
+    private healthCheckService: HealthCheckService,
+    private dalHealthIndicator: DalServiceHealthIndicator,
+    private wsQueueHealthIndicator: WsQueueServiceHealthIndicator
+  ) {}
 
   @Get()
   @HealthCheck()
   async healthCheck(): Promise<HealthCheckResult> {
     const result = await this.healthCheckService.check([
       () => this.dalHealthIndicator.isHealthy(),
+      () => this.wsQueueHealthIndicator.isHealthy(),
       async () => {
         return {
           apiVersion: {
