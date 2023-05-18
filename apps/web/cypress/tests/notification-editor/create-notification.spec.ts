6739cc8b700891a83bc73972e0fec288330c6953
Paweł <pawel.tymczuk@gmail.com>
2023-05-18 23:02:12 +0200

    chore(web): fixing failing e2e tests

--- apps/web/cypress/tests/notification-editor/create-notification.spec.ts
+++ apps/web/cypress/tests/notification-editor/create-notification.spec.ts
@@ -2,7 +2,7 @@ import { addAndEditChannel, clickWorkflow, dragAndDrop, editChannel, fillBasicNo
 
 describe('Creation functionality', function () {
   beforeEach(function () {
-    cy.initializeSession().as('session');
+    cy.initializeSession({ showOnBoardingTour: false }).as('session');
   });
 
   it('should create in-app notification', function () {
@@ -350,7 +350,7 @@ describe('Creation functionality', function () {
     cy.waitLoadTemplatePage(() => {
       cy.visit('/templates/create');
     });
-    cy.getByTestId('scratch-workflow-tooltip-skip-button').should('have.text', 'Watch later').click();
+
     fillBasicNotificationDetails('Test 15 Nodes');
     goBack();
     cy.waitForNetworkIdle(500);
