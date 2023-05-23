73fdd1e7af89268baa87852360ecaa403b78bf0a
David Söderberg <granskog_@hotmail.com>
2023-05-23 12:09:05 +0200

    feat: add guides to in app modal in integration store

--- apps/web/src/design-system/icons/index.ts
+++ apps/web/src/design-system/icons/index.ts
@@ -99,5 +99,10 @@ export { TurnOnGradient } from './gradient/TurnOnGradient';
 export { LevelsGradient } from './gradient/LevelsGradient';
 export { TimerGradient } from './gradient/TimerGradient';
 export { PageGradient } from './gradient/PageGradient';
+export { ReactGradient } from './gradient/ReactGradient';
+export { AngularGradient } from './gradient/AngularGradient';
+export { VueGradient } from './gradient/VueGradient';
+export { JsGradient } from './gradient/JsGradient';
+export { CodeGradient } from './gradient/CodeGradient';
 
 export { GitHub } from './social/GitHub';
--- apps/web/src/pages/quick-start/steps/Setup.tsx
+++ apps/web/src/pages/quick-start/steps/Setup.tsx
@@ -1,46 +1,17 @@
-import styled from '@emotion/styled';
-import { Stack, Timeline, useMantineColorScheme } from '@mantine/core';
-import { useQuery } from '@tanstack/react-query';
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
-import { getApiKeys } from '../../../api/environment';
-import { getInAppActivated } from '../../../api/integration';
 import { useSegment } from '../../../components/providers/SegmentProvider';
-import { When } from '../../../components/utils/When';
-import { API_ROOT, WS_URL } from '../../../config';
 import { ROUTES } from '../../../constants/routes.enum';
-import { colors, shadows, Text } from '../../../design-system';
-import { useEnvController } from '../../../hooks';
-import { PrismOnCopy } from '../../settings/tabs/components/Prism';
 import { QuickStartWrapper } from '../components/QuickStartWrapper';
-import { SetupStatus } from '../components/SetupStatus';
-import {
-  API_KEY,
-  APPLICATION_IDENTIFIER,
-  BACKEND_API_URL,
-  BACKEND_SOCKET_URL,
-  demoSetupSecondaryTitle,
-  frameworkInstructions,
-  OnBoardingAnalyticsEnum,
-} from '../consts';
+import { SetupTimeline } from '../components/SetupTimeline';
+import { demoSetupSecondaryTitle, OnBoardingAnalyticsEnum } from '../consts';
 
 export function Setup() {
+  const navigate = useNavigate();
   const segment = useSegment();
   const { framework } = useParams();
-  const { environment } = useEnvController();
-  const { data: apiKeys } = useQuery<{ key: string }[]>(['getApiKeys'], getApiKeys);
-  const apiKey = apiKeys?.length ? apiKeys[0].key : '';
-  const { colorScheme } = useMantineColorScheme();
-  const isDark = colorScheme === 'dark';
 
-  const { data: inAppData } = useQuery<IGetInAppActivatedResponse>(['inAppActive'], async () => getInAppActivated(), {
-    refetchInterval: (data) => stopIfInAppActive(data),
-    initialData: { active: false },
-  });
-
-  const instructions = frameworkInstructions.find((instruction) => instruction.key === framework)?.value ?? [];
-  const environmentIdentifier = environment?.identifier ? environment.identifier : '';
   const goBackRoute = framework === 'demo' ? ROUTES.QUICK_START_NOTIFICATION_CENTER : ROUTES.QUICK_START_SETUP;
   const secondaryTitle = framework === 'demo' ? demoSetupSecondaryTitle : '';
 
@@ -55,105 +26,20 @@ export function Setup() {
 
   return (
     <QuickStartWrapper faq={true} secondaryTitle={secondaryTitle} goBackPath={goBackRoute}>
-      <Stack align="center" sx={{ width: '100%' }}>
-        <TimelineWrapper isDark={isDark}>
-          <Timeline
-            active={instructions?.length + 1}
-            bulletSize={40}
-            lineWidth={2}
-            styles={{
-              itemBullet: {
-                backgroundColor: 'grey',
-              },
-            }}
-          >
-            {instructions.map((instruction, index) => {
-              return (
-                <Timeline.Item
-                  bullet={
-                    <div style={{}}>
-                      <Text>{index + 1}</Text>
-                    </div>
-                  }
-                  key={index}
-                  title={<div>{instruction.instruction}</div>}
-                >
-                  <div style={{ marginTop: 10 }}>
-                    <PrismOnCopy
-                      language={instruction.language}
-                      index={index}
-                      code={`${updateCodeSnippet(instruction.snippet, environmentIdentifier, apiKey)}   `}
-                      onCopy={handleOnCopy}
-                    />
-                  </div>
-                </Timeline.Item>
-              );
-            })}
-            <Timeline.Item
-              bullet={
-                <div style={{}}>
-                  <Text>{instructions?.length + 1}</Text>
-                </div>
-              }
-              title={'Render the components and run application'}
-            >
-              <LoaderWrapper>
-                <SetupStatus
-                  appInitialized={inAppData.active}
-                  navigatePath={`/quickstart/notification-center/set-up/${framework}/success`}
-                  framework={framework}
-                />
-              </LoaderWrapper>
-            </Timeline.Item>
-          </Timeline>
-        </TimelineWrapper>
-
-        <When truthy={framework === 'demo'}>{<OpenBrowser />}</When>
-      </Stack>
+      <SetupTimeline
+        framework={framework || 'react'}
+        onDone={() => {
+          navigate(`/quickstart/notification-center/set-up/${framework}/success`);
+        }}
+        onCopy={handleOnCopy}
+        onConfigureLater={() => {
+          navigate(ROUTES.TEMPLATES);
+          segment.track(OnBoardingAnalyticsEnum.CONFIGURE_LATER_CLICK, {
+            screen: 'framework instructions',
+            framework,
+          });
+        }}
+      />
     </QuickStartWrapper>
   );
 }
-
-const LoaderWrapper = styled.div`
-  margin-bottom: 20px;
-  margin-top: 10px;
-`;
-
-function updateCodeSnippet(codeSnippet: string, environmentIdentifier: string, apiKey: string) {
-  const concatUrls = process.env.REACT_APP_ENVIRONMENT !== 'production' || !!process.env.REACT_APP_DOCKER_HOSTED_ENV;
-
-  return codeSnippet
-    .replace(APPLICATION_IDENTIFIER, environmentIdentifier)
-    .replace(API_KEY, apiKey ?? '')
-    .replace(BACKEND_API_URL, concatUrls ? API_ROOT : '')
-    .replace(BACKEND_SOCKET_URL, concatUrls ? WS_URL : '');
-}
-
-export function OpenBrowser() {
-  return (
-    <span style={{ color: colors.B60 }}>
-      If your browser did not automatically open, go to localhost at http://localhost:3000
-    </span>
-  );
-}
-
-interface IGetInAppActivatedResponse {
-  active: boolean;
-}
-
-function stopIfInAppActive(data) {
-  return data?.active ? false : 3000;
-}
-
-const TimelineWrapper = styled.div<{ isDark: boolean }>`
-  width: 100%;
-  padding: 10px;
-  border-radius: 7px;
-  .mantine-Timeline-itemBullet {
-    background-color: ${({ isDark }) => (isDark ? colors.B30 : colors.BGLight)};
-    color: white;
-    font-size: 16px;
-    font-weight: bold;
-    box-shadow: ${({ isDark }) => (isDark ? shadows.dark : shadows.light)};
-  }
-`;
