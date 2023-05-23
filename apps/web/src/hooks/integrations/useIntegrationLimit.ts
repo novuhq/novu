632708a158cd708de279479ee54abc978a3ff7f3
Biswajeet Das <biswajeetdas18@gmail.com>
2023-05-23 17:34:33 +0530

    refactor: limitreached logic

--- apps/web/src/hooks/integrations/useIntegrationLimit.ts
+++ apps/web/src/hooks/integrations/useIntegrationLimit.ts
@@ -5,18 +5,15 @@ import { useMemo } from 'react';
 import { IS_DOCKER_HOSTED } from '../../config/index';
 import { useActiveIntegrations } from './useActiveIntegrations';
 
+const isLimitFetchingEnabled = !IS_DOCKER_HOSTED;
 export function useIntegrationLimit(type: ChannelTypeEnum) {
-  const { integrations = [] } = useActiveIntegrations();
-
-  const isLimitFetchingEnabled = useMemo<boolean>(() => {
-    return !IS_DOCKER_HOSTED && integrations.filter((integration) => integration.channel === type).length === 0;
-  }, [integrations, type, IS_DOCKER_HOSTED]);
-
   const {
     data = { limit: 0, count: 0 },
     isLoading,
     refetch,
-  } = useQuery(['integrationLimit', type], () => getIntegrationLimit(type));
+  } = useQuery(['integrationLimit', type], () => getIntegrationLimit(type), {
+    enabled: isLimitFetchingEnabled,
+  });
 
   const isLimitReached = isLimitFetchingEnabled && data.limit === data.count;
 
