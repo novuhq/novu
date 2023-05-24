74669f597f41c5622d54c040cf3f2755e3005b6f
Michał Dziuba <mail@michaldziuba.dev>
2023-05-24 14:29:26 +0200

    chore: switch from console to logger for migration logs

--- apps/api/migrations/secure-to-boolean/secure-to-boolean-migration.ts
+++ apps/api/migrations/secure-to-boolean/secure-to-boolean-migration.ts
@@ -1,7 +1,7 @@
-/* eslint-disable no-console */
 import '../../src/config';
 import { IntegrationRepository } from '@novu/dal';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from '../../src/app.module';
 
 const integrationRepository = new IntegrationRepository();
@@ -11,17 +11,17 @@ export async function run() {
     logger: false,
   });
 
-  console.log('Start migration - update credentials.secure from string to boolean');
+  Logger.log('Start migration - update credentials.secure from string to boolean');
 
-  console.log('Updating from "true" to true...');
+  Logger.log('Updating from "true" to true...');
   const resultTrue = await updateTrueValues();
-  console.log(`Matched: ${resultTrue.matchedCount}\nModified: ${resultTrue.modifiedCount} \n`);
+  Logger.log(`Matched: ${resultTrue.matchedCount}  Modified: ${resultTrue.modifiedCount} \n`);
 
-  console.log('Updating from "false" to false...');
+  Logger.log('Updating from "false" to false...');
   const resultFalse = await updateFalseValues();
-  console.log(`Matched: ${resultFalse.matchedCount}\nModified: ${resultFalse.modifiedCount}`);
+  Logger.log(`Matched: ${resultFalse.matchedCount}  Modified: ${resultFalse.modifiedCount}`);
 
-  console.log('End migration.');
+  Logger.log('End migration.');
 }
 
 type UpdateResult = { matchedCount: number; modifiedCount: number };
