fa2d9f4b38d30e1ea190532b7b973e5c3b61e874
Satyajeet Sahoo <03081996s@gmail.com>
2023-05-24 13:22:37 +0530

    Bulksms Handler part moved from Services to application generic part

---
+++ packages/application-generic/src/factories/sms/handlers/bulksms.handler.ts
@@ -0,0 +1,25 @@
+import { ChannelTypeEnum } from '@novu/shared';
+import { ICredentials } from '@novu/dal';
+import { BulkSmsSmsProvider } from '@novu/bulksms';
+import { BaseSmsHandler } from './base.handler';
+
+export class BulkSmsSmsHandler extends BaseSmsHandler {
+  constructor() {
+    super('bulksms', ChannelTypeEnum.SMS);
+  }
+  buildProvider(credentials: ICredentials) {
+    const config: {
+      authKey?: string;
+      sender?: string;
+      route?: string;
+      dltEntity?: string;
+    } = {
+      authKey: credentials.apiKey,
+      sender: credentials.from,
+      route: credentials.route,
+      dltEntity: credentials.dltEntityId,
+    };
+
+    this.provider = new BulkSmsSmsProvider(config);
+  }
+}
--- packages/application-generic/src/factories/sms/handlers/index.ts
+++ packages/application-generic/src/factories/sms/handlers/index.ts
@@ -3,6 +3,7 @@ export * from './telnyx.handler';
 export * from './twilio.handler';
 export * from './plivo.handler';
 export * from './sms77.handler';
+export * from './bulksms.handler';
 export * from './termii.handler';
 export * from './gupshup.handler';
 export * from './sms-central.handler';
