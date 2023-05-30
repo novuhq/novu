8de96d2a395cff6c9ac357c07f8043ca41ddf0cd
Kartik Jolapara <kjmickey003@gmail.com>
2023-05-30 15:02:38 +0530

    fix: Change the _feedId and _layoutId to feedId and layoutId respectively
    Signed-off-by: Kartik Jolapara <kjmickey003@gmail.com>

--- packages/node/src/lib/notification-template/notification-template.interface.ts
+++ packages/node/src/lib/notification-template/notification-template.interface.ts
@@ -1,4 +1,8 @@
-import { INotificationTemplateStep, IPreferenceChannels } from '@novu/shared';
+import {
+  IMessageTemplate,
+  INotificationTemplateStep,
+  IPreferenceChannels,
+} from '@novu/shared';
 
 export interface INotificationTemplates {
   create(data: INotificationTemplatePayload);
@@ -8,12 +12,21 @@ export interface INotificationTemplates {
   updateStatus(templateId: string, active: boolean);
 }
 
+interface IMessageTemplateModified extends IMessageTemplate {
+  feedId?: string;
+  layoutId?: string;
+}
+
+interface INotificationTemplateStepModified extends INotificationTemplateStep {
+  template: IMessageTemplateModified;
+}
+
 export interface INotificationTemplatePayload {
   name: string;
   notificationGroupId: string;
   tags?: string[];
   description?: string;
-  steps?: INotificationTemplateStep[];
+  steps?: INotificationTemplateStepModified[];
   active?: boolean;
   draft?: boolean;
   critical?: boolean;
