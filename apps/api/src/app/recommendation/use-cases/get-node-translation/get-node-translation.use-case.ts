4b08f8d512bbdbee0e457d80c48eb99f3680299a
Tomer Barnea <com.barnea@gmail.com>
2023-06-06 00:47:39 +0100

    feat(api): recommendation module

--- apps/api/src/app/recommendation/use-cases/get-node-translation/get-node-translation.use-case.ts
+++ apps/api/src/app/recommendation/use-cases/get-node-translation/get-node-translation.use-case.ts
@@ -13,7 +13,8 @@ export class GetNodeTranslationUseCase {
     const prompt = `
     Use [language] value of ${command.dstLanguage} in our conversation.
 Use [source] value of “${command.prompt}” in our conversation.
-I want you to act as an [language] translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in [language]. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My first sentence is [source].
+I want you to act as an [language] translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in [language]. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. If you have an improvement or a correction reply only that. Do not reply the original source. My first sentence is [source].
+
 `;
 
     try {
