7331eae6ee314f74d9dceff7ea13be95aee9511f
Gosha <djabarov.george@gmail.com>
2023-06-18 12:06:40 +0300

    feat: add popular template

--- libs/shared/src/consts/template-store/index.ts
+++ libs/shared/src/consts/template-store/index.ts
@@ -1,4 +1,4 @@
-const productionIds = ['646c77cf693b8e668a900a73', '646f123c720b54f89ed2130a'];
+const productionIds = ['646c77cf693b8e668a900a73', '646f123c720b54f89ed2130a', '646c7aee958d8bed2e00b8e9'];
 const developmentIds = ['64731d4e1084f5a48293ceab', '64731d4e1084f5a48293ce9f'];
 
 export function getPopularTemplateIds({ production }: { production: boolean }) {
