fc346ba27cd32dbe59dea8d480defb62a9a88a8d
Gosha <djabarov.george@gmail.com>
2023-06-25 15:25:52 +0300

    feat: update after pr comments

--- apps/api/src/app/tenant/dtos/update-tenant-request.dto.ts
+++ apps/api/src/app/tenant/dtos/update-tenant-request.dto.ts
@@ -1,13 +1,19 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { TenantCustomData } from '@novu/shared';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateTenantRequestDto {
-  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({ type: String })
   identifier?: string;
 
-  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({ type: String })
   name?: string;
 
-  @ApiProperty()
+  @IsOptional()
+  @ApiPropertyOptional()
   data?: TenantCustomData;
 }
