39db665b2123fb29856ff39f1734b656e161c1b1
Paweł <pawel.tymczuk@gmail.com>
2023-06-26 12:31:17 +0200

    chore(web): added suggestions from pr #3644

--- apps/web/src/pages/integrations/components/IntegrationChannelCell.tsx
+++ apps/web/src/pages/integrations/components/IntegrationChannelCell.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { colors, IExtendedCellProps, withCellLoading } from '../../../design-system';
-import { CHANNEL_TYPE_TO_ICON } from '../constants';
+import { CHANNEL_TYPE_TO_ICON_NAME } from '../constants';
 import type { ITableIntegration } from '../types';
 
 const ChannelCellHolder = styled.div`
@@ -24,7 +24,7 @@ const Text = styled.span`
 const IntegrationChannel = ({ row: { original } }: IExtendedCellProps<ITableIntegration>) => {
   return (
     <ChannelCellHolder data-test-id="integration-channel-cell">
-      <IconStyled icon={CHANNEL_TYPE_TO_ICON[original.channelType] as any} />
+      <IconStyled icon={CHANNEL_TYPE_TO_ICON_NAME[original.channelType] as any} />
       <Text>{original.channel}</Text>
     </ChannelCellHolder>
   );
