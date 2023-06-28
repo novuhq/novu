c0dc08556f871f71e2650281f8341228abde8428
Biswajeet Das <biswajeetdas18@gmail.com>
2023-06-28 12:45:46 +0530

    fix: tests

--- packages/application-generic/src/usecases/select-integration/select-integration.spec.ts
+++ packages/application-generic/src/usecases/select-integration/select-integration.spec.ts
@@ -1,20 +1,39 @@
 import { Test } from '@nestjs/testing';
 import { ChannelTypeEnum, EmailProviderIdEnum } from '@novu/shared';
-import { IntegrationEntity, IntegrationRepository } from '@novu/dal';
+import {
+  IntegrationEntity,
+  IntegrationRepository,
+  MessageRepository,
+  OrganizationRepository,
+} from '@novu/dal';
 import * as sinon from 'sinon';
 import { UserSession } from '@novu/testing';
 import { SelectIntegration } from './select-integration.usecase';
 import { SelectIntegrationCommand } from './select-integration.command';
+import { GetNovuIntegration } from '../get-novu-integration';
+import { CalculateLimitNovuIntegration } from '../calculate-limit-novu-integration';
+import { AnalyticsService } from '../../services';
 
 describe('select integration', function () {
   let useCase: SelectIntegration;
   let integrationRepository: IntegrationRepository;
-
   let session: UserSession;
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
   beforeEach(async function () {
     const moduleRef = await Test.createTestingModule({
-      imports: [SelectIntegration, IntegrationRepository],
-      providers: [],
+      imports: [],
+      providers: [
+        GetNovuIntegration,
+        CalculateLimitNovuIntegration,
+        IntegrationRepository,
+        OrganizationRepository,
+        AnalyticsService,
+        MessageRepository,
+      ],
     }).compile();
 
     session = new UserSession();
