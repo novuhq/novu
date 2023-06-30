308c91e94935713d6b98adf8ee0b4e77110cb355
David Söderberg <granskog_@hotmail.com>
2023-06-30 11:07:36 +0200

    fix: after pr comment

--- apps/web/src/utils/providers.ts
+++ apps/web/src/utils/providers.ts
@@ -2,9 +2,5 @@ import { ColorScheme } from '@mantine/core';
 import { CONTEXT_PATH } from '../config';
 
 export const getLogoFileName = (id, schema: ColorScheme): string => {
-  if (schema === 'dark') {
-    return `${CONTEXT_PATH}/static/images/providers/dark/square/${id}.svg`;
-  }
-
-  return `${CONTEXT_PATH}/static/images/providers/light/square/${id}.svg`;
+  return `${CONTEXT_PATH}/static/images/providers/${schema}/square/${id}.svg`;
 };
