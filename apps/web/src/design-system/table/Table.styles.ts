585e9d69c66913937a701b077195be4b9c5c45e9
Biswajeet Das <biswajeetdas18@gmail.com>
2023-08-04 19:51:42 +0530

    fix: table alignment

--- apps/web/src/design-system/table/Table.styles.ts
+++ apps/web/src/design-system/table/Table.styles.ts
@@ -16,16 +16,16 @@ export default createStyles((theme: MantineTheme, _params, getRef) => {
       borderCollapse: 'collapse',
       borderSpacing: '0px 20px',
       'tr td:first-of-type': {
-        paddingLeft: 0,
+        paddingLeft: 24,
       },
       'tr th:first-of-type': {
-        paddingLeft: 0,
+        paddingLeft: 24,
       },
       'tr td:last-child': {
-        paddingRight: 0,
+        paddingRight: 10,
       },
       'tr th:last-child': {
-        paddingRight: 0,
+        paddingRight: 10,
       },
       '& thead tr': {
         borderBottom: `1px solid ${dark ? colors.B30 : colors.B98}`,
