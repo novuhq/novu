a15f9125f51d5a3a20fd232a6b36a25280cd5235
Gosha <djabarov.george@gmail.com>
2023-08-14 23:29:51 +0300

    feat: test with auto pipeline and small pool

--- packages/application-generic/src/services/cache/cache.service.ts
+++ packages/application-generic/src/services/cache/cache.service.ts
@@ -38,7 +38,7 @@ export class CacheService implements ICacheService {
     : 1;
   private readonly MAX_CLIENT_POOL = process.env.REDIS_CACHE_MAX_CLIENT_POOL
     ? Number(process.env.REDIS_CACHE_MAX_CLIENT_POOL)
-    : 100;
+    : 10;
   private redisPool: Pool<InMemoryProviderClient>;
 
   constructor(private inMemoryProviderService: InMemoryProviderService) {
