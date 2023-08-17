a1dd0c84612203f93fce9e37c5813a69fbe4c7d7
Dima Grossman <dima@grossman.io>
2023-08-17 12:38:24 +0300

    fix: update env location

--- apps/web/src/components/layout/components/PageMeta.tsx
+++ apps/web/src/components/layout/components/PageMeta.tsx
@@ -8,12 +8,14 @@ function PageMeta({ title }: Props) {
   return (
     <Helmet>
       <title>{title ? `${title} | ` : ``}Novu Manage Platform</title>
-      {!!process?.env?.REACT_APP_PLAUSIBLE_DOMAIN && (
+      {typeof process !== undefined && !!process?.env?.REACT_APP_PLAUSIBLE_DOMAIN ? (
         <script
           defer
           data-domain={new URL(window.location.href).hostname}
-          src={`${process.env.REACT_APP_PLAUSIBLE_DOMAIN}/js/script.tagged-events.js`}
+          src={`${process?.env?.REACT_APP_PLAUSIBLE_DOMAIN}/js/script.tagged-events.js`}
         />
+      ) : (
+        ''
       )}
     </Helmet>
   );
