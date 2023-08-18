0554f56e4788517f483256236e34fcab7d5cca5e
p-fernandez <pablo.fernandez.otero@gmail.com>
2023-08-18 10:40:53 +0100

    fix(ws): missing worker implementation for ws

--- apps/ws/src/socket/socket.module.ts
+++ apps/ws/src/socket/socket.module.ts
@@ -1,34 +1,20 @@
 import { Inject, Module, OnModuleInit, Provider } from '@nestjs/common';
-import { BullMqService } from '@novu/application-generic';
 import { JobTopicNameEnum } from '@novu/shared';
+import { WebSocketsWorkerService } from '@novu/application-generic';
 
 import { WSGateway } from './ws.gateway';
 import { SharedModule } from '../shared/shared.module';
 import { ExternalServicesRoute } from './usecases/external-services-route';
-import { SocketQueueConsumerService } from './services/socket-queue-consumer.service';
+
+import { WebSocketWorker } from './services';
 
 const USE_CASES: Provider[] = [ExternalServicesRoute];
 
-const SERVICES: Provider[] = [SocketQueueConsumerService, BullMqService];
+const PROVIDERS: Provider[] = [WSGateway, WebSocketsWorkerService, WebSocketWorker];
 
 @Module({
   imports: [SharedModule],
-  providers: [WSGateway, ...SERVICES, ...USE_CASES],
+  providers: [...PROVIDERS, ...USE_CASES],
   exports: [WSGateway],
 })
-export class SocketModule implements OnModuleInit {
-  constructor(private wsGateway: WSGateway, @Inject(BullMqService) public readonly bullMqService: BullMqService) {}
-
-  async onModuleInit() {
-    this.bullMqService.createWorker(
-      JobTopicNameEnum.WEB_SOCKETS,
-      async (job) => {
-        this.wsGateway.sendMessage(job.data.userId, job.data.event, job.data.payload);
-      },
-      {
-        lockDuration: 90000,
-        concurrency: 5,
-      }
-    );
-  }
-}
+export class SocketModule {}
