6897b1a23380a118a0154314a9861c2d39eeb82a
Gosha <djabarov.george@gmail.com>
2023-08-29 09:42:06 +0300

    feat: add check in url fallback

--- apps/worker/src/app/shared/utils/cron-health.ts
+++ apps/worker/src/app/shared/utils/cron-health.ts
@@ -1,6 +1,8 @@
 import { Logger } from '@nestjs/common';
 
-const url = process.env.CRON_CHECKIN_URL;
+const url = process.env.CRON_CHECKIN_URL
+  ? process.env.CRON_CHECKIN_URL
+  : 'https://uptime.betterstack.com/api/v1/heartbeat/';
 const LOG_CONTEXT = 'cronHealth';
 export async function checkinForCronJob(cronId?: string) {
   if (process.env.NOVU_MANAGED_SERVICE && process.env.NODE_ENV === 'production' && cronId && url) {
