e8d6d58981f85d0123b7921a806454696df11de3
David Söderberg <granskog_@hotmail.com>
2023-09-21 12:40:56 +0200

    fix: after pr comments

--- apps/worker/src/app/workflow/usecases/run-job/run-job.usecase.ts
+++ apps/worker/src/app/workflow/usecases/run-job/run-job.usecase.ts
@@ -8,7 +8,6 @@ import { RunJobCommand } from './run-job.command';
 import { QueueNextJob, QueueNextJobCommand } from '../queue-next-job';
 import { SendMessage, SendMessageCommand } from '../send-message';
 import { PlatformException, EXCEPTION_MESSAGE_ON_WEBHOOK_FILTER } from '../../../shared/utils';
-import { SetJobAsCommand, SetJobAsCompleted } from '../update-job-status';
 
 const LOG_CONTEXT = 'RunJob';
 
@@ -19,7 +18,6 @@ export class RunJob {
     private sendMessage: SendMessage,
     private queueNextJob: QueueNextJob,
     private storageHelperService: StorageHelperService,
-    private setJobAsCompleted: SetJobAsCompleted,
     private logger?: PinoLogger
   ) {}
 
@@ -80,14 +78,7 @@ export class RunJob {
         })
       );
 
-      await this.setJobAsCompleted.execute(
-        SetJobAsCommand.create({
-          jobId: job._id,
-          environmentId: job._environmentId,
-          organizationId: job._organizationId,
-          userId: job._userId,
-        })
-      );
+      await this.jobRepository.updateStatus(job._environmentId, job._id, JobStatusEnum.COMPLETED);
     } catch (error: any) {
       Logger.error({ error }, `Running job ${job._id} has thrown an error`, LOG_CONTEXT);
       if (job.step.shouldStopOnFail || this.shouldBackoff(error)) {
