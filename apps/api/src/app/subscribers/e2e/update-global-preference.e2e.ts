673bb50b02a9dbbcfdb30eb294b366c4522e8833
Biswajeet Das <biswajeetdas18@gmail.com>
2023-09-26 22:39:31 +0530

    test: added tests with validations and multiple channels

--- apps/api/src/app/subscribers/e2e/update-global-preference.e2e.ts
+++ apps/api/src/app/subscribers/e2e/update-global-preference.e2e.ts
@@ -12,6 +12,40 @@ describe('Update Subscribers global preferences - /subscribers/:subscriberId/pre
     await session.initialize();
   });
 
+  it('should validate the payload', async function () {
+    const badPayload = {
+      enabled: true,
+      preferences: false,
+    };
+
+    try {
+      const firstResponse = await updateGlobalPreferences(badPayload as any, session);
+      expect(firstResponse).to.not.be.ok;
+    } catch (error) {
+      expect(error.toJSON()).to.have.include({
+        status: 400,
+        name: 'AxiosError',
+        message: 'Request failed with status code 400',
+      });
+    }
+
+    const yetAnotherBadPayload = {
+      enabled: 'hello',
+      preferences: [{ type: ChannelTypeEnum.EMAIL, enabled: true }],
+    };
+
+    try {
+      const secondResponse = await updateGlobalPreferences(yetAnotherBadPayload as any, session);
+      expect(secondResponse).to.not.be.ok;
+    } catch (error) {
+      expect(error.toJSON()).to.have.include({
+        status: 400,
+        name: 'AxiosError',
+        message: 'Request failed with status code 400',
+      });
+    }
+  });
+
   it('should update user global preferences', async function () {
     const payload = {
       enabled: true,
@@ -21,8 +55,31 @@ describe('Update Subscribers global preferences - /subscribers/:subscriberId/pre
     const response = await updateGlobalPreferences(payload, session);
 
     expect(response.data.data.preference.enabled).to.eql(true);
+    expect(response.data.data.preference.channels).to.not.eql({
+      [ChannelTypeEnum.IN_APP]: true,
+    });
     expect(response.data.data.preference.channels).to.eql({
       [ChannelTypeEnum.EMAIL]: true,
+    });
+  });
+
+  it('should update user global preferences for multiple channels', async function () {
+    const payload = {
+      enabled: true,
+      preferences: [
+        { type: ChannelTypeEnum.PUSH, enabled: true },
+        { type: ChannelTypeEnum.IN_APP, enabled: false },
+        { type: ChannelTypeEnum.SMS, enabled: true },
+      ],
+    };
+
+    const response = await updateGlobalPreferences(payload, session);
+
+    expect(response.data.data.preference.enabled).to.eql(true);
+    expect(response.data.data.preference.channels).to.eql({
+      [ChannelTypeEnum.PUSH]: true,
+      [ChannelTypeEnum.IN_APP]: false,
+      [ChannelTypeEnum.SMS]: true,
     });
   });
 
