46b1a8a3937ea2d1eb86c4e66f13a42001da0eaa
Uzair Khan <uzairkhanuk3215@gmail.com>
2023-09-30 19:23:40 +0530

    fix: changed the logout icon from a trash to that of a log out icon

--- apps/web/src/components/layout/components/HeaderNav.tsx
+++ apps/web/src/components/layout/components/HeaderNav.tsx
@@ -8,7 +8,7 @@ import LogRocket from 'logrocket';
 import { CONTEXT_PATH, INTERCOM_APP_ID, IS_DOCKER_HOSTED, LOGROCKET_ID } from '../../../config';
 import { ROUTES } from '../../../constants/routes.enum';
 import { colors, Dropdown, shadows, Text, Tooltip } from '../../../design-system';
-import { Ellipse, Mail, Moon, Question, Sun, Trash } from '../../../design-system/icons';
+import { Ellipse, Mail, Moon, Question, Sun, Trash, Logout } from '../../../design-system/icons';
 import { useLocalThemePreference } from '../../../hooks';
 import { discordInviteUrl } from '../../../pages/quick-start/consts';
 import { useAuthContext } from '../../providers/AuthProvider';
@@ -111,7 +111,7 @@ export function HeaderNav({ isIntercomOpened }: Props) {
       {
         id: 'sign-out',
         title: 'Sign out',
-        icon: <Trash />,
+        icon: <Logout />,
         onTrigger: () => {
           logout();
         },
@@ -153,7 +153,7 @@ export function HeaderNav({ isIntercomOpened }: Props) {
         </Dropdown.Item>
       </Link>
     )),
-    <Dropdown.Item key="logout" icon={<Trash />} onClick={logout} data-test-id="logout-button">
+    <Dropdown.Item key="logout" icon={<Logout />} onClick={logout} data-test-id="logout-button">
       Sign Out
     </Dropdown.Item>,
   ];
