bc26da10637dc851c7ed2c77070d0647d4978958
Piyush Garg <piyushgarg.dev@gmail.com>
2023-09-30 13:23:52 +0530

    fix: Get senderName from cred

--- packages/application-generic/src/factories/mail/handlers/resend.handler.ts
+++ packages/application-generic/src/factories/mail/handlers/resend.handler.ts
@@ -6,11 +6,11 @@ export class ResendHandler extends BaseHandler {
   constructor() {
     super('resend', ChannelTypeEnum.EMAIL);
   }
-  buildProvider(credentials: ICredentials, from?: string, senderName?: string) {
+  buildProvider(credentials: ICredentials, from?: string) {
     const config: { apiKey: string; from: string; senderName?: string } = {
       from: from as string,
       apiKey: credentials.apiKey as string,
-      senderName,
+      senderName: credentials.senderName,
     };
 
     this.provider = new ResendEmailProvider(config);
