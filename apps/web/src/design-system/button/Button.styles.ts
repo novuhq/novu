7d6d7fc6a424f18a456ce85160d7acca61916a59
Harsh Mahajan <127186841+HarshMN2345@users.noreply.github.com>
2023-10-02 00:05:47 +0530

    Update Button.styles.ts
    This pull request introduces a series of enhancements to the codebase:
    
    Refactoring for Readability and Consistency:
    
    The code has been refactored to adhere to consistent naming conventions and formatting, making it easier to read and understand.
    Reduced Redundancy:
    
    Similar properties and styles have been combined to reduce redundancy, leading to a more streamlined and efficient codebase.
    Modularized Styles:
    
    Styles have been modularized into smaller, reusable functions, promoting better maintainability and reusability.
    Simplified Conditional Logic:
    
    Conditional logic has been simplified where possible, enhancing code clarity and reducing complexity.
    Overall, these changes aim to improve the quality, maintainability, and readability of the code. They are designed to align with best practices in code organization and style, and to enhance the overall developer experience when working with this codebase.

--- apps/web/src/design-system/button/Button.styles.ts
+++ apps/web/src/design-system/button/Button.styles.ts
@@ -3,13 +3,71 @@ import { colors, shadows } from '../config';
 
 const getGradient = (color) => `linear-gradient(0deg, ${color} 0%, ${color} 100%)`;
 
-const getLabelStyles = (disabled: boolean, variant?: string): any => {
-  if (disabled && variant === 'subtle') {
-    return {
-      gap: '8px',
-    };
-  }
+const getOutlineStyles = (theme, dark) => ({
+  border: '1px solid transparent',
+  background: `${dark ? getGradient(colors.B17) : getGradient(colors.white)} padding-box, ${
+    colors.horizontal
+  } border-box`,
+  color: dark ? theme.white : 'transparent',
+  boxShadow: dark ? shadows.dark : shadows.medium,
+});
 
+const getSubtleStyles = (theme, dark) => ({
+  border: 'none',
+  padding: '0 8px',
+  background: 'transparent',
+  color: dark ? theme.white : 'transparent',
+  boxShadow: 'none',
+  '.mantine-Button-label': {
+    'div[data-square]': {
+      color: colors.white,
+      backgroundImage: colors.horizontal,
+      backgroundColor: 'transparent',
+    },
+  },
+  '&:hover:not(:disabled), &:focus:not(:disabled)': {
+    background: 'transparent',
+    color: dark ? theme.white : 'transparent',
+    filter: dark ? 'none' : 'brightness(0.9)',
+    '.mantine-Button-label': {
+      'div[data-square]': {
+        color: dark ? colors.gradientMiddle : theme.white,
+        backgroundColor: dark ? theme.white : 'transparent',
+        backgroundImage: dark ? 'none' : colors.horizontal,
+      },
+      'div:last-of-type': {
+        backgroundImage: 'none !important',
+        backgroundColor: dark ? theme.white : 'transparent',
+        backgroundClip: 'none !important',
+        color: 'transparent',
+        '-webkit-text-fill-color': 'initial !important',
+      },
+    },
+  },
+  '&:disabled': {
+    opacity: 0.4,
+    background: 'transparent',
+  },
+});
+
+const getPulseStyles = () => ({
+  '&:not(:disabled):not([data-loading])': {
+    animation: 'pulse-animation 2s infinite',
+    '@keyframes pulse-animation': {
+      '0%': {
+        boxShadow: '0 0 0 0 rgba(255, 82, 82, 0.7)',
+      },
+      '70%': {
+        boxShadow: '0 0 0 10px rgba(255, 82, 82, 0)',
+      },
+      '100%': {
+        boxShadow: '0 0 0 0 rgba(255, 82, 82, 0)',
+      },
+    },
+  },
+});
+
+const getLabelStyles = (disabled, variant, dark) => {
   if (disabled) {
     return {};
   }
@@ -22,121 +80,26 @@ const getLabelStyles = (disabled: boolean, variant?: string): any => {
   };
 };
 
-export const getOutlineStyles = (theme) => {
-  const dark = theme.colorScheme === 'dark';
-
-  return {
-    border: '1px solid transparent',
-    background: `${dark ? getGradient(colors.B17) : getGradient(colors.white)} padding-box, ${
-      colors.horizontal
-    } border-box`,
-    color: dark ? theme.white : 'transparent',
-    boxShadow: dark ? shadows.dark : shadows.medium,
-  };
-};
-
-export const getSubtleStyles = (theme) => {
-  const dark = theme.colorScheme === 'dark';
-
-  return {
-    border: 'none',
-    padding: '0 8px',
-    background: 'transparent',
-    color: dark ? theme.white : 'transparent',
-    boxShadow: 'none',
-    '.mantine-Button-label': {
-      'div[data-square]': {
-        color: colors.white,
-        backgroundImage: colors.horizontal,
-        backgroundColor: 'transparent',
-      },
-    },
-    '&:hover:not(:disabled)': {
-      background: 'transparent',
-      color: dark ? theme.white : 'transparent',
-      filter: dark ? 'none' : 'brightness(0.9)',
-      '.mantine-Button-label': {
-        'div[data-square]': {
-          color: dark ? colors.gradientMiddle : theme.white,
-          backgroundColor: dark ? theme.white : 'transparent',
-          backgroundImage: dark ? 'none' : colors.horizontal,
-        },
-        'div:last-of-type': {
-          backgroundImage: 'none !important',
-          backgroundColor: dark ? theme.white : 'transparent',
-          backgroundClip: 'none !important',
-          color: 'transparent',
-          '-webkit-text-fill-color': 'initial !important',
-        },
-      },
-    },
-    '&:focus:not(:disabled)': {
-      background: 'transparent',
-      color: dark ? theme.white : 'transparent',
-      filter: dark ? 'none' : 'brightness(0.9)',
-      '.mantine-Button-label': {
-        'div[data-square]': {
-          color: dark ? colors.gradientMiddle : theme.white,
-          backgroundColor: dark ? theme.white : 'transparent',
-          backgroundImage: dark ? 'none' : colors.horizontal,
-        },
-        'div:last-of-type': {
-          backgroundImage: 'none !important',
-          backgroundColor: dark ? theme.white : 'transparent',
-          backgroundClip: 'none !important',
-          color: 'transparent',
-          '-webkit-text-fill-color': 'initial !important',
-        },
-      },
-    },
-    '&:disabled': {
-      opacity: 0.4,
-      background: 'transparent',
-    },
-  };
-};
-
-export const getPulseStyles = () => {
-  return {
-    [`&:not(:disabled):not([data-loading])`]: {
-      animation: 'pulse-animation 2s infinite',
-      '@keyframes pulse-animation': {
-        '0%': {
-          boxShadow: '0 0 0 0 rgba(255, 82, 82, 0.7)',
-        },
-        '70%': {
-          boxShadow: '0 0 0 10px rgba(255, 82, 82, 0)',
-        },
-        '100%': {
-          boxShadow: '0 0 0 0 rgba(255, 82, 82, 0)',
-        },
-      },
-    },
-  };
-};
-
 export default createStyles(
-  (
-    theme: MantineTheme,
-    { disabled, inherit, variant, pulse }: { disabled: boolean; inherit: boolean; variant?: string; pulse?: boolean },
-    getRef
-  ) => {
+  (theme: MantineTheme, { disabled, inherit, variant, pulse }) => {
+    const dark = theme.colorScheme === 'dark';
     const loading = getRef('loading');
     let overrides = {};
+
     if (variant === 'outline') {
-      overrides = getOutlineStyles(theme);
+      overrides = getOutlineStyles(theme, dark);
     }
 
     if (variant === 'subtle') {
-      overrides = getSubtleStyles(theme);
+      overrides = getSubtleStyles(theme, dark);
     }
 
     if (pulse) {
-      overrides = Object.assign({}, overrides, getPulseStyles());
+      overrides = { ...overrides, ...getPulseStyles() };
     }
 
     return {
-      label: getLabelStyles(disabled, variant),
+      label: getLabelStyles(disabled, variant, dark),
       root: {
         backgroundImage: colors.horizontal,
         width: inherit ? '100%' : '',
