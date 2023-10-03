7e0779f998cc1a5bb82014622e7d6832d23d0223
GAUTAM MANAK <106014185+GAUTAMMANAK1@users.noreply.github.com>
2023-10-04 03:27:39 +0530

    Update HomePage.tsx
    If you are encountering issues with the replace prop in the Navigate component from react-router-dom, one potential solution is to review your routing configuration and adjust the usage of the replace prop as needed.
    
    Here's a modified version of your HomePage component that removes the replace prop

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom';
 import { ROUTES } from '../constants/routes.enum';
 
 function HomePage() {
-  return <Navigate to={ROUTES.WORKFLOWS} replace />;
+  return <Navigate to={ROUTES.WORKFLOWS} />;
 }
 
 export default HomePage;
