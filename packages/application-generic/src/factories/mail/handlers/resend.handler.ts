ea79915b9f9148edabec4b196c00d75995d992b1
nmn41 <nm7613029@gmail.com>
2023-10-05 11:28:37 +0530

    fix: Fixed send the sender name in email when using resend email provider

--- packages/application-generic/src/factories/mail/handlers/resend.handler.ts
+++ packages/application-generic/src/factories/mail/handlers/resend.handler.ts
@@ -6,10 +6,11 @@ export class ResendHandler extends BaseHandler {
   constructor() {
     super('resend', ChannelTypeEnum.EMAIL);
   }
-  buildProvider(credentials: ICredentials, from?: string) {
-    const config: { apiKey: string; from: string } = {
+  buildProvider(credentials: ICredentials, from?: string, senderName?: string) {
+    const config: { apiKey: string; from: string; senderName: string } = {
       from: from as string,
       apiKey: credentials.apiKey as string,
+      senderName,
     };
 
     this.provider = new ResendEmailProvider(config);
