bfcfb6f876ac1e446a076202b371e4fd76d3028f
praxter11 <parveshsaini619@gmail.com>
2023-10-11 19:55:55 +0530

    fix(provider): updated the testing file clicksend.spec.ts

--- providers/clicksend/src/lib/clicksend.provider.spec.ts
+++ providers/clicksend/src/lib/clicksend.provider.spec.ts
@@ -1,28 +1,30 @@
 import { ClicksendSmsProvider } from './clicksend.provider';
 
-describe('ClicksendSMSProvider', () => {
-  let provider: ClicksendSmsProvider;
-
-  beforeEach(() => {
-    provider = new ClicksendSmsProvider({
-      username: '<your-clicksend-username>',
-      apiKey: '<your-clicksend-APIkey>',
-    });
+test('should trigger ClicksendSmsProvider library correctly', async () => {
+  const provider = new ClicksendSmsProvider({
+    username: '<your-clciksend-username>',
+    apiKey: '<your-clciksend-APIkey>',
   });
 
-  it('should be defined', () => {
-    expect(provider).toBeDefined();
+  const spy = jest
+    .spyOn(provider, 'sendMessage')
+    .mockImplementation(async () => {
+      return {
+        id: '12345-67a8',
+        date: new Date().toISOString(),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } as any;
+    });
+
+  await provider.sendMessage({
+    to: '+0451111111',
+    content: 'test message',
   });
 
-  describe('sendMessage', () => {
-    it('should return a response with an id and date', async () => {
-      const response = await provider.sendMessage({
-        to: '+0451111111',
-        content: 'test message',
-      });
+  expect(spy).toHaveBeenCalled();
 
-      expect(response).toHaveProperty('id');
-      expect(response).toHaveProperty('date');
-    });
+  expect(spy).toHaveBeenCalledWith({
+    to: '+0451111111',
+    content: 'test message',
   });
 });
