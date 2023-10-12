1669d030de92969b1beee54e9c5cfa68bf420326
Biswajeet Das <biswajeetdas18@gmail.com>
2023-10-12 15:18:27 +0530

    feat: add variant flow

--- apps/web/src/pages/templates/hooks/useBasePath.ts
+++ apps/web/src/pages/templates/hooks/useBasePath.ts
@@ -1,7 +1,12 @@
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 
 export const useBasePath = () => {
-  const { templateId = '' } = useParams<{ templateId: string }>();
+  const { templateId = '', variantUuid = '' } = useParams<{ templateId: string; variantUuid?: string }>();
+  const { pathname } = useLocation();
+  // remove variantUuid param from the Url and redirect to the url without the variantUuid
+  if (variantUuid && pathname.includes(variantUuid)) {
+    return pathname.replace(`/${variantUuid}`, '');
+  }
 
   return `/workflows/edit/${templateId}`;
 };
