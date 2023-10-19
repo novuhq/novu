18bed7d4260d4e745d40cc57c757c32d0a8d210e
KRISH SONI <67964054+krishvsoni@users.noreply.github.com>
2023-10-19 19:58:31 +0530

    modified roles.guard.ts

--- apps/api/src/app/auth/framework/roles.guard.ts
+++ apps/api/src/app/auth/framework/roles.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { IJwtPayload } from '@novu/shared';
 import * as jwt from 'jsonwebtoken';
@@ -9,27 +9,39 @@ export class RolesGuard implements CanActivate {
 
   canActivate(context: ExecutionContext): boolean {
     const roles = this.reflector.get<string[]>('roles', context.getHandler());
-    if (!roles) {
+
+    if (!roles || roles.length === 0) {
+      // If no roles are specified, allow access
       return true;
     }
 
     const request = context.switchToHttp().getRequest();
-    if (!request.headers.authorization) return false;
-
-    const token = request.headers.authorization.split(' ')[1];
-    if (!token) return false;
-
     const authorizationHeader = request.headers.authorization;
-    if (!authorizationHeader?.includes('ApiKey')) {
-      const user = jwt.decode(token) as IJwtPayload;
-      if (!user) return false;
-    }
 
-    /*
+    if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+      throw new UnauthorizedException('Unauthorized');
+    }
+     /*
      * TODO: The roles check implementation is currently not enabled
      * As we are not using roles in the system at this point
      */
+    const token = authorizationHeader.split(' ')[1];
+
+    if (!token) {
+      throw new UnauthorizedException('Unauthorized');
+    }
+
+    const user = jwt.decode(token) as IJwtPayload;
+
+    if (!user || !user.roles || !this.checkRoles(user.roles, roles)) {
+      throw new UnauthorizedException('Unauthorized');
+    }
 
     return true;
+  }
+
+  private checkRoles(userRoles: string[], requiredRoles: string[]): boolean {
+    // Check if the user has any of the required roles
+    return requiredRoles.some(role => userRoles.includes(role));
   }
 }
