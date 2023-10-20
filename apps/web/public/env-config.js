012985aea724319a1d58fe415cc5faf58413158b
Rhythm Garg <rhythmgarg05@gmail.com>
2023-10-20 16:20:53 +0000

    fix: accidental changes

--- apps/web/public/env-config.js
+++ apps/web/public/env-config.js
@@ -2,12 +2,4 @@ window._env_ = {
   SKIP_PREFLIGHT_CHECK: 'true',
   REACT_APP_ENVIRONMENT: 'dev',
   REACT_APP_VERSION: '$npm_package_version',
-  REACT_APP_API_URL: 'https://3000-itisohm-novu-cgqx6qiezes.ws-us105.gitpod.io',
-  REACT_APP_WS_URL: 'https://3002-itisohm-novu-cgqx6qiezes.ws-us105.gitpod.io',
-  REACT_APP_WS_URL: 'https://3002-itisohm-novu-cgqx6qiezes.ws-us105.gitpod.io',
-  REACT_APP_WS_URL: 'https://3002-itisohm-novu-cgqx6qiezes.ws-us105.gitpod.io',
-  REACT_APP_WS_URL: 'https://3002-itisohm-novu-cgqx6qiezes.ws-us105.gitpod.io',
-  REACT_APP_WS_URL: 'https://3002-itisohm-novu-cgqx6qiezes.ws-us105.gitpod.io',
-  REACT_APP_WS_URL: 'https://3002-itisohm-novu-cgqx6qiezes.ws-us105.gitpod.io',
-  REACT_APP_WS_URL: 'https://3002-itisohm-novu-cgqx6qiezes.ws-us105.gitpod.io',
 };
--- apps/widget/public/env-config.js
+++ apps/widget/public/env-config.js
@@ -1,7 +1,3 @@
 window._env_ = {
   SKIP_PREFLIGHT_CHECK: 'true',
-  REACT_APP_API_URL: 'https://3000-itisohm-novu-cgqx6qiezes.ws-us105.gitpod.io',
-  REACT_APP_WS_URL: 'https://3002-itisohm-novu-cgqx6qiezes.ws-us105.gitpod.io',
-  REACT_APP_ENVIRONMENT: 'dev',
-  BROWSER: '/.supervisor/browser.sh',
 };
