b2103994e027ee757fadc1157ff1ec3054f6e7f7
David Söderberg <granskog_@hotmail.com>
2023-10-23 07:02:40 +0200

    fix: after pr comments

--- apps/web/src/hooks/useInlineComponent.tsx
+++ apps/web/src/hooks/useInlineComponent.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
 import type { ComponentType, ReactNode } from 'react';
-import { useDataRef } from '.';
+import { useDataRef } from '@novu/design-system';
 
 export const useInlineComponent: <T>(
   Component: ComponentType<T>,
