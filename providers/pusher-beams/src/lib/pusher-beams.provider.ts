a22abfb064ef6b813c3d27bd10d13e97d1f4edd8
Muhammad Luqman <im.imperativa@gmail.com>
2023-10-24 17:12:03 +0700

    fix: better beams payload

--- providers/pusher-beams/src/lib/pusher-beams.provider.ts
+++ providers/pusher-beams/src/lib/pusher-beams.provider.ts
@@ -26,10 +26,44 @@ export class PusherBeamsPushProvider implements IPushProvider {
   async sendMessage(
     options: IPushOptions
   ): Promise<ISendMessageSuccessResponse> {
-    const res = await this.beamsClient.publishToUsers(
-      options.target,
-      options.payload as PushNotifications.PublishRequest
-    );
+    const { sound, badge, ...overrides } = options.overrides ?? {};
+
+    const res = await this.beamsClient.publishToUsers(options.target, {
+      apns: {
+        aps: {
+          alert: {
+            title: options.title,
+            body: options.content,
+          },
+          badge: badge,
+          category: overrides.categoryId,
+          sound: sound,
+        },
+      },
+      fcm: {
+        notification: {
+          title: options.title,
+          body: options.content,
+          android_channel_id: overrides.channelId,
+          click_action: overrides.clickAction,
+          color: overrides.color,
+          icon: overrides.icon,
+          sound: sound,
+          tag: overrides.tag,
+        },
+        data: options.payload,
+        time_to_live: overrides.ttl,
+      },
+      web: {
+        notification: {
+          title: options.title,
+          body: options.content,
+          icon: overrides.icon,
+        },
+        data: options.payload,
+        time_to_live: overrides.ttl,
+      },
+    });
 
     return {
       id: res.publishId,
