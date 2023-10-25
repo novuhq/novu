4403ca89ebd7c6ed0589d7ba3ca1a3632a82902d
Richard Fontein <32132657+rifont@users.noreply.github.com>
2023-10-25 15:01:03 +0100

    test(api): Fix idempotency header fixture naming

--- apps/api/e2e/idempotency.e2e.ts
+++ apps/api/e2e/idempotency.e2e.ts
@@ -5,7 +5,7 @@ describe('Idempotency Test', async () => {
   let session: UserSession;
   const path = '/v1/testing/idempotency';
   const HEADER_KEYS = {
-    IDEMPOTENCY: 'idempotency-key',
+    IDEMPOTENCY_KEY: 'idempotency-key',
     RETRY_AFTER: 'retry-after',
     IDEMPOTENCY_CONFLICT: 'x-idempotency-conflict',
   };
@@ -23,26 +23,26 @@ describe('Idempotency Test', async () => {
       const key = `1`;
       const { body, headers } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({ data: 201 })
         .expect(201);
       const { body: bodyDupe, headers: headerDupe } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({ data: 201 })
         .expect(201);
       expect(typeof body.data.number === 'number').to.be.true;
       expect(body.data.number).to.equal(bodyDupe.data.number);
-      expect(headers[HEADER_KEYS.IDEMPOTENCY]).to.eq(key);
-      expect(headerDupe[HEADER_KEYS.IDEMPOTENCY]).to.eq(key);
+      expect(headers[HEADER_KEYS.IDEMPOTENCY_KEY]).to.eq(key);
+      expect(headerDupe[HEADER_KEYS.IDEMPOTENCY_KEY]).to.eq(key);
     });
     it('should return cached and use correct cache key when apiKey is used', async () => {
       const key = `2`;
       const { body, headers } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({ data: 201 })
         .expect(201);
@@ -53,20 +53,20 @@ describe('Idempotency Test', async () => {
       expect(JSON.stringify(body)).to.eq(cacheVal);
       const { body: bodyDupe, headers: headerDupe } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({ data: 201 })
         .expect(201);
       expect(typeof body.data.number === 'number').to.be.true;
       expect(body.data.number).to.equal(bodyDupe.data.number);
-      expect(headers[HEADER_KEYS.IDEMPOTENCY]).to.eq(key);
-      expect(headerDupe[HEADER_KEYS.IDEMPOTENCY]).to.eq(key);
+      expect(headers[HEADER_KEYS.IDEMPOTENCY_KEY]).to.eq(key);
+      expect(headerDupe[HEADER_KEYS.IDEMPOTENCY_KEY]).to.eq(key);
     });
     it('should return cached and use correct cache key when authToken and apiKey combination is used', async () => {
       const key = `3`;
       const { body, headers } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', session.token)
         .send({ data: 201 })
         .expect(201);
@@ -77,31 +77,31 @@ describe('Idempotency Test', async () => {
       expect(JSON.stringify(body)).to.eq(cacheVal);
       const { body: bodyDupe, headers: headerDupe } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({ data: 201 })
         .expect(201);
       expect(typeof body.data.number === 'number').to.be.true;
       expect(body.data.number).to.equal(bodyDupe.data.number);
-      expect(headers[HEADER_KEYS.IDEMPOTENCY]).to.eq(key);
-      expect(headerDupe[HEADER_KEYS.IDEMPOTENCY]).to.eq(key);
+      expect(headers[HEADER_KEYS.IDEMPOTENCY_KEY]).to.eq(key);
+      expect(headerDupe[HEADER_KEYS.IDEMPOTENCY_KEY]).to.eq(key);
     });
     it('should return conflict when concurrent requests are made', async () => {
       const key = `4`;
       const [{ headers, body, status }, { headers: headerDupe, body: bodyDupe, status: statusDupe }] =
         await Promise.all([
-          session.testAgent.post(path).set(HEADER_KEYS.IDEMPOTENCY, key).send({ data: 250 }),
-          session.testAgent.post(path).set(HEADER_KEYS.IDEMPOTENCY, key).send({ data: 250 }),
+          session.testAgent.post(path).set(HEADER_KEYS.IDEMPOTENCY_KEY, key).send({ data: 250 }),
+          session.testAgent.post(path).set(HEADER_KEYS.IDEMPOTENCY_KEY, key).send({ data: 250 }),
         ]);
       const oneSuccess = status === 201 || statusDupe === 201;
       const oneConflict = status === 429 || statusDupe === 429;
       const conflictBody = status === 201 ? bodyDupe : body;
       const retryHeader = headers[HEADER_KEYS.RETRY_AFTER] || headerDupe[HEADER_KEYS.RETRY_AFTER];
-      const conflictHeader = headers[HEADER_KEYS.IDEMPOTENCY_CONFLICT] || headerDupe[HEADER_KEYS.IDEMPOTENCY_CONFLICT];
+      const conflictHeader = headers[HEADER_KEYS.IDEMPOTENCY_KEY_CONFLICT] || headerDupe[HEADER_KEYS.IDEMPOTENCY_KEY_CONFLICT];
       expect(oneSuccess).to.be.true;
       expect(oneConflict).to.be.true;
-      expect(headers[HEADER_KEYS.IDEMPOTENCY]).to.eq(key);
-      expect(headerDupe[HEADER_KEYS.IDEMPOTENCY]).to.eq(key);
+      expect(headers[HEADER_KEYS.IDEMPOTENCY_KEY]).to.eq(key);
+      expect(headerDupe[HEADER_KEYS.IDEMPOTENCY_KEY]).to.eq(key);
       expect(retryHeader).to.eq(`1`);
       expect(conflictHeader).to.eq('REQUEST_PROCESSING');
       expect(JSON.stringify(conflictBody)).to.eq(
@@ -115,23 +115,23 @@ describe('Idempotency Test', async () => {
       const key = '5';
       const { headers, body, status } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({ data: 250 });
       const {
         headers: headerDupe,
         body: bodyDupe,
         status: statusDupe,
-      } = await session.testAgent.post(path).set(HEADER_KEYS.IDEMPOTENCY, key).send({ data: 251 });
+      } = await session.testAgent.post(path).set(HEADER_KEYS.IDEMPOTENCY_KEY, key).send({ data: 251 });
 
       const oneSuccess = status === 201 || statusDupe === 201;
       const oneConflict = status === 422 || statusDupe === 422;
       const conflictBody = status === 201 ? bodyDupe : body;
-      const conflictHeader = headers[HEADER_KEYS.IDEMPOTENCY_CONFLICT] || headerDupe[HEADER_KEYS.IDEMPOTENCY_CONFLICT];
+      const conflictHeader = headers[HEADER_KEYS.IDEMPOTENCY_KEY_CONFLICT] || headerDupe[HEADER_KEYS.IDEMPOTENCY_KEY_CONFLICT];
       expect(oneSuccess).to.be.true;
       expect(oneConflict).to.be.true;
-      expect(headers[HEADER_KEYS.IDEMPOTENCY]).to.eq(key);
-      expect(headerDupe[HEADER_KEYS.IDEMPOTENCY]).to.eq(key);
+      expect(headers[HEADER_KEYS.IDEMPOTENCY_KEY]).to.eq(key);
+      expect(headerDupe[HEADER_KEYS.IDEMPOTENCY_KEY]).to.eq(key);
       expect(conflictHeader).to.eq('DIFFERENT_BODY');
       expect(JSON.stringify(conflictBody)).to.eq(
         JSON.stringify({
@@ -146,61 +146,61 @@ describe('Idempotency Test', async () => {
       const key1 = '7';
       const { body, headers } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({ data: 201 })
         .expect(201);
 
       const { body: bodyDupe, headers: headerDupe } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key1)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key1)
         .send({ data: 201 })
         .expect(201);
       expect(typeof body.data.number === 'number').to.be.true;
       expect(typeof bodyDupe.data.number === 'number').to.be.true;
       expect(body.data.number).not.to.equal(bodyDupe.data.number);
-      expect(headers[HEADER_KEYS.IDEMPOTENCY]).to.eq(key);
-      expect(headerDupe[HEADER_KEYS.IDEMPOTENCY]).to.eq(key1);
+      expect(headers[HEADER_KEYS.IDEMPOTENCY_KEY]).to.eq(key);
+      expect(headerDupe[HEADER_KEYS.IDEMPOTENCY_KEY]).to.eq(key1);
     });
     it('should return non cached response for GET requests', async () => {
       const key = '8';
       const { body, headers } = await session.testAgent
         .get(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({})
         .expect(200);
 
       const { body: bodyDupe } = await session.testAgent
         .get(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({})
         .expect(200);
       expect(typeof body.data.number === 'number').to.be.true;
       expect(typeof bodyDupe.data.number === 'number').to.be.true;
       expect(body.data.number).not.to.equal(bodyDupe.data.number);
-      expect(headers[HEADER_KEYS.IDEMPOTENCY]).to.eq(undefined);
+      expect(headers[HEADER_KEYS.IDEMPOTENCY_KEY]).to.eq(undefined);
     });
     it('should return cached error response for duplicate requests', async () => {
       const key = '9';
       const { body, headers } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({ data: 422 })
         .expect(422);
 
       const { body: bodyDupe, headers: headerDupe } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({ data: 422 })
         .expect(422);
       expect(JSON.stringify(body)).to.equal(JSON.stringify(bodyDupe));
 
-      expect(headers[HEADER_KEYS.IDEMPOTENCY]).to.eq(key);
-      expect(headerDupe[HEADER_KEYS.IDEMPOTENCY]).to.eq(key);
+      expect(headers[HEADER_KEYS.IDEMPOTENCY_KEY]).to.eq(key);
+      expect(headerDupe[HEADER_KEYS.IDEMPOTENCY_KEY]).to.eq(key);
     });
   });
 
@@ -215,14 +215,14 @@ describe('Idempotency Test', async () => {
       const key = '10';
       const { body } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({ data: 201 })
         .expect(201);
 
       const { body: bodyDupe } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({ data: 201 })
         .expect(201);
@@ -234,14 +234,14 @@ describe('Idempotency Test', async () => {
       const key1 = '12';
       const { body } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({ data: 201 })
         .expect(201);
 
       const { body: bodyDupe } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key1)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key1)
         .send({ data: 201 })
         .expect(201);
       expect(typeof body.data.number === 'number').to.be.true;
@@ -250,11 +250,11 @@ describe('Idempotency Test', async () => {
     });
     it('should return non cached response for GET requests', async () => {
       const key = '13';
-      const { body } = await session.testAgent.get(path).set(HEADER_KEYS.IDEMPOTENCY, key).send({}).expect(200);
+      const { body } = await session.testAgent.get(path).set(HEADER_KEYS.IDEMPOTENCY_KEY, key).send({}).expect(200);
 
       const { body: bodyDupe } = await session.testAgent
         .get(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({})
         .expect(200);
@@ -266,14 +266,14 @@ describe('Idempotency Test', async () => {
       const key = '14';
       const { body } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({ data: 500 })
         .expect(500);
 
       const { body: bodyDupe } = await session.testAgent
         .post(path)
-        .set(HEADER_KEYS.IDEMPOTENCY, key)
+        .set(HEADER_KEYS.IDEMPOTENCY_KEY, key)
         .set('authorization', `ApiKey ${session.apiKey}`)
         .send({ data: 500 })
         .expect(500);
