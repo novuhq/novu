07ce5b7061d9bf6deace31c969c8f2c96952db02
Atharva Kulkarni <atharvakulkarni172003@gmail.com>
2023-10-30 00:22:22 +0530

    Update de.ts

--- packages/notification-center/src/i18n/languages/de.ts
+++ packages/notification-center/src/i18n/languages/de.ts
@@ -4,7 +4,7 @@ export const DE: ITranslationEntry = {
   translations: {
     notifications: 'Benachrichtigungen',
     markAllAsRead: 'Alle als gelesen markieren',
-    poweredBy: 'von',
+    poweredBy: 'Angetrieben von',
     settings: 'Einstellungen',
     noNewNotification: 'Hier gibt es noch nichts Neues zu sehen',
   },
