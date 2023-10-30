d96b40075ece30e6cb58cb534555ed6120c0be51
Richard Fontein <32132657+rifont@users.noreply.github.com>
2023-10-30 23:50:10 +0000

    fix(dal): Update transformation generic typings to use template literal types

---
+++ libs/dal/src/types/helpers.spec.ts
@@ -0,0 +1,74 @@
+import { Types } from 'mongoose';
+import { ObjectIdKey, TransformValues, ChangePropsValueType } from './helpers';
+
+/**
+ * ObjectIdKey tests
+ */
+// Valid Key
+export const validObjectIdKey: ObjectIdKey = '_somethingId';
+
+// @ts-expect-error - incorrect format
+export const invalidObjectIdKey: ObjectIdKey = 'someId';
+
+/**
+ * TransformValues tests
+ */
+type TestTransformObject = {
+  foo: string;
+  bar?: number;
+};
+type TestTransformedObject = TransformValues<TestTransformObject, 'foo', number>;
+
+// Valid transformed object WITHOUT undefined keys
+export const validTransformedObject: TestTransformedObject = {
+  foo: 123,
+};
+
+// Valid changed object WITH undefined keys
+export const validTransformedUndefinedKeysObject: TestTransformedObject = {
+  foo: 123,
+  bar: undefined,
+};
+
+// Valid changed object with union type
+export const validTransformedUnionKeysObject: TransformValues<TestTransformObject, 'foo', number | boolean> = {
+  foo: false,
+};
+
+export const invalidTransformedDefinedKeysObject: TestTransformedObject = {
+  // @ts-expect-error - foo should be of type `number`
+  foo: '12345',
+};
+
+// @ts-expect-error - `foo` is not defined
+export const missingDefinedKeysObject: TestTransformedObject = {};
+
+/**
+ * ChangePropsValueType tests
+ */
+type TestChangeObject = {
+  _fooId: string;
+  _barId?: string;
+  baz: string;
+  qux?: number;
+};
+type TestChangedObject = ChangePropsValueType<TestChangeObject>;
+
+// Valid changed object WITHOUT undefined keys and non transformed key
+export const validKeysObject: TestChangedObject = {
+  _fooId: new Types.ObjectId('12345'),
+  baz: 'something',
+};
+
+// Valid changed object WITH undefined keys and non transformed undefined key
+export const validUndefinedKeysObject: TestChangedObject = {
+  _fooId: new Types.ObjectId('12345'),
+  _barId: undefined,
+  baz: 'something',
+  qux: undefined,
+};
+
+export const invalidDefinedKeysObject: TestChangedObject = {
+  // @ts-expect-error - `foo` is transformed to type 'ObjectId'.
+  _fooId: '12345',
+};
--- libs/dal/src/types/helpers.ts
+++ libs/dal/src/types/helpers.ts
@@ -1,5 +1,25 @@
 import { Types } from 'mongoose';
 
-export type ChangePropsValueType<T, K extends keyof T, V = Types.ObjectId> = Omit<T, K> & {
-  [P in K]: V;
-};
+/**
+ * The template literal type of an ObjectKey identifier
+ */
+export type ObjectIdKey = `_${string}Id`;
+
+/**
+ * Flatten type T to make it easier to read
+ */
+type Id<T> = { [P in keyof T]: T[P] };
+
+/**
+ * Transform the values of T with keys in union of type U to type V
+ */
+export type TransformValues<T, U, V> = Id<
+  Omit<T, Extract<keyof T, U>> & {
+    [P in keyof Pick<T, Extract<keyof T, U>>]: V;
+  }
+>;
+
+/**
+ * Transform the values of T with keys in union of type `ObjectIdKey` to type `Types.ObjectId`
+ */
+export type ChangePropsValueType<T> = TransformValues<T, ObjectIdKey, Types.ObjectId>;
