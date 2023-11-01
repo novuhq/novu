b551d5572ed04a4614dbeaa0120ddce16e053d65
Richard Fontein <32132657+rifont@users.noreply.github.com>
2023-11-01 18:32:32 +0000

    fix(dal): Add mongo dot notation helpers

--- libs/dal/src/types/helpers.spec.ts
+++ libs/dal/src/types/helpers.spec.ts
@@ -1,5 +1,5 @@
 import { Types } from 'mongoose';
-import { ObjectIdKey, TransformValues, TransformEntityToDbModel, LeafKeys, DotPrefix } from './helpers';
+import { ObjectIdKey, TransformValues, TransformEntityToDbModel, Dot, DeepKeys, ExtractDotNotation } from './helpers';
 
 /**
  * ObjectIdKey tests
@@ -114,37 +114,149 @@ export const invalidDateKeysObject: TestChangedObject = {
 /**
  * DotPrefix tests
  */
-// Valid dot prefix
-export const validDotPrefix: DotPrefix<'foo'> = '.foo';
+// Valid dot join with single union
+export const validSingleUnionPrefix: Dot<'foo', 'bar'> = 'foo.bar';
 
-// Valid empty string
-export const validEmptyString: DotPrefix<''> = '';
+// Valid dot join with double union
+export const validDoubleUnionPrefix: Dot<'foo', 'bar' | 'baz'> = 'foo.baz';
 
-// @ts-expect-error - invalid dot prefix
-export const invalidDotPrefix: DotPrefix<'foo'> = 'foo';
+// Valid empty first string
+export const validEmptyFirstString: Dot<'', 'test'> = '.test';
+
+// Valid empty second string
+export const validEmptySecondString: Dot<'for', ''> = 'for';
+
+// Valid empty second string
+export const validEmptyDoubleString: Dot<'', ''> = '';
+
+// @ts-expect-error - missing dot join
+export const invalidDotPrefix: Dot<'foo', 'bar'> = 'foobar';
 
 /**
- * LeafKeys tests
+ * DeepKeys tests
  */
-type TestLeafObject = {
-  foo: string;
-  bar?: number;
+type TestDeepKeysObject = {
+  bird: string;
   dog: {
     owner: {
       name: string;
     };
   };
+  cats?: Array<{
+    breed: string;
+    address: {
+      suburb: string;
+      lines: {
+        lines1: string;
+      };
+    };
+    contact: Array<{
+      mobile: number;
+    }>;
+  }>;
 };
-type TestLeafKeys = LeafKeys<TestLeafObject>;
+type TestDeepKeys = DeepKeys<TestDeepKeysObject>;
 
-// Valid top level key
-export const validTopLevelKey: TestLeafKeys = 'foo';
+export const validRootObjectKey: TestDeepKeys = 'bird';
+export const validNestedObjectKey: TestDeepKeys = 'dog.owner';
+export const validLeafObjectKey: TestDeepKeys = 'dog.owner.name';
+export const validRootArrayKey: TestDeepKeys = 'cats';
+export const validArrayLeafObjectKey: TestDeepKeys = 'cats.breed';
+export const validArrayNestedObjectKey: TestDeepKeys = 'cats.address';
+export const validArrayObjectLeafKey: TestDeepKeys = 'cats.address.suburb';
+export const validArrayObjectNestedKey: TestDeepKeys = 'cats.address.lines';
+export const validArrayObjectObjectLeafKey: TestDeepKeys = 'cats.address.lines.lines1';
+export const validArrayArrayNestedKey: TestDeepKeys = 'cats.contact';
+export const validArrayArrayLeafKey: TestDeepKeys = 'cats.contact.mobile';
 
-// Valid nested key
-export const validnestedLevelKey: TestLeafKeys = 'dog.owner.name';
+// @ts-expect-error - invalid root object key
+export const invalidRootObjectKey: TestDeepKeys = 'invalid';
 
 // @ts-expect-error - invalid nested object key
-export const invalidNestedObjectKey: TestLeafKeys = 'dog.owner';
+export const invalidNestedObjectKey: TestDeepKeys = 'dog.invalid';
 
-// @ts-expect-error - bar is optional and should not be captured as undefined
-export const invalidOptionalKey: TestLeafKeys = undefined;
+// @ts-expect-error - invalid nested key
+export const invalidLeafObjectKey: TestDeepKeys = 'dog.owner.invalid';
+
+// @ts-expect-error - invalid array object key
+export const invalidArrayLeafObjectKey: TestDeepKeys = 'cats.invalid';
+
+// @ts-expect-error - invalid array object  key
+export const invalidArrayObjectLeafKey: TestDeepKeys = 'cats.address.invalid';
+
+// @ts-expect-error - invalid array object leafkey
+export const invalidArrayObjectObjectLeafKey: TestDeepKeys = 'cats.address.lines.invalid';
+
+/**
+ * ExtractDotNotation tests
+ */
+interface IBar {
+  baz: boolean;
+}
+class Qux {
+  quux: string;
+}
+enum ThudEnum {
+  FOO = 'foo',
+  BAR = 'bar',
+}
+type HogeUnion = 'unionOne' | 'unionTwo';
+type TestNestedObject = {
+  foo?: string;
+  bar?: IBar;
+  qux: Qux;
+  corge: Array<number>;
+  grault: Array<{
+    garply: number;
+    waldo: IBar;
+    fred: Array<Qux>;
+  }>;
+  plugh?: {
+    xyzzy: string;
+  };
+  thud: ThudEnum;
+  hoge: HogeUnion;
+  fuga: {
+    thud: ThudEnum;
+    hoge: HogeUnion;
+  };
+};
+export type ValidConstrainedRootGenericParam = ExtractDotNotation<TestNestedObject, 'foo'>;
+export type ValidConstrainedNestedGenericParam = ExtractDotNotation<TestNestedObject, 'bar.baz'>;
+// @ts-expect-error - invalid generic param
+export type InvalidConstrainedRootGenericParam = ExtractDotNotation<TestNestedObject, 'invalid'>;
+// @ts-expect-error - invalid generic param
+export type InvalidConstrainedNestedGenericParam = ExtractDotNotation<TestNestedObject, 'bar.invalid'>;
+
+export const validRootValue: ExtractDotNotation<TestNestedObject, 'foo'> = 'something';
+export const validRootInterfaceValue: ExtractDotNotation<TestNestedObject, 'bar'> = {
+  baz: true,
+};
+export const validRootClassValue: ExtractDotNotation<TestNestedObject, 'qux'> = {
+  quux: 'something',
+};
+export const validRootSimpleArrayValue: ExtractDotNotation<TestNestedObject, 'corge'> = [1, 2, 3];
+export const validRootComplexArrayValue: ExtractDotNotation<TestNestedObject, 'grault'> = [
+  {
+    garply: 123,
+    waldo: {
+      baz: true,
+    },
+    fred: [
+      {
+        quux: 'something',
+      },
+    ],
+  },
+];
+export const validRootOptionalObjectValue: ExtractDotNotation<TestNestedObject, 'plugh'> = {
+  xyzzy: 'something',
+};
+export const validRootEnumValue: ExtractDotNotation<TestNestedObject, 'thud'> = ThudEnum.FOO;
+
+export const validNestedInterfaceValue: ExtractDotNotation<TestNestedObject, 'bar.baz'> = true;
+export const validNestedClassValue: ExtractDotNotation<TestNestedObject, 'qux.quux'> = 'something';
+export const validNestedOptionalObjectValue: ExtractDotNotation<TestNestedObject, 'plugh.xyzzy'> = 'something';
+
+// @ts-expect-error - invalid enum value
+export const invalidRootEnumValue: ExtractDotNotation<TestNestedObject, 'thud'> = 'invalidEnum';
--- libs/dal/src/types/helpers.ts
+++ libs/dal/src/types/helpers.ts
@@ -1,4 +1,6 @@
 import { Types } from 'mongoose';
+import { UserEntity } from '../repositories/user';
+import { AuthProviderEnum } from 'libs/shared/dist/cjs';
 
 /**
  * The type of an ObjectKey identifier.
@@ -44,20 +46,118 @@ export type TransformValues<T, U, V> = Identity<
 export type TransformEntityToDbModel<T> = TransformValues<TransformValues<T, ObjectIdKey, ObjectIdType>, DateKey, Date>;
 
 /**
- * Construct a type with a `.` prefixed to `T` if `T` is not an empty string
+ * Types that should stop the recursion of the flattened leaf keys.
  */
-export type DotPrefix<T extends string> = T extends '' ? '' : `.${T}`;
+type StopTypes = number | string | boolean | symbol | bigint | Date;
 
 /**
- * Construct a union type from the leaf key paths in T
- *
- * The following reference specifically mentions Mongo as a use-case for the generic.
- * @see https://stackoverflow.com/a/68404823
+ * Types that should be excluded from the flattened leaf keys.
  */
-export type LeafKeys<T> = (
-  T extends object
-    ? { [K in Exclude<keyof T, symbol>]: `${K}${DotPrefix<LeafKeys<T[K]>>}` }[Exclude<keyof T, symbol>]
-    : ''
-) extends infer D
-  ? Extract<D, string>
-  : never;
+type ExcludedTypes = (...args: unknown[]) => unknown;
+
+/**
+ * Construct a template literal type with `.` joining T and U if U is not an empty string
+ */
+export type Dot<T extends string, U extends string> = '' extends U ? T : `${T}.${U}`;
+
+/**
+ * Construct a type union from the key paths in T.
+ *
+ * Array objects are flattened to their item keys.
+ *
+ * @example
+ * type TestNestedObject = {
+ *   name: string;
+ *   address?: {
+ *     suburb: boolean;
+ *   };
+ *   phones: Array<{
+ *     mobile: number;
+ *   }>;
+ * };
+ * type Test = FlattenedLeafKeys<TestNestedObject>;
+ * // type Test = "name" | "address" | "address.suburb" | "phones" | "phones.mobile"
+ */
+export type DeepKeys<T> = T extends StopTypes
+  ? ''
+  : T extends readonly unknown[]
+  ? DeepKeys<T[number]>
+  : {
+      [K in keyof T & string]: T[K] extends StopTypes
+        ? K
+        : T[K] extends ExcludedTypes
+        ? never
+        : Dot<K, DeepKeys<T[K]>> | K;
+    }[keyof T & string];
+
+/**
+ * Extract the type of TObject at TPath by dot notation
+ */
+export type ExtractDotNotation<
+  TObject,
+  TPath extends DeepKeys<TObject>,
+  /*
+   * The objects that we recurse into. Constrained to `Required` so we don't
+   * need to check for optional properties when accessing the object
+   */
+  TReqObject = Required<TObject>
+> =
+  // Check if the path contains a dot and infer the types. Constraining TKey so we don't need to check if its keyof TObject
+  TPath extends `${infer TKey extends keyof TReqObject & string}.${infer TRest}`
+    ? // Check if the value of the key is an array
+      TReqObject[TKey] extends readonly unknown[]
+      ? // If the value is an array, recurse into the array type. This allows us to flatten the array
+        TRest extends DeepKeys<TReqObject[TKey][number]>
+        ? // Constraining TRest to the required type for array object access
+          ExtractDotNotation<TReqObject[TKey][number], TRest>
+        : never
+      : // Else the value is not an array, recurse into the object type
+      TRest extends DeepKeys<TReqObject[TKey]>
+      ? // Constraining TRest to the required type for object access
+        ExtractDotNotation<TReqObject[TKey], TRest>
+      : never
+    : // Else we have arrived at a leaf. Check if the key exists in the object
+    TPath extends keyof TReqObject
+    ? // If the key exists in object, return the value
+      TReqObject[TPath]
+    : // Else the key does not exist, return never. This handles the prototype chain methods such as `toString`
+      never;
+
+interface IBar {
+  baz: boolean;
+}
+type TestNestedObject = {
+  foo?: string;
+  bar?: IBar;
+  qux: Array<{
+    quux: number;
+  }>;
+};
+
+const test4: FlattenedLeaf<TestNestedObject> = {
+  foo: 'foo',
+  'qux.quux': 1,
+  qux: [
+    {
+      quux: 1,
+    },
+  ],
+  'bar.baz': true,
+};
+
+type te4 = keyof TestNestedObject;
+const t4: te4 = 'foo';
+
+const dotNotedOrg: ExtractDotNotation<UserEntity, 'resetTokenCount.reqInDay'> = 'ltr';
+
+export type FlattenedLeaf<T> = {
+  [Key in DeepKeys<T>]: ExtractDotNotation<T, Key>;
+};
+
+const partialDotNotedOrg: Partial<FlattenedLeaf<UserEntity>> = {
+  'resetTokenCount.reqInDay': 1,
+  'tokens.provider': AuthProviderEnum.GITHUB,
+  'servicesHashes.intercom': '123',
+};
+
+type test6 = ExtractDotNotation<Array<string>, ''>;
