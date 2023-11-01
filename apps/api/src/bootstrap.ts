f5c8358297deeb7c097036e5de427dcc024e2a43
Gosha <djabarov.george@gmail.com>
2023-11-01 10:36:44 +0200

    chore: remove console log

--- apps/api/src/bootstrap.ts
+++ apps/api/src/bootstrap.ts
@@ -23,9 +23,6 @@ import * as packageJson from '../package.json';
 
 const extendedBodySizeRoutes = ['/v1/events', '/v1/notification-templates', '/v1/workflows', '/v1/layouts'];
 
-// eslint-disable-next-line no-console
-console.log('trigger ci');
-
 if (process.env.SENTRY_DSN) {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
