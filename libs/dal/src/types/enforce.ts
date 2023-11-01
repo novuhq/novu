99ab61900ebda0d3311af023b2d2bfa5b3338b44
Richard Fontein <32132657+rifont@users.noreply.github.com>
2023-11-01 18:31:31 +0000

    fix(dal): Fix enforce type

--- libs/dal/src/types/enforce.ts
+++ libs/dal/src/types/enforce.ts
@@ -3,4 +3,4 @@ import { ObjectIdType } from './helpers';
 export type EnforceOrgId = { _organizationId: ObjectIdType };
 export type EnforceEnvId = { _environmentId: ObjectIdType };
 export type EnforceEnvOrOrgIds = EnforceOrgId | EnforceEnvId;
-export type NoEnforce = Record<string, unknown>;
+export type NoEnforce = object;
