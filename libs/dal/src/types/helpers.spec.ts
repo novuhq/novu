c062060adfa13823885b1885fdab38b55c92d3d1
Richard Fontein <32132657+rifont@users.noreply.github.com>
2023-11-02 13:54:22 +0000

    fix(dal): Add tests for all dot notation generic types

--- libs/dal/src/types/helpers.spec.ts
+++ libs/dal/src/types/helpers.spec.ts
@@ -1,5 +1,5 @@
 import { Types } from 'mongoose';
-import { ObjectIdKey, TransformValues, TransformEntityToDbModel, Dot, DeepKeys, ExtractDotNotation } from './helpers';
+import { ObjectIdKey, TransformValues, TransformEntityToDbModel, Dot, DeepKeys, ExtractDot } from './helpers';
 
 /**
  * ObjectIdKey tests
@@ -187,6 +187,9 @@ export const invalidArrayObjectLeafKey: TestDeepKeys = 'cats.address.invalid';
 // @ts-expect-error - invalid array object leafkey
 export const invalidArrayObjectObjectLeafKey: TestDeepKeys = 'cats.address.lines.invalid';
 
+// @ts-expect-error - Type 'string' is not assignable to type 'never'
+export const invalidSingleDepthStopTypeKey: DeepKeys<string> = 'invalid';
+
 /**
  * ExtractDotNotation tests
  */
@@ -198,65 +201,327 @@ class Qux {
 }
 enum ThudEnum {
   FOO = 'foo',
-  BAR = 'bar',
 }
 type HogeUnion = 'unionOne' | 'unionTwo';
-type TestNestedObject = {
-  foo?: string;
-  bar?: IBar;
+type PiyoFunc = () => void;
+type TestDotObj = {
+  foo: string;
+  fuga: number;
+  hodo: boolean;
+  thud: ThudEnum;
+  hoge: HogeUnion;
+  frob: symbol;
+  mung: Date;
+  piyo: PiyoFunc;
   qux: Qux;
-  corge: Array<number>;
-  grault: Array<{
-    garply: number;
-    waldo: IBar;
-    fred: Array<Qux>;
+  bar: IBar;
+  tutu: {
+    titi: string;
+  };
+  toto: Array<{
+    hodo: string;
   }>;
+  corge: Array<number>;
+  hogera?: string;
   plugh?: {
     xyzzy: string;
   };
-  thud: ThudEnum;
-  hoge: HogeUnion;
-  fuga: {
+  quux?: Array<number>;
+  waldo?: Array<{
+    baz: boolean;
+  }>;
+  hede: {
+    foo: string;
+    fuga: number;
+    hodo: boolean;
     thud: ThudEnum;
     hoge: HogeUnion;
+    frob: symbol;
+    mung: Date;
+    piyo: PiyoFunc;
+    qux: Qux;
+    bar: IBar;
+    tutu: {
+      titi: string;
+    };
+    toto: Array<{
+      hodo: string;
+    }>;
+    corge: Array<number>;
+    hogera?: string;
+    plugh?: {
+      xyzzy: string;
+    };
+    quux?: Array<number>;
+    waldo?: Array<{
+      baz: boolean;
+    }>;
   };
+  grault: Array<{
+    foo: string;
+    fuga: number;
+    hodo: boolean;
+    thud: ThudEnum;
+    hoge: HogeUnion;
+    frob: symbol;
+    mung: Date;
+    piyo: PiyoFunc;
+    qux: Qux;
+    bar: IBar;
+    tutu: {
+      titi: string;
+    };
+    toto: Array<{
+      hodo: string;
+    }>;
+    corge: Array<number>;
+    hogera?: string;
+    plugh?: {
+      xyzzy: string;
+    };
+    quux?: Array<number>;
+    waldo?: Array<{
+      baz: boolean;
+    }>;
+  }>;
 };
-export type ValidConstrainedRootGenericParam = ExtractDotNotation<TestNestedObject, 'foo'>;
-export type ValidConstrainedNestedGenericParam = ExtractDotNotation<TestNestedObject, 'bar.baz'>;
-// @ts-expect-error - invalid generic param
-export type InvalidConstrainedRootGenericParam = ExtractDotNotation<TestNestedObject, 'invalid'>;
-// @ts-expect-error - invalid generic param
-export type InvalidConstrainedNestedGenericParam = ExtractDotNotation<TestNestedObject, 'bar.invalid'>;
 
-export const validRootValue: ExtractDotNotation<TestNestedObject, 'foo'> = 'something';
-export const validRootInterfaceValue: ExtractDotNotation<TestNestedObject, 'bar'> = {
-  baz: true,
+// GENERIC PARAMETER TESTS
+export type ValidConstrainedRootGenericParam = ExtractDot<TestDotObj, 'foo'>;
+export type ValidConstrainedNestedGenericParam = ExtractDot<TestDotObj, 'bar.baz'>;
+// @ts-expect-error - invalid generic root param
+export type InvalidConstrainedRootGenericParam = ExtractDot<TestDotObj, 'invalid'>;
+// @ts-expect-error - invalid generic nested param
+export type InvalidConstrainedNestedGenericParam = ExtractDot<TestDotObj, 'bar.invalid'>;
+
+// ROOT VALUE TESTS
+export const validRootStringValue: ExtractDot<TestDotObj, 'foo'> = 'something';
+export const validRootNumberValue: ExtractDot<TestDotObj, 'fuga'> = 123;
+export const validRootBooleanValue: ExtractDot<TestDotObj, 'hodo'> = true;
+export const validRootEnumValue: ExtractDot<TestDotObj, 'thud'> = ThudEnum.FOO;
+export const validRootUnionValue: ExtractDot<TestDotObj, 'hoge'> = 'unionOne';
+export const validRootSymbolValue: ExtractDot<TestDotObj, 'frob'> = Symbol('something');
+export const validRootDateValue: ExtractDot<TestDotObj, 'mung'> = new Date();
+export const validRootFunctionValue: ExtractDot<TestDotObj, 'piyo'> = () => {};
+export const validRootInterfaceValue: ExtractDot<TestDotObj, 'bar'> = { baz: true };
+export const validRootClassValue: ExtractDot<TestDotObj, 'qux'> = { quux: 'something' };
+export const validRootSimpleObjectValue: ExtractDot<TestDotObj, 'tutu'> = { titi: 'something' };
+export const validRootArraySimpleObjectValue: ExtractDot<TestDotObj, 'toto'> = [{ hodo: 'something' }];
+export const validRootSimpleArrayValue: ExtractDot<TestDotObj, 'corge'> = [1, 2, 3];
+export const validRootOptionalValue: ExtractDot<TestDotObj, 'hogera'> = 'something';
+export const validRootOptionalObjectValue: ExtractDot<TestDotObj, 'plugh'> = { xyzzy: 'something' };
+export const validRootComplexObjectValue: ExtractDot<TestDotObj, 'hede'> = {
+  foo: 'something',
+  fuga: 123,
+  hodo: true,
+  thud: ThudEnum.FOO,
+  hoge: 'unionOne',
+  frob: Symbol('something'),
+  mung: new Date(),
+  piyo: () => {},
+  bar: { baz: true },
+  qux: { quux: 'something' },
+  tutu: { titi: 'something' },
+  toto: [{ hodo: 'something' }],
+  corge: [1, 2, 3],
+  hogera: 'something',
+  plugh: { xyzzy: 'something' },
 };
-export const validRootClassValue: ExtractDotNotation<TestNestedObject, 'qux'> = {
-  quux: 'something',
-};
-export const validRootSimpleArrayValue: ExtractDotNotation<TestNestedObject, 'corge'> = [1, 2, 3];
-export const validRootComplexArrayValue: ExtractDotNotation<TestNestedObject, 'grault'> = [
+export const validRootComplexArrayValue: ExtractDot<TestDotObj, 'grault'> = [
   {
-    garply: 123,
-    waldo: {
-      baz: true,
-    },
-    fred: [
-      {
-        quux: 'something',
-      },
-    ],
+    foo: 'something',
+    fuga: 123,
+    hodo: true,
+    thud: ThudEnum.FOO,
+    hoge: 'unionOne',
+    frob: Symbol('something'),
+    mung: new Date(),
+    piyo: () => {},
+    bar: { baz: true },
+    qux: { quux: 'something' },
+    tutu: { titi: 'something' },
+    toto: [{ hodo: 'something' }],
+    corge: [1, 2, 3],
+    hogera: 'something',
+    plugh: { xyzzy: 'something' },
   },
 ];
-export const validRootOptionalObjectValue: ExtractDotNotation<TestNestedObject, 'plugh'> = {
-  xyzzy: 'something',
-};
-export const validRootEnumValue: ExtractDotNotation<TestNestedObject, 'thud'> = ThudEnum.FOO;
 
-export const validNestedInterfaceValue: ExtractDotNotation<TestNestedObject, 'bar.baz'> = true;
-export const validNestedClassValue: ExtractDotNotation<TestNestedObject, 'qux.quux'> = 'something';
-export const validNestedOptionalObjectValue: ExtractDotNotation<TestNestedObject, 'plugh.xyzzy'> = 'something';
-
+// @ts-expect-error - invalid string value
+export const invalidRootStringValue: ExtractDot<TestDotObj, 'foo'> = 123;
+// @ts-expect-error - invalid number value
+export const invalidRootNumberValue: ExtractDot<TestDotObj, 'fuga'> = 'invalidNumber';
+// @ts-expect-error - invalid boolean value
+export const invalidRootBooleanValue: ExtractDot<TestDotObj, 'hodo'> = 'invalidBoolean';
 // @ts-expect-error - invalid enum value
-export const invalidRootEnumValue: ExtractDotNotation<TestNestedObject, 'thud'> = 'invalidEnum';
+export const invalidRootEnumValue: ExtractDot<TestDotObj, 'thud'> = 'invalidEnum';
+// @ts-expect-error - invalid union value
+export const invalidRootUnionValue: ExtractDot<TestDotObj, 'hoge'> = 'invalidUnion';
+// @ts-expect-error - invalid symbol value
+export const invalidRootSymbolValue: ExtractDot<TestDotObj, 'frob'> = 'invalidSymbol';
+// @ts-expect-error - invalid date value
+export const invalidRootDateValue: ExtractDot<TestDotObj, 'mung'> = 'invalidDate';
+// @ts-expect-error - invalid function value
+export const invalidRootFunctionValue: ExtractDot<TestDotObj, 'piyo'> = 'invalidFunction';
+// @ts-expect-error - invalid interface value
+export const invalidRootInterfaceValue: ExtractDot<TestDotObj, 'bar'> = 'invalidInterface';
+// @ts-expect-error - invalid class value
+export const invalidRootClassValue: ExtractDot<TestDotObj, 'qux'> = 'invalidClass';
+// @ts-expect-error - invalid simple object value
+export const invalidRootSimpleObjectValue: ExtractDot<TestDotObj, 'tutu'> = 'invalidSimpleObject';
+// @ts-expect-error - invalid simple array value
+export const invalidRootSimpleArrayValue: ExtractDot<TestDotObj, 'corge'> = 'invalidSimpleArray';
+// @ts-expect-error - invalid optional value
+export const invalidRootOptionalValue: ExtractDot<TestDotObj, 'hogera'> = undefined;
+// @ts-expect-error - invalid optional object value
+export const invalidRootOptionalObjectValue: ExtractDot<TestDotObj, 'plugh'> = 'invalidOptionalObject';
+// @ts-expect-error - invalid complex object value
+export const invalidRootComplexObjectValue: ExtractDot<TestDotObj, 'hede'> = 'invalidComplexObject';
+// @ts-expect-error - invalid complex array value
+export const invalidRootComplexArrayValue: ExtractDot<TestDotObj, 'grault'> = 'invalidComplexArray';
+// @ts-expect-error - missing 'titi' property
+export const invalidRootObjectValue: ExtractDot<TestDotObj, 'tutu'> = {};
+// @ts-expect-error - missing 'hodo' property
+export const invalidRootArrayObjectValue: ExtractDot<TestDotObj, 'toto'> = [{}];
+export const invalidRootObjectPropertyValue: ExtractDot<TestDotObj, 'tutu'> = {
+  // @ts-expect-error - 'invalid' does not exist in object
+  invalid: true,
+  titi: 'something',
+};
+export const invalidRootArrayObjectPropertyValue: ExtractDot<TestDotObj, 'toto'> = [
+  {
+    // @ts-expect-error - 'invalid' does not exist in object
+    invalid: true,
+    hodo: 'something',
+  },
+];
+
+// ROOT OBJECT VALUE TESTS
+export const validNestedObjectStringValue: ExtractDot<TestDotObj, 'hede.foo'> = 'something';
+export const validNestedObjectNumberValue: ExtractDot<TestDotObj, 'hede.fuga'> = 123;
+export const validNestedObjectBooleanValue: ExtractDot<TestDotObj, 'hede.hodo'> = true;
+export const validNestedObjectEnumValue: ExtractDot<TestDotObj, 'hede.thud'> = ThudEnum.FOO;
+export const validNestedObjectUnionValue: ExtractDot<TestDotObj, 'hede.hoge'> = 'unionOne';
+export const validNestedObjectSymbolValue: ExtractDot<TestDotObj, 'hede.frob'> = Symbol('something');
+export const validNestedObjectDateValue: ExtractDot<TestDotObj, 'hede.mung'> = new Date();
+export const validNestedObjectFunctionValue: ExtractDot<TestDotObj, 'hede.piyo'> = () => {};
+export const validNestedObjectInterfaceValue: ExtractDot<TestDotObj, 'hede.bar'> = { baz: true };
+export const validNestedObjectClassValue: ExtractDot<TestDotObj, 'hede.qux'> = { quux: 'something' };
+export const validNestedObjectSimpleObjectValue: ExtractDot<TestDotObj, 'hede.tutu'> = { titi: 'something' };
+export const validNestedObjectSimpleArrayValue: ExtractDot<TestDotObj, 'hede.corge'> = [1, 2, 3];
+export const validNestedObjectOptionalValue: ExtractDot<TestDotObj, 'hede.hogera'> = 'something';
+export const validNestedObjectNestedObjectValue: ExtractDot<TestDotObj, 'hede.plugh'> = { xyzzy: 'something' };
+
+// @ts-expect-error - invalid string value
+export const invalidNestedObjectStringValue: ExtractDot<TestDotObj, 'foo'> = 123;
+// @ts-expect-error - invalid number value
+export const invalidNestedObjectNumberValue: ExtractDot<TestDotObj, 'fuga'> = 'invalidNumber';
+// @ts-expect-error - invalid boolean value
+export const invalidNestedObjectBooleanValue: ExtractDot<TestDotObj, 'hodo'> = 'invalidBoolean';
+// @ts-expect-error - invalid enum value
+export const invalidNestedObjectEnumValue: ExtractDot<TestDotObj, 'hede.thud'> = 'invalidEnum';
+// @ts-expect-error - invalid union value
+export const invalidNestedObjectUnionValue: ExtractDot<TestDotObj, 'hede.hoge'> = 'invalidUnion';
+// @ts-expect-error - invalid symbol value
+export const invalidNestedObjectSymbolValue: ExtractDot<TestDotObj, 'hede.frob'> = 'invalidSymbol';
+// @ts-expect-error - invalid date value
+export const invalidNestedObjectDateValue: ExtractDot<TestDotObj, 'hede.mung'> = 'invalidDate';
+// @ts-expect-error - invalid function value
+export const invalidNestedObjectFunctionValue: ExtractDot<TestDotObj, 'hede.piyo'> = 'invalidFunction';
+// @ts-expect-error - invalid interface value
+export const invalidNestedObjectInterfaceValue: ExtractDot<TestDotObj, 'hede.bar'> = 'invalidInterface';
+// @ts-expect-error - invalid class value
+export const invalidNestedObjectClassValue: ExtractDot<TestDotObj, 'hede.qux'> = 'invalidClass';
+// @ts-expect-error - invalid simple object value
+export const invalidNestedObjectSimpleObjectValue: ExtractDot<TestDotObj, 'hede.tutu'> = 'invalidSimpleObject';
+// @ts-expect-error - invalid simple array value
+export const invalidNestedObjectSimpleArrayValue: ExtractDot<TestDotObj, 'hede.corge'> = 'invalidSimpleArray';
+// @ts-expect-error - invalid optional value
+export const invalidNestedObjectOptionalValue: ExtractDot<TestDotObj, 'hede.hogera'> = undefined;
+// @ts-expect-error - invalid nested object value
+export const invalidNestedObjectNestedObjectValue: ExtractDot<TestDotObj, 'hede.plugh'> = 'invalidNestedObject';
+// @ts-expect-error - invalid nested object value
+export const invalidNestedObjectNestedArrayValue: ExtractDot<TestDotObj, 'hede.quux'> = 'invalidNestedArray';
+// @ts-expect-error - missing 'titi' property
+export const invalidNestedObjectValue: ExtractDot<TestDotObj, 'hede.tutu'> = {};
+// @ts-expect-error - missing 'hodo' property
+export const invalidNestedObjectArrayValue: ExtractDot<TestDotObj, 'hede.corge'> = [{}];
+export const invalidNestedObjectNonExistingPropertyValue: ExtractDot<TestDotObj, 'hede.waldo'> = {
+  // @ts-expect-error - 'invalid' does not exist in object
+  invalid: true,
+  baz: true,
+};
+export const invalidNestedObjectNonExistingPropertiesValue: ExtractDot<TestDotObj, 'hede'> = {
+  garply: 123,
+  // @ts-expect-error - 'invalid' does not exist in type 'IBar'
+  waldo: { baz: true, invalid: true },
+  // @ts-expect-error - 'invalid' does not exist in type 'IBar'
+  hogera: [{ baz: true, invalid: true }],
+};
+
+// ROOT ARRAY VALUE TESTS
+export const validNestedArrayStringValue: ExtractDot<TestDotObj, 'grault.foo'> = 'something';
+export const validNestedArrayNumberValue: ExtractDot<TestDotObj, 'grault.fuga'> = 123;
+export const validNestedArrayBooleanValue: ExtractDot<TestDotObj, 'grault.hodo'> = true;
+export const validNestedArrayEnumValue: ExtractDot<TestDotObj, 'grault.thud'> = ThudEnum.FOO;
+export const validNestedArrayUnionValue: ExtractDot<TestDotObj, 'grault.hoge'> = 'unionOne';
+export const validNestedArraySymbolValue: ExtractDot<TestDotObj, 'grault.frob'> = Symbol('something');
+export const validNestedArrayDateValue: ExtractDot<TestDotObj, 'grault.mung'> = new Date();
+export const validNestedArrayFunctionValue: ExtractDot<TestDotObj, 'grault.piyo'> = () => {};
+export const validNestedArrayInterfaceValue: ExtractDot<TestDotObj, 'grault.bar'> = { baz: true };
+export const validNestedArrayClassValue: ExtractDot<TestDotObj, 'grault.qux'> = { quux: 'something' };
+export const validNestedArraySimpleObjectValue: ExtractDot<TestDotObj, 'grault.tutu'> = { titi: 'something' };
+export const validNestedArraySimpleArrayValue: ExtractDot<TestDotObj, 'grault.corge'> = [1, 2, 3];
+export const validNestedArrayOptionalValue: ExtractDot<TestDotObj, 'grault.hogera'> = 'something';
+export const validNestedArrayNestedObjectValue: ExtractDot<TestDotObj, 'grault.plugh'> = { xyzzy: 'something' };
+
+// @ts-expect-error - invalid string value
+export const invalidNestedArrayStringValue: ExtractDot<TestDotObj, 'foo'> = 123;
+// @ts-expect-error - invalid number value
+export const invalidNestedArrayNumberValue: ExtractDot<TestDotObj, 'fuga'> = 'invalidNumber';
+// @ts-expect-error - invalid boolean value
+export const invalidNestedArrayBooleanValue: ExtractDot<TestDotObj, 'hodo'> = 'invalidBoolean';
+// @ts-expect-error - invalid enum value
+export const invalidNestedArrayEnumValue: ExtractDot<TestDotObj, 'grault.thud'> = 'invalidEnum';
+// @ts-expect-error - invalid union value
+export const invalidNestedArrayUnionValue: ExtractDot<TestDotObj, 'grault.hoge'> = 'invalidUnion';
+// @ts-expect-error - invalid symbol value
+export const invalidNestedArraySymbolValue: ExtractDot<TestDotObj, 'grault.frob'> = 'invalidSymbol';
+// @ts-expect-error - invalid date value
+export const invalidNestedArrayDateValue: ExtractDot<TestDotObj, 'grault.mung'> = 'invalidDate';
+// @ts-expect-error - invalid function value
+export const invalidNestedArrayFunctionValue: ExtractDot<TestDotObj, 'grault.piyo'> = 'invalidFunction';
+// @ts-expect-error - invalid interface value
+export const invalidNestedArrayInterfaceValue: ExtractDot<TestDotObj, 'grault.bar'> = 'invalidInterface';
+// @ts-expect-error - invalid class value
+export const invalidNestedArrayClassValue: ExtractDot<TestDotObj, 'grault.qux'> = 'invalidClass';
+// @ts-expect-error - invalid simple object value
+export const invalidNestedArraySimpleObjectValue: ExtractDot<TestDotObj, 'grault.tutu'> = 'invalidSimpleObject';
+// @ts-expect-error - invalid simple array value
+export const invalidNestedArraySimpleArrayValue: ExtractDot<TestDotObj, 'grault.corge'> = 'invalidSimpleArray';
+// @ts-expect-error - invalid optional value
+export const invalidNestedArrayOptionalValue: ExtractDot<TestDotObj, 'grault.hogera'> = undefined;
+// @ts-expect-error - invalid nested object value
+export const invalidNestedArrayNestedObjectValue: ExtractDot<TestDotObj, 'grault.plugh'> = 'invalidNestedArray';
+// @ts-expect-error - invalid nested object value
+export const invalidNestedArrayNestedArrayValue: ExtractDot<TestDotObj, 'grault.quux'> = 'invalidNestedArray';
+// @ts-expect-error - missing 'titi' property
+export const invalidNestedArrayValue: ExtractDot<TestDotObj, 'grault.tutu'> = {};
+// @ts-expect-error - missing 'hodo' property
+export const invalidNestedArrayArrayValue: ExtractDot<TestDotObj, 'grault.corge'> = [{}];
+export const invalidNestedArrayNonExistingPropertyValue: ExtractDot<TestDotObj, 'grault.waldo'> = [
+  {
+    // @ts-expect-error - 'invalid' does not exist in object
+    invalid: true,
+    baz: true,
+  },
+];
+export const invalidNestedArrayNonExistingPropertiesValue: ExtractDot<TestDotObj, 'grault'> = [
+  {
+    garply: 123,
+    // @ts-expect-error - 'invalid' does not exist in type 'IBar'
+    waldo: { baz: true, invalid: true },
+    // @ts-expect-error - 'invalid' does not exist in type 'IBar'
+    hogera: [{ baz: true, invalid: true }],
+  },
+];
--- libs/dal/src/types/helpers.ts
+++ libs/dal/src/types/helpers.ts
@@ -46,14 +46,9 @@ export type TransformValues<T, U, V> = Identity<
 export type TransformEntityToDbModel<T> = TransformValues<TransformValues<T, ObjectIdKey, ObjectIdType>, DateKey, Date>;
 
 /**
- * Types that should stop the recursion of the flattened leaf keys.
+ * Types that should stop the recursion of the deep keys.
  */
 type StopTypes = number | string | boolean | symbol | bigint | Date;
-
-/**
- * Types that should be excluded from the flattened leaf keys.
- */
-type ExcludedTypes = (...args: unknown[]) => unknown;
 
 /**
  * Construct a template literal type with `.` joining T and U if U is not an empty string
@@ -79,21 +74,17 @@ export type Dot<T extends string, U extends string> = '' extends U ? T : `${T}.$
  * // type Test = "name" | "address" | "address.suburb" | "phones" | "phones.mobile"
  */
 export type DeepKeys<T> = T extends StopTypes
-  ? ''
+  ? never
   : T extends readonly unknown[]
   ? DeepKeys<T[number]>
   : {
-      [K in keyof T & string]: T[K] extends StopTypes
-        ? K
-        : T[K] extends ExcludedTypes
-        ? never
-        : Dot<K, DeepKeys<T[K]>> | K;
+      [K in keyof T & string]: T[K] extends StopTypes ? K : Dot<K, DeepKeys<T[K]>> | K;
     }[keyof T & string];
 
 /**
  * Extract the type of TObject at TPath by dot notation
  */
-export type ExtractDotNotation<
+export type ExtractDot<
   TObject,
   TPath extends DeepKeys<TObject>,
   /*
@@ -102,62 +93,35 @@ export type ExtractDotNotation<
    */
   TReqObject = Required<TObject>
 > =
-  // Check if the path contains a dot and infer the types. Constraining TKey so we don't need to check if its keyof TObject
+  /*
+   * Check if the path contains a dot and infer the types.
+   * Constraining TKey so we don't need to check if it is keyof TObject
+   */
   TPath extends `${infer TKey extends keyof TReqObject & string}.${infer TRest}`
     ? // Check if the value of the key is an array
       TReqObject[TKey] extends readonly unknown[]
       ? // If the value is an array, recurse into the array type. This allows us to flatten the array
         TRest extends DeepKeys<TReqObject[TKey][number]>
-        ? // Constraining TRest to the required type for array object access
-          ExtractDotNotation<TReqObject[TKey][number], TRest>
+        ? // Constraining the array object to the required type for array object access
+          ExtractDot<TReqObject[TKey][number], TRest>
         : never
       : // Else the value is not an array, recurse into the object type
       TRest extends DeepKeys<TReqObject[TKey]>
-      ? // Constraining TRest to the required type for object access
-        ExtractDotNotation<TReqObject[TKey], TRest>
+      ? // Constraining the object to the required type for object access
+        ExtractDot<TReqObject[TKey], TRest>
       : never
     : // Else we have arrived at a leaf. Check if the key exists in the object
     TPath extends keyof TReqObject
-    ? // If the key exists in object, return the value
-      TReqObject[TPath]
-    : // Else the key does not exist, return never. This handles the prototype chain methods such as `toString`
+    ? TReqObject[TPath]
+    : // Else the key does not exist, return never.
       never;
 
-interface IBar {
-  baz: boolean;
-}
-type TestNestedObject = {
-  foo?: string;
-  bar?: IBar;
-  qux: Array<{
-    quux: number;
-  }>;
+export type FlatKeys<T> = {
+  [Key in DeepKeys<T>]: ExtractDot<T, Key>;
 };
 
-const test4: FlattenedLeaf<TestNestedObject> = {
-  foo: 'foo',
-  'qux.quux': 1,
-  qux: [
-    {
-      quux: 1,
-    },
-  ],
-  'bar.baz': true,
-};
-
-type te4 = keyof TestNestedObject;
-const t4: te4 = 'foo';
-
-const dotNotedOrg: ExtractDotNotation<UserEntity, 'resetTokenCount.reqInDay'> = 'ltr';
-
-export type FlattenedLeaf<T> = {
-  [Key in DeepKeys<T>]: ExtractDotNotation<T, Key>;
-};
-
-const partialDotNotedOrg: Partial<FlattenedLeaf<UserEntity>> = {
+const partialDotNotedOrg: Partial<FlatKeys<UserEntity>> = {
   'resetTokenCount.reqInDay': 1,
   'tokens.provider': AuthProviderEnum.GITHUB,
   'servicesHashes.intercom': '123',
 };
-
-type test6 = ExtractDotNotation<Array<string>, ''>;
