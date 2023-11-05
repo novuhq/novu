d03308169edb83c898a4fa18a73a39b476360d5a
Dima Grossman <dima@grossman.io>
2023-11-05 21:35:58 +0200

    fix: clicksend refactor

--- providers/clicksend/src/lib/clicksend.provider.ts
+++ providers/clicksend/src/lib/clicksend.provider.ts
@@ -4,48 +4,44 @@ import {
   ISmsOptions,
   ISmsProvider,
 } from '@novu/stateless';
-import axios, {AxiosInstance} from 'axios';
-
-
+import axios from 'axios';
 
 export class ClicksendSmsProvider implements ISmsProvider {
   id = 'clicksend';
   channelType = ChannelTypeEnum.SMS as ChannelTypeEnum.SMS;
-
-  private axiosInstance: AxiosInstance;
-
 
   constructor(
     private config: {
       username: string;
       apiKey: string;
     }
-  ) {
-   
-    this.axiosInstance = axios.create({
-      baseURL: "https://rest.clicksend.com/v3",
-      headers: {
-        username: config.username,
-        password: config.apiKey
-      }
-    })
-  }
+  ) {}
 
   async sendMessage(
     options: ISmsOptions
   ): Promise<ISendMessageSuccessResponse> {
-
-    const smsOptions = {
-      source: "sdk",
-      to: options.to,
-      body: options.content
-    }
-
-    const res = await this.axiosInstance.post("/sms/send", { messages: [smsOptions] });
+    const response = await axios.post(
+      'https://rest.clicksend.com/v3/sms/send',
+      {
+        messages: [
+          {
+            to: options.to,
+            body: options.content,
+          },
+        ],
+      },
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `${this.config.username}:${this.config.apiKey}`
+          ).toString('base64')}`,
+        },
+      }
+    );
 
     return {
-      id: res.data.data.messages[0].message_id,
-      date: res.data.data.messages[0].date,
+      id: response.data.data.messages[0].message_id,
+      date: response.data.data.messages[0].date,
     };
   }
 }
