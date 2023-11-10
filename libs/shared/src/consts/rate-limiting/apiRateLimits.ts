179586b8f2b5023492f80fd28ec80c26c101bb26
Richard Fontein <32132657+rifont@users.noreply.github.com>
2023-11-10 11:43:42 +0000

    chore(shared): Remove redundant constant

--- libs/shared/src/consts/rate-limiting/apiRateLimits.ts
+++ libs/shared/src/consts/rate-limiting/apiRateLimits.ts
@@ -1,15 +1,6 @@
 import { ApiServiceLevelTypeEnum, ApiRateLimitCategoryTypeEnum, IServiceApiRateLimits } from '../../types';
 
 /**
- * A decimal 0 > `x` <= 1 determining the proportion of default rate limit
- * applied as a default for non-production environments.
- *
- * For example an `x` of 0.25 would result in a rate limit for
- * non-production environments of 25% of the default limits.
- */
-export const DEFAULT_NON_PRODUCTION_ENVIRONMENT_API_RATE_LIMIT_MULTIPLIER = 1;
-
-/**
  * API Rate Limiting defaults applied to production environments.
  * Value units are requests per second.
  */
