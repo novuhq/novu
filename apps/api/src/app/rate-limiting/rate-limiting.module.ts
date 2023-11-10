a7d672bf6b717fb9493f992a80ff9742ba64f22b
Richard Fontein <32132657+rifont@users.noreply.github.com>
2023-11-10 17:10:11 +0000

    fix(api): Add module import for get-api-rate-limit use case

--- apps/api/src/app/rate-limiting/rate-limiting.module.ts
+++ apps/api/src/app/rate-limiting/rate-limiting.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { USE_CASES } from './usecases';
+import { SharedModule } from '../shared/shared.module';
 
 @Module({
+  imports: [SharedModule],
   providers: [...USE_CASES],
   exports: [...USE_CASES],
 })
