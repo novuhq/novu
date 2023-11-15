ad845529ca05ddcc483e29391e190ca3aeef021f
Richard Fontein <32132657+rifont@users.noreply.github.com>
2023-11-15 21:38:05 +0000

    feat(api): Add @nestjs/throttler and rate limit guard to RateLimitingModule

--- apps/api/src/app/rate-limiting/rate-limiting.module.ts
+++ apps/api/src/app/rate-limiting/rate-limiting.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { USE_CASES } from './usecases';
 import { SharedModule } from '../shared/shared.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { ApiRateLimitGuard } from './guards';
 
 @Module({
-  imports: [SharedModule],
-  providers: [...USE_CASES],
-  exports: [...USE_CASES],
+  imports: [
+    SharedModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
+  providers: [...USE_CASES, ApiRateLimitGuard],
+  exports: [...USE_CASES, ApiRateLimitGuard],
 })
 export class RateLimitingModule {}
