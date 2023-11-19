9f7cec8e84bdc1803922b4b8682a9121c4ab398d
Dima Grossman <dima@grossman.io>
2023-11-19 11:42:10 +0200

    refactor(ws): refactor handle disconnect and add newrelic events

--- libs/shared/src/config/job-queue.ts
+++ libs/shared/src/config/job-queue.ts
@@ -13,4 +13,6 @@ export enum ObservabilityBackgroundTransactionEnum {
   SUBSCRIBER_PROCESSING_QUEUE = 'subscriber-processing-queue',
   TRIGGER_HANDLER_QUEUE = 'trigger-handler-queue',
   WS_SOCKET_QUEUE = 'ws_socket_queue',
+  WS_HANDLE_SOCKET_CONNECTION = 'ws_handle_socket_connection',
+  WS_HANDLE_SOCKET_DISCONNECT = 'ws_handle_socket_disconnect',
 }
