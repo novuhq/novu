ffc2f9ea55199d7a29e0f8f21764719e223f0b11
Richard Fontein <32132657+rifont@users.noreply.github.com>
2023-11-30 23:55:01 +0000

    feat(api): Add eslint rule to block @nestjs swagger ApiResponse decorator imports

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -2,6 +2,40 @@ module.exports = {
   extends: ['../../.eslintrc.js'],
   rules: {
     'func-names': 'off',
+    "no-restricted-imports": ["error", {
+      "patterns": [{
+        "group": ["@nestjs/swagger"],
+        "importNames": [
+          'ApiOkResponse',
+          'ApiCreatedResponse',
+          'ApiAcceptedResponse',
+          'ApiNoContentResponse',
+          'ApiMovedPermanentlyResponse',
+          'ApiFoundResponse',
+          'ApiBadRequestResponse',
+          'ApiUnauthorizedResponse',
+          'ApiTooManyRequestsResponse',
+          'ApiNotFoundResponse',
+          'ApiInternalServerErrorResponse',
+          'ApiBadGatewayResponse',
+          'ApiConflictResponse',
+          'ApiForbiddenResponse',
+          'ApiGatewayTimeoutResponse',
+          'ApiGoneResponse',
+          'ApiMethodNotAllowedResponse',
+          'ApiNotAcceptableResponse',
+          'ApiNotImplementedResponse',
+          'ApiPreconditionFailedResponse',
+          'ApiPayloadTooLargeResponse',
+          'ApiRequestTimeoutResponse',
+          'ApiServiceUnavailableResponse',
+          'ApiUnprocessableEntityResponse',
+          'ApiUnsupportedMediaTypeResponse',
+          'ApiDefaultResponse',
+        ],
+        "message": "Use 'Api<Error>Response' from '/shared/framework/response.decorator' instead."
+      }]
+    }]
   },
   parserOptions: {
     project: './tsconfig.json',
