d85a13c5789e273ce42c7a49d0e444aedcedcefd
Dima Grossman <dima@grossman.io>
2023-12-02 14:30:36 +0200

    fix: ws service bootstrap

--- apps/ws/src/socket/socket.module.ts
+++ apps/ws/src/socket/socket.module.ts
@@ -1,6 +1,5 @@
 import { Inject, Module, OnModuleInit, Provider } from '@nestjs/common';
-import { JobTopicNameEnum } from '@novu/shared';
-import { WebSocketsWorkerService } from '@novu/application-generic';
+import { WebSocketsWorkerService, WorkflowInMemoryProviderService } from '@novu/application-generic';
 
 import { WSGateway } from './ws.gateway';
 import { SharedModule } from '../shared/shared.module';
@@ -10,7 +9,18 @@ import { WebSocketWorker } from './services';
 
 const USE_CASES: Provider[] = [ExternalServicesRoute];
 
-const PROVIDERS: Provider[] = [WSGateway, WebSocketsWorkerService, WebSocketWorker];
+const memoryQueueService = {
+  provide: WorkflowInMemoryProviderService,
+  useFactory: async () => {
+    const memoryService = new WorkflowInMemoryProviderService();
+
+    await memoryService.initialize();
+
+    return memoryService;
+  },
+};
+
+const PROVIDERS: Provider[] = [memoryQueueService, WSGateway, WebSocketsWorkerService, WebSocketWorker];
 
 @Module({
   imports: [SharedModule],
