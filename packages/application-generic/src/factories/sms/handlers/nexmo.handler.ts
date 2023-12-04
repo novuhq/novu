e58c5848cb2b3617e5d3f5a94c84c67816b975b3
sr <sachinrana99@yahoo.com>
2023-12-04 17:46:41 +0530

    fix: version up of nexmo provider

---
+++ packages/application-generic/src/factories/sms/handlers/nexmo.handler.ts
@@ -0,0 +1,18 @@
+import { MessageBirdSmsProvider } from '@novu/messagebird';
+import { ChannelTypeEnum, ICredentials } from '@novu/shared';
+import { BaseSmsHandler } from './base.handler';
+import { NexmoSmsProvider } from '@novu/nexmo';
+
+export class NexmoHandler extends BaseSmsHandler {
+  constructor() {
+    super('nexmo', ChannelTypeEnum.SMS);
+  }
+
+  buildProvider(credentials: ICredentials) {
+    this.provider = new NexmoSmsProvider({
+      apiKey: credentials.apiKey,
+      from: credentials.from,
+      apiSecret: credentials.secretKey,
+    });
+  }
+}
