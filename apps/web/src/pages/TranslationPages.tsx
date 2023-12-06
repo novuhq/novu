470425ce45b64306825a632bbaadac0fbf89590f
David Söderberg <granskog_@hotmail.com>
2023-12-06 15:08:03 +0100

    refactor: last try this way

--- apps/web/src/pages/TranslationPages.tsx
+++ apps/web/src/pages/TranslationPages.tsx
@@ -1,15 +1,14 @@
 import { IS_DOCKER_HOSTED } from '../config';
 
-export const TranslationRoutes = () => {
-  let Routes = () => null;
-  if (IS_DOCKER_HOSTED) {
-    return <Routes />;
-  }
+let Routes = () => null;
 
+if (!IS_DOCKER_HOSTED) {
   try {
     const module = require('@novu/ee-translation-web');
     Routes = module.Routes;
   } catch (e) {}
+}
 
+export const TranslationRoutes = () => {
   return <Routes />;
 };
