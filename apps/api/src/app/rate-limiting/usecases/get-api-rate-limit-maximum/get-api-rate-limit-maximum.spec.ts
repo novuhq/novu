48a68055116e763cd66328290c48f6e8b23a6f95
Richard Fontein <32132657+rifont@users.noreply.github.com>
2023-12-12 16:59:00 +0000

    fix(api): Revert accidental change

--- apps/api/src/app/rate-limiting/usecases/get-api-rate-limit-maximum/get-api-rate-limit-maximum.spec.ts
+++ apps/api/src/app/rate-limiting/usecases/get-api-rate-limit-maximum/get-api-rate-limit-maximum.spec.ts
@@ -207,7 +207,7 @@ describe('GetApiRateLimitMaximum', async () => {
           GetApiRateLimitMaximumCommand.create({
             organizationId: session.organization._id,
             environmentId: session.environment._id,
-            apiRateLimitCategory: ApiRateLimitCategoryEnum.GLOBAL,
+            apiRateLimitCategory: mockApiRateLimitCategory,
           })
         );
         throw new Error('Should not reach here');
