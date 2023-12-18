fce9008582056353181ecbba2b85d1e804d07b13
Paweł <pawel.tymczuk@gmail.com>
2023-12-18 12:48:00 +0100

    chore(web): web ignore webpack warning for the ee-translation-web

--- apps/web/config-overrides.js
+++ apps/web/config-overrides.js
@@ -4,7 +4,15 @@ const { useBabelRc, override } = require('customize-cra');
 function overrideConfig(config, env) {
   const plugins = [...config.plugins /* new BundleAnalyzerPlugin() */];
 
-  return { ...config, plugins };
+  return {
+    ...config,
+    plugins,
+    ignoreWarnings: [
+      {
+        message: /Module not found: Error: Can't resolve \'@novu\/ee-translation-web\' .*/,
+      },
+    ],
+  };
 }
 
 module.exports = override(useBabelRc(), overrideConfig);
