ac89dcb6f47ea303e0d587a18d26e0ed3ac599f5
David Söderberg <granskog_@hotmail.com>
2023-12-18 12:12:42 +0100

    feat: add translation web to config overrides

--- apps/web/config-overrides.js
+++ apps/web/config-overrides.js
@@ -2,9 +2,15 @@ const { useBabelRc, override } = require('customize-cra');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 function overrideConfig(config, env) {
-  const plugins = [...config.plugins, /* new BundleAnalyzerPlugin() */];
-  
-  return { ...config, plugins };
+  const plugins = [...config.plugins /* new BundleAnalyzerPlugin() */];
+
+  return {
+    ...config,
+    plugins,
+    externals: {
+      '@novu/ee-translation-web': "require('@novu/ee-translation-web')",
+    },
+  };
 }
 
 module.exports = override(useBabelRc(), overrideConfig);
