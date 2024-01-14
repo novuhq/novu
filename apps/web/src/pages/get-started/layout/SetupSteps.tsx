22549df5430954a8dcb62bf3718998b753cb5180
Gosha <djabarov.george@gmail.com>
2024-01-14 17:55:53 +0200

    feat: create get started use cases layout

---
+++ apps/web/src/pages/get-started/layout/SetupSteps.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function SetupSteps() {
+  return (
+    <div>
+      <h1>Left section</h1>
+    </div>
+  );
+}
---
+++ apps/web/src/pages/get-started/layout/UseCaseDemo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function UseCaseDemo() {
+  return (
+    <div>
+      <h1>Right section</h1>
+    </div>
+  );
+}
