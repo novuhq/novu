74e6b64b5c52fcff7a4cdd2d1b033dcf0b3d9095
Joel Anton <joel@novu.co>
2024-01-23 13:13:17 -0800

    feat: Use ds Select, improve behavior

--- libs/design-system/src/pagination/PageSizeSelect.tsx
+++ libs/design-system/src/pagination/PageSizeSelect.tsx
@@ -1,30 +1,37 @@
-import { ChangeEventHandler } from 'react';
+import { forwardRef, useContext, useMemo } from 'react';
+import { ISelectProps, Select } from '../select/Select';
 import { DEFAULT_PAGINATION_PAGE_SIZES } from './Pagination.const';
+import { PaginationContext } from './PaginationContext';
 
-export interface IPageSizeSelectProps {
+export type TPageSizeSelectOption = string;
+export interface IPageSizeSelectProps extends Omit<ISelectProps, 'onChange'> {
   onPageSizeChange: (pageSize: number) => void;
-  pageSizes?: number[];
+  pageSizes?: (number | TPageSizeSelectOption)[];
+  className?: string;
 }
 
 /**
  * Component for selecting the desired page size for Pagination.
  * @requires this component to be a child of a Pagination component
  */
-export const PageSizeSelect: React.FC<IPageSizeSelectProps> = ({
-  onPageSizeChange,
-  pageSizes = DEFAULT_PAGINATION_PAGE_SIZES,
-}) => {
-  const handlePageSizeChange: ChangeEventHandler<HTMLSelectElement> = (event) => {
-    onPageSizeChange(+event.currentTarget.value);
-  };
+export const PageSizeSelect: React.FC<IPageSizeSelectProps> = forwardRef<HTMLInputElement, IPageSizeSelectProps>(
+  ({ onPageSizeChange, pageSizes = DEFAULT_PAGINATION_PAGE_SIZES, ...selectProps }, selectRef) => {
+    const { pageSize } = useContext(PaginationContext);
 
-  return (
-    <select onChange={handlePageSizeChange}>
-      {pageSizes.map((pageSize) => (
-        <option key={`row-settings-${pageSize}`} value={pageSize}>
-          {pageSize}
-        </option>
-      ))}
-    </select>
-  );
-};
+    const handlePageSizeChange = (val: string | string[]) => {
+      onPageSizeChange(+val);
+    };
+
+    const options = useMemo(() => pageSizes.map((val) => `${val}`), [pageSizes]);
+
+    return (
+      <Select
+        ref={selectRef}
+        data={options}
+        onChange={handlePageSizeChange}
+        value={selectProps.value ?? `${pageSize}`}
+        className={selectProps.className}
+      />
+    );
+  }
+);
