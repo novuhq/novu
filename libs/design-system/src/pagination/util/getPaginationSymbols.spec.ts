25b1a6c4e5fcd4044f09d6efa1346f625c17a750
Joel Anton <joel@novu.co>
2024-01-25 12:38:34 -0800

    test: Fail tests with shrinking window size

--- libs/design-system/src/pagination/util/getPaginationSymbols.spec.ts
+++ libs/design-system/src/pagination/util/getPaginationSymbols.spec.ts
@@ -1,4 +1,5 @@
 import { expect, it, describe } from 'vitest';
+import { MAX_PAGE_COUNT_WITHOUT_ELLIPSIS } from '../Pagination.const';
 import { getPaginationSymbols, PaginationSymbol } from './getPaginationSymbols';
 
 describe(getPaginationSymbols.name, () => {
@@ -10,20 +11,34 @@ describe(getPaginationSymbols.name, () => {
     expect(getPaginationSymbols({ totalPageCount: 3, currentPageNumber: 1, siblingCount: 2 })).toEqual([1, 2, 3]);
   });
 
-  it('should show all page numbers for 10 pages regardless of sibling count', () => {
-    expect(getPaginationSymbols({ totalPageCount: 10, currentPageNumber: 3, siblingCount: 0 })).toEqual([
-      1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    ]);
-    expect(getPaginationSymbols({ totalPageCount: 10, currentPageNumber: 3, siblingCount: 2 })).toEqual([
-      1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    ]);
-    expect(getPaginationSymbols({ totalPageCount: 10, currentPageNumber: 3, siblingCount: 6 })).toEqual([
-      1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    ]);
+  it('should show all page numbers for "max pages without ellipsis" regardless of sibling count', () => {
+    expect(
+      getPaginationSymbols({ totalPageCount: MAX_PAGE_COUNT_WITHOUT_ELLIPSIS, currentPageNumber: 3, siblingCount: 0 })
+    ).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    expect(
+      getPaginationSymbols({ totalPageCount: MAX_PAGE_COUNT_WITHOUT_ELLIPSIS, currentPageNumber: 3, siblingCount: 2 })
+    ).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    expect(
+      getPaginationSymbols({ totalPageCount: MAX_PAGE_COUNT_WITHOUT_ELLIPSIS, currentPageNumber: 3, siblingCount: 6 })
+    ).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   });
 
-  it('should handle being near the start smoothly', () => {
-    expect(getPaginationSymbols({ totalPageCount: 11, currentPageNumber: 4, siblingCount: 2 })).toEqual([
+  it('should show omit ellipsis and show page numbers when within the "window size" - 1 of page 1', () => {
+    expect(getPaginationSymbols({ totalPageCount: 11, currentPageNumber: 5, siblingCount: 2 })).toEqual([
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      'ELLIPSIS',
+      11,
+    ] as PaginationSymbol[]);
+  });
+
+  it('should show show ellipsis a when at "window size" or above of page 1', () => {
+    expect(getPaginationSymbols({ totalPageCount: 11, currentPageNumber: 6, siblingCount: 2 })).toEqual([
       1,
       'ELLIPSIS',
       4,
@@ -31,7 +46,6 @@ describe(getPaginationSymbols.name, () => {
       6,
       7,
       8,
-      9,
       'ELLIPSIS',
       11,
     ] as PaginationSymbol[]);
@@ -46,7 +60,6 @@ describe(getPaginationSymbols.name, () => {
       24,
       25,
       26,
-      27,
       'ELLIPSIS',
       48,
     ] as PaginationSymbol[]);
@@ -66,41 +79,62 @@ describe(getPaginationSymbols.name, () => {
     ] as PaginationSymbol[]);
   });
 
-  // FIXME: expect this to fail because I will not implement it until I get word from Nik
-  it.fails('should show asymmetric siblings for page counts >= 10000', () => {
-    expect(getPaginationSymbols({ totalPageCount: 25000, currentPageNumber: 1003, siblingCount: 2 })).toEqual([
-      1,
-      'ELLIPSIS',
-      1001,
-      1002,
-      1003,
-      1004,
-      'ELLIPSIS',
-      25000,
-    ] as PaginationSymbol[]);
-  });
+  /** FIXME: tests for Nik's shrinking window size + asymmetric windows */
+  describe('tests that rely upon a window size that changes based on total page count', () => {
+    // FIXME: expect this to fail because I will not implement asymmetry until I get word from Nik
+    it.fails('should show assymetric siblings for a >10 pages', () => {
+      expect(getPaginationSymbols({ totalPageCount: 48, currentPageNumber: 24, siblingCount: 2 })).toEqual([
+        1,
+        'ELLIPSIS',
+        22,
+        23,
+        24,
+        25,
+        26,
+        27,
+        'ELLIPSIS',
+        48,
+      ] as PaginationSymbol[]);
+    });
 
-  it('should work for 5digit page numbers', () => {
-    expect(getPaginationSymbols({ totalPageCount: 25000, currentPageNumber: 11111, siblingCount: 2 })).toEqual([
-      1,
-      'ELLIPSIS',
-      11110,
-      11111,
-      11112,
-      'ELLIPSIS',
-      25000,
-    ] as PaginationSymbol[]);
-  });
+    // FIXME: expect this to fail because I will not implement it until I get word from Nik
+    it.fails('should show asymmetric siblings for page counts >= 10000', () => {
+      expect(getPaginationSymbols({ totalPageCount: 25000, currentPageNumber: 1003, siblingCount: 2 })).toEqual([
+        1,
+        'ELLIPSIS',
+        1001,
+        1002,
+        1003,
+        1004,
+        'ELLIPSIS',
+        25000,
+      ] as PaginationSymbol[]);
+    });
 
-  it('should show the last 5 values when within (2 * siblingCount + 1) pages of the last page', () => {
-    expect(getPaginationSymbols({ totalPageCount: 25000, currentPageNumber: 25000, siblingCount: 2 })).toEqual([
-      1,
-      'ELLIPSIS',
-      24996,
-      24997,
-      24998,
-      24999,
-      25000,
-    ] as PaginationSymbol[]);
+    // FIXME: Fails because this uses the shrinking window size
+    it.fails('should work for 5-digit page numbers', () => {
+      expect(getPaginationSymbols({ totalPageCount: 25000, currentPageNumber: 11111, siblingCount: 2 })).toEqual([
+        1,
+        'ELLIPSIS',
+        11110,
+        11111,
+        11112,
+        'ELLIPSIS',
+        25000,
+      ] as PaginationSymbol[]);
+    });
+
+    // FIXME: Fails because this uses the shrinking window size
+    it.fails('should show the last 5 values when within (2 * siblingCount + 1) pages of the last page', () => {
+      expect(getPaginationSymbols({ totalPageCount: 25000, currentPageNumber: 25000, siblingCount: 2 })).toEqual([
+        1,
+        'ELLIPSIS',
+        24996,
+        24997,
+        24998,
+        24999,
+        25000,
+      ] as PaginationSymbol[]);
+    });
   });
 });
