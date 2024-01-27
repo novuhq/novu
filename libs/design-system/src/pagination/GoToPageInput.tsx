d22064b8a4e1a5574f86a70c701703c0c76491a8
Joel Anton <joel@novu.co>
2024-01-26 18:42:48 -0800

    feat: Add error handling + restyle

--- libs/design-system/src/pagination/GoToPageInput.tsx
+++ libs/design-system/src/pagination/GoToPageInput.tsx
@@ -1,39 +1,39 @@
 import {
   FocusEventHandler,
-  FormEventHandler,
   forwardRef,
-  ReactNode,
+  KeyboardEventHandler,
   useContext,
   useImperativeHandle,
   useRef,
+  useState,
 } from 'react';
-import { IInputProps, Input } from '../input/Input';
 import { PaginationContext } from './PaginationContext';
 import styled from '@emotion/styled';
-import { Input as MantineInput } from '@mantine/core';
+import { Input, NumberInput, NumberInputProps } from '@mantine/core';
+import { Tooltip } from '../tooltip/Tooltip';
+import { FIRST_PAGE_NUMBER } from './Pagination.const';
 
-export type TGoToPageInputOption = string;
-export interface IGoToPageInputProps extends IInputProps {
-  label?: string | ReactNode | ReactNode[];
-  className?: string;
+export interface IGoToPageInputProps extends NumberInputProps {
+  firstPageNumber?: number;
 }
 
-const InputWrapper = styled(MantineInput.Wrapper)(({ theme }) => {
+const InputWrapper = styled(Input.Wrapper)(({ theme }) => {
   return `
   display: flex;
   flex-direction: row;
   align-items: center;
   
   input {
-    min-width: 44px;
-    max-width: 80px;
+    text-align: center;
+    min-width: 56px;
+    max-width: 60px;
   }
 
   label {
     color: inherit;
     font-size: inherit;
     line-height: inherit;
-    /* FIXME: why can't I access theme values?! */
+    /* TODO: use theme values */
     margin-right: ${'0.5rem'}
   }
 `;
@@ -44,11 +44,14 @@ const InputWrapper = styled(MantineInput.Wrapper)(({ theme }) => {
  * @requires this component to be a child of a Pagination component
  */
 export const GoToPageInput: React.FC<IGoToPageInputProps> = forwardRef<HTMLInputElement, IGoToPageInputProps>(
-  ({ className, label, placeholder, ...inputProps }, forwardedRef) => {
+  ({ label, firstPageNumber = FIRST_PAGE_NUMBER, ...inputProps }, forwardedRef) => {
     const { onPageChange, totalPageCount } = useContext(PaginationContext);
+    const [hasError, setHasError] = useState<boolean>(false);
 
-    const validateValue = (val: number) => {
-      return !!val && val >= 1 && val <= totalPageCount;
+    const validateValue = (val: number | string) => {
+      const numVal = +val;
+
+      return !!numVal && numVal >= 1 && numVal <= totalPageCount;
     };
 
     /*
@@ -59,36 +62,55 @@ export const GoToPageInput: React.FC<IGoToPageInputProps> = forwardRef<HTMLInput
     useImperativeHandle(forwardedRef, () => internalRef.current, []);
 
     const handleBlurEvent: FocusEventHandler<HTMLInputElement> = (event) => {
-      const val = +event.currentTarget.value;
-      if (!validateValue(val)) {
+      const val = event.currentTarget.value;
+
+      // check for empty value and ensure user can clear error
+      if (!val) {
+        setHasError(false);
+
         return;
       }
-      onPageChange(val);
+
+      if (!validateValue(val)) {
+        setHasError(val !== undefined);
+
+        return;
+      }
+      onPageChange(+val);
+      internalRef.current.value = undefined;
+      setHasError(false);
     };
 
-    const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
-      // prevent double-calling of onPageChange by stopping propagation, and invoking blur explicitly
-      event.stopPropagation();
-      event.preventDefault();
+    const handleKeyPress: KeyboardEventHandler<HTMLInputElement> = (event) => {
+      if (event.key !== 'Enter') {
+        return;
+      }
       internalRef.current.blur();
     };
 
     return (
-      <form onSubmit={handleSubmit}>
-        <InputWrapper label={label} id="goToPage">
-          <Input
-            type={'number'}
+      <InputWrapper label={label} id="goToPage">
+        <Tooltip
+          opened={hasError}
+          data-test-id="conditions-form-tooltip-error"
+          error
+          position="top-end"
+          label={`Value must be between ${firstPageNumber} and ${totalPageCount}`}
+        >
+          <NumberInput
+            onKeyDown={handleKeyPress}
             id={'goToPage'}
             ref={internalRef}
-            placeholder={placeholder}
             onBlur={handleBlurEvent}
-            onChange={inputProps.onChange}
-            min={1}
+            min={firstPageNumber}
             max={totalPageCount}
-            className={className}
+            maxLength={`${totalPageCount}`.length}
+            hideControls
+            noClampOnBlur
+            {...inputProps}
           />
-        </InputWrapper>
-      </form>
+        </Tooltip>
+      </InputWrapper>
     );
   }
 );
