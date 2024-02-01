2b1ff9e182f3587604f8fceb408ece4188b33bcb
Joel Anton <joel@novu.co>
2024-02-01 08:38:27 -0800

    feat: Add param names

--- apps/web/src/hooks/usePaginationState.ts
+++ apps/web/src/hooks/usePaginationState.ts
@@ -1,41 +1,33 @@
 import { useCallback } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
+const URL_PARAM_NAME_PAGE_SIZE = 'size';
+const URL_PARAM_NAME_PAGE_NUMBER = 'page';
 export interface IUsePaginationOptions {
   startingPageNumber?: number;
   pageSizes?: number[];
+  pageSizeParamName?: string;
+  pageNumberParamName?: string;
 }
-
-export const URL_PARAM_NAME_PAGE_SIZE = 'size';
-export const URL_PARAM_NAME_PAGE_NUMBER = 'page';
-export interface IPaginationSearchParams {
-  size: number;
-  page: number;
-}
-
-const test: IPaginationSearchParams = {
-  size: 10,
-  page: 1,
-};
 
 export function useSearchParamsState(
   searchParamName: string,
   defaultValue: number
-): readonly [searchParamsState: number, setSearchParamsState: (newState: number) => void] {
+): readonly [number, (newState: number) => void] {
   const [searchParams, setSearchParams] = useSearchParams({ [searchParamName]: `${defaultValue}` });
 
   const acquiredSearchParam = searchParams.get(searchParamName);
   const searchParamsState = acquiredSearchParam ? +acquiredSearchParam : defaultValue;
 
   const setSearchParamsState = useCallback(
-    (newState: number) => {
+    (newVal: number) => {
       const next = Object.assign(
         {},
         Object.entries(Object.fromEntries(searchParams)).reduce(
           (outputParams, [key, value]) => ({ ...outputParams, [key]: value }),
           {}
         ),
-        { [searchParamName]: `${newState}` }
+        { [searchParamName]: `${newVal}` }
       );
       setSearchParams(next, { replace: true });
     },
@@ -48,9 +40,11 @@ export function useSearchParamsState(
 export const usePaginationState = ({
   startingPageNumber = 1,
   pageSizes = [10, 25, 50, 100],
+  pageNumberParamName = URL_PARAM_NAME_PAGE_NUMBER,
+  pageSizeParamName = URL_PARAM_NAME_PAGE_SIZE,
 }: IUsePaginationOptions) => {
-  const [pageSize, setPageSize] = useSearchParamsState('size', pageSizes[0]);
-  const [currentPageNumber, setCurrentPageNumber] = useSearchParamsState('page', startingPageNumber);
+  const [pageSize, setPageSize] = useSearchParamsState(pageSizeParamName, pageSizes[0]);
+  const [currentPageNumber, setCurrentPageNumber] = useSearchParamsState(pageNumberParamName, startingPageNumber);
 
   return {
     currentPageNumber,
