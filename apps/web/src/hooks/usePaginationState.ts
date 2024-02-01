fa494543044fda9d11ae66121706581f0230903f
Joel Anton <joel@novu.co>
2024-02-01 14:09:02 -0800

    feat: search param hook

--- apps/web/src/hooks/usePaginationState.ts
+++ apps/web/src/hooks/usePaginationState.ts
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 const URL_PARAM_NAME_PAGE_SIZE = 'size';
@@ -10,59 +10,46 @@ export interface IUsePaginationOptions {
   pageNumberParamName?: string;
 }
 
-export function useSearchParamsState({
-  startingPageNumber,
-  pageNumberParamName,
-  pageSizeParamName,
-  pageSizes,
-}: Required<IUsePaginationOptions>) {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const getParamVal = (name: string, defaultValue: number) => {
-    const param = searchParams.get(name);
-
-    return param ? +param : defaultValue;
-  };
-
-  const setSearchParamsState = (searchParamName: string) => (newVal: number) => {
-    const next = Object.assign(
-      {},
-      Object.entries(Object.fromEntries(searchParams)).reduce(
-        (outputParams, [key, value]) => ({ ...outputParams, [key]: value }),
-        {}
-      ),
-      { [searchParamName]: `${newVal}` }
-    );
-    setSearchParams(next, { replace: true });
-  };
-
-  const pageSize = getParamVal(pageSizeParamName, pageSizes[0]);
-  const currentPageNumber = getParamVal(pageNumberParamName, startingPageNumber);
-
-  return {
-    pageSize,
-    currentPageNumber,
-    setPageSize: setSearchParamsState(pageSizeParamName),
-    setCurrentPageNumber: setSearchParamsState(pageNumberParamName),
-  };
-}
-
 export const usePaginationState = ({
   startingPageNumber = 1,
   pageSizes = [10, 25, 50, 100],
   pageNumberParamName = URL_PARAM_NAME_PAGE_NUMBER,
   pageSizeParamName = URL_PARAM_NAME_PAGE_SIZE,
 }: IUsePaginationOptions) => {
-  const { pageSize, setPageSize, currentPageNumber, setCurrentPageNumber } = useSearchParamsState({
-    startingPageNumber,
-    pageSizes,
-    pageSizeParamName,
-    pageNumberParamName,
-  });
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [pageSize, setPageSize] = useState<number>(pageSizes[0]);
+  const [currentPageNumber, setCurrentPageNumber] = useState<number>(startingPageNumber);
+
+  // Load url params or defaults ONLY on mount
+  useEffect(() => {
+    const pageStr = searchParams.get(pageNumberParamName);
+    const sizeStr = searchParams.get(pageSizeParamName);
+
+    const page = pageStr ? parseInt(pageStr, 10) : startingPageNumber;
+    setCurrentPageNumber(page);
+
+    const sizeValUnchecked = parseInt(sizeStr ?? 'NaN', 10);
+    const size = pageSizes.includes(sizeValUnchecked) ? sizeValUnchecked : pageSizes[0];
+    setPageSize(size);
+
+    // must only be on mount
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    setSearchParams(
+      {
+        [pageNumberParamName]: `${currentPageNumber}`,
+        [pageSizeParamName]: `${pageSize}`,
+      },
+      { replace: true }
+    );
+  }, [pageSize, currentPageNumber, setSearchParams, pageNumberParamName, pageSizeParamName]);
 
   return {
-    currentPageNumber,
     pageSize,
+    currentPageNumber,
     setPageSize,
     setCurrentPageNumber,
   };
