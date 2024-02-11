cd06a7b941947a98e52c51d3d422b1e20110231b
Biswajeet Das <biswajeetdas18@gmail.com>
2024-02-11 13:31:40 +0530

    fix: chat blur overlay

--- apps/web/src/components/workflow/Preview/chat/ChatContent.tsx
+++ apps/web/src/components/workflow/Preview/chat/ChatContent.tsx
@@ -19,53 +19,52 @@ export function ChatContent({ isLoading, content, errorMsg }) {
   };
 
   return (
-    <Group
-      spacing={16}
-      align="flex-start"
-      noWrap
-      pt={24}
-      pb={20}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      sx={{
-        position: 'relative',
-      }}
-    >
-      <When truthy={isLoading}>
-        <Skeleton radius={8} height={40} width={40} />
-        <Stack spacing={3}>
-          <Skeleton height={20} width={80} />
-          <Skeleton height={20} width={280} radius="xs" />
-        </Stack>
-      </When>
-      <When truthy={!isLoading}>
-        {isEditOverlayVisible && <PreviewEditOverlay />}
-        <div>
-          <NovuGreyIcon color={isDark ? colors.B30 : colors.BGLight} width="32px" height="32px" />
-        </div>
-        <Stack spacing={5}>
-          <Group spacing={8} align="flex-start" noWrap>
-            <Text
-              weight="bold"
-              color={isDark ? colors.B80 : colors.B20}
-              size="lg"
-              style={{
-                lineHeight: '20px',
-              }}
-            >
-              Your App
-            </Text>
-            <PillStyled isDark={isDark}>APP</PillStyled>
-            <Text color={isDark ? colors.B60 : colors.B70}>now</Text>
-          </Group>
-          {errorMsg ? (
-            <Text color={colors.error}>{errorMsg}</Text>
-          ) : (
-            <Text color={isDark ? colors.B80 : colors.B20}>{content}</Text>
-          )}
-        </Stack>
-      </When>
-    </Group>
+    <ContentAndOVerlayWrapperStyled onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      {isEditOverlayVisible && <PreviewEditOverlay />}
+      <Group
+        spacing={16}
+        align="flex-start"
+        noWrap
+        sx={{
+          position: 'relative',
+          ...(isEditOverlayVisible && { filter: 'blur(2px)' }),
+        }}
+      >
+        <When truthy={isLoading}>
+          <Skeleton radius={8} height={40} width={40} />
+          <Stack spacing={3}>
+            <Skeleton height={20} width={80} />
+            <Skeleton height={20} width={280} radius="xs" />
+          </Stack>
+        </When>
+        <When truthy={!isLoading}>
+          <div>
+            <NovuGreyIcon color={isDark ? colors.B30 : colors.BGLight} width="32px" height="32px" />
+          </div>
+          <Stack spacing={5}>
+            <Group spacing={8} align="flex-start" noWrap>
+              <Text
+                weight="bold"
+                color={isDark ? colors.B80 : colors.B20}
+                size="lg"
+                style={{
+                  lineHeight: '20px',
+                }}
+              >
+                Your App
+              </Text>
+              <PillStyled isDark={isDark}>APP</PillStyled>
+              <Text color={isDark ? colors.B60 : colors.B70}>now</Text>
+            </Group>
+            {errorMsg ? (
+              <Text color={colors.error}>{errorMsg}</Text>
+            ) : (
+              <Text color={isDark ? colors.B80 : colors.B20}>{content}</Text>
+            )}
+          </Stack>
+        </When>
+      </Group>
+    </ContentAndOVerlayWrapperStyled>
   );
 }
 
@@ -78,4 +77,10 @@ const PillStyled = styled.div<{ isDark: boolean }>`
   font-size: 10px;
   font-weight: 400;
   line-height: 20px;
+`;
+
+const ContentAndOVerlayWrapperStyled = styled.div`
+  position: relative;
+  padding-top: 24px;
+  padding-bottom: 20px;
 `;
