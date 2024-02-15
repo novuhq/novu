80106bbeaf3d1183959c2071fe700e3e339d4979
Biswajeet Das <biswajeetdas18@gmail.com>
2024-02-15 16:46:38 +0530

    feat: initial setup

---
+++ apps/web/src/components/workflow/preview/in-app/InAppPreview.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@novu/design-system';
+import Content from './Content';
+
+export function InAppPreview() {
+  return (
+    <div>
+      <Container>
+        <Content />
+      </Container>
+    </div>
+  );
+}
