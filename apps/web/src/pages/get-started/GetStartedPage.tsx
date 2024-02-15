3ebe329f4208fe4df63d02f2c48920bba928c442
Joel Anton <joel@novu.co>
2024-02-15 14:21:14 -0800

    refactor: Remove unnecessary header

--- apps/web/src/pages/get-started/GetStartedPage.tsx
+++ apps/web/src/pages/get-started/GetStartedPage.tsx
@@ -1,4 +1,3 @@
-import { HeaderLayout } from './layout/HeaderLayout';
 import PageContainer from '../../components/layout/components/PageContainer';
 import PageHeader from '../../components/layout/components/PageHeader';
 import { GetStartedTabs } from './components/get-started-tabs/GetStartedTabs';
@@ -15,9 +14,7 @@ export function GetStartedPage() {
 
   return (
     <PageContainer title={PAGE_TITLE}>
-      <HeaderLayout>
-        <PageHeader title={PAGE_TITLE} />
-      </HeaderLayout>
+      <PageHeader title={PAGE_TITLE} />
       {currentOrganization ? (
         <GetStartedTabs currentTab={currentTab} setTab={setTab} />
       ) : (
