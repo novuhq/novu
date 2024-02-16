14c9287c4ed0414323cdd34a6440602c84c5adea
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-02-16 17:58:57 +0000

    test(api): Add billing e2e tests for webhook and usage logic

--- apps/api/src/app/testing/billing/webhook.e2e-ee.ts
+++ apps/api/src/app/testing/billing/webhook.e2e-ee.ts
@@ -1,119 +1,154 @@
 import * as sinon from 'sinon';
-import { OrganizationRepository } from '@novu/dal';
 import { expect } from 'chai';
 import { ApiServiceLevelEnum } from '@novu/shared';
 
-describe('Stripe webhooks', async () => {
-  it('Should handle setup intent succeded event', async () => {
-    if (!require('@novu/ee-billing').SetupIntentSucceededHandler || !require('@novu/ee-billing').UpsertSubscription) {
-      throw new Error("SetupIntentSucceededHandler doesn't exist");
-    }
-    const stubObject = {
-      customers: {
-        retrieve: () => {},
-        update: () => {},
-      },
-      subscriptions: {
-        update: () => {},
-      },
-      prices: {
-        list: () => {},
-      },
-    };
-    const listPricesStub = sinon.stub(stubObject.prices, 'list').resolves({
-      data: [
-        {
-          id: 'price_id',
-        },
-      ],
-    });
-    const updateCustomerStub = sinon.stub(stubObject.customers, 'update').resolves({});
-    const getCustomerStub = sinon.stub(stubObject.customers, 'retrieve').resolves(
-      Promise.resolve({
-        deleted: false,
-        metadata: {
-          organizationId: 'organization_id',
-        },
-        subscriptions: {
-          data: [
-            {
-              id: 'subscription_id',
-              items: { data: [{ id: 'item_id' }] },
-            },
-          ],
-        },
-      })
-    );
-    const repo = new OrganizationRepository();
-    const updateSubscriptionStub = sinon.stub(stubObject.subscriptions, 'update').resolves({});
-    const updateOrgStub = sinon.stub(repo, 'update').resolves({ matched: 1, modified: 1 });
-    const upsertSubscription = new (require('@novu/ee-billing').UpsertSubscription)(stubObject, repo);
-    const handler = new (require('@novu/ee-billing').SetupIntentSucceededHandler)(upsertSubscription, stubObject);
-
-    await handler.handle({
-      type: 'setup_intent.succeeded',
-      data: {
-        object: {
-          id: '',
-          object: 'setup_intent',
-          created: 0,
-          customer: 'customer_id',
-          livemode: false,
-          metadata: {
-            apiServiceLevel: ApiServiceLevelEnum.BUSINESS,
-          },
-          payment_method_options: null,
-          payment_method_types: [],
-          status: 'succeeded',
-          application: null,
-          automatic_payment_methods: null,
-          cancellation_reason: null,
-          client_secret: null,
-          description: null,
-          flow_directions: null,
-          last_setup_error: null,
-          latest_attempt: null,
-          mandate: null,
-          next_action: null,
-          on_behalf_of: null,
-          payment_method: 'payment_method_id',
-          single_use_mandate: null,
-          usage: '',
-        },
-      },
+const mockSetupIntentSucceededEvent = {
+  type: 'setup_intent.succeeded',
+  data: {
+    object: {
       id: '',
-      object: 'event',
-      api_version: null,
+      object: 'setup_intent',
       created: 0,
+      customer: 'customer_id',
       livemode: false,
-      pending_webhooks: 0,
-      request: null,
-    });
-
-    expect(getCustomerStub.lastCall.args.at(0)).to.deep.equal('customer_id');
-    expect(getCustomerStub.lastCall.args.at(1)).to.deep.equal({
-      expand: ['subscriptions'],
-    });
-
-    expect(updateSubscriptionStub.lastCall.args.at(0)).to.equal('subscription_id');
-    expect(updateSubscriptionStub.lastCall.args.at(1)).to.deep.equal({
-      items: [
-        {
-          price: 'price_id',
-        },
-      ],
-    });
-    expect(updateOrgStub.lastCall.args).to.deep.equal([{ _id: 'organization_id' }, { apiServiceLevel: 'business' }]);
-
-    expect(updateCustomerStub.lastCall.args).to.deep.equal([
-      'customer_id',
-      { invoice_settings: { default_payment_method: 'payment_method_id' } },
-    ]);
-
-    expect(listPricesStub.lastCall.args).to.deep.equal([
-      {
-        lookup_keys: ['business_flat_monthly', 'business_usage_notifications'],
+      metadata: {
+        apiServiceLevel: ApiServiceLevelEnum.BUSINESS,
       },
-    ]);
+      payment_method_options: null,
+      payment_method_types: [] as string[],
+      status: 'succeeded',
+      application: null,
+      automatic_payment_methods: null,
+      cancellation_reason: null,
+      client_secret: null,
+      description: null,
+      flow_directions: null,
+      last_setup_error: null,
+      latest_attempt: null,
+      mandate: null,
+      next_action: null,
+      on_behalf_of: null,
+      payment_method: 'payment_method_id',
+      single_use_mandate: null,
+      usage: '',
+    },
+  },
+  id: '',
+  object: 'event',
+  api_version: null,
+  created: 0,
+  livemode: false,
+  pending_webhooks: 0,
+  request: null,
+} as const;
+
+describe('Stripe webhooks', async () => {
+  const stripeStub = {
+    customers: {
+      update: () => {},
+    },
+  };
+
+  const eeBilling = await import('@novu/ee-billing');
+  if (!eeBilling) {
+    throw new Error('ee-billing does not exist');
+  }
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const { SetupIntentSucceededHandler, UpsertSubscription, VerifyCustomer } = eeBilling;
+
+  describe('setup_intent.succeeded', () => {
+    let updateCustomerStub: sinon.SinonStub;
+
+    let verifyCustomerStub: sinon.SinonStub;
+    let upsertSubscriptionStub: sinon.SinonStub;
+
+    beforeEach(() => {
+      verifyCustomerStub = sinon.stub(VerifyCustomer.prototype, 'execute').resolves({
+        customer: {
+          id: 'customer_id',
+          deleted: false,
+          metadata: {
+            organizationId: 'organization_id',
+          },
+          subscriptions: {
+            data: [
+              {
+                id: 'subscription_id',
+                items: { data: [{ id: 'item_id_usage_notifications' }, { id: 'item_id_flat' }] },
+              },
+            ],
+          },
+        },
+        organization: { _id: 'organization_id', apiServiceLevel: ApiServiceLevelEnum.FREE },
+      } as any);
+      upsertSubscriptionStub = sinon.stub(UpsertSubscription.prototype, 'execute').resolves({
+        id: 'subscription_id',
+      } as any);
+      updateCustomerStub = sinon.stub(stripeStub.customers, 'update').resolves({});
+    });
+
+    const createHandler = () => {
+      const handler = new SetupIntentSucceededHandler(
+        stripeStub as any,
+        { execute: verifyCustomerStub } as any,
+        { execute: upsertSubscriptionStub } as any
+      );
+
+      return handler;
+    };
+
+    it('should exit early with invalid api service level', async () => {
+      const disallowedApiServiceLevel = ApiServiceLevelEnum.FREE;
+
+      const handler = createHandler();
+      await handler.handle({
+        ...mockSetupIntentSucceededEvent,
+        data: {
+          ...mockSetupIntentSucceededEvent.data,
+          object: {
+            ...mockSetupIntentSucceededEvent.data.object,
+            metadata: {
+              apiServiceLevel: disallowedApiServiceLevel,
+            },
+          },
+        },
+      });
+
+      expect(verifyCustomerStub.callCount).to.equal(0);
+    });
+
+    it('Should exit early with unknown organization', async () => {
+      verifyCustomerStub.resolves({ organization: null });
+      const handler = createHandler();
+      await handler.handle(mockSetupIntentSucceededEvent);
+
+      expect(updateCustomerStub.callCount).to.equal(0);
+    });
+
+    it('Should update the customer with the default payment method', async () => {
+      const handler = createHandler();
+      await handler.handle(mockSetupIntentSucceededEvent);
+
+      expect(updateCustomerStub.callCount).to.equal(1);
+    });
+
+    it('Should upsert a subscription', async () => {
+      const handler = createHandler();
+      await handler.handle(mockSetupIntentSucceededEvent);
+
+      expect(upsertSubscriptionStub.callCount).to.equal(1);
+    });
+  });
+
+  describe('customer.subscription.created', () => {
+    it('Should handle customer.subscription.created event with known organization', async () => {
+      // @TODO: Implement test
+      expect(true).to.equal(true);
+    });
+
+    it('Should exit early with unknown organization', async () => {
+      // @TODO: Implement test
+      expect(true).to.equal(true);
+    });
   });
 });
