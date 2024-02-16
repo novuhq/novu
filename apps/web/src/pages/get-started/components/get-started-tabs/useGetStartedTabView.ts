3c9a5ed5d4913abfa1bcc3e474b8ac25ad3449f7
Joel Anton <joel@novu.co>
2024-02-16 08:43:18 -0800

    refactor: use location hash instead

--- apps/web/src/pages/get-started/components/get-started-tabs/useGetStartedTabView.ts
+++ apps/web/src/pages/get-started/components/get-started-tabs/useGetStartedTabView.ts
@@ -1,30 +1,17 @@
-import { URLSearchParamsInit, useSearchParams } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { GetStartedTabsViewsEnum } from '../../consts/GetStartedTabsViewsEnum';
 
-const TAB_VIEW_SEARCH_PARAM_NAME = 'v';
-
-interface GetStartedTabSearchParams {
-  [TAB_VIEW_SEARCH_PARAM_NAME]?: GetStartedTabsViewsEnum;
-}
-
-const DEFAULT_PARAMS: GetStartedTabSearchParams = {} satisfies URLSearchParamsInit;
-
 export const useGetStartedTabView = () => {
-  const [params, setParams] = useSearchParams(DEFAULT_PARAMS as unknown as URLSearchParamsInit);
+  // const [params, setParams] = useSearchParams(DEFAULT_PARAMS as unknown as URLSearchParamsInit);
+  const navigate = useNavigate();
+  const loc = useLocation();
 
-  const currentView = params.get(TAB_VIEW_SEARCH_PARAM_NAME) as GetStartedTabsViewsEnum | undefined;
+  // remove the leading #
+  const currentView = loc.hash.slice(1) as GetStartedTabsViewsEnum | undefined;
+
+  // remove hash if null or add the specific view
   const setView = (view: GetStartedTabsViewsEnum | null) => {
-    if (view === null) {
-      params.delete(TAB_VIEW_SEARCH_PARAM_NAME);
-    } else {
-      params.set(TAB_VIEW_SEARCH_PARAM_NAME, view);
-    }
-
-    setParams(
-      params,
-      // replace is used so that changing the search params isn't considered a change in page
-      { replace: true }
-    );
+    navigate({ pathname: './', search: loc.search, hash: view === null ? undefined : view }, { replace: true });
   };
 
   return {
