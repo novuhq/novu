251f71e822cfede656625c63b5b792200ba8bd0a
Biswajeet Das <biswajeetdas18@gmail.com>
2024-02-18 12:06:27 +0530

    feat: ligh theme fixes

--- apps/web/src/components/workflow/preview/in-app/Header.tsx
+++ apps/web/src/components/workflow/preview/in-app/Header.tsx
@@ -2,6 +2,9 @@ import styled from '@emotion/styled';
 import { colors } from '@novu/design-system';
 import { InAppHeaderIcons, LocaleSelect } from '../common';
 
+const LIGHT_THEME_BG = `linear-gradient(270deg, ${colors.B98} 0%, rgba(255, 255, 255, 0.00) 100%);`;
+const DARK_THEME_BG = `linear-gradient(90deg, ${colors.B17} 0%, #24242d 48.44%, #292934 100%), #292933`;
+
 export function Header({ selectedLocale, locales, areLocalesLoading, onLocaleChange }) {
   return (
     <ContainerStyled>
@@ -29,5 +32,5 @@ const ContainerStyled = styled.div`
   align-items: center;
   align-self: stretch;
   border-radius: 0px 8px 0px 0px;
-  background: linear-gradient(90deg, ${colors.B17} 0%, #24242d 48.44%, #292934 100%), #292933;
+  background: ${({ theme }) => (theme.colorScheme === 'dark' ? DARK_THEME_BG : LIGHT_THEME_BG)};
 `;
