ad6fe0561f3c09eace07062f5802a78469b2ff2b
Joel Anton <joel@novu.co>
2024-02-20 15:09:21 -0800

    feat: Create animation component

--- apps/web/src/pages/get-started/components/GetStartedAnimation.tsx
+++ apps/web/src/pages/get-started/components/GetStartedAnimation.tsx
@@ -1,16 +1,67 @@
-import { FC } from 'react';
-import { useRive } from '@rive-app/react-canvas';
+import { FC, useEffect } from 'react';
+import { useRive, useStateMachineInput } from '@rive-app/react-canvas';
+import { useTheme } from '@emotion/react';
+import { OnboardingUseCasesTabsEnum } from '../consts/OnboardingUseCasesTabsEnum';
+import styled from '@emotion/styled';
 
 interface IGetStartedAnimationProps {
-  a?: any;
+  useCase: OnboardingUseCasesTabsEnum;
 }
 
-export const GetStartedAnimation: FC<IGetStartedAnimationProps> = (props) => {
+// uses `public` as the default base directory
+const ROOT_ANIMATION_PATH = `animations/get-started`;
+const STATE_MACHINE_NAME = 'SM';
+const STATE_MACHINE_INPUT_NAME = 'white theme';
+
+const getAnimationPath = (useCase: OnboardingUseCasesTabsEnum) => `${ROOT_ANIMATION_PATH}/${useCase}.riv`;
+
+const AnimationContainer = styled.div`
+  height: 520px;
+`;
+
+export const GetStartedAnimation: FC<IGetStartedAnimationProps> = ({ useCase }) => {
+  const { colorScheme } = useTheme();
   const { rive, RiveComponent } = useRive({
-    src: 'https://cdn.rive.app/animations/vehicles.riv',
-    stateMachines: 'bumpy',
-    autoplay: false,
+    src: getAnimationPath(useCase),
+    stateMachines: STATE_MACHINE_NAME,
+    autoplay: true,
+    /*
+     * assetLoader: (asset, bytes) => {
+     *   console.log('Tell our asset importer if we are going to load the asset contents', {
+     *     name: asset.name,
+     *     fileExtension: asset.fileExtension,
+     *     cdnUuid: asset.cdnUuid,
+     *     isFont: asset.isFont,
+     *     isImage: asset.isImage,
+     *     bytes,
+     *   });
+     */
+
+    /*
+     *   return true;
+     * },
+     */
   });
 
-  return <RiveComponent onMouseEnter={() => rive && rive.play()} onMouseLeave={() => rive && rive.pause()} />;
+  const stateMachineInput = useStateMachineInput(
+    rive,
+    STATE_MACHINE_NAME,
+    STATE_MACHINE_INPUT_NAME,
+    colorScheme === 'light'
+  );
+
+  // change animation color scheme input on color scheme change
+  useEffect(() => {
+    if (!stateMachineInput) {
+      return;
+    }
+
+    stateMachineInput.value = colorScheme === 'light';
+  }, [colorScheme, stateMachineInput]);
+
+  return (
+    <AnimationContainer>
+      <RiveComponent />
+    </AnimationContainer>
+  );
 };
