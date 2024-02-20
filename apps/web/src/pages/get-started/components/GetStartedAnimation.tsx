90c50f35a563412b9097003e3d9cb4f4cb45432f
Joel Anton <joel@novu.co>
2024-02-20 15:43:38 -0800

    feat: Load fonts

--- apps/web/src/pages/get-started/components/GetStartedAnimation.tsx
+++ apps/web/src/pages/get-started/components/GetStartedAnimation.tsx
@@ -1,5 +1,5 @@
 import { FC, useEffect } from 'react';
-import { useRive, useStateMachineInput } from '@rive-app/react-canvas';
+import { useRive, useStateMachineInput, decodeFont, FontAsset } from '@rive-app/react-canvas';
 import { useTheme } from '@emotion/react';
 import { OnboardingUseCasesTabsEnum } from '../consts/OnboardingUseCasesTabsEnum';
 import styled from '@emotion/styled';
@@ -25,22 +25,44 @@ export const GetStartedAnimation: FC<IGetStartedAnimationProps> = ({ useCase }) 
     src: getAnimationPath(useCase),
     stateMachines: STATE_MACHINE_NAME,
     autoplay: true,
-    /*
-     * assetLoader: (asset, bytes) => {
-     *   console.log('Tell our asset importer if we are going to load the asset contents', {
-     *     name: asset.name,
-     *     fileExtension: asset.fileExtension,
-     *     cdnUuid: asset.cdnUuid,
-     *     isFont: asset.isFont,
-     *     isImage: asset.isImage,
-     *     bytes,
-     *   });
-     */
+    assetLoader: (asset, bytes) => {
+      /*
+       * If the asset has a `cdnUuid`, return false to let the runtime handle
+       * loading it in from a CDN. Or if there are bytes found for the asset
+       * (aka, it was embedded), return false as there's no work needed here
+       */
+      if (asset.cdnUuid.length > 0 || bytes.length > 0) {
+        return false;
+      }
 
-    /*
-     *   return true;
-     * },
-     */
+      /*
+       * Here, we load a font asset with a random font on load of the Rive file
+       * and return true, because this callback handler is responsible for loading
+       * the asset, as opposed to the runtime
+       */
+      if (asset.isFont) {
+        /*
+         * decodeFont creates a Rive-specific Font object that `setFont()` takes
+         * on the asset from assetLoader
+         */
+        fetch('/fonts/Lato.ttf').then(async (res) => {
+          const font = await decodeFont(new Uint8Array(await res.arrayBuffer()));
+          (asset as FontAsset).setFont(font);
+
+          /*
+           * Be sure to call unref to release any references.
+           * This allows the engine to clean it up when it is not used by any more animations.
+           */
+          font.unref();
+
+          return;
+        });
+
+        return true;
+      } else {
+        return false;
+      }
+    },
   });
 
   const stateMachineInput = useStateMachineInput(
