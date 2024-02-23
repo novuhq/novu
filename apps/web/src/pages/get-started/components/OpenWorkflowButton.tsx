60e3c66d436ef3033b9b62967393750cea6fae2f
Paweł <pawel.tymczuk@gmail.com>
2024-02-23 23:24:30 +0100

    chore(web): refactor onboarding create workflow from blueprint logic

--- apps/web/src/pages/get-started/components/OpenWorkflowButton.tsx
+++ apps/web/src/pages/get-started/components/OpenWorkflowButton.tsx
@@ -1,14 +1,28 @@
+import { errorMessage } from '@novu/design-system';
 import { INotificationTemplate } from '@novu/shared';
 
-import { ROUTES } from '../../../constants/routes.enum';
-import { useCreateTemplateFromBlueprint } from '../../../api/hooks';
-import { errorMessage } from '../../../utils/notifications';
-import { getBlueprintTemplateById, getTemplateById } from '../../../api/notification-templates';
-import { getWorkflowBlueprintDetails } from '../../../utils';
 import { TemplateCreationSourceEnum } from '../../templates/shared';
 import { useSegment } from '../../../components/providers/SegmentProvider';
 import { OnboardingWorkflowRouteEnum } from '../consts/types';
 import { StyledLink } from '../consts/shared';
+import { useCreateWorkflowFromBlueprint } from '../../../hooks';
+import { openInNewTab } from '../../../utils';
+import { parseUrl } from '../../../utils/routeUtils';
+import { ROUTES } from '../../../constants/routes.enum';
+
+function buildWorkflowEditorUrl(workflow: INotificationTemplate, step?: OnboardingWorkflowRouteEnum) {
+  const workflowRoute = parseUrl(ROUTES.WORKFLOWS_EDIT_TEMPLATEID, { templateId: workflow._id! });
+  if (step === OnboardingWorkflowRouteEnum.TEST_WORKFLOW) {
+    return `${window.location.origin}${workflowRoute}/${OnboardingWorkflowRouteEnum.TEST_WORKFLOW}`;
+  }
+
+  const stepUuid = workflow.steps.find((el) => el.name?.toLowerCase() === step?.toLowerCase())?.uuid;
+  if (step && stepUuid) {
+    return `${window.location.origin}${workflowRoute}/${step}/${stepUuid}`;
+  }
+
+  return `${window.location.origin}${workflowRoute}`;
+}
 
 export function OpenWorkflowButton({
   blueprintIdentifier,
@@ -20,87 +34,22 @@ export function OpenWorkflowButton({
   node?: OnboardingWorkflowRouteEnum;
 }) {
   const segment = useSegment();
-
-  const { createTemplateFromBlueprint } = useCreateTemplateFromBlueprint({
+  const { createWorkflowFromBlueprint } = useCreateWorkflowFromBlueprint({
     onSuccess: (template) => {
-      const url = buildUrl(template, node);
-      openNewTab(url);
+      openInNewTab(buildWorkflowEditorUrl(template, node));
     },
-    onError: (error) => {
+    onError: () => {
       errorMessage('Something went wrong while creating a template from the blueprint. Please try again later.');
     },
   });
 
-  const openNewTab = (url) => window.open(url, '_blank', 'noreferrer noopener');
-
-  const handleOpenWorkflowClick = async () => {
+  const handleOpenWorkflowClick = () => {
     segment.track('[Get Started] Click Create Notification Template', {
       templateIdentifier: blueprintIdentifier,
       location: TemplateCreationSourceEnum.ONBOARDING_GET_STARTED,
     });
-
-    const blueprintData = await getBlueprint();
-
-    if (!blueprintData) {
-      return;
-    }
-
-    try {
-      const workflowIdentifier = blueprintData.name.toLowerCase();
-      const workflowData = await getTemplateById(workflowIdentifier);
-      if (workflowData) {
-        const url = buildUrl(workflowData, node);
-        openNewTab(url);
-      }
-    } catch (error) {
-      createTemplateFromBlueprint({
-        blueprint: blueprintData,
-        params: { __source: TemplateCreationSourceEnum.ONBOARDING_GET_STARTED },
-      });
-    }
+    createWorkflowFromBlueprint({ blueprintIdentifier });
   };
 
-  const getBlueprint = async (): Promise<INotificationTemplate | undefined> => {
-    let blueprintData: INotificationTemplate | undefined;
-
-    try {
-      blueprintData = await getBlueprintTemplateById(blueprintIdentifier);
-      if (blueprintData) {
-        const { name: blueprintName } = getWorkflowBlueprintDetails(blueprintData.name);
-        blueprintData = { ...blueprintData, name: blueprintName };
-      } else {
-        errorMessage('Failed to fetch blueprint data');
-
-        return;
-      }
-    } catch (error) {
-      errorMessage('Failed to fetch blueprint data');
-
-      return;
-    }
-
-    return blueprintData;
-  };
-
-  return <StyledLink onClick={() => handleOpenWorkflowClick()}>{children}</StyledLink>;
-}
-
-function buildUrl(workflowData, node?: OnboardingWorkflowRouteEnum) {
-  const nodeRoute = getNodeRoute(node, workflowData);
-
-  return `${window.location.origin}${ROUTES.WORKFLOWS_EDIT_TEMPLATEID.replace(
-    ':templateId',
-    workflowData._id
-  )}${nodeRoute}`;
-}
-function getNodeRoute(node: OnboardingWorkflowRouteEnum | undefined, workflowData: INotificationTemplate): string {
-  if (!node) {
-    return '';
-  }
-
-  const nodeUuid = workflowData.steps.find((step) => step.name?.toLowerCase() === node.toLowerCase())?.uuid;
-  const idRoute = nodeUuid ? `/${nodeUuid}` : '';
-  const nodeRoute = `/${node}`;
-
-  return `${nodeRoute}${idRoute}`;
+  return <StyledLink onClick={handleOpenWorkflowClick}>{children}</StyledLink>;
 }
