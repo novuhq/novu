03c804ef9d9222ac5a91897da5b4302c9bbfaff6
Joel Anton <joel@novu.co>
2024-02-23 15:43:22 -0800

    [Onboarding] NV-3348: Questionnaire Styling Fix & Tests (#5229)

--- apps/web/src/pages/get-started/components/get-started-tabs/GetStartedTabs.const.tsx
+++ apps/web/src/pages/get-started/components/get-started-tabs/GetStartedTabs.const.tsx
@@ -1,4 +1,4 @@
-import { RingingBell, MultiChannel, Digest, HalfClock, Translation } from '@novu/design-system';
+import { Digest, HalfClock, IconOutlineNotificationsActive, IconOutlinePublic, Translation } from '@novu/design-system';
 import { CSSProperties } from 'react';
 import { OnboardingUseCasesTabsEnum } from '../../consts/OnboardingUseCasesTabsEnum';
 
@@ -7,31 +7,34 @@ export interface GetStartedTabConfig {
   icon: JSX.Element;
   title: string;
 }
-
 const ICON_STYLE: Partial<CSSProperties> = { height: 20, width: 20, marginBottom: '12px' };
+
 export const TAB_CONFIGS: GetStartedTabConfig[] = [
   {
     value: OnboardingUseCasesTabsEnum.IN_APP,
-    icon: <RingingBell style={ICON_STYLE} />,
+    icon: <IconOutlineNotificationsActive style={ICON_STYLE} />,
     title: 'In-app',
   },
   {
     value: OnboardingUseCasesTabsEnum.MULTI_CHANNEL,
-    icon: <MultiChannel style={ICON_STYLE} />,
+    icon: <IconOutlinePublic style={ICON_STYLE} />,
     title: 'Multi-channel',
   },
   {
     value: OnboardingUseCasesTabsEnum.DIGEST,
+    // TODO: discuss Material Design Icon with Nik: <IconOutlineAutoAwesome />,
     icon: <Digest style={ICON_STYLE} />,
     title: 'Digest',
   },
   {
     value: OnboardingUseCasesTabsEnum.DELAY,
+    // TODO: discuss Material Design Icon with Nik: <IconOutlineAvTimer />,
     icon: <HalfClock style={ICON_STYLE} />,
     title: 'Delay',
   },
   {
     value: OnboardingUseCasesTabsEnum.TRANSLATION,
+    // TODO: discuss Material Design Icon with Nik: <IconOutlineTranslate />,
     icon: <Translation style={ICON_STYLE} />,
     title: 'Translate',
   },
