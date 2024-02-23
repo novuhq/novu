00057025dc09f338173a7bd9554a759b85b74883
Joel Anton <joel@novu.co>
2024-02-23 09:15:57 -0800

    fix: Use new animations

--- apps/web/src/pages/get-started/components/GetStartedAnimation.tsx
+++ apps/web/src/pages/get-started/components/GetStartedAnimation.tsx
@@ -1,8 +1,8 @@
-import { FC, useEffect } from 'react';
-import { useRive, useStateMachineInput, decodeFont, FontAsset } from '@rive-app/react-canvas';
 import { useTheme } from '@emotion/react';
-import { OnboardingUseCasesTabsEnum } from '../consts/OnboardingUseCasesTabsEnum';
 import styled from '@emotion/styled';
+import { Alignment, Fit, Layout, useRive, useStateMachineInput } from '@rive-app/react-canvas';
+import { FC, useEffect } from 'react';
+import { OnboardingUseCasesTabsEnum } from '../consts/OnboardingUseCasesTabsEnum';
 
 interface IGetStartedAnimationProps {
   useCase: OnboardingUseCasesTabsEnum;
@@ -28,45 +28,7 @@ export const GetStartedAnimation: FC<IGetStartedAnimationProps> = ({ useCase }) 
     src: getAnimationPath(useCase),
     stateMachines: STATE_MACHINE_NAME,
     autoplay: true,
-    // https://help.rive.app/runtimes/loading-assets#handling-assets
-    assetLoader: (asset, bytes) => {
-      /*
-       * If the asset has a `cdnUuid`, return false to let the runtime handle
-       * loading it in from a CDN. Or if there are bytes found for the asset
-       * (aka, it was embedded), return false as there's no work needed here
-       */
-      if (asset.cdnUuid.length > 0 || bytes.length > 0) {
-        return false;
-      }
-
-      /*
-       * Here, we load a font asset with a random font on load of the Rive file
-       * and return true, because this callback handler is responsible for loading
-       * the asset, as opposed to the runtime
-       */
-      if (asset.isFont) {
-        /*
-         * decodeFont creates a Rive-specific Font object that `setFont()` takes
-         * on the asset from assetLoader
-         */
-        fetch('/fonts/Lato.ttf').then(async (res) => {
-          const font = await decodeFont(new Uint8Array(await res.arrayBuffer()));
-          (asset as FontAsset).setFont(font);
-
-          /*
-           * Be sure to call unref to release any references.
-           * This allows the engine to clean it up when it is not used by any more animations.
-           */
-          font.unref();
-
-          return;
-        });
-
-        return true;
-      } else {
-        return false;
-      }
-    },
+    layout: new Layout({ fit: Fit.FitWidth, alignment: Alignment.TopCenter }),
   });
 
   const stateMachineInput = useStateMachineInput(
