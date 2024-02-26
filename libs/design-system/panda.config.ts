177cb2f648781cae204d1542c184c3fd52377596
Joel Anton <joel@novu.co>
2024-02-26 12:10:42 -0800

    chore: Panda setup

---
+++ libs/design-system/panda.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from '@pandacss/dev';
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  // Where to look for your css declarations
+  include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
+
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+  theme: {
+    extend: {},
+  },
+
+  // The output directory for your css system
+  outdir: 'styled-system',
+});
