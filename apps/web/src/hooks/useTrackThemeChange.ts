06577b838154e018aad729fa14da289b88c4e461
Biswajeet Das <biswajeetdas18@gmail.com>
2024-02-29 13:37:58 +0530

    feat: refactor hook

--- apps/web/src/hooks/useTrackThemeChange.ts
+++ apps/web/src/hooks/useTrackThemeChange.ts
@@ -1,16 +1,17 @@
-import { ColorScheme } from '@mantine/core';
+import { ColorScheme, useMantineColorScheme } from '@mantine/core';
 import { useLocalThemePreference, useSegment } from '@novu/shared-web';
 import { useEffect } from 'react';
 import { useDebounce } from './useDebounce';
 
-type ThemeChangeProps = { colorScheme: ColorScheme };
+type ThemeChange = { colorScheme: ColorScheme; themeStatus: string };
 
-export default function useTrackThemeChange({ colorScheme }: ThemeChangeProps) {
+export default function useTrackThemeChange() {
   const { themeStatus } = useLocalThemePreference();
+  const { colorScheme } = useMantineColorScheme();
 
   const segment = useSegment();
 
-  const trackThemeChange = useDebounce<ThemeChangeProps>((args) => {
+  const trackThemeChange = useDebounce<ThemeChange>((args) => {
     segment.track('Theme is set - [Theme]', args);
   }, 500);
 
