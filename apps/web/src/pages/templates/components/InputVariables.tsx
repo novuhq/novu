302512fc56b3c3f1f9e5049b0ad79aa371d50197
David Söderberg <granskog_@hotmail.com>
2024-02-29 09:33:38 +0100

    refactor: input variables

--- apps/web/src/pages/templates/components/InputVariables.tsx
+++ apps/web/src/pages/templates/components/InputVariables.tsx
@@ -1,3 +1,4 @@
+import { IS_DOCKER_HOSTED } from '@novu/shared-web';
 import { useFormContext } from 'react-hook-form';
 import { useStepFormPath } from '../hooks/useStepFormPath';
 
@@ -5,6 +6,17 @@ export const InputVariables = () => {
   const stepFormPath = useStepFormPath();
   const { watch } = useFormContext();
   const input = watch(`${stepFormPath}.input`);
+
+  if (IS_DOCKER_HOSTED) {
+    return null;
+  }
+
+  try {
+    const module = require('@novu/ee-billing-web');
+    const InputVariablesComponent = module.InputVariables;
+
+    return <InputVariablesComponent />;
+  } catch (e) {}
 
   return null;
 };
