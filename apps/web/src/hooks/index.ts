a6bab3913ca98e5489f6ac2c0842064e4fe603c7
Biswajeet Das <biswajeetdas18@gmail.com>
2024-03-05 11:18:20 +0530

    feat: add useisdarktheme hook

--- apps/web/src/hooks/index.ts
+++ apps/web/src/hooks/index.ts
@@ -25,3 +25,4 @@ export * from './useCreateWorkflowFromBlueprint';
 export * from './useHover';
 export * from './useBootIntercom';
 export * from './useThemeChange';
+export * from './useIsDarkTheme';
