6e43ff3f67ea658f75fdbead93eb4cccb6999b43
Joel Anton <joel@novu.co>
2024-03-06 14:25:44 -0800

    test: Revert this! Demo working text + colors w/ dark mode

--- apps/web/src/components/layout/components/PageHeader.tsx
+++ apps/web/src/components/layout/components/PageHeader.tsx
@@ -1,11 +1,18 @@
 import { Group } from '@mantine/core';
-import { Title, Container } from '@novu/design-system';
+import { Container } from '@novu/design-system';
+import { css } from '../../../styled-system/css';
+import { styled } from '../../../styled-system/jsx';
+import { title as titleRecipe } from '../../../styled-system/recipes';
+
+const Title = styled('h2', titleRecipe);
+
+const containerStyles = css({ bg: 'surface.page' });
 
 function PageHeader({ actions, title }: { actions?: JSX.Element; title: string }) {
   return (
-    <Container fluid sx={{ padding: '20px 24px' }}>
+    <Container fluid sx={{ padding: '20px 24px' }} className={containerStyles}>
       <Group position="apart">
-        <Title>{title}</Title>
+        <Title variant={'subsection'}>{title}</Title>
         {actions && <div>{actions}</div>}
       </Group>
     </Container>
