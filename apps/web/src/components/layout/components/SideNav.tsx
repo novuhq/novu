219525b6a71119ad38fe87593e37ed4352e7b31f
Biswajeet Das <biswajeetdas18@gmail.com>
2024-03-07 12:07:27 +0530

    feat: merge

--- apps/web/src/components/layout/components/SideNav.tsx
+++ apps/web/src/components/layout/components/SideNav.tsx
@@ -80,6 +80,12 @@ export function SideNav({}: Props) {
   } = useUserOnboardingStatus();
   const showOnBoarding = isLoadingShowOnBoarding ? false : showOnBoardingState;
   const isInformationArchitectureEnabled = useFeatureFlag(FeatureFlagsKeysEnum.IS_INFORMATION_ARCHITECTURE_ENABLED);
+  const {
+    showOnboarding: showOnBoardingState,
+    isLoading: isLoadingShowOnBoarding,
+    updateOnboardingStatus,
+  } = useUserOnboardingStatus();
+  const showOnBoarding = isLoadingShowOnBoarding ? false : showOnBoardingState;
 
   useEffect(() => {
     removeItems(['toggle-environment']);
