70c850c1b138160720baa0dd51181e771ba20822
Joel Anton <joel@novu.co>
2024-03-13 17:20:19 -0700

    feat: React-scanner + Storybook in design-system

---
+++ libs/design-system/src/component-audit/ComponentAuditTable.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StoryFn, Meta } from '@storybook/react';
+import { ComponentAuditTable } from './ComponentAuditTable';
+import { css } from '../../styled-system/css';
+
+import scanJson from './component-scans/scan.json';
+
+export default {
+  title: 'ComponentAudit',
+  component: ComponentAuditTable,
+  argTypes: {},
+} as Meta<typeof ComponentAuditTable>;
+
+const TableWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div
+      className={css({
+        overflowX: 'auto',
+        maxWidth: '100%',
+      })}
+    >
+      {children}
+    </div>
+  );
+};
+
+const Template: StoryFn<typeof ComponentAuditTable> = ({ ...args }) => (
+  <TableWrapper>
+    <h3>
+      If no data is appearing below, please run `pnpm audit-components` in your terminal in the `design-system`
+      directory
+    </h3>
+    <br />
+    <ComponentAuditTable data={scanJson ?? {}} />
+  </TableWrapper>
+);
+
+export const ComponentAudit = Template.bind({});
+ComponentAudit.args = {};
