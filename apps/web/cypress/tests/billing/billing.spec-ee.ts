f66d99223e92301c86f5850f29ab0f0da8c97285
David Söderberg <granskog_@hotmail.com>
2024-03-15 09:32:03 +0100

    feat: add cypress tests for billing

---
+++ apps/web/cypress/tests/billing/billing.spec-ee.ts
@@ -0,0 +1,114 @@
+import { addDays, subDays, startOfDay, endOfDay } from 'date-fns';
+
+describe('Billing', function () {
+  beforeEach(function () {
+    cy.initializeSession().as('session');
+  });
+
+  it('should display billing page', function () {
+    cy.intercept('GET', '**/v1/billing/subscription', {
+      data: {
+        trialStart: null,
+        trialEnd: null,
+        hasPaymentMethod: false,
+      },
+    }).as('getSubscription');
+
+    cy.visit('/settings/billing');
+
+    cy.wait(['@getSubscription']);
+
+    cy.getByTestId('plan-title').should('have.text', 'Plans');
+  });
+
+  it('should display free trial widget', function () {
+    cy.intercept('GET', '**/v1/billing/subscription', {
+      data: {
+        trialStart: startOfDay(new Date()),
+        trialEnd: addDays(endOfDay(new Date()), 30),
+        hasPaymentMethod: false,
+      },
+    }).as('getSubscription');
+
+    cy.visit('/workflows');
+
+    cy.wait(['@getSubscription']);
+    cy.getByTestId('free-trial-widget-text').should('have.text', '30 days left on your Business trial');
+    cy.getByTestId('free-trial-widget-button').should('have.text', 'Upgrade to Business');
+    cy.getByTestId('free-trial-widget-progress').find('.mantine-Progress-bar').should('have.css', 'width', '0px');
+  });
+
+  it('should display free trial widget after 10 days', function () {
+    cy.intercept('GET', '**/v1/billing/subscription', {
+      data: {
+        trialStart: subDays(startOfDay(new Date()), 10),
+        trialEnd: addDays(endOfDay(new Date()), 20),
+        hasPaymentMethod: false,
+      },
+    }).as('getSubscription');
+
+    cy.visit('/workflows');
+
+    cy.wait(['@getSubscription']);
+    cy.getByTestId('free-trial-widget-text').should('have.text', '20 days left on your Business trial');
+    cy.getByTestId('free-trial-widget-button').should('have.text', 'Upgrade to Business');
+    cy.getByTestId('free-trial-widget-progress')
+      .find('.mantine-Progress-bar')
+      .should('have.css', 'background-color', 'rgb(77, 153, 128)');
+  });
+
+  it('should display free trial widget after 20 days', function () {
+    cy.intercept('GET', '**/v1/billing/subscription', {
+      data: {
+        trialStart: subDays(startOfDay(new Date()), 20),
+        trialEnd: addDays(endOfDay(new Date()), 10),
+        hasPaymentMethod: false,
+      },
+    }).as('getSubscription');
+
+    cy.visit('/workflows');
+
+    cy.wait(['@getSubscription']);
+    cy.getByTestId('free-trial-widget-text').should('have.text', '10 days left on your Business trial');
+    cy.getByTestId('free-trial-widget-button').should('have.text', 'Upgrade to Business');
+    cy.getByTestId('free-trial-widget-progress')
+      .find('.mantine-Progress-bar')
+      .should('have.css', 'background-color', 'rgb(253, 224, 68)');
+    cy.getByTestId('free-trial-banner').should('exist');
+    cy.getByTestId('free-trial-banner-upgrade').should('have.text', 'Upgrade');
+    cy.getByTestId('free-trial-banner-contact-sales').should('have.text', 'Contact sales');
+  });
+
+  it('should not display free trial widget after 30 days', function () {
+    cy.intercept('GET', '**/v1/billing/subscription', {
+      data: {
+        trialStart: null,
+        trialEnd: null,
+        hasPaymentMethod: false,
+      },
+    }).as('getSubscription');
+
+    cy.visit('/settings/billing');
+
+    cy.wait(['@getSubscription']);
+    cy.getByTestId('free-trial-widget-text').should('not.exist');
+    cy.getByTestId('free-trial-plan-widget').should('not.exist');
+  });
+
+  it('should display free trail info on billing page', function () {
+    cy.intercept('GET', '**/v1/billing/subscription', {
+      data: {
+        trialStart: startOfDay(new Date()),
+        trialEnd: addDays(endOfDay(new Date()), 30),
+        hasPaymentMethod: false,
+      },
+    }).as('getSubscription');
+
+    cy.visit('/settings/billing');
+
+    cy.wait(['@getSubscription']);
+
+    cy.getByTestId('plan-title').should('have.text', 'Plans');
+    cy.getByTestId('free-trial-plan-widget').should('have.text', '30 days left on your trial');
+  });
+});
