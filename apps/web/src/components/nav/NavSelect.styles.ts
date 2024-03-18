65eebce9f90349ad40aea4478f3b41a0e2a28d81
Joel Anton <joel@novu.co>
2024-03-18 16:02:04 -0700

    feat: Add tooltip

--- apps/web/src/components/nav/NavSelect.styles.ts
+++ apps/web/src/components/nav/NavSelect.styles.ts
@@ -6,3 +6,11 @@ export const navSelectStyles = css({
   '& .mantine-Select-icon': { width: 'inherit !important' },
   '& .mantine-Select-dropdown': { top: '50px !important' },
 });
+
+export const tooltipStyles = css({ p: '100 !important' });
+export const arrowStyles = css({
+  w: '100 !important',
+  h: '100 !important',
+  left: '-50 !important',
+  top: '18px !important',
+});
--- apps/web/src/components/nav/OrganizationSelect/v2/OrganizationSelect.tsx
+++ apps/web/src/components/nav/OrganizationSelect/v2/OrganizationSelect.tsx
@@ -1,6 +1,6 @@
-import { Select } from '@novu/design-system';
+import { Select, Tooltip } from '@novu/design-system';
 import { css } from '../../../../styled-system/css';
-import { navSelectStyles } from '../../NavSelect.styles';
+import { arrowStyles, navSelectStyles, tooltipStyles } from '../../NavSelect.styles';
 import { useOrganizationSelect } from '../useOrganizationSelect';
 
 interface INavOrganizationSelectRendererProps {
@@ -26,32 +26,42 @@ export const OrganizationSelectRenderer: React.FC<INavOrganizationSelectRenderer
   data,
 }) => {
   return (
-    <Select
-      data-test-id="organization-switch"
-      className={navSelectStyles}
-      creatable
-      searchable
-      loading={loadingAddOrganization || loadingSwitch}
-      getCreateLabel={(newOrganization) => <div>+ Add "{newOrganization}"</div>}
-      onCreate={addOrganizationItem}
-      value={value}
-      onChange={switchOrgCallback}
-      allowDeselect={false}
-      onSearchChange={setSearch}
-      data={data}
-      icon={
-        <img
-          // TODO: use actual organization photo
-          src="https://assets.super.so/1e9f5a51-c4c6-4fca-b6e8-25fa0186f139/uploads/favicon/d82d95bb-0983-4980-8b3f-dda6ecb0c22c.png"
-          className={css({
-            w: '200',
-            h: '200',
-            // TODO: use design system values when available
-            borderRadius: '8px',
-          })}
-        />
-      }
-    />
+    <Tooltip
+      label="Type a name to add organization"
+      position="left"
+      classNames={{
+        tooltip: tooltipStyles,
+        arrow: arrowStyles,
+      }}
+      withinPortal
+    >
+      <Select
+        data-test-id="organization-switch"
+        className={navSelectStyles}
+        creatable
+        searchable
+        loading={loadingAddOrganization || loadingSwitch}
+        getCreateLabel={(newOrganization) => <div>+ Add "{newOrganization}"</div>}
+        onCreate={addOrganizationItem}
+        value={value}
+        onChange={switchOrgCallback}
+        allowDeselect={false}
+        onSearchChange={setSearch}
+        data={data}
+        icon={
+          <img
+            // TODO: use actual organization photo
+            src="https://assets.super.so/1e9f5a51-c4c6-4fca-b6e8-25fa0186f139/uploads/favicon/d82d95bb-0983-4980-8b3f-dda6ecb0c22c.png"
+            className={css({
+              w: '200',
+              h: '200',
+              // TODO: use design system values when available
+              borderRadius: '8px',
+            })}
+          />
+        }
+      />
+    </Tooltip>
   );
 };
 
