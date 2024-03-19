18a6a6910b7fc3acd72e7846b6f38c8a1512189d
ainouzgali <ainouzgali@gmail.com>
2024-03-19 12:58:24 +0200

    fix: remove console log

--- apps/api/src/app/shared/helpers/content.service.ts
+++ apps/api/src/app/shared/helpers/content.service.ts
@@ -38,8 +38,6 @@ export class ContentService {
 
       return getTemplateVariables(ast.body);
     } catch (e) {
-      console.log(e);
-
       throw new ApiException('Failed to extract variables');
     }
   }
