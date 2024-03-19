e247e133cf0918c548160145d506dfe66cb592af
Pawan Jain <jainpawan211199@gmail.com>
2024-03-19 17:32:50 +0530

    fix: remove unused imports

--- apps/web/src/pages/auth/components/useAcceptInvite.ts
+++ apps/web/src/pages/auth/components/useAcceptInvite.ts
@@ -1,18 +1,15 @@
 import { useCallback } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useNavigate } from 'react-router-dom';
 import * as Sentry from '@sentry/react';
 import type { IResponseError } from '@novu/shared';
 
 import { api } from '../../../api/api.client';
 import { useAuthContext } from '../../../components/providers/AuthProvider';
 import { applyToken } from '../../../hooks';
-import { ROUTES } from '../../../constants/routes.enum';
 import { errorMessage } from '../../../utils/notifications';
 
 export function useAcceptInvite() {
   const { setToken } = useAuthContext();
-  const navigate = useNavigate();
   const queryClient = useQueryClient();
 
   const { isLoading, mutateAsync, error, isError } = useMutation<string, IResponseError, string>((tokenItem) =>
