9c7a17b8ebaf3caa5b6c3042051f92b666f677a1
David Söderberg <granskog_@hotmail.com>
2024-03-21 10:12:42 +0100

    refactor: use product feature hook

--- libs/shared-web/src/hooks/useProductFeature.ts
+++ libs/shared-web/src/hooks/useProductFeature.ts
@@ -1,12 +1,16 @@
 import { ApiServiceLevelEnum, productFeatureEnabledForServiceLevel, ProductFeatureKeyEnum } from '@novu/shared';
+import { useEffect, useState } from 'react';
 import { useAuthController } from './useAuthController';
-import { useMemo } from 'react';
 
 export const useProductFeature = (feature: ProductFeatureKeyEnum) => {
   const { organization } = useAuthController();
+  const [enabled, setEnabled] = useState(false);
 
-  return useMemo(
-    () => productFeatureEnabledForServiceLevel[feature].includes(organization?.apiServiceLevel as ApiServiceLevelEnum),
-    [feature, organization.apiServiceLevel]
-  );
+  useEffect(() => {
+    setEnabled(
+      productFeatureEnabledForServiceLevel[feature].includes(organization?.apiServiceLevel as ApiServiceLevelEnum)
+    );
+  }, [feature, organization?.apiServiceLevel]);
+
+  return enabled;
 };
