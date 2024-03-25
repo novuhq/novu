b9775aa4a6e332d7d45df1c174d2d58e4da150aa
David Söderberg <granskog_@hotmail.com>
2024-03-25 10:12:41 +0100

    feat: add more tests

--- apps/web/cypress/tests/billing/billing.spec-ee.ts
+++ apps/web/cypress/tests/billing/billing.spec-ee.ts
@@ -129,4 +129,128 @@ describe('Billing', function () {
     cy.getByTestId('plan-business-current').should('exist');
     cy.getByTestId('plan-business-add-payment').should('exist');
   });
+
+  it('should be able to manage subscription', function () {
+    cy.intercept('GET', '**/v1/billing/subscription', {
+      data: {
+        trialStart: null,
+        trialEnd: null,
+        hasPaymentMethod: true,
+        status: 'active',
+      },
+    }).as('getSubscription');
+
+    cy.intercept('GET', '**/v1/organizations', (request) => {
+      request.reply((response) => {
+        if (!response.body.data) {
+          return response;
+        }
+
+        response.body['data'] = [
+          {
+            ...response.body.data[0],
+            apiServiceLevel: 'business',
+          },
+        ];
+        return response;
+      });
+    }).as('organizations');
+
+    cy.visit('/settings/billing');
+
+    cy.wait(['@getSubscription']);
+
+    cy.getByTestId('plan-title').should('have.text', 'Plans');
+    cy.getByTestId('plan-business-current').should('exist');
+    cy.getByTestId('plan-business-manage').should('exist');
+  });
+
+  it('should be able to upgrade from free', function () {
+    cy.intercept('GET', '**/v1/billing/subscription', {
+      data: {
+        trialStart: null,
+        trialEnd: null,
+        hasPaymentMethod: null,
+        status: 'active',
+      },
+    }).as('getSubscription');
+
+    cy.intercept('GET', '**/v1/organizations', (request) => {
+      request.reply((response) => {
+        if (!response.body.data) {
+          return response;
+        }
+
+        response.body['data'] = [
+          {
+            ...response.body.data[0],
+            apiServiceLevel: 'free',
+          },
+        ];
+        return response;
+      });
+    }).as('organizations');
+
+    cy.visit('/settings/billing');
+
+    cy.wait(['@getSubscription']);
+
+    cy.getByTestId('plan-title').should('have.text', 'Plans');
+    cy.getByTestId('plan-free-current').should('exist');
+    cy.getByTestId('plan-business-upgrade').should('exist');
+  });
+
+  it('should display billing page', function () {
+    cy.intercept('GET', '**/v1/billing/subscription', {
+      data: {
+        trialStart: subDays(startOfDay(new Date()), 20),
+        trialEnd: addDays(endOfDay(new Date()), 10),
+        hasPaymentMethod: false,
+        status: 'trialing',
+      },
+    }).as('getSubscription');
+
+    cy.intercept('GET', '**/v1/organizations', (request) => {
+      request.reply((response) => {
+        if (!response.body.data) {
+          return response;
+        }
+
+        response.body['data'] = [
+          {
+            ...response.body.data[0],
+            apiServiceLevel: 'business',
+          },
+        ];
+        return response;
+      });
+    }).as('organizations');
+
+    cy.visit('/settings/billing');
+
+    cy.wait(['@getSubscription']);
+
+    cy.getByTestId('plan-title').should('have.text', 'Plans');
+    cy.getByTestId('plan-business-current').should('exist');
+    cy.getByTestId('plan-business-add-payment').should('exist');
+    cy.getByTestId('free-trial-plan-widget').should('have.text', '10 days left on your trial');
+    cy.getByTestId('free-trial-widget-text').should('have.text', '10 days left on your Business trial');
+    cy.getByTestId('free-trial-banner').should('exist');
+
+    cy.intercept('GET', '**/v1/billing/subscription', {
+      data: {
+        trialStart: startOfDay(new Date()),
+        trialEnd: addDays(endOfDay(new Date()), 30),
+        hasPaymentMethod: true,
+        status: 'trialing',
+      },
+    }).as('getSubscription');
+
+    cy.visit('/settings/billing');
+
+    cy.wait(['@getSubscription']);
+    cy.getByTestId('free-trial-plan-widget').should('not.exist');
+    cy.getByTestId('free-trial-widget-text').should('not.exist');
+    cy.getByTestId('free-trial-banner').should('not.exist');
+  });
 });
