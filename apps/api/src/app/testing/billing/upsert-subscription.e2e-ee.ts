991a33dcc35e4ec40f7cf90475a998203ff8f1ee
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-03-25 13:52:37 +0000

    test(api): Fix upsert-subscription tests

--- apps/api/src/app/testing/billing/upsert-subscription.e2e-ee.ts
+++ apps/api/src/app/testing/billing/upsert-subscription.e2e-ee.ts
@@ -2,6 +2,7 @@ import * as sinon from 'sinon';
 import { OrganizationRepository } from '@novu/dal';
 import { expect } from 'chai';
 import { ApiServiceLevelEnum } from '@novu/shared';
+import { StripeBillingIntervalEnum, StripeUsageTypeEnum } from '@novu/ee-billing/src/stripe/types';
 
 describe('UpsertSubscription', () => {
   const eeBilling = require('@novu/ee-billing');
@@ -15,16 +16,18 @@ describe('UpsertSubscription', () => {
     subscriptions: {
       create: () => {},
       update: () => {},
+      del: () => {},
     },
   };
   let updateSubscriptionStub: sinon.SinonStub;
   let createSubscriptionStub: sinon.SinonStub;
+  let deleteSubscriptionStub: sinon.SinonStub;
 
   let getPricesStub: sinon.SinonStub;
   const repo = new OrganizationRepository();
   let updateOrgStub: sinon.SinonStub;
 
-  const mockCustomer = {
+  const mockCustomerBase = {
     id: 'customer_id',
     deleted: false,
     metadata: {
@@ -36,8 +39,11 @@ describe('UpsertSubscription', () => {
           id: 'subscription_id',
           items: {
             data: [
-              { id: 'item_id_usage_notifications', price: { recurring: { usage_type: 'metered' } } },
-              { id: 'item_id_flat', price: { recurring: { usage_type: 'licensed' } } },
+              {
+                id: 'item_id_usage_notifications',
+                price: { recurring: { usage_type: StripeUsageTypeEnum.METERED } },
+              },
+              { id: 'item_id_flat', price: { recurring: { usage_type: StripeUsageTypeEnum.LICENSED } } },
             ],
           },
         },
@@ -49,18 +55,19 @@ describe('UpsertSubscription', () => {
     getPricesStub = sinon.stub(GetPrices.prototype, 'execute').resolves({
       metered: [
         {
-          id: 'price_id_metered_1',
+          id: 'price_id_metered',
         },
       ],
       licensed: [
         {
-          id: 'price_id_licensed_1',
+          id: 'price_id_licensed',
         },
       ],
     } as any);
     updateOrgStub = sinon.stub(repo, 'update').resolves({ matched: 1, modified: 1 });
     createSubscriptionStub = sinon.stub(stripeStub.subscriptions, 'create');
     updateSubscriptionStub = sinon.stub(stripeStub.subscriptions, 'update');
+    deleteSubscriptionStub = sinon.stub(stripeStub.subscriptions, 'del');
   });
 
   afterEach(() => {
@@ -68,6 +75,7 @@ describe('UpsertSubscription', () => {
     updateOrgStub.reset();
     createSubscriptionStub.reset();
     updateSubscriptionStub.reset();
+    deleteSubscriptionStub.reset();
   });
 
   const createUseCase = () => {
@@ -77,16 +85,20 @@ describe('UpsertSubscription', () => {
   };
 
   describe('Subscription upserting', () => {
-    describe('No subscriptions', () => {
+    describe('ZERO active subscriptions', () => {
+      const mockCustomerNoSubscriptions = {
+        ...mockCustomerBase,
+        subscriptions: { data: [] },
+      };
+
       it('should create a single subscription with monthly prices when billingInterval is month', async () => {
         const useCase = createUseCase();
-        const customer = { ...mockCustomer, subscriptions: { data: [] } };
 
         await useCase.execute(
           UpsertSubscriptionCommand.create({
-            customer: customer as any,
+            customer: mockCustomerNoSubscriptions as any,
             apiServiceLevel: ApiServiceLevelEnum.BUSINESS,
-            billingInterval: 'month',
+            billingInterval: StripeBillingIntervalEnum.MONTH,
           })
         );
 
@@ -95,10 +107,10 @@ describe('UpsertSubscription', () => {
             customer: 'customer_id',
             items: [
               {
-                price: 'price_id_metered_1',
+                price: 'price_id_metered',
               },
               {
-                price: 'price_id_licensed_1',
+                price: 'price_id_licensed',
               },
             ],
           },
@@ -107,13 +119,12 @@ describe('UpsertSubscription', () => {
 
       it('should create two subscriptions, one with monthly prices and one with annual prices when billingInterval is year', async () => {
         const useCase = createUseCase();
-        const customer = { ...mockCustomer, subscriptions: { data: [] } };
 
         await useCase.execute(
           UpsertSubscriptionCommand.create({
-            customer: customer as any,
+            customer: mockCustomerNoSubscriptions as any,
             apiServiceLevel: ApiServiceLevelEnum.BUSINESS,
-            billingInterval: 'year',
+            billingInterval: StripeBillingIntervalEnum.YEAR,
           })
         );
 
@@ -123,7 +134,7 @@ describe('UpsertSubscription', () => {
             customer: 'customer_id',
             items: [
               {
-                price: 'price_id_licensed_1',
+                price: 'price_id_licensed',
               },
             ],
           },
@@ -131,7 +142,7 @@ describe('UpsertSubscription', () => {
             customer: 'customer_id',
             items: [
               {
-                price: 'price_id_metered_1',
+                price: 'price_id_metered',
               },
             ],
           },
@@ -139,15 +150,61 @@ describe('UpsertSubscription', () => {
       });
     });
 
-    describe('One subscription', () => {
-      it('should create a new annual subscription and update the existing subscription if the customer has one subscription and billingInterval is month', async () => {
+    describe('ONE active subscription', () => {
+      const mockCustomerOneSubscription = {
+        ...mockCustomerBase,
+        subscriptions: {
+          data: [
+            {
+              id: 'subscription_id',
+              items: {
+                data: [
+                  {
+                    id: 'item_id_usage_notifications',
+                    price: { recurring: { usage_type: StripeUsageTypeEnum.METERED } },
+                  },
+                  { id: 'item_id_flat', price: { recurring: { usage_type: StripeUsageTypeEnum.LICENSED } } },
+                ],
+              },
+            },
+          ],
+        },
+      };
+
+      it('should update the existing subscription if the customer has one subscription and billingInterval is month', async () => {
         const useCase = createUseCase();
 
         await useCase.execute(
           UpsertSubscriptionCommand.create({
-            customer: mockCustomer as any,
+            customer: mockCustomerOneSubscription as any,
             apiServiceLevel: ApiServiceLevelEnum.FREE,
-            billingInterval: 'month',
+            billingInterval: StripeBillingIntervalEnum.MONTH,
+          })
+        );
+
+        expect(updateSubscriptionStub.lastCall.args).to.deep.equal([
+          'subscription_id',
+          {
+            items: [
+              {
+                price: 'price_id_metered',
+              },
+              {
+                price: 'price_id_licensed',
+              },
+            ],
+          },
+        ]);
+      });
+
+      it('should create a new annual subscription and update the existing subscription if the customer has one subscription and billingInterval is year', async () => {
+        const useCase = createUseCase();
+
+        await useCase.execute(
+          UpsertSubscriptionCommand.create({
+            customer: mockCustomerOneSubscription as any,
+            apiServiceLevel: ApiServiceLevelEnum.FREE,
+            billingInterval: StripeBillingIntervalEnum.YEAR,
           })
         );
 
@@ -156,44 +213,125 @@ describe('UpsertSubscription', () => {
             customer: 'customer_id',
             items: [
               {
-                price: 'price_id_metered_1',
+                price: 'price_id_licensed',
+              },
+            ],
+          },
+        ]);
+
+        expect(updateSubscriptionStub.lastCall.args).to.deep.equal([
+          'subscription_id',
+          {
+            items: [
+              {
+                price: 'price_id_metered',
+              },
+            ],
+          },
+        ]);
+      });
+    });
+
+    describe('TWO active subscriptions', () => {
+      const mockCustomerTwoSubscriptions = {
+        ...mockCustomerBase,
+        subscriptions: {
+          data: [
+            {
+              id: 'subscription_id_1',
+              items: {
+                data: [{ id: 'item_id_flat', price: { recurring: { usage_type: StripeUsageTypeEnum.LICENSED } } }],
+              },
+            },
+            {
+              id: 'subscription_id_2',
+              items: {
+                data: [
+                  {
+                    id: 'item_id_usage_notifications',
+                    price: { recurring: { usage_type: StripeUsageTypeEnum.METERED } },
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      };
+      it('should delete the licensed subscription and update the metered subscription if the customer has two subscriptions and billingInterval is month', async () => {
+        const useCase = createUseCase();
+
+        await useCase.execute(
+          UpsertSubscriptionCommand.create({
+            customer: mockCustomerTwoSubscriptions as any,
+            apiServiceLevel: ApiServiceLevelEnum.FREE,
+            billingInterval: StripeBillingIntervalEnum.MONTH,
+          })
+        );
+
+        expect(deleteSubscriptionStub.lastCall.args).to.deep.equal(['subscription_id_1', { prorate: true }]);
+
+        expect(updateSubscriptionStub.lastCall.args).to.deep.equal([
+          'subscription_id_2',
+          {
+            items: [
+              {
+                price: 'price_id_metered',
               },
               {
-                price: 'price_id_licensed_1',
+                price: 'price_id_licensed',
               },
             ],
           },
         ]);
       });
 
-      it('should throw an error if the billing interval is not supported', async () => {
+      it('should update the existing subscriptions if the customer has two subscriptions and billingInterval is year', async () => {
         const useCase = createUseCase();
-        const customer = { ...mockCustomer, subscriptions: { data: [{}, {}] } };
 
-        try {
-          await useCase.execute(
-            UpsertSubscriptionCommand.create({
-              customer: customer as any,
-              apiServiceLevel: ApiServiceLevelEnum.FREE,
-              billingInterval: 'invalid',
-            })
-          );
-          throw new Error('Should not reach here');
-        } catch (e) {
-          expect(e.message).to.equal(`Invalid billing interval: 'invalid'`);
-        }
+        await useCase.execute(
+          UpsertSubscriptionCommand.create({
+            customer: mockCustomerTwoSubscriptions as any,
+            apiServiceLevel: ApiServiceLevelEnum.FREE,
+            billingInterval: StripeBillingIntervalEnum.YEAR,
+          })
+        );
+
+        expect(updateSubscriptionStub.getCalls().map((call) => call.args)).to.deep.equal([
+          [
+            'subscription_id_1',
+            {
+              items: [
+                {
+                  price: 'price_id_licensed',
+                },
+              ],
+            },
+          ],
+          [
+            'subscription_id_2',
+            {
+              items: [
+                {
+                  price: 'price_id_metered',
+                },
+              ],
+            },
+          ],
+        ]);
       });
+    });
 
+    describe('More than TWO active subscriptions', () => {
       it('should throw an error if the customer has more than two subscription', async () => {
         const useCase = createUseCase();
-        const customer = { ...mockCustomer, subscriptions: { data: [{}, {}, {}] } };
+        const customer = { ...mockCustomerBase, subscriptions: { data: [{}, {}, {}] } };
 
         try {
           await useCase.execute(
             UpsertSubscriptionCommand.create({
               customer: customer as any,
               apiServiceLevel: ApiServiceLevelEnum.FREE,
-              billingInterval: 'month',
+              billingInterval: StripeBillingIntervalEnum.MONTH,
             })
           );
           throw new Error('Should not reach here');
@@ -203,13 +341,33 @@ describe('UpsertSubscription', () => {
       });
     });
 
+    it('should throw an error if the billing interval is not supported', async () => {
+      const useCase = createUseCase();
+      const customer = { ...mockCustomerBase, subscriptions: { data: [{}, {}] } };
+
+      try {
+        await useCase.execute(
+          UpsertSubscriptionCommand.create({
+            customer: customer as any,
+            apiServiceLevel: ApiServiceLevelEnum.FREE,
+            billingInterval: 'invalid',
+          })
+        );
+        throw new Error('Should not reach here');
+      } catch (e) {
+        expect(e.message).to.equal(`Invalid billing interval: 'invalid'`);
+      }
+    });
+
     describe('Organization entity update', () => {
       it('should update the organization with the new apiServiceLevel', async () => {
         const useCase = createUseCase();
+        const customer = { ...mockCustomerBase, subscriptions: { data: [{}, {}] } };
 
         await useCase.execute(
           UpsertSubscriptionCommand.create({
-            customer: mockCustomer as any,
+            customer: mockCustomerBase as any,
+            billingInterval: StripeBillingIntervalEnum.MONTH,
             apiServiceLevel: ApiServiceLevelEnum.BUSINESS,
           })
         );
