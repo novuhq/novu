ca10b08c6fa01cea931db3d76985db693fdb2ef8
David Söderberg <granskog_@hotmail.com>
2024-04-08 14:45:38 +0200

    feat: add feature flag check for free trial widget

--- apps/web/src/components/layout/components/FreeTrialSidebarWidget.tsx
+++ apps/web/src/components/layout/components/FreeTrialSidebarWidget.tsx
@@ -1,7 +1,14 @@
-import { IS_DOCKER_HOSTED } from '@novu/shared-web';
+import { FeatureFlagsKeysEnum } from '@novu/shared';
+import { IS_DOCKER_HOSTED, useFeatureFlag } from '@novu/shared-web';
 
 export const FreeTrialSidebarWidget = () => {
+  const isEnabled = useFeatureFlag(FeatureFlagsKeysEnum.IS_BILLING_REVERSE_TRIAL_ENABLED);
+
   if (IS_DOCKER_HOSTED) {
+    return null;
+  }
+
+  if (!isEnabled) {
     return null;
   }
 
