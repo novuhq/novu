90bf6dc99612e0fc6d699f65844f03c3ebabde46
Amit Barletz <amit@amplication.com>
2024-04-08 16:53:48 +0300

    chore(worker): remove generate metadata and metadata files

--- apps/worker/src/generate-metadata.ts
+++
@@ -1,9 +0,0 @@
-import { PluginMetadataGenerator } from '@nestjs/cli/lib/compiler/plugins';
-
-const generator = new PluginMetadataGenerator();
-generator.generate({
-  visitors: [],
-  outputDir: __dirname,
-  watch: true,
-  tsconfigPath: 'apps/worker/tsconfig.build.json',
-});
