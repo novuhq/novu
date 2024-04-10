33dea1b303f9a4d48cc26b9c9a0392dd2d5d84ae
Amit Barletz <amit@amplication.com>
2024-04-10 14:28:31 +0300

    chore(worker): move swc from webpack to nest cli

--- apps/worker/webpack.config.js
+++ apps/worker/webpack.config.js
@@ -1,20 +1,6 @@
-const swcDefaultConfig = require('@nestjs/cli/lib/compiler/defaults/swc-defaults').swcDefaultsFactory().swcOptions;
-
 module.exports = function (options) {
   return {
     ...options,
     devtool: 'source-map',
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'swc-loader',
-            options: swcDefaultConfig,
-          },
-        },
-      ],
-    },
   };
 };
