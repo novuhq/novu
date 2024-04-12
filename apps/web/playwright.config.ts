b4f9c622bbb5646dfb35a55b6c09c369e3c4b4d0
Paweł <pawel.tymczuk@gmail.com>
2024-04-12 23:03:45 +0200

    chore(ci): playwright config

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -40,16 +40,16 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    /*
+     * {
+     *   name: 'firefox',
+     *   use: { ...devices['Desktop Firefox'] },
+     * },
+     * {
+     *   name: 'webkit',
+     *   use: { ...devices['Desktop Safari'] },
+     * },
+     */
 
     /* Test against mobile viewports. */
     /*
