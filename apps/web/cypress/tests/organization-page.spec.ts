08faf74ed028daa3410e5f1b316628c25f67d2ef
Biswajeet Das <biswajeetdas18@gmail.com>
2024-04-12 22:12:28 +0530

    fix: test

--- apps/web/cypress/tests/organization-page.spec.ts
+++ apps/web/cypress/tests/organization-page.spec.ts
@@ -2,30 +2,7 @@ describe('Organization Page', function () {
   beforeEach(function () {
     cy.initializeSession().as('session');
     cy.visit('/settings/organization');
-    cy.intercept('*/organizations/branding').as('updateOrganization');
     cy.intercept('*/organizations').as('renameOrganization');
-  });
-
-  it('should update logo', function () {
-    cy.intercept('*/storage/upload-url*').as('uploadLogo');
-
-    cy.fixture('test-logo.png', {}).then((contents) => {
-      cy.getByTestId('image-input-container')
-        .find('input')
-        .selectFile(
-          {
-            contents: Buffer.from(contents),
-            fileName: 'test-logo.png',
-            mimeType: 'image/png',
-          },
-          { force: true }
-        );
-    });
-
-    cy.wait('@uploadLogo');
-    cy.waitForNetworkIdle(500);
-    cy.getByTestId('preview-img').should('have.attr', 'src').should('include', '.png');
-    cy.getByTestId('preview-img').should('have.attr', 'src').should('include', this.session.organization._id);
   });
 
   it('update the organization name', function () {
