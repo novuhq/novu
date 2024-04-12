00ab4adf26ed9157d72ace655ecd82c79c0320c0
Joel Anton <joel@novu.co>
2024-04-12 16:06:49 -0700

    feat: Wrap Teams page

--- apps/web/src/SettingsRoutes.tsx
+++ apps/web/src/SettingsRoutes.tsx
@@ -14,6 +14,7 @@ import { SettingsPageNew as SettingsPage } from './pages/settings/SettingsPageNe
 import { ApiKeysCard } from './pages/settings/tabs';
 import { EmailSettings } from './pages/settings/tabs/EmailSettings';
 import { OrganizationPage } from './pages/settings/organization';
+import { TeamPage } from './pages/settings/TeamPage';
 
 /** Note: using a hook is the only way to separate routes */
 export const useSettingsRoutes = () => {
@@ -31,7 +32,7 @@ export const useSettingsRoutes = () => {
         <Route path={ROUTES.BRAND_SETTINGS} element={<BrandingForm />} />
         {/* TODO: replace with actual component */}
         <Route path={ROUTES.ORGANIZATION} element={<OrganizationPage />} />
-        <Route path={ROUTES.TEAM_SETTINGS} element={<MembersInvitePageNew />} />
+        <Route path={ROUTES.TEAM_SETTINGS} element={<TeamPage />} />
         <Route path={ROUTES.BILLING} element={<BillingRoutes />} />
         <Route path={ROUTES.WEBHOOK} element={<EmailSettings />} />
         <Route
