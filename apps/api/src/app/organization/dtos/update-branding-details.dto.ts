ce0ef2a99d19addd5bfa5725eafd1d78ba085526
Biswajeet Das <biswajeetdas18@gmail.com>
2024-04-19 15:26:45 +0530

    fix: allow http only in local env

--- apps/api/src/app/organization/dtos/update-branding-details.dto.ts
+++ apps/api/src/app/organization/dtos/update-branding-details.dto.ts
@@ -1,8 +1,9 @@
 import { IsHexColor, IsOptional, IsString, IsUrl } from 'class-validator';
 import { IsImageUrl } from '../../shared/validators/image.validator';
 
-const protocols = process.env.NODE_ENV === 'production' ? ['https'] : ['http', 'https'];
-const hostWhitelist = ['production', 'test'].includes(process.env.NODE_ENV) ? undefined : ['localhost', 'web.novu.co'];
+const environments = ['production', 'test'];
+const protocols = environments.includes(process.env.NODE_ENV) ? ['https'] : ['http', 'https'];
+const hostWhitelist = environments.includes(process.env.NODE_ENV) ? undefined : ['localhost', 'web.novu.co'];
 
 export class UpdateBrandingDetailsDto {
   @IsUrl({
