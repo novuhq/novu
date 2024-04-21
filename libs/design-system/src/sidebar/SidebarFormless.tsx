863492e281d8f1910fb29b3745f50b5d6a0e16bd
Joel Anton <joel@novu.co>
2024-04-21 13:24:13 -0700

    refactor: Extract SidebarFormless and reuse const + styles

--- libs/design-system/src/sidebar/SidebarFormless.tsx
+++ libs/design-system/src/sidebar/SidebarFormless.tsx
@@ -1,29 +1,32 @@
-import styled from '@emotion/styled';
+import { cx } from '@emotion/css';
 import { Drawer, Loader, Stack } from '@mantine/core';
 import { useKeyDown } from '@novu/shared-web';
-
 import { ActionButton } from '../button/ActionButton';
 import { colors } from '../config';
 import { ArrowLeft } from '../icons';
 import { When } from '../when';
 import { Close } from './Close';
-import { BodyHolder, FooterHolder, HeaderHolder, scrollable, useDrawerStyles } from './Sidebar.styles';
+import {
+  BodyHolder,
+  FooterHolder,
+  HeaderHolder,
+  scrollable,
+  sidebarDrawerContentClassName,
+  useDrawerStyles,
+} from './Sidebar.styles';
 import { ISidebarBaseProps } from './Sidebar.types';
 
-const Form = styled.form<{ isParentScrollable: boolean }>`
-  height: 100%;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  gap: 24px;
-  ${(props) => props.isParentScrollable && scrollable};
-`;
+/**
+ * A Sidebar component without the form element that wraps content.
+ *
+ * This is a temporary solution to a overloaded pattern. The Sidebar component should
+ * not have an embedded form as it removes the caller from properly controlling their own form.
+ * We will refactor the Sidebar later on as part of the design system work.
+ *
+ * https://linear.app/novu/issue/NV-3632/de-couple-the-sidebar-from-its-internal-form
+ */
 
-export interface ISidebarProps extends ISidebarBaseProps {
-  onSubmit?: React.FormEventHandler<HTMLFormElement>;
-}
-
-export const Sidebar = ({
+export const SidebarFormless = ({
   customFooter,
   customHeader,
   children,
@@ -35,8 +38,7 @@ export const Sidebar = ({
   'data-test-id': dataTestId,
   onClose,
   onBack,
-  onSubmit,
-}: ISidebarProps) => {
+}: ISidebarBaseProps) => {
   const { classes: drawerClasses } = useDrawerStyles({ isExpanded });
   const onCloseCallback = () => {
     onClose();
@@ -58,15 +60,9 @@ export const Sidebar = ({
       trapFocus={false}
       data-expanded={isExpanded}
     >
-      <Form
-        name="form-name"
-        noValidate
-        onSubmit={onSubmit}
+      <div
         data-test-id={dataTestId}
-        isParentScrollable={isParentScrollable}
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
+        className={cx(sidebarDrawerContentClassName, { [scrollable]: isParentScrollable })}
       >
         <HeaderHolder className="sidebar-header-holder">
           {isExpanded && onBack && (
@@ -111,7 +107,7 @@ export const Sidebar = ({
           <When truthy={!isLoading}>{children}</When>
         </BodyHolder>
         {customFooter && <FooterHolder className="sidebar-footer-holder">{customFooter}</FooterHolder>}
-      </Form>
+      </div>
     </Drawer>
   );
 };
