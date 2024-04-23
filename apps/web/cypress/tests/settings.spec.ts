22b38d2778a16342f8294cb1471f970c90cf9990
Joel Anton <joel@novu.co>
2024-04-23 09:55:58 -0700

    test: Add & use feature flag util

--- apps/web/cypress/tests/settings.spec.ts
+++ apps/web/cypress/tests/settings.spec.ts
@@ -5,12 +5,12 @@ describe('Settings Screen', function () {
     cy.intercept('*/organizations/branding').as('updateBrandingSettings');
     cy.mockFeatureFlags({ IS_INFORMATION_ARCHITECTURE_ENABLED: false });
 
-    cy.waitLoadEnv(() => {
+    cy.waitLoadFeatureFlags(() => {
       cy.visit('/');
     });
   });
 
-  it.skip('should display the api key of the app', function () {
+  it('should display the api key of the app', function () {
     cy.visit('/settings');
     cy.get('.mantine-Tabs-tabsList').contains('API Keys').click();
     cy.getByTestId('api-key-container').should('have.value', this.session.environment.apiKeys[0].key);
