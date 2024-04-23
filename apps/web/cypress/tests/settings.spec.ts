f08557286dc4c21855f8a8170c0904b55196571e
Joel Anton <joel@novu.co>
2024-04-23 14:39:46 -0700

    ci: Do it again, but please stop failing

--- apps/web/cypress/tests/settings.spec.ts
+++ apps/web/cypress/tests/settings.spec.ts
@@ -1,13 +1,15 @@
 describe('Settings Screen', function () {
   beforeEach(function () {
+    cy.mockFeatureFlags({ IS_INFORMATION_ARCHITECTURE_ENABLED: false });
+
     cy.initializeSession().as('session');
     cy.intercept('*/channels/email/settings').as('updateEmailSettings');
     cy.intercept('*/organizations/branding').as('updateBrandingSettings');
-    cy.mockFeatureFlags({ IS_INFORMATION_ARCHITECTURE_ENABLED: false });
 
     cy.waitLoadFeatureFlags(() => {
       cy.visit('/');
     });
+    // avoid reloading the page (instead of visit('/settings') due to conditional routes behind the feature flag
     cy.contains('a', 'Settings').click();
   });
 
