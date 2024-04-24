83f142db47982488c81450b045c67861bb9d0956
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-04-24 09:52:05 +0100

    fix: bad diff

--- apps/api/src/app/testing/testing.controller.ts
+++ apps/api/src/app/testing/testing.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, HttpException, NotFoundException, Post, UseGuards } from '@nestjs/common';
-import { ProductFeatureKeyEnum, ResourceEnum } from '@novu/shared';
 import { DalService, UserEntity } from '@novu/dal';
+import { ProductFeatureKeyEnum, ResourceEnum } from '@novu/shared';
 
 import { ISeedDataResponseDto, SeedDataBodyDto } from './dtos/seed-data.dto';
 import { IdempotencyBodyDto } from './dtos/idempotency.dto';
