0553c349d8c1dc98d4d37dcb6a2bb81aba2e90c6
Joel Anton <joel@novu.co>
2024-04-25 15:39:04 -0500

    fix: Font weight for selects

--- apps/web/src/components/nav/NavSelect.styles.ts
+++ apps/web/src/components/nav/NavSelect.styles.ts
@@ -2,6 +2,7 @@ import { css } from '../../styled-system/css';
 
 // TODO: these are ugly, but necessary for overriding Mantine + our design-system overrides
 export const navSelectStyles = css({
+  fontWeight: 'strong !important',
   '& input': { bg: 'transparent', border: 'none !important', pl: '3.25rem !important' },
   '& .mantine-Select-icon': { width: 'inherit !important' },
   '& .mantine-Select-dropdown': { top: '50px !important' },
