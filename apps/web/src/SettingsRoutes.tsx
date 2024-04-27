58da753df9b851dcf8b342ff2cf795f92c80b2b1
Joel Anton <joel@novu.co>
2024-04-27 13:02:19 -0500

    fix: Conditional redirects

--- apps/web/src/SettingsRoutes.tsx
+++ apps/web/src/SettingsRoutes.tsx
@@ -24,64 +24,78 @@ export const useSettingsRoutes = () => {
   return (
     <>
       <Route path={ROUTES.SETTINGS} element={isInformationArchitectureEnabled ? <SettingsPage /> : <SettingsPageOld />}>
-        <Route path="" element={<ApiKeysCard />} />
-        <Route path="billing" element={<BillingRoutes />} />
+        <Route
+          path=""
+          element={isInformationArchitectureEnabled ? <Navigate to={ROUTES.PROFILE} replace /> : <ApiKeysCard />}
+        />
+        <Route path="billing/*" element={<BillingRoutes />} />
         <Route path="email" element={<EmailSettings />} />
         <Route path="team" element={<MembersInvitePageNew />} />
         <Route path="brand" element={<BrandingFormOld />} />
+        {/* ensure legacy routes re-route to base settings page */}
         <Route
           path="permissions"
           element={
-            <ProductLead
-              icon={<UserAccess />}
-              id="rbac-permissions"
-              title="Role-based access control"
-              text="Securely manage users' permissions to access system resources."
-              closeable={false}
-            />
+            !isInformationArchitectureEnabled ? (
+              <ProductLead
+                icon={<UserAccess />}
+                id="rbac-permissions"
+                title="Role-based access control"
+                text="Securely manage users' permissions to access system resources."
+                closeable={false}
+              />
+            ) : (
+              <Navigate to={ROUTES.SECURITY} replace />
+            )
           }
         />
         <Route
           path="sso"
           element={
-            <ProductLead
-              icon={<SSO />}
-              id="sso-settings"
-              title="Single Sign-On (SSO)"
-              text="Simplify user authentication and enhance security."
-              closeable={false}
-            />
+            !isInformationArchitectureEnabled ? (
+              <ProductLead
+                icon={<SSO />}
+                id="sso-settings"
+                title="Single Sign-On (SSO)"
+                text="Simplify user authentication and enhance security."
+                closeable={false}
+              />
+            ) : (
+              <Navigate to={ROUTES.SETTINGS} replace />
+            )
           }
         />
         <Route
           path="data-integrations"
           element={
-            <ProductLead
-              icon={<Cloud />}
-              id="data-integrations-settings"
-              title="Data Integrations"
-              text="Share data with 3rd party services via Segment and Datadog integrations to monitor analytics."
-              closeable={false}
-            />
+            !isInformationArchitectureEnabled ? (
+              <ProductLead
+                icon={<Cloud />}
+                id="data-integrations-settings"
+                title="Data Integrations"
+                text="Share data with 3rd party services via Segment and Datadog integrations to monitor analytics."
+                closeable={false}
+              />
+            ) : (
+              <Navigate to={ROUTES.SETTINGS} replace />
+            )
           }
         />
-        {isInformationArchitectureEnabled && <Route path={ROUTES.API_KEYS} element={<ApiKeysPage />} />}
-        {isInformationArchitectureEnabled && <Route path={ROUTES.PROFILE} element={<UserProfilePage />} />}
-        {isInformationArchitectureEnabled && <Route path={ROUTES.BRAND_SETTINGS} element={<BrandingPage />} />}
-        {/* TODO: replace with actual component */}
-        {isInformationArchitectureEnabled && <Route path={ROUTES.ORGANIZATION} element={<OrganizationPage />} />}
-        {isInformationArchitectureEnabled && <Route path={ROUTES.TEAM_SETTINGS} element={<TeamPage />} />}
-        {isInformationArchitectureEnabled && <Route path={ROUTES.BILLING} element={<BillingPage />} />}
-        {isInformationArchitectureEnabled && <Route path={ROUTES.WEBHOOK} element={<WebhookPage />} />}
-        {isInformationArchitectureEnabled && <Route path={ROUTES.SECURITY} element={<AccessSecurityPage />} />}
-        {/* Default settings route in case we didn't match with the existing */}
+        {/* TODO: remove all routes above once information architecture is fully enabled */}
         {isInformationArchitectureEnabled && (
-          <Route path={`${ROUTES.SETTINGS}`} element={<Navigate to={ROUTES.PROFILE} replace />} />
+          <>
+            <Route path={ROUTES.API_KEYS} element={<ApiKeysPage />} />
+            <Route path={ROUTES.PROFILE} element={<UserProfilePage />} />
+            <Route path={ROUTES.BRAND_SETTINGS} element={<BrandingPage />} />
+            <Route path={ROUTES.ORGANIZATION} element={<OrganizationPage />} />
+            <Route path={ROUTES.TEAM_SETTINGS} element={<TeamPage />} />
+            <Route path={`${ROUTES.BILLING}/*`} element={<BillingPage />} />
+            <Route path={ROUTES.WEBHOOK} element={<WebhookPage />} />
+            <Route path={ROUTES.SECURITY} element={<AccessSecurityPage />} />
+            <Route path={`${ROUTES.SETTINGS}`} element={<Navigate to={ROUTES.PROFILE} replace />} />
+          </>
         )}
-        <Route
-          path={`${ROUTES.SETTINGS}/*`}
-          element={<Navigate to={isInformationArchitectureEnabled ? ROUTES.PROFILE : ''} replace />}
-        />
+        <Route path={`${ROUTES.SETTINGS}/*`} element={<Navigate to={ROUTES.SETTINGS} replace />} />
       </Route>
     </>
   );
