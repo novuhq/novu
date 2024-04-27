e0b9d632745390a0d5f73aec957733c5fe6e51b5
Joel Anton <joel@novu.co>
2024-04-27 12:20:24 -0500

    fix: Routing

--- apps/web/src/SettingsRoutes.tsx
+++ apps/web/src/SettingsRoutes.tsx
@@ -21,66 +21,68 @@ import { WebhookPage } from './pages/settings/WebhookPage';
 export const useSettingsRoutes = () => {
   const isInformationArchitectureEnabled = useFeatureFlag(FeatureFlagsKeysEnum.IS_INFORMATION_ARCHITECTURE_ENABLED);
 
-  return isInformationArchitectureEnabled ? (
+  return (
     <>
-      <Route path={ROUTES.SETTINGS} element={<SettingsPage />}>
-        <Route path={ROUTES.API_KEYS} element={<ApiKeysPage />} />
-        <Route path={ROUTES.PROFILE} element={<UserProfilePage />} />
-        <Route path={ROUTES.BRAND_SETTINGS} element={<BrandingPage />} />
+      <Route path={ROUTES.SETTINGS} element={isInformationArchitectureEnabled ? <SettingsPage /> : <SettingsPageOld />}>
+        <Route path="" element={<ApiKeysCard />} />
+        <Route path="billing" element={<BillingRoutes />} />
+        <Route path="email" element={<EmailSettings />} />
+        <Route path="team" element={<MembersInvitePageNew />} />
+        <Route path="brand" element={<BrandingFormOld />} />
+        <Route
+          path="permissions"
+          element={
+            <ProductLead
+              icon={<UserAccess />}
+              id="rbac-permissions"
+              title="Role-based access control"
+              text="Securely manage users' permissions to access system resources."
+              closeable={false}
+            />
+          }
+        />
+        <Route
+          path="sso"
+          element={
+            <ProductLead
+              icon={<SSO />}
+              id="sso-settings"
+              title="Single Sign-On (SSO)"
+              text="Simplify user authentication and enhance security."
+              closeable={false}
+            />
+          }
+        />
+        <Route
+          path="data-integrations"
+          element={
+            <ProductLead
+              icon={<Cloud />}
+              id="data-integrations-settings"
+              title="Data Integrations"
+              text="Share data with 3rd party services via Segment and Datadog integrations to monitor analytics."
+              closeable={false}
+            />
+          }
+        />
+        {isInformationArchitectureEnabled && <Route path={ROUTES.API_KEYS} element={<ApiKeysPage />} />}
+        {isInformationArchitectureEnabled && <Route path={ROUTES.PROFILE} element={<UserProfilePage />} />}
+        {isInformationArchitectureEnabled && <Route path={ROUTES.BRAND_SETTINGS} element={<BrandingPage />} />}
         {/* TODO: replace with actual component */}
-        <Route path={ROUTES.ORGANIZATION} element={<OrganizationPage />} />
-        <Route path={ROUTES.TEAM_SETTINGS} element={<TeamPage />} />
-        <Route path={ROUTES.BILLING} element={<BillingPage />} />
-        <Route path={ROUTES.WEBHOOK} element={<WebhookPage />} />
-        <Route path={ROUTES.SECURITY} element={<AccessSecurityPage />} />
+        {isInformationArchitectureEnabled && <Route path={ROUTES.ORGANIZATION} element={<OrganizationPage />} />}
+        {isInformationArchitectureEnabled && <Route path={ROUTES.TEAM_SETTINGS} element={<TeamPage />} />}
+        {isInformationArchitectureEnabled && <Route path={ROUTES.BILLING} element={<BillingPage />} />}
+        {isInformationArchitectureEnabled && <Route path={ROUTES.WEBHOOK} element={<WebhookPage />} />}
+        {isInformationArchitectureEnabled && <Route path={ROUTES.SECURITY} element={<AccessSecurityPage />} />}
         {/* Default settings route in case we didn't match with the existing */}
-        <Route path={`${ROUTES.SETTINGS}/*`} element={<Navigate to={ROUTES.PROFILE} replace />} />
-        <Route path={`${ROUTES.SETTINGS}`} element={<Navigate to={ROUTES.PROFILE} replace />} />
+        {isInformationArchitectureEnabled && (
+          <Route path={`${ROUTES.SETTINGS}`} element={<Navigate to={ROUTES.PROFILE} replace />} />
+        )}
+        <Route
+          path={`${ROUTES.SETTINGS}/*`}
+          element={<Navigate to={isInformationArchitectureEnabled ? ROUTES.PROFILE : ''} replace />}
+        />
       </Route>
     </>
-  ) : (
-    <Route path={ROUTES.SETTINGS} element={<SettingsPageOld />}>
-      <Route path="" element={<ApiKeysCard />} />
-      <Route path="billing/*" element={<BillingRoutes />} />
-      <Route path="email" element={<EmailSettings />} />
-      <Route path="team" element={<MembersInvitePageNew />} />
-      <Route path="brand" element={<BrandingFormOld />} />
-      <Route
-        path="permissions"
-        element={
-          <ProductLead
-            icon={<UserAccess />}
-            id="rbac-permissions"
-            title="Role-based access control"
-            text="Securely manage users' permissions to access system resources."
-            closeable={false}
-          />
-        }
-      />
-      <Route
-        path="sso"
-        element={
-          <ProductLead
-            icon={<SSO />}
-            id="sso-settings"
-            title="Single Sign-On (SSO)"
-            text="Simplify user authentication and enhance security."
-            closeable={false}
-          />
-        }
-      />
-      <Route
-        path="data-integrations"
-        element={
-          <ProductLead
-            icon={<Cloud />}
-            id="data-integrations-settings"
-            title="Data Integrations"
-            text="Share data with 3rd party services via Segment and Datadog integrations to monitor analytics."
-            closeable={false}
-          />
-        }
-      />
-    </Route>
   );
 };
