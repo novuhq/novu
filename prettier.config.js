85040b8c583bb4c3816ccea94f9b9e803bce36fe
Joel Anton <joel@novu.co>
2024-05-01 10:17:43 -0500

    feat: Change prettier config to js so it can be 'extended' (#5480)

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 120,
+  trailingComma: 'es5',
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  arrowParens: 'always',
+};
+
+export default config;
