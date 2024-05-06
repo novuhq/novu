53647b30378989a60adff5b1bc467a069383585b
Joel Anton <joel@novu.co>
2024-05-06 11:20:44 -0600

    refactor: Use async launch darkly

--- apps/web/src/Providers.tsx
+++ apps/web/src/Providers.tsx
@@ -1,8 +1,9 @@
+import { Loader } from '@mantine/core';
 import { CONTEXT_PATH, LAUNCH_DARKLY_CLIENT_SIDE_ID, SegmentProvider } from '@novu/shared-web';
 import * as Sentry from '@sentry/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { withLDProvider } from 'launchdarkly-react-client-sdk';
-import { PropsWithChildren } from 'react';
+import { asyncWithLDProvider } from 'launchdarkly-react-client-sdk';
+import { PropsWithChildren, useEffect, useRef, useState } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 import { api } from './api/api.client';
@@ -22,28 +23,46 @@ const queryClient = new QueryClient({
   },
 });
 
+type GenericProvider = ({ children }: { children: React.ReactNode }) => JSX.Element;
+
 /**
  * Centralized Provider hierarchy.
  */
 const Providers: React.FC<PropsWithChildren<{}>> = ({ children }) => {
+  const LDProvider = useRef<GenericProvider>((props) => <>{props.children}</>);
+  const [isLDReady, setIsLDReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchLDProvider = async () => {
+      LDProvider.current = await asyncWithLDProvider({
+        clientSideID: LAUNCH_DARKLY_CLIENT_SIDE_ID,
+        reactOptions: {
+          useCamelCaseFlagKeys: false,
+        },
+        deferInitialization: true,
+      });
+      setIsLDReady(true);
+    };
+    fetchLDProvider();
+  });
+
+  if (!isLDReady) {
+    return <Loader size={32} />;
+  }
+
   return (
-    <SegmentProvider>
-      <HelmetProvider>
-        <BrowserRouter basename={CONTEXT_PATH}>
-          <QueryClientProvider client={queryClient}>
-            <AuthProvider>{children}</AuthProvider>
-          </QueryClientProvider>
-        </BrowserRouter>
-      </HelmetProvider>
-    </SegmentProvider>
+    <LDProvider.current>
+      <SegmentProvider>
+        <HelmetProvider>
+          <BrowserRouter basename={CONTEXT_PATH}>
+            <QueryClientProvider client={queryClient}>
+              <AuthProvider>{children}</AuthProvider>
+            </QueryClientProvider>
+          </BrowserRouter>
+        </HelmetProvider>
+      </SegmentProvider>
+    </LDProvider.current>
   );
 };
 
-export default Sentry.withProfiler(
-  withLDProvider({
-    clientSideID: LAUNCH_DARKLY_CLIENT_SIDE_ID,
-    reactOptions: {
-      useCamelCaseFlagKeys: false,
-    },
-  })(Providers)
-);
+export default Sentry.withProfiler(Providers);
