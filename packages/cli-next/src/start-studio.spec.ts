3ff34279ea10bb15d3c1fd7764722212bc72a333
ainouzgali <ainouzgali@gmail.com>
2024-05-07 16:45:00 +0300

    test(wip): initial addition of test suite

--- packages/cli-next/src/start-studio.spec.ts
+++ packages/cli-next/src/start-studio.spec.ts
@@ -4,4 +4,7 @@ describe('test use of novus node package - Events', () => {
   test('should ss', () => {
     expect(1).toBe(1);
   });
+  test('should fail', () => {
+    expect(1).toBe(2);
+  });
 });
