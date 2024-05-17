d7af8afec9a832a8cd43990e92fbf2bd3b6fcd85
Joel Anton <joel@novu.co>
2024-05-17 10:50:28 -0700

    [Design System] Storybook & Vitest (#5569)
    * refactor: Setup storybook & vitest

---
+++ libs/novui/postcss.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  /**
+   * must use this syntax for vite + panda
+   * https://panda-css.com/docs/installation/storybook#install-panda
+   */
+  plugins: [require('@pandacss/dev/postcss')()],
+};
