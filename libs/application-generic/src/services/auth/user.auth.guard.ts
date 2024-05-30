6509087f28603eb35ee14ee84a12f527c3c41e85
Adam Chmara <adam.chmara1@gmail.com>
2024-05-30 09:45:03 +0200

    feat(auth): use clerk auth strategy in enterprise version

--- libs/application-generic/src/services/auth/user.auth.guard.ts
+++ libs/application-generic/src/services/auth/user.auth.guard.ts
@@ -1,99 +1,60 @@
-import {
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
-import { AuthGuard, IAuthModuleOptions } from '@nestjs/passport';
-import { Reflector } from '@nestjs/core';
-import {
-  ApiAuthSchemeEnum,
-  IJwtPayload,
-  PassportStrategyEnum,
-} from '@novu/shared';
-import { Instrument } from '../../instrumentation';
+import { Injectable, ExecutionContext, Logger } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
+import { IAuthGuard, IAuthModuleOptions } from '@nestjs/passport';
+import { IJwtClaims } from '@novu/shared';
+import { CommunityUserAuthGuard } from './community.user.auth.guard';
 import { Observable } from 'rxjs';
+import { Instrument } from '../../instrumentation';
+import { PlatformException } from '../../utils/exceptions';
 
-type SentryUser = {
-  id: string;
-  username: string;
-  domain: string;
-};
-type HandledUser = (IJwtPayload & SentryUser) | false;
+function initiateAuthGuard(moduleRef: ModuleRef) {
+  try {
+    if (process.env.NOVU_ENTERPRISE === 'true') {
+      const eeAuthModule = require('@novu/ee-auth');
+      if (!eeAuthModule?.EEUserAuthGuard) {
+        throw new PlatformException('EEUserAuthGuard is not loaded');
+      }
 
-const NONE_AUTH_SCHEME = 'None';
+      return moduleRef.get(eeAuthModule.EEUserAuthGuard, { strict: false });
+    } else {
+      return moduleRef.get(CommunityUserAuthGuard, { strict: false });
+    }
+  } catch (e) {
+    Logger.error(
+      e,
+      'Unexpected error while importing enterprise modules',
+      'EEUserAuthGuard'
+    );
+    throw e;
+  }
+}
 
 @Injectable()
-export class UserAuthGuard extends AuthGuard([
-  PassportStrategyEnum.JWT,
-  PassportStrategyEnum.HEADER_API_KEY,
-]) {
-  constructor(private readonly reflector: Reflector) {
-    super();
+export class UserAuthGuard {
+  private readonly authGuard: IAuthGuard;
+
+  constructor(moduleRef: ModuleRef) {
+    this.authGuard = initiateAuthGuard(moduleRef);
   }
 
   @Instrument()
   canActivate(
     context: ExecutionContext
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return super.canActivate(context);
+    return this.authGuard.canActivate(context);
   }
 
   getAuthenticateOptions(context: ExecutionContext): IAuthModuleOptions<any> {
-    const request = context.switchToHttp().getRequest();
-    const authorizationHeader = request.headers.authorization;
-
-    const authScheme = authorizationHeader?.split(' ')[0] || NONE_AUTH_SCHEME;
-    request.authScheme = authScheme;
-
-    switch (authScheme) {
-      case ApiAuthSchemeEnum.BEARER:
-        return {
-          session: false,
-          defaultStrategy: PassportStrategyEnum.JWT,
-        };
-      case ApiAuthSchemeEnum.API_KEY:
-        const apiEnabled = this.reflector.get<boolean>(
-          'external_api_accessible',
-          context.getHandler()
-        );
-        if (!apiEnabled)
-          throw new UnauthorizedException('API endpoint not available');
-
-        return {
-          session: false,
-          defaultStrategy: PassportStrategyEnum.HEADER_API_KEY,
-        };
-      case NONE_AUTH_SCHEME:
-        throw new UnauthorizedException('Missing authorization header');
-      default:
-        throw new UnauthorizedException(
-          `Invalid authentication scheme: "${authScheme}"`
-        );
-    }
+    return this.authGuard.getAuthenticateOptions(context);
   }
 
-  handleRequest<TUser = IJwtPayload>(
+  handleRequest<TUser = IJwtClaims>(
     err: any,
-    user: IJwtPayload | false,
+    user: IJwtClaims | false,
     info: any,
     context: ExecutionContext,
     status?: any
   ): TUser {
-    let handledUser: HandledUser;
-    if (typeof user === 'object') {
-      /**
-       * This helps with sentry and other tools that need to know who the user is based on `id` property.
-       */
-      handledUser = {
-        ...user,
-        id: user._id,
-        username: (user.firstName || '').trim(),
-        domain: user.email?.split('@')[1],
-      };
-    } else {
-      handledUser = user;
-    }
-
-    return super.handleRequest(err, handledUser, info, context, status);
+    return this.authGuard.handleRequest(err, user, info, context, status);
   }
 }
--- libs/shared/src/types/auth/auth.types.ts
+++ libs/shared/src/types/auth/auth.types.ts
@@ -20,5 +20,16 @@ export enum ApiAuthSchemeEnum {
 
 export enum PassportStrategyEnum {
   JWT = 'jwt',
+  JWT_CLERK = 'jwt-clerk',
   HEADER_API_KEY = 'headerapikey',
 }
+
+export type SentryUser = {
+  id: string;
+  username: string;
+  domain: string;
+};
+
+export type HandledUser = (IJwtClaims & SentryUser) | false;
+
+export const NONE_AUTH_SCHEME = 'None';
