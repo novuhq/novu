37512613651ebd7131c0eb3147e99056b2606df6
Joel Anton <joel@novu.co>
2024-05-31 17:52:49 +0200

    feat(novui,web): Setup Mantine v7 and demonstrate ability to style in lib and app (#5651)
    * feat: Add mantine7
    
    * feat: Centralized core type

--- libs/novui/postcss.config.cjs
+++ libs/novui/postcss.config.cjs
@@ -1,7 +1,20 @@
 module.exports = {
   /**
-   * must use this syntax for vite + panda
+   * Must use array + require syntax for vite + postcss & panda
+   * https://vitejs.dev/config/shared-options.html#css-postcss
    * https://panda-css.com/docs/installation/storybook#install-panda
    */
-  plugins: [require('@pandacss/dev/postcss')()],
+  plugins: [
+    require('@pandacss/dev/postcss')(),
+    require('postcss-preset-mantine')(),
+    require('postcss-simple-vars')({
+      variables: {
+        'mantine-breakpoint-xs': '36em',
+        'mantine-breakpoint-sm': '48em',
+        'mantine-breakpoint-md': '62em',
+        'mantine-breakpoint-lg': '75em',
+        'mantine-breakpoint-xl': '88em',
+      },
+    }),
+  ],
 };
