0674f5ddd66e26c7d23820283599263797c53eb5
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-05-31 19:39:15 +0100

    refactor(build): migrate to tsup and update configs

---
+++ packages/echo/tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'tsup';
+import { type SupportedFrameworkName } from './src';
+
+const frameworks: SupportedFrameworkName[] = ['h3', 'express', 'next', 'nuxt'];
+
+export default defineConfig({
+  entry: ['src/index.ts', ...frameworks.map((framework) => `src/${framework}.ts`)],
+  splitting: false,
+  sourcemap: false,
+  clean: true,
+  treeshake: true,
+  dts: true,
+  format: ['cjs', 'esm'],
+  minify: true,
+  minifyWhitespace: true,
+  minifyIdentifiers: true,
+  minifySyntax: true,
+});
