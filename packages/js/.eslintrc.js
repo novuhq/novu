13ae73874de9f3d344e85ff0c33108ee7f35230c
Paweł <pawel.tymczuk@gmail.com>
2024-05-31 16:12:20 +0200

    feat(js): the base js sdk package scaffolding

---
+++ packages/js/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['../../.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        filter: '_',
+        selector: 'variableLike',
+        leadingUnderscore: 'allow',
+        format: ['PascalCase', 'camelCase', 'UPPER_CASE'],
+      },
+    ],
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+};
