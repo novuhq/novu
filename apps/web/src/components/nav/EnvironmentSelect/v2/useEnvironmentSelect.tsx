8862253174c2213fb26510cbcc1ec4cfd4054256
Joel Anton <joel@novu.co>
2024-05-31 19:05:46 +0200

    feat: Add New EnvironmentSelector

---
+++ apps/web/src/components/nav/EnvironmentSelect/v2/useEnvironmentSelect.tsx
@@ -0,0 +1,63 @@
+import { type IIconProps, IconConstruction, IconRocketLaunch, IconComputer } from '@novu/design-system';
+import { useEnvController, ROUTES } from '@novu/shared-web';
+import { useState } from 'react';
+import { type ISelectProps } from '@novu/design-system';
+import { matchPath, useLocation, useMatch, useNavigate } from 'react-router-dom';
+import { EnvironmentEnum } from './EnvironmentEnum';
+
+const ENVIRONMENT_ICON_LOOKUP: Record<EnvironmentEnum, React.ReactElement<IIconProps>> = {
+  [EnvironmentEnum.LOCAL]: <IconComputer />,
+  [EnvironmentEnum.DEVELOPMENT]: <IconConstruction />,
+  [EnvironmentEnum.PRODUCTION]: <IconRocketLaunch />,
+};
+
+export const useEnvironmentSelect = () => {
+  const [isPopoverOpened, setIsPopoverOpened] = useState<boolean>(false);
+  const location = useLocation();
+
+  const { setEnvironment, isLoading, environment, readonly } = useEnvController({
+    onSuccess: (newEnvironment) => {
+      setIsPopoverOpened(!!newEnvironment?._parentId);
+    },
+  });
+
+  async function handlePopoverLinkClick(e) {
+    e.preventDefault();
+
+    await setEnvironment(EnvironmentEnum.DEVELOPMENT, { route: ROUTES.CHANGES });
+  }
+
+  const onChange: ISelectProps['onChange'] = async (value) => {
+    if (typeof value !== 'string') {
+      return;
+    }
+
+    /*
+     * this navigates users to the "base" page of the application to avoid sub-pages opened with data from other
+     * environments -- unless the path itself is based on a specific environment (e.g. API Keys)
+     */
+    const urlParts = location.pathname.replace('/', '').split('/');
+    const redirectRoute: string | undefined = checkIfEnvBasedRoute() ? undefined : urlParts[0];
+    await setEnvironment(value as EnvironmentEnum, { route: redirectRoute });
+  };
+
+  return {
+    loading: isLoading,
+    data: Object.values(EnvironmentEnum).map((value) => ({
+      label: value,
+      value,
+    })),
+    value: environment?.name,
+    onChange,
+    readonly,
+    icon: environment?.name ? ENVIRONMENT_ICON_LOOKUP[environment.name] : null,
+    isPopoverOpened,
+    setIsPopoverOpened,
+    handlePopoverLinkClick,
+  };
+};
+
+/** Determine if the current pathname is dependent on the current env */
+function checkIfEnvBasedRoute() {
+  return [ROUTES.API_KEYS, ROUTES.WEBHOOK].some((route) => matchPath(route, window.location.pathname));
+}
