bf06aa690ed6d5f65fed553045edf84ec57632fe
Paweł <pawel.tymczuk@gmail.com>
2024-06-03 16:12:58 +0200

    feat(js): lazy session initialization and interface fixes

---
+++ packages/js/src/novu.spec.ts
@@ -0,0 +1,83 @@
+import { Novu } from './novu';
+
+const mockFeedResponse = {
+  data: [],
+  hasMore: true,
+  totalCount: 0,
+  pageSize: 10,
+  page: 1,
+};
+
+const initializeSession = jest.fn().mockResolvedValue({ token: 'token', profile: 'profile' });
+const getNotificationsList = jest.fn(() => mockFeedResponse);
+
+jest.mock('@novu/client', () => ({
+  ...jest.requireActual('@novu/client'),
+  ApiService: jest.fn().mockImplementation(() => {
+    const apiService = {
+      isAuthenticated: false,
+      initializeSession,
+      setAuthorizationToken: jest.fn(() => {
+        apiService.isAuthenticated = true;
+      }),
+      getNotificationsList,
+    };
+
+    return apiService;
+  }),
+}));
+
+describe('Novu', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('lazy session initialization', () => {
+    test('should call the queued feeds.fetch after the session is initialized', async () => {
+      const page = 1;
+      const novu = new Novu({ applicationIdentifier: 'applicationIdentifier', subscriberId: 'subscriberId' });
+      const res = await novu.feeds.fetch({ page });
+
+      expect(initializeSession).toHaveBeenCalledTimes(1);
+      expect(getNotificationsList).toHaveBeenCalledWith(page, {});
+      expect(res).toEqual(mockFeedResponse);
+    });
+
+    test('should call the feeds.fetch right away when session is already initialized', async () => {
+      const page = 1;
+      const novu = new Novu({ applicationIdentifier: 'applicationIdentifier', subscriberId: 'subscriberId' });
+      // await for session initialization
+      await new Promise((resolve) => setTimeout(resolve, 10));
+
+      const res = await novu.feeds.fetch({ page });
+
+      expect(initializeSession).toHaveBeenCalledTimes(1);
+      expect(getNotificationsList).toHaveBeenCalledWith(page, {});
+      expect(res).toEqual(mockFeedResponse);
+    });
+
+    test('should reject the queued feeds.fetch if session initialization fails', async () => {
+      const page = 1;
+      const error = new Error('Failed to initialize session');
+      initializeSession.mockRejectedValueOnce(error);
+      const novu = new Novu({ applicationIdentifier: 'applicationIdentifier', subscriberId: 'subscriberId' });
+
+      const fetchPromise = novu.feeds.fetch({ page });
+
+      await expect(fetchPromise).rejects.toEqual(error);
+    });
+
+    test('should reject the feeds.fetch right away when session initialization has failed', async () => {
+      const page = 1;
+      const error = new Error('Failed to initialize session');
+      initializeSession.mockRejectedValueOnce(error);
+      const novu = new Novu({ applicationIdentifier: 'applicationIdentifier', subscriberId: 'subscriberId' });
+      // await for session initialization
+      await new Promise((resolve) => setTimeout(resolve, 10));
+
+      const fetchPromise = novu.feeds.fetch({ page });
+
+      await expect(fetchPromise).rejects.toEqual(error);
+    });
+  });
+});
