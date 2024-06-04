095a903a8d8c1e5f3c4dab7cd944551a871a3860
Joel Anton <joel@novu.co>
2024-06-04 17:54:40 +0200

    refactor: Move icons export to `@novu/novui/icons`

--- libs/design-system/src/iconsV2/Icon.types.ts
+++ libs/design-system/src/iconsV2/Icon.types.ts
@@ -1,22 +1,22 @@
 import { CSSProperties, SVGAttributes } from 'react';
 
 /**
- * @deprecated Use @novu/novui instead.
+ * @deprecated Use @novu/novui/icons instead.
  * Icon Size in pixels (to be replaced with values directly from Design System)
  */
 export type IconSize = '16' | '20' | '24';
 
 /**
- * @deprecated Use @novu/novui instead.
+ * @deprecated Use @novu/novui/icons instead.
  * A temporary type that accepts a typical CSS color until we have our strict Design System types defined
  */
 type UnsafeIconColor = CSSProperties['color'];
 
-/** @deprecated Use @novu/novui instead. */
+/** @deprecated Use @novu/novui/icons instead. */
 export interface IIconStyleProps {
   /**
    *
-   * @deprecated Use @novu/novui instead.
+   * @deprecated Use @novu/novui/icons instead.
    * Default: '20px'.
    *
    * The actual display size in pixels.
@@ -24,7 +24,7 @@ export interface IIconStyleProps {
   size?: IconSize;
   /**
    *
-   * @deprecated Use @novu/novui instead.
+   * @deprecated Use @novu/novui/icons instead.
    * Default: B60
    *
    * WARNING: this is only to be used with colors from the design system, and will later be enforced more strictly
@@ -32,7 +32,7 @@ export interface IIconStyleProps {
   color?: UnsafeIconColor;
 }
 
-/** @deprecated Use @novu/novui instead. */
+/** @deprecated Use @novu/novui/icons instead. */
 export interface IIconProps extends Omit<SVGAttributes<SVGElement>, 'color'>, IIconStyleProps {
   /**
    * Description for accessibility. This is mandatory so that we make a11y a priority with icons
@@ -40,5 +40,5 @@ export interface IIconProps extends Omit<SVGAttributes<SVGElement>, 'color'>, II
   title: string;
 }
 
-/** @deprecated Use @novu/novui instead. */
+/** @deprecated Use @novu/novui/icons instead. */
 export type IconType = (props: IIconProps) => JSX.Element;
