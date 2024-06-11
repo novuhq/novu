9e9cf52335181718ccdca30e14a92deccb443274
Pawan Jain <jainpawan211199@gmail.com>
2024-06-11 14:24:17 +0530

    fix(web): Hubspot onboarding invite member flow (#5674)
    * fix: handle user invite redirect to hubspot question form
    
    * fix: handle github sign in redirect
    
    * fix: invitation login and sign up cases
    
    * fix: spell check error
    
    * Update apps/web/src/pages/auth/components/LoginForm.tsx
    
    Co-authored-by: Sokratis Vidros <SokratisVidros@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sokratis Vidros <SokratisVidros@users.noreply.github.com>

--- apps/web/src/pages/auth/components/gitHubUtils.ts
+++ apps/web/src/pages/auth/components/gitHubUtils.ts
@@ -2,11 +2,20 @@ import { SignUpOriginEnum } from '@novu/shared';
 
 import { API_ROOT } from '../../../config';
 
-export const buildGithubLink = ({ invitationToken }: { invitationToken?: string }) => {
+export const buildGithubLink = ({
+  invitationToken,
+  isLoginPage,
+}: {
+  invitationToken?: string;
+  isLoginPage?: boolean;
+}) => {
   const queryParams = new URLSearchParams();
   queryParams.append('source', SignUpOriginEnum.WEB);
   if (invitationToken) {
     queryParams.append('invitationToken', invitationToken);
+  }
+  if (isLoginPage) {
+    queryParams.append('isLoginPage', 'true');
   }
 
   return `${API_ROOT}/v1/auth/github?${queryParams.toString()}`;
