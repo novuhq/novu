3ef41bfa7eaab9d3bb80350af643981ada253d30
Paweł Tymczuk <LetItRock@users.noreply.github.com>
2024-06-11 10:20:37 +0200

    feat(js): lazy session initialization and interface fixes (#5665)

--- packages/js/src/preferences/Preferences.ts
+++
@@ -1,9 +0,0 @@
-import { NovuEventEmitter } from '../event-emitter';
-
-export class Preferences {
-  #emitter: NovuEventEmitter;
-
-  constructor(emitter: NovuEventEmitter) {
-    this.#emitter = emitter;
-  }
-}
