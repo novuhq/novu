b732016a5b0cf4230a8904a8b25042ef4d334693
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-06-11 13:25:11 +0100

    refactor(config): simplify env file path logic

--- apps/api/src/config/index.ts
+++ apps/api/src/config/index.ts
@@ -5,19 +5,16 @@ const path = require('path');
 
 dotenv.config();
 
-const envFileMapper = {
+const envFileMapper: Record<typeof process.env.NODE_ENV, string> = {
   production: '.env.production',
   test: '.env.test',
   ci: '.env.ci',
   local: '.env',
   dev: '.env.development',
 };
-const selectedEnvFile = envFileMapper[process.env.NODE_ENV as any] || '.env';
+const selectedEnvFile = envFileMapper[process.env.NODE_ENV] || '.env';
 
-let pathToDotEnv = `${__dirname}/${process.env.E2E_RUNNER ? '..' : 'src'}/${selectedEnvFile}`;
-if (process.env.MIGRATION) {
-  pathToDotEnv = path.join(__dirname, `../${selectedEnvFile}`);
-}
+const pathToDotEnv = path.join(__dirname, '..', selectedEnvFile);
 
 const { error } = dotenv.config({ path: pathToDotEnv });
 
