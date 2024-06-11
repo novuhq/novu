92377f7a80cb7f971b7bdee1456ab625cdfa1004
Joel Anton <joel@novu.co>
2024-06-11 10:13:43 +0200

    feat(novui): Map panda values to mantine theme (#5692)
    * feat: Map Mantine config
    
    * fix: Use gradient default
    
    * feat: Add WIP button
    
    * feat: Include mantine styles & use example

--- libs/novui/src/components/NovuiProvider.tsx
+++ libs/novui/src/components/NovuiProvider.tsx
@@ -1,12 +1,14 @@
 import { MantineProvider } from '@mantine/core';
 import { FC, PropsWithChildren } from 'react';
 import { IconProvider } from '../icons/IconProvider';
+import { MANTINE_THEME } from './mantine-theme.config';
+
 type INovuiProviderProps = PropsWithChildren;
 
 /** Used to export a v7 Mantine provider */
 export const NovuiProvider: FC<INovuiProviderProps> = ({ children }) => {
   return (
-    <MantineProvider>
+    <MantineProvider theme={MANTINE_THEME}>
       <IconProvider>{children}</IconProvider>
     </MantineProvider>
   );
