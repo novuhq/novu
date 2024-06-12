cf235e5bc6867f6e7ad059ae4c1eac868d3b6392
Biswajeet Das <biswajeetdas18@gmail.com>
2024-06-12 20:15:05 +0530

    feat: ui solid

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,43 @@
 import { AppRoutes } from './AppRoutes';
 import Providers from './Providers';
 import { ApplicationReadyGuard } from './ApplicationReadyGuard';
+import { UI } from '@novu/js';
 
 export default function App() {
   return (
     <Providers>
       <ApplicationReadyGuard>
         <AppRoutes />
+        <Mounter mount={(el) => {}} />
       </ApplicationReadyGuard>
     </Providers>
   );
+}
+
+// export const novu = Novu({});
+
+import { useLayoutEffect, useRef } from 'react';
+
+type MountProps = {
+  mount: (node: HTMLDivElement) => void;
+  unmount?: (node: HTMLDivElement) => void;
+};
+
+export function Mounter({ mount, unmount }: MountProps) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    const elementRef = ref.current;
+    if (elementRef && mount) {
+      mount(elementRef);
+    }
+
+    return () => {
+      if (elementRef && unmount) {
+        unmount(elementRef);
+      }
+    };
+  }, [ref, mount, unmount]);
+
+  return <div ref={ref} />;
 }
--- packages/js/src/global.d.ts
+++ packages/js/src/global.d.ts
@@ -1,7 +1,9 @@
 import { Novu } from './novu';
+import { UI } from './ui';
 
 declare global {
   interface Window {
     Novu: typeof Novu;
+    UI: typeof UI;
   }
 }
