31e5e1f3f5b886f401b4d1c9f249c12d61a5b28f
Joel Anton <joel@novu.co>
2024-06-13 08:37:05 -0700

    fix: Duplicate

--- libs/novui/src/ingredients/colorPaletteGradient.ingredient.ts
+++ libs/novui/src/ingredients/colorPaletteGradient.ingredient.ts
@@ -1,5 +1,4 @@
 import { css } from '../../styled-system/css';
-import {} from '../../styled-system/types';
 
 /** Must override `bgGradient` property to chose the direction */
 export const colorPaletteGradient = css.raw({
