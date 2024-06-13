08c196b63010fb104804f97d6bb8c97c9abcec19
Joel Anton <joel@novu.co>
2024-06-13 11:39:54 -0700

    feat: Add action icon

---
+++ libs/novui/src/components/button/IconButton.tsx
@@ -0,0 +1,41 @@
+import { ActionIcon } from '@mantine/core';
+import { FC } from 'react';
+import { token } from '../../../styled-system/tokens';
+import { css, cx } from '../../../styled-system/css';
+import { button, type ButtonVariant } from '../../../styled-system/recipes';
+import { IconType } from '../../icons';
+import { CoreProps } from '../../types';
+
+interface IIconButtonProps
+  extends CoreProps,
+    Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'children'>,
+    Partial<ButtonVariant> {
+  Icon: IconType;
+  loading?: boolean;
+}
+
+/**
+ * A button with only an Icon.
+ *
+ * TODO: there are not specifications for these in the Design System, so this just follows the Button recipe.
+ */
+export const IconButton: FC<IIconButtonProps> = ({
+  Icon,
+  onClick,
+  className,
+  loading,
+  variant = 'transparent',
+  ...buttonProps
+}) => {
+  return (
+    <ActionIcon
+      onClick={onClick}
+      className={cx(button({ variant }).root, className)}
+      variant={variant}
+      loading={loading}
+      {...buttonProps}
+    >
+      <Icon title="action-icon" color={variant === 'filled' ? token('colors.button.icon.filled') : undefined} />
+    </ActionIcon>
+  );
+};
