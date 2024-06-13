c56e14171847f33d8440ebe4340a1de95404a65c
Joel Anton <joel@novu.co>
2024-06-13 09:13:51 -0700

    fix: Fix build-timing issue

--- libs/novui/src/ingredients/colorPaletteGradient.ingredient.ts
+++ libs/novui/src/ingredients/colorPaletteGradient.ingredient.ts
@@ -1,7 +1,7 @@
-import { css } from '../../styled-system/css';
+import { defineStyles } from '@pandacss/dev';
 
 /** Must override `bgGradient` property to chose the direction */
-export const colorPaletteGradient = css.raw({
+export const colorPaletteGradient = defineStyles({
   bgGradient: `to-l !important`,
   gradientFrom: 'colorPalette.start !important',
   gradientTo: 'colorPalette.end !important',
