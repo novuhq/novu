60e735c5ef41e35607c24f9e0c2ba0eac4a0deab
David Söderberg <granskog_@hotmail.com>
2024-06-14 08:29:45 +0200

    fix: template html

--- packages/create-novu-app/templates/app/ts/app/echo/client.ts
+++ packages/create-novu-app/templates/app/ts/app/echo/client.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { Echo } from "@novu/echo";
 
 export const echo = new Echo({
@@ -17,20 +18,18 @@ echo.workflow("onboarding", async ({ step }) => {
             <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
           </head>
-          <body style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif">
-            <div style="text-align: center">
+          <body style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px;">
+            <div style="text-align: center; margin-bottom: 24px;">
               <img width="200px" src="https://web.novu.co/static/images/logo.png" />
             </div>
-            <h1>Notification workflows rooted in how YOU work</h1>
-            <p>Hi!</p>
-            <p>Good to have you here! We continuously work on giving you the flexibility to build any notification setup you need
-              - through code, right from your IDE - and to give your manager an easy way to adjust the notification content. Check
+            <h1 style="margin: 0; margin-bottom: 16px;">Notification workflows rooted in how YOU work</h1>
+            <p style="margin: 0; margin-bottom: 8px;">Hi!</p>
+            <p style="margin: 0; margin-bottom: 8px;">Good to have you here! We continuously work on giving you the flexibility to
+              build any notification setup you need - through code, right from your IDE - and to give your manager an easy way to adjust the notification content. Check
               out our <a href="https://docs.novu.co/echo/quickstart">docs</a> to learn more.</p>
-            <p>Questions or problems? Our <a href="https://discord.com/channels/895029566685462578/1019663407915483176">Discord
-              support channel</a> is here for you.</p>
-            <p>Feedback? Head over to our <a href="https://roadmap.novu.co/roadmap">public roadmap</a> to submit it, or simply
-              poke us on Discord or via email. We’re here to make your life easier!</p>
-            <p>Cheers,<br />Novu Team</p>
+            <p style="margin: 0; margin-bottom: 8px;">Questions or problems? Our <a href="https://discord.com/channels/895029566685462578/1019663407915483176">Discord support channel</a> is here for you.</p>
+            <p style="margin: 0; margin-bottom: 8px;">Feedback? Head over to our <a href="https://roadmap.novu.co/roadmap">public roadmap</a> to submit it, or simply poke us on Discord or via email. We’re here to make your life easier!</p>
+            <p style="margin: 0;">Cheers,<br />Novu Team</p>
           </body>
         </html>`,
     };
