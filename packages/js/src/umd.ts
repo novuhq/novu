a79e3bb9603dfc7ab6b614f62b30c9bc949cd703
Biswajeet Das <biswajeetdas18@gmail.com>
2024-06-16 19:27:00 +0530

    feat: add ui to umd

--- packages/js/src/umd.ts
+++ packages/js/src/umd.ts
@@ -1,5 +1,7 @@
 import { Novu } from './novu';
+import { InboxUI } from './ui';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 window.Novu = Novu;
+window.UI = InboxUI;
