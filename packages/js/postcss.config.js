93701b598130d6bb868a726cf55f08474167af50
Biswajeet Das <biswajeetdas18@gmail.com>
2024-06-17 11:28:05 +0530

    feat: add tailwind

--- packages/js/postcss.config.js
+++ packages/js/postcss.config.js
@@ -1,6 +1,5 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: [require('autoprefixer'), require('postcss-nested')],
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+  },
 };
-
-module.exports = config;
