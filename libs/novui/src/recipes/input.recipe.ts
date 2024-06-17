478822956c1678cbfd64c12c90f9aa3e353c5689
Joel Anton <joel@novu.co>
2024-06-17 12:33:12 -0700

    feat: WIP input. Still some styling issues

--- libs/novui/src/recipes/input.recipe.ts
+++ libs/novui/src/recipes/input.recipe.ts
@@ -1,7 +1,16 @@
-import { InputStylesNames } from '@mantine/core';
+import { InputStylesNames, InputWrapperStylesNames } from '@mantine/core';
 import { defineSlotRecipe } from '@pandacss/dev';
 
-const SLOTS: InputStylesNames[] = ['input', 'wrapper', 'section'];
+const SLOTS: (InputStylesNames | InputWrapperStylesNames)[] = [
+  'root',
+  'input',
+  'wrapper',
+  'section',
+  'description',
+  'error',
+  'required',
+  'label',
+];
 
 export const INPUT_RECIPE = defineSlotRecipe({
   className: 'input',
@@ -9,33 +18,66 @@ export const INPUT_RECIPE = defineSlotRecipe({
   slots: SLOTS,
   base: {
     input: {
-      // default color palette
-      colorPalette: 'mode.cloud',
+      background: 'input.surface !important',
+      // borderColor: 'input.border.default !important',
+      borderRadius: 'input !important',
+      lineHeight: '125 !important',
+      color: 'typography.text.main !important',
+      '& svg': {
+        color: 'typography.text.main !important',
+      },
+
+      marginTop: 'margins.layout.Input.titleBottom',
+      paddingX: '75 !important',
+      paddingTop: '100 !important',
+      paddingBottom: '100 !important',
+      height: 'auto !important',
+
       _disabled: {
-        opacity: 'disabled',
+        bg: 'input.surface.disabled !important',
+        borderColor: 'input.border.disabled !important',
       },
       '&:hover:not(:disabled)': {
         opacity: 'hover',
       },
 
-      _active: {
-        color: 'typography.text.main !important',
-        '& svg': {
-          color: 'typography.text.main !important',
+      _invalid: {
+        border: 'solid !important',
+        borderColor: 'input.border.error !important',
+      },
+
+      '&:focus,&:focus-within': {
+        outline: 'solid !important',
+        outlineColor: 'input.border.active !important',
+        _invalid: {
+          borderColor: 'input.border.error !important',
+          outlineColor: 'input.border.active !important',
         },
-        border: 'double',
-        borderColor: 'colorPalette.start !important',
+      },
+      _placeholder: {
+        color: 'typography.text.secondary',
       },
 
       _hover: {
         background: 'none !important',
-        color: 'typography.text.main !important',
-        '& svg': {
-          color: 'typography.text.main !important',
-        },
       },
     },
+    label: {
+      color: 'typography.text.main',
+      fontWeight: 'strong !important',
+      fontSize: '88 !important',
+    },
+    error: {
+      // TODO: figure out about the typography error token
+      color: 'input.border.error !important',
+      fontSize: '75 !important',
+      paddingTop: '50 !important',
+    },
     wrapper: {},
-    section: {},
+    section: { paddingRight: '75' },
+    description: {
+      color: 'typography.text.tertiary !important',
+      fontSize: '88 !important',
+    },
   },
 });
