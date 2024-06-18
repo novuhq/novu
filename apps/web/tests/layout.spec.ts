ce71526aa8f3a63399bbe858f679e495d55fa5cf
Sokratis Vidros <sokratis.vidros@gmail.com>
2024-06-18 14:55:37 +0300

    chore(web): Restore layouts page
    It was accidentally removed at 2da2cd213051e02fbfd28081ae0ca0f8d269984a. An e2e spec has been also added to prevent this from happening again.

---
+++ apps/web/tests/layout.spec.ts
@@ -0,0 +1,14 @@
+import { test } from './utils/baseTest';
+import { LayoutsPage } from './page-models/layoutsPage';
+import { initializeSession } from './utils/browser';
+
+test.beforeEach(async ({ page }) => {
+  await initializeSession(page);
+});
+
+test('should display layouts list', async ({ page }) => {
+  const subscribersPage = await LayoutsPage.goTo(page);
+  await subscribersPage.assertIsVisible();
+  await subscribersPage.assertLayoutsTableHeaders(['Name', 'Description', 'Created At', 'Last Updated']);
+  await subscribersPage.assertLayoutsTableRowCount(1);
+});
