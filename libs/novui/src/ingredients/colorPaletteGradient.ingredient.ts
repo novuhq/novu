3965b04cf4658dfd330f7438d3020a995baf930d
Joel Anton <joel@novu.co>
2024-06-19 16:06:44 -0700

    feat: Gradient text

--- libs/novui/src/ingredients/colorPaletteGradient.ingredient.ts
+++ libs/novui/src/ingredients/colorPaletteGradient.ingredient.ts
@@ -1,13 +1,18 @@
 import { defineStyles } from '@pandacss/dev';
 
-/** Must override `bgGradient` property to chose the direction */
 export const colorPaletteGradientHorizontal = defineStyles({
-  bgGradient: `to-l !important`,
+  bgGradient: `to-r !important`,
   gradientFrom: 'colorPalette.start !important',
   gradientTo: 'colorPalette.end !important',
 });
 
-/** Must override `bgGradient` property to chose the direction */
+export const colorPaletteGradientText = defineStyles({
+  ...colorPaletteGradientHorizontal,
+  backgroundClip: 'text',
+  WebkitBackgroundClip: 'text',
+  WebkitTextFillColor: 'transparent',
+});
+
 export const colorPaletteGradientVertical = defineStyles({
   bgGradient: `to-b !important`,
   gradientFrom: 'colorPalette.start !important',
