41dde1b32657bceb4ea5b81aef0154ec0c2234c3
David Söderberg <granskog_@hotmail.com>
2024-06-20 10:34:50 +0200

    feat: add docs for add workflow button

--- apps/web/src/studio/components/workflows/list/WorkflowsListPage.tsx
+++ apps/web/src/studio/components/workflows/list/WorkflowsListPage.tsx
@@ -6,21 +6,29 @@ import { PageTemplate } from '../../../layout/index';
 import { WorkflowsTable } from '../table/index';
 import { useQuery } from '@tanstack/react-query';
 import { bridgeApi } from '../../../../api/bridge/bridge.api';
+import { useState } from 'react';
+import { DocsModal } from '../../../../components/docs/DocsModal';
 
 export const WorkflowsListPage = () => {
+  const [docsOpen, setDocsOpen] = useState<boolean>(false);
   const { data, isLoading } = useQuery(['bridge-workflows'], async () => {
     return bridgeApi.discover();
   });
 
+  const toggleDocs = () => {
+    setDocsOpen((prevOpen) => !prevOpen);
+  };
+
   return (
     <PageTemplate title="Workflows">
       <Flex justify={'space-between'}>
-        <Button onClick={() => alert('Add workflow!')} Icon={IconAddBox} size={'sm'} variant="transparent">
+        <Button onClick={toggleDocs} Icon={IconAddBox} size={'sm'} variant="transparent">
           Add workflow
         </Button>
         <SearchInput placeholder="Type name or identifier..." />
       </Flex>
       <WorkflowsTable workflows={data?.workflows || []} isLoading={isLoading} />
+      <DocsModal open={docsOpen} toggle={toggleDocs} path="echo/concepts/workflows" />
     </PageTemplate>
   );
 };
