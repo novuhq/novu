e47e2af7c58ee5afc32e1d4c436f594a7485ba28
Denis Kralj <denis@novu.co>
2024-06-21 14:18:40 +0200

    chore: hide button under feature flag

--- apps/web/src/components/layout/components/v2/HeaderNav.tsx
+++ apps/web/src/components/layout/components/v2/HeaderNav.tsx
@@ -3,7 +3,7 @@ import { IconHelpOutline, IconOutlineCloudUpload } from '@novu/novui/icons';
 import { Tooltip } from '@novu/design-system';
 import { Button } from '@novu/novui';
 import { IS_DOCKER_HOSTED } from '../../../../config';
-import { useBootIntercom } from '../../../../hooks';
+import { useBootIntercom, useFeatureFlag } from '../../../../hooks';
 import useThemeChange from '../../../../hooks/useThemeChange';
 import { discordInviteUrl } from '../../../../pages/quick-start/consts';
 import { css } from '@novu/novui/css';
@@ -13,13 +13,14 @@ import { HEADER_NAV_HEIGHT } from '../../constants';
 import { NotificationCenterWidget } from '../NotificationCenterWidget';
 import { HeaderMenuItems } from './HeaderMenuItems';
 import { useLocation } from 'react-router-dom';
-import { isStudioRoute } from '../../../../studio/utils/isStudioRoute';
+import { FeatureFlagsKeysEnum } from '@novu/shared';
 
 export function HeaderNav() {
   const { currentUser } = useAuth();
   const isSelfHosted = IS_DOCKER_HOSTED;
   const location = useLocation();
-  const isStudio = isStudioRoute(location.pathname);
+
+  const isV2ExperienceEnabled = useFeatureFlag(FeatureFlagsKeysEnum.IS_V2_EXPERIENCE_ENABLED);
 
   useBootIntercom();
   const { themeIcon, themeLabel, toggleColorScheme } = useThemeChange();
@@ -38,8 +39,7 @@ export function HeaderNav() {
     >
       {/* TODO: Change position: right to space-between for breadcrumbs */}
       <HStack flexWrap={'nowrap'} justifyContent="flex-end" gap={'100'}>
-        {/* TODO: Use a more robust / realistic way to conditionally show this button */}
-        {isStudio && (
+        {isV2ExperienceEnabled && (
           <Button size="xs" Icon={IconOutlineCloudUpload}>
             Sync
           </Button>
