286462bf8c3783ed5c87dce8cddd474316fa1818
George Desipris <george@Georges-MBP.router.wind>
2024-06-21 13:19:01 +0300

    feat(js): Create variable set

--- packages/js/src/ui/helpers/utils.ts
+++ packages/js/src/ui/helpers/utils.ts
@@ -26,14 +26,14 @@ function generateUniqueRandomString(set: Set<string>, length: number): string {
 }
 
 function generateRandomString(length: number): string {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
 
-  return result.toLowerCase();
+  return result;
 }
 
 function generateUniqueRandomString(set: Set<string>, length: number): string {
