d3a67618bc390a20e31088b2573be7f2eb4bf777
Biswajeet Das <biswajeetdas18@gmail.com>
2024-06-21 18:34:27 +0530

    refactor: remove js package from web app

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,65 +1,13 @@
 import { AppRoutes } from './AppRoutes';
 import Providers from './Providers';
 import { ApplicationReadyGuard } from './ApplicationReadyGuard';
-import { InboxUI } from '@novu/js/ui';
-import '@novu/js/ui/index.css';
-import { useLayoutEffect, useRef } from 'react';
 
 export default function App() {
   return (
     <Providers>
-      <Mounter
-        mount={(el) => {
-          const ui = new InboxUI();
-          console.log('mounting', ui);
-          ui.mount(el, {
-            name: 'biswa from Solid-js rendered inside React.js',
-            options: {
-              applicationIdentifier: 'kgqMYcgJrAAp',
-              subscriberId: '1234',
-            },
-            appearance: {
-              elements: {
-                button: {
-                  backgroundColor: 'var(--nv-color-primary-500)',
-                },
-              },
-              variables: {
-                colorPrimary: '#ebb523', //orange
-              },
-            },
-          });
-        }}
-      />
       <ApplicationReadyGuard>
         <AppRoutes />
       </ApplicationReadyGuard>
     </Providers>
   );
-}
-
-// export const novu = Novu({});
-
-type MountProps = {
-  mount: (node: HTMLDivElement) => void;
-  unmount?: (node: HTMLDivElement) => void;
-};
-
-export function Mounter({ mount, unmount }: MountProps) {
-  const ref = useRef<HTMLDivElement>(null);
-
-  useLayoutEffect(() => {
-    const elementRef = ref.current;
-    if (elementRef && mount) {
-      mount(elementRef);
-    }
-
-    return () => {
-      if (elementRef && unmount) {
-        unmount(elementRef);
-      }
-    };
-  }, [ref, mount, unmount]);
-
-  return <div ref={ref} />;
 }
