47dc8ed0effc3e360ef25098a413210ca3f32040
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-06-24 10:57:11 +0200

    style(types): update comment style and TODO note

--- packages/framework/src/types/provider.types.ts
+++ packages/framework/src/types/provider.types.ts
@@ -11,8 +11,8 @@ export type Providers<T_StepType extends keyof typeof providerSchemas, T_Input, 
      * The outputs of the step.
      */
     outputs: T_Output;
-    // @ts-expect-error - FIXME: Fix conditional types
+    // eslint-disable-next-line multiline-comment-style
+    // TODO: fix the typing for `type` to use the keyof providerSchema[channelType]
+    // @ts-expect-error - Types of parameters 'options' and 'options' are incompatible.
   }) => Promise<FromSchema<(typeof providerSchemas)[T_StepType][K]['output']>>;
 };
-
-type Test = typeof providerSchemas;
