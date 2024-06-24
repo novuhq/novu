be34a5583c14108fa2613ed8a248df90e1bd3d15
David Söderberg <2233092+davidsoderberg@users.noreply.github.com>
2024-06-24 13:28:40 +0200

    feat: add empty properties state (#5800)

--- apps/web/src/studio/components/workflows/step-editor/WorkflowStepEditorInputsPanel.tsx
+++ apps/web/src/studio/components/workflows/step-editor/WorkflowStepEditorInputsPanel.tsx
@@ -1,6 +1,9 @@
 import { Button, JsonSchemaForm, Tabs } from '@novu/novui';
 import { IconOutlineEditNote, IconOutlineTune } from '@novu/novui/icons';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
+import { useDocsModal } from '../../../../components/docs/useDocsModal';
+import { When } from '../../../../components/utils/When';
+import { InputsEmptyPanel } from './InputsEmptyPanel';
 
 interface IWorkflowStepEditorInputsPanelProps {
   step: any;
@@ -19,52 +22,88 @@ export const WorkflowStepEditorInputsPanel: FC<IWorkflowStepEditorInputsPanelPro
   defaultInputs,
   isLoadingSave,
 }) => {
-  return (
-    <Tabs
-      defaultValue="payload"
-      tabConfigs={[
-        {
-          icon: <IconOutlineTune />,
-          value: 'payload',
-          label: 'Payload',
-          content: (
-            <JsonSchemaForm
-              onChange={(data) => onChange('payload', data)}
-              schema={workflow?.options?.payloadSchema || {}}
-              formData={{}}
-            />
-          ),
-        },
-        {
-          icon: <IconOutlineEditNote />,
-          value: 'step-inputs',
-          label: 'Step inputs',
-          content: (
-            <>
-              {onSave && (
-                <div style={{ display: 'flex', justifyContent: 'end' }}>
-                  <Button
-                    loading={isLoadingSave}
-                    variant={'filled'}
-                    size={'xs'}
-                    onClick={() => {
-                      onSave();
-                    }}
-                  >
-                    Save
-                  </Button>
-                </div>
-              )}
+  const { Component, toggle, setPath } = useDocsModal();
+  const havePayloadProperties = useMemo(() => {
+    return Object.keys(workflow?.options?.payloadSchema?.properties || {}).length > 0;
+  }, [workflow?.options?.payloadSchema]);
 
-              <JsonSchemaForm
-                onChange={(data) => onChange('step', data)}
-                schema={step?.inputs?.schema || {}}
-                formData={defaultInputs || {}}
-              />
-            </>
-          ),
-        },
-      ]}
-    />
+  const haveInputProperties = useMemo(() => {
+    return Object.keys(step?.inputs?.schema?.properties || {}).length > 0;
+  }, [step?.inputs?.schema]);
+
+  return (
+    <>
+      <Tabs
+        defaultValue="payload"
+        tabConfigs={[
+          {
+            icon: <IconOutlineTune />,
+            value: 'payload',
+            label: 'Payload',
+            content: (
+              <>
+                <When truthy={havePayloadProperties}>
+                  <JsonSchemaForm
+                    onChange={(data) => onChange('payload', data)}
+                    schema={workflow?.options?.payloadSchema || {}}
+                    formData={{}}
+                  />
+                </When>
+                <When truthy={!havePayloadProperties}>
+                  <InputsEmptyPanel
+                    content="Payload ensures correct formatting and data validity."
+                    onDocsClick={() => {
+                      setPath('framework/concepts/payload');
+                      toggle();
+                    }}
+                  />
+                </When>
+              </>
+            ),
+          },
+          {
+            icon: <IconOutlineEditNote />,
+            value: 'step-inputs',
+            label: 'Step inputs',
+            content: (
+              <>
+                <When truthy={haveInputProperties}>
+                  {onSave && (
+                    <div style={{ display: 'flex', justifyContent: 'end' }}>
+                      <Button
+                        loading={isLoadingSave}
+                        variant={'filled'}
+                        size={'xs'}
+                        onClick={() => {
+                          onSave();
+                        }}
+                      >
+                        Save
+                      </Button>
+                    </div>
+                  )}
+
+                  <JsonSchemaForm
+                    onChange={(data) => onChange('step', data)}
+                    schema={step?.inputs?.schema || {}}
+                    formData={defaultInputs || {}}
+                  />
+                </When>
+                <When truthy={!haveInputProperties}>
+                  <InputsEmptyPanel
+                    content="Modifiable controls defined by the code schema."
+                    onDocsClick={() => {
+                      setPath('framework/concepts/inputs');
+                      toggle();
+                    }}
+                  />
+                </When>
+              </>
+            ),
+          },
+        ]}
+      />
+      <Component />
+    </>
   );
 };
