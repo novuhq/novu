1bd3c143f5fe84477744ac51ae10da644536601e
Biswajeet Das <biswajeetdas18@gmail.com>
2024-06-24 23:14:47 +0530

    feat: add novu context

---
+++ packages/js/src/ui/context/novu-context.tsx
@@ -0,0 +1,24 @@
+import { createContext, JSX, useContext } from 'solid-js';
+import { Novu, NovuOptions } from '../../novu';
+
+type NovuProviderProps = {
+  options: NovuOptions;
+  children: JSX.Element;
+};
+
+const NovuContext = createContext<Novu | undefined>(undefined);
+
+export function NovuProvider(props: NovuProviderProps) {
+  const novu = new Novu(props.options);
+
+  return <NovuContext.Provider value={novu}>{props.children}</NovuContext.Provider>;
+}
+
+export function useNovu() {
+  const context = useContext(NovuContext);
+  if (!context) {
+    throw new Error('useNovu must be used within a NovuProvider');
+  }
+
+  return context;
+}
