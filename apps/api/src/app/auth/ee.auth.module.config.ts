61f711f43356deaa250c27db2a6660c541f387bc
Adam Chmara <adam.chmara1@gmail.com>
2024-06-26 15:19:41 +0200

    refactor(auth): simplify ee auth module init

--- apps/api/src/app/auth/ee.auth.module.config.ts
+++ apps/api/src/app/auth/ee.auth.module.config.ts
@@ -1,10 +1,9 @@
 import {
   AuthService,
-  IAuthService,
-  PlatformException,
   SwitchEnvironment,
   SwitchOrganization,
   injectRepositories,
+  PlatformException,
 } from '@novu/application-generic';
 import { RolesGuard } from './framework/roles.guard';
 import { RootEnvironmentGuard } from './framework/root-environment-guard.service';
@@ -17,7 +16,6 @@ import {
   Provider,
   RequestMethod,
 } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
 import {
   EnvironmentRepository,
   MemberRepository,
@@ -27,78 +25,42 @@ import {
 } from '@novu/dal';
 import { ApiKeyStrategy } from './services/passport/apikey.strategy';
 import { JwtSubscriberStrategy } from './services/passport/subscriber-jwt.strategy';
-import { JwtModule, JwtService } from '@nestjs/jwt';
 import { NextFunction } from 'express';
-
-const eeAuthServiceProvider = {
-  provide: 'AUTH_SERVICE',
-  useFactory: (
-    userRepository: UserRepository,
-    environmentRepository: EnvironmentRepository,
-    subscriberRepository: SubscriberRepository,
-    jwtService: JwtService
-  ): IAuthService => {
-    const eeAuthPackage = require('@novu/ee-auth');
-    if (!eeAuthPackage?.EEAuthService) {
-      throw new PlatformException('EEAuthService is not loaded');
-    }
-
-    return new eeAuthPackage.EEAuthService(userRepository, environmentRepository, subscriberRepository, jwtService);
-  },
-  inject: [UserRepository, EnvironmentRepository, SubscriberRepository, JwtService],
-};
-
-const eeUserAuthGuard = {
-  provide: 'USER_AUTH_GUARD',
-  useFactory: (reflector: Reflector) => {
-    const eeAuthPackage = require('@novu/ee-auth');
-    if (!eeAuthPackage?.EEUserAuthGuard) {
-      throw new PlatformException('EEUserAuthGuard is not loaded');
-    }
-
-    return new eeAuthPackage.EEUserAuthGuard(reflector);
-  },
-  inject: [Reflector],
-};
 
 export function getEEModuleConfig(): ModuleMetadata {
   const eeAuthPackage = require('@novu/ee-auth');
-  const clerkStrategy = eeAuthPackage?.ClerkStrategy;
+  const eeAuthModule = eeAuthPackage?.eEAuthModule;
 
-  const AUTH_STRATEGIES: Provider[] = [clerkStrategy, ApiKeyStrategy, JwtSubscriberStrategy];
-  const EE_AUTH_PROVIDERS: Provider[] = [eeAuthServiceProvider, AuthService, eeUserAuthGuard];
+  if (!eeAuthModule) {
+    throw new PlatformException('ee-auth module is not loaded');
+  }
 
   return {
-    imports: [
-      JwtModule.register({
-        secret: `${process.env.JWT_SECRET}`,
-        signOptions: {
-          expiresIn: 360000,
-        },
-      }),
-    ],
+    imports: [...eeAuthModule.imports],
+    controllers: [...eeAuthModule.controllers],
     providers: [
-      ...AUTH_STRATEGIES,
-      ...EE_AUTH_PROVIDERS,
-      ...injectRepositories(),
+      ...eeAuthModule.providers,
       // original repositories need to be here for the DI to work
+      ...injectRepositories(),
       UserRepository,
       MemberRepository,
       OrganizationRepository,
       EnvironmentRepository,
       SubscriberRepository,
       // reused services
+      ApiKeyStrategy,
+      JwtSubscriberStrategy,
+      AuthService,
       SwitchEnvironment,
       SwitchOrganization,
       RolesGuard,
       RootEnvironmentGuard,
     ],
     exports: [
+      ...eeAuthModule.exports,
       RolesGuard,
       RootEnvironmentGuard,
       AuthService,
-      'AUTH_SERVICE',
-      'USER_AUTH_GUARD',
       'USER_REPOSITORY',
       'MEMBER_REPOSITORY',
       'ORGANIZATION_REPOSITORY',
