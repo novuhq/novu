6ace37842288420b889402972cb0d8189dd48a81
Adam Chmara <adam.chmara1@gmail.com>
2024-06-26 16:15:16 +0200

    refactor(clerk): remove ee user controller

--- apps/api/src/app/user/user.module.ts
+++ apps/api/src/app/user/user.module.ts
@@ -1,20 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SharedModule } from '../shared/shared.module';
-import { EEUserController } from './ee.user.controller';
 import { USE_CASES } from './usecases';
 import { UsersController } from './user.controller';
 
-function getControllers() {
-  if (process.env.NOVU_ENTERPRISE === 'true') {
-    return [EEUserController];
-  }
-
-  return [UsersController];
-}
-
 @Module({
   imports: [SharedModule],
-  controllers: [...getControllers()],
+  controllers: [UsersController],
   providers: [...USE_CASES],
   exports: [...USE_CASES],
 })
