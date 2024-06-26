634df282f3e2885af6de912f7bc1842722e68757
David Söderberg <granskog_@hotmail.com>
2024-06-26 06:39:06 +0200

    feat: add segment tracking for bridge update

--- apps/web/src/components/layout/components/v2/BridgeUpdateModal.tsx
+++ apps/web/src/components/layout/components/v2/BridgeUpdateModal.tsx
@@ -11,6 +11,7 @@ import { isStudioRoute } from '../../../../studio/utils/isStudioRoute';
 import { DocsButton } from '../../../docs/DocsButton';
 import { useBridgeUrl } from '../../../../studio/hooks/useBridgeUrl';
 import { hstack } from '@novu/novui/patterns';
+import { useSegment } from '../../../providers/SegmentProvider';
 
 export type BridgeUpdateModalProps = {
   isOpen: boolean;
@@ -21,6 +22,7 @@ export const BridgeUpdateModal: FC<BridgeUpdateModalProps> = ({ isOpen, toggleOp
   const [inputUrl, setInputUrl] = useState<string>('');
   const [isUpdating, setIsUpdating] = useState(false);
   const [urlError, setUrlError] = useState<string>('');
+  const segment = useSegment();
 
   const { bridgeUrl, setBridgeUrl } = useBridgeUrl();
 
@@ -46,6 +48,7 @@ export const BridgeUpdateModal: FC<BridgeUpdateModalProps> = ({ isOpen, toggleOp
         throw new Error(`Tested URL isn't valid`);
       }
       await storeInProperLocation(inputUrl);
+      segment.track('Update endpoint clicked');
       successMessage('You have successfuly updated your Novu endpoint configuration');
       toggleOpen();
     } catch {
