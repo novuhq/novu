79605c8f59c0aa56cd030cd17fc7890802c8ec34
David Söderberg <2233092+davidsoderberg@users.noreply.github.com>
2024-06-26 14:39:58 +0200

    feat(web): fix so scroll in onboarding is correct (#5834)

--- apps/web/src/pages/studio-onboarding/components/Wrapper.tsx
+++ apps/web/src/pages/studio-onboarding/components/Wrapper.tsx
@@ -1,14 +1,19 @@
 import { css, cx } from '@novu/novui/css';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 
 export const Wrapper = ({ children, className }: { children: ReactNode; className?: string }) => {
+  useEffect(() => {
+    document.body.setAttribute('style', 'overflow: auto');
+  }, []);
+
   return (
     <div
       className={cx(
         css({
           width: '100dvw',
-          height: '100dvh',
+          minHeight: '100dvh',
           colorPalette: 'mode.cloud',
+          paddingBottom: '4rem',
         }),
         className
       )}
