452c5b8411dbc23231266cc62152f06e74250f8c
Denis Kralj <168424106+denis-kralj-novu@users.noreply.github.com>
2024-06-26 20:41:25 +0200

    feat(framework): update novu framework headers (#5837)
    * feat(framework): update novu framework headers
    
    * feat(framework): update novu framework headers
    
    PR comments
    
    * feat(framework): update novu framework headers
    
    Fix tests, add new test

--- packages/framework/src/version.ts
+++ packages/framework/src/version.ts
@@ -1,3 +1,4 @@
 import packageJson from '../package.json';
 
-export const VERSION = packageJson.version;
+export const SDK_VERSION = packageJson.version;
+export const FRAMEWORK_VERSION = '2024-06-26';
