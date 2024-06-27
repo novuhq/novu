1c21b620e7a300666fc22ce6eede9f4e5305b8d1
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-06-27 10:46:17 +0100

    feat(framework): export constants from index file

--- packages/framework/src/index.ts
+++ packages/framework/src/index.ts
@@ -1,4 +1,5 @@
 export { Client } from './client';
 export * from './types';
+export * from './constants';
 export { NovuRequestHandler, ServeHandlerOptions } from './handler';
 export { workflow } from './workflow';
