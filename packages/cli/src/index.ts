3dde98b732c49bb575b76bfdc90537612ee910b8
Denis Kralj <168424106+denis-kralj-novu@users.noreply.github.com>
2024-06-28 10:56:06 +0200

    feat(novu): move sync command (#5852)
    * feat(cli): move sync command
    
    Add sync command to novu cli
    Add deprecation warning to legacy command
    Remove unused command
    Add tests and related packages
    
    * feat(novu): move sync command
    
    PR recommendations
    
    * feat(novu): move sync command
    
    PR recommendations
    
    * feat(novu): move sync command
    
    Remove debug console statement
    
    * feat(novu): move sync command
    
    Make description more generic
    
    * Update packages/cli/src/index.ts
    
    * Update packages/cli/src/index.ts
    
    * feat(cli): move sync command
    
    Exchange warn for log
    
    * feat(cli): move sync command
    
    Fix typo
    Add linting script
    
    ---------
    
    Co-authored-by: Richard Fontein <32132657+rifont@users.noreply.github.com>

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,7 +2,18 @@
 
 import { Command } from 'commander';
 import { initCommand, devCommand, DevCommandOptions } from './commands';
+import { sync } from './commands/sync';
 
+import { v4 as uuidv4 } from 'uuid';
+import { AnalyticService, ConfigService } from './services';
+
+const analytics = new AnalyticService();
+const config = new ConfigService();
+if (process.env.NODE_ENV === 'development') {
+  config.clearStore();
+}
+const anonymousIdLocalState = config.getValue('anonymousId');
+const anonymousId = anonymousIdLocalState || uuidv4();
 const program = new Command();
 
 program.name('novu').description('A CLI tool to interact with the novu API');
@@ -12,6 +23,26 @@ program
   .description('Initialize a new project and create an account')
   .action(() => {
     initCommand();
+  });
+
+program
+  .command('sync')
+  .option('-a, --api-url <apiUrl>', 'The Novu Cloud API URL', 'https://api.novu.co')
+  .requiredOption(
+    '-b, --bridge-url <bridgeUrl>',
+    'The Novu endpoint URL hosted in the Bridge application, by convention ends in /api/novu'
+  )
+  .requiredOption('-s, --secret-key <secretKey>', 'The Novu secret key')
+  .description('Sync your state with Novu Cloud')
+  .action(async (options) => {
+    analytics.track({
+      identity: {
+        anonymousId: anonymousId,
+      },
+      data: {},
+      event: 'Sync Novu Endpoint State',
+    });
+    await sync(options.bridgeUrl, options.secretKey, options.apiUrl);
   });
 
 program
@@ -31,5 +62,6 @@ program
     'Studio Origin SPA, used for staging environment and local development, defaults to us',
     'us'
   )
-  .action((options: DevCommandOptions) => devCommand(options))
-  .parse(process.argv);
+  .action((options: DevCommandOptions) => devCommand(options));
+
+program.parse(process.argv);
