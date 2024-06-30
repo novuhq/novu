f116663c2f71af4eae70c62bf41f935e417f8af9
Gosha <djabarov.george@gmail.com>
2024-06-30 14:51:11 +0300

    fix(framework): fetch bad request response

--- packages/framework/src/utils/http.utils.ts
+++ packages/framework/src/utils/http.utils.ts
@@ -1,18 +1,26 @@
+import { UnknownError } from '../errors';
+
 export const initApiClient = (apiKey: string, baseURL = 'https://api.novu.co') => {
   const apiUrl = process.env.NOVU_API_URL || baseURL;
 
   return {
     post: async (route: string, data: Record<string, unknown>) => {
-      return (
-        await fetch(apiUrl + '/v1' + route, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `ApiKey ${apiKey}`,
-          },
-          body: JSON.stringify(data),
-        })
-      ).json();
+      const response = await fetch(apiUrl + '/v1' + route, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `ApiKey ${apiKey}`,
+        },
+        body: JSON.stringify(data),
+      });
+
+      const resJson = await response.json();
+
+      if (response.ok) {
+        return resJson;
+      } else {
+        throw new UnknownError(resJson.statusCode, resJson.error, resJson.message);
+      }
     },
   };
 };
