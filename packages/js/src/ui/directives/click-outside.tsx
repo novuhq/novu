91c6c882afe5d6c59316519d774fdd784898c5c3
Biswajeet Das <biswajeetdas18@gmail.com>
2024-07-01 10:46:16 +0530

    feat: add event listeners

--- packages/js/src/ui/directives/click-outside.tsx
+++
@@ -1,8 +0,0 @@
-import { onCleanup } from 'solid-js';
-
-export function clickOutside(el: HTMLElement, accessor: any) {
-  const onClick = (e: any) => !el.contains(e.target) && accessor()?.();
-  document.body.addEventListener('click', onClick);
-
-  onCleanup(() => document.body.removeEventListener('click', onClick));
-}
