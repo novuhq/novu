0e86145288f838ef3d067b7cf0d8a1d80fe0a78e
Sokratis Vidros <sokratis.vidros@gmail.com>
2024-07-03 17:27:24 +0300

    chore(root): Remove cli-next package
    The package is deprecated. Use cli instead.

--- packages/cli-next/src/constants/constants.ts
+++
@@ -1,7 +0,0 @@
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
-// segment analytics
-export const ANALYTICS_ENABLED = process.env.ANALYTICS_ENABLED === 'false' ? false : true;
-export const SEGMENTS_WRITE_KEY = process.env.CLI_SEGMENT_WRITE_KEY || 'tz68K6ytWx6AUqDl30XAwiIoUfr7iWVW';
