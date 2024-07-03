09e175ae20973b56461a569a51afa84a2f25bb97
David Söderberg <granskog_@hotmail.com>
2024-07-03 10:33:43 +0200

    feat: add event for workflow open

--- apps/web/src/studio/components/workflows/node-view/WorkflowsDetailPage.tsx
+++ apps/web/src/studio/components/workflows/node-view/WorkflowsDetailPage.tsx
@@ -3,9 +3,12 @@ import { css } from '@novu/novui/css';
 import { IconCable, IconPlayArrow } from '@novu/novui/icons';
 import { Stack } from '@novu/novui/jsx';
 import { token } from '@novu/novui/tokens';
+import { useEffect } from 'react';
+import { useTelemetry } from '../../../../hooks/useNovuAPI';
 import { useWorkflow } from '../../../hooks/useBridgeAPI';
 import { useStudioWorkflowsNavigation } from '../../../hooks/useStudioWorkflowsNavigation';
 import { PageContainer } from '../../../layout/PageContainer';
+import { useStudioState } from '../../../StudioStateProvider';
 import { OutlineButton } from '../../OutlineButton';
 import { WorkflowsPageTemplate } from '../layout/WorkflowsPageTemplate';
 import { WorkflowBackgroundWrapper } from './WorkflowBackgroundWrapper';
@@ -15,6 +18,16 @@ import { WorkflowNodes } from './WorkflowNodes';
 export const WorkflowsDetailPage = () => {
   const { currentWorkflowId, goToStep, goToTest } = useStudioWorkflowsNavigation();
   const { data: workflow, isLoading } = useWorkflow(currentWorkflowId);
+  const track = useTelemetry();
+  const { isLocalStudio } = useStudioState() || {};
+
+  useEffect(() => {
+    track('Workflow open - [Studio]', {
+      workflowId: currentWorkflowId,
+      env: isLocalStudio ? 'local' : 'cloud',
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   if (isLoading) {
     return <WorkflowsContentLoading />;
