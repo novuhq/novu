6cda52ca7bdaaa78f2d47526236f2cb0253a73c5
Biswajeet Das <biswajeetdas18@gmail.com>
2024-07-10 19:32:46 +0530

    feat: add button key

--- packages/js/src/ui/components/Header/ActionsContainer.tsx
+++ packages/js/src/ui/components/Header/ActionsContainer.tsx
@@ -9,7 +9,7 @@ export const ActionsContainer = () => {
       <MoreActionsDropdown />
       <button
         class={style(
-          'settingsIconContainer',
+          ['button', 'settingsIconContainer'],
           'nt-h-6 nt-w-6 nt-flex nt-justify-center nt-items-center nt-rounded-md nt-relative hover:nt-bg-foreground-alpha-50 nt-text-foreground-alpha-600'
         )}
       >
