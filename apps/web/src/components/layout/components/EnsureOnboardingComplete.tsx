6cbb825d49dde460b0f19ea46c75cb0011c8d0da
Adam Chmara <adam.chmara1@gmail.com>
2024-07-11 16:17:16 +0200

    fix: onboarding check

--- apps/web/src/components/layout/components/EnsureOnboardingComplete.tsx
+++ apps/web/src/components/layout/components/EnsureOnboardingComplete.tsx
@@ -3,19 +3,32 @@ import { useAuth } from '../../../hooks/useAuth';
 import { ROUTES } from '../../../constants/routes';
 import { IS_EE_AUTH_ENABLED } from '../../../config/index';
 import { useBlueprint, useRedirectURL } from '../../../hooks';
+import { useEffect, useState } from 'react';
 
 export function EnsureOnboardingComplete({ children }: any) {
   useBlueprint();
   const location = useLocation();
   const { getRedirectURL } = useRedirectURL();
   const { currentOrganization, environmentId } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (currentOrganization) {
+      setIsLoading(false);
+    }
+  }, [currentOrganization]);
 
   function isOnboardingComplete() {
     if (IS_EE_AUTH_ENABLED) {
+      // TODO: replace with actual check property (e.g. isOnboardingCompleted)
       return currentOrganization?.productUseCases !== undefined;
     }
 
     return currentOrganization && environmentId;
+  }
+
+  if (isLoading) {
+    return null;
   }
 
   if (!isOnboardingComplete() && location.pathname !== ROUTES.AUTH_APPLICATION) {
