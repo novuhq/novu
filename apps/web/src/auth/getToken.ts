73331bfacfd903e345d83d90d8e39c557e8063f1
Adam Chmara <adam.chmara1@gmail.com>
2024-07-11 12:07:40 +0200

    fix: get token return empty string

--- apps/web/src/auth/getToken.ts
+++ apps/web/src/auth/getToken.ts
@@ -3,11 +3,9 @@ import { eeAuthTokenCookie } from '../utils/cookies';
 import { LOCAL_STORAGE_AUTH_TOKEN_KEY } from './auth.const';
 
 export function getToken(): string {
+  console.log(IS_EE_AUTH_ENABLED);
+
   const token = IS_EE_AUTH_ENABLED ? eeAuthTokenCookie.get() : localStorage.getItem(LOCAL_STORAGE_AUTH_TOKEN_KEY);
 
-  if (!token) {
-    throw new Error('No auth token found');
-  }
-
-  return token;
+  return token || '';
 }
