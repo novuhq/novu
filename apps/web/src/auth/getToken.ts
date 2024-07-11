9ce41532299b9853d22d4965a3a18f22fa8febb7
Adam Chmara <adam.chmara1@gmail.com>
2024-07-11 12:32:29 +0200

    fix: remove console.log

--- apps/web/src/auth/getToken.ts
+++ apps/web/src/auth/getToken.ts
@@ -3,8 +3,6 @@ import { eeAuthTokenCookie } from '../utils/cookies';
 import { LOCAL_STORAGE_AUTH_TOKEN_KEY } from './auth.const';
 
 export function getToken(): string {
-  console.log(IS_EE_AUTH_ENABLED);
-
   const token = IS_EE_AUTH_ENABLED ? eeAuthTokenCookie.get() : localStorage.getItem(LOCAL_STORAGE_AUTH_TOKEN_KEY);
 
   return token || '';
