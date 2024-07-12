7a65b6ff7aa0f1462409ea8be4146305cae206e4
David Söderberg <2233092+davidsoderberg@users.noreply.github.com>
2024-07-12 14:24:06 +0200

    feat(framework): add first five schemas for providers (#6039)
    Co-authored-by: Richard Fontein <32132657+rifont@users.noreply.github.com>

---
+++ packages/framework/src/schemas/providers/email/mailgun.schema.ts
@@ -0,0 +1,88 @@
+import { Schema } from '../../../types/schema.types';
+
+/**
+ * Mailgun `POST /messages` schema
+ *
+ * @see https://documentation.mailgun.com/en/latest/api-sending.html#sending
+ */
+const mailgunOutputSchema = {
+  type: 'object',
+  properties: {
+    to: {
+      anyOf: [{ type: 'string' }, { type: 'array', items: { type: 'string' } }],
+      description: `Email address of the recipient(s). Example: "Bob bob@host.com". You can use commas to separate multiple recipients (e.g.: "test@example.com,test@example.com" or ["test@example.com", "test@example.com"]).`,
+    },
+    from: { type: 'string' },
+    subject: { type: 'string', description: `Subject of the message.` },
+    text: { type: 'string', description: `Text version of the message.` },
+    html: { type: 'string', description: `HTML version of the message.` },
+    message: {
+      type: 'string',
+      description: `MIME string of the message. Make sure to use multipart/form-data to send this as a file upload.`,
+    },
+    cc: {
+      anyOf: [{ type: 'string' }, { type: 'array', items: { type: 'string' } }],
+      description: `Same as To but for carbon copy`,
+    },
+    bcc: {
+      anyOf: [{ type: 'string' }, { type: 'array', items: { type: 'string' } }],
+      description: `Same as To but for blind carbon copy`,
+    },
+    'amp-html': { type: 'string' },
+    't:version': { type: 'string' },
+    't:text': {
+      anyOf: [{ type: 'string', enum: ['yes', 'no'] }, { type: 'boolean' }],
+    },
+    'o:tag': {
+      anyOf: [{ type: 'string' }, { type: 'array', items: { type: 'string' } }],
+      description: `Tag string. See Tagging for more information.`,
+    },
+    'o:dkim': {
+      anyOf: [{ type: 'string', enum: ['yes', 'no'] }, { type: 'boolean' }],
+      description: `Enables/disabled DKIM signatures on per-message basis. Pass yes or no`,
+    },
+    'o:deliverytime': {
+      type: 'string',
+      description: `Desired time of delivery. See Date Format. Note: Messages can be scheduled for a maximum of 3 days in the future.`,
+    },
+    'o:deliverytime-optimize-period': { type: 'string' },
+    'o:time-zone-localize': { type: 'string' },
+    'o:testmode': {
+      anyOf: [{ type: 'string', enum: ['yes', 'no'] }, { type: 'boolean' }],
+      description: `Enables sending in test mode. Pass yes if needed. See Sending in Test Mode`,
+    },
+    'o:tracking': {
+      anyOf: [{ type: 'string', enum: ['yes', 'no'] }, { type: 'boolean' }],
+      description: `Toggles tracking on a per-message basis, see Tracking Messages for details. Pass yes or no.`,
+    },
+    'o:tracking-clicks': {
+      anyOf: [{ type: 'string', enum: ['yes', 'no', 'htmlonly'] }, { type: 'boolean' }],
+      description: `Toggles clicks tracking on a per-message basis. Has higher priority than domain-level setting. Pass yes, no or htmlonly.`,
+    },
+    'o:tracking-opens': {
+      anyOf: [{ type: 'string', enum: ['yes', 'no'] }, { type: 'boolean' }],
+      description: `Toggles opens tracking on a per-message basis. Has higher priority than domain-level setting. Pass yes or no.`,
+    },
+    'o:require-tls': {
+      anyOf: [{ type: 'string', enum: ['yes', 'no'] }, { type: 'boolean' }],
+    },
+    'o:skip-verification': {
+      anyOf: [{ type: 'string', enum: ['yes', 'no'] }, { type: 'boolean' }],
+    },
+    'recipient-variables': { type: 'string' },
+    'h:X-My-Header': {
+      type: 'string',
+      description: `h: prefix followed by an arbitrary value allows to append a custom MIME header to the message (X-My-Header in this case). For example, h:Reply-To to specify Reply-To address.`,
+    },
+    'v:my-var': {
+      type: 'string',
+      description: `v: prefix followed by an arbitrary name allows to attach a custom JSON data to the message. See Attaching Data to Messages for more information.`,
+    },
+  },
+  required: [],
+  additionalProperties: true,
+} as const satisfies Schema;
+
+export const mailgunProviderSchemas = {
+  output: mailgunOutputSchema,
+};
--- packages/framework/src/schemas/providers/push/index.ts
+++ packages/framework/src/schemas/providers/push/index.ts
@@ -1,12 +1,16 @@
 import { PushProviderIdEnum } from '@novu/shared';
 import { Schema } from '../../../types';
 import { genericProviderSchemas } from '../generic.schema';
+import { apnsProviderSchemas } from './apns.schema';
+import { expoProviderSchemas } from './expo.schema';
+import { fcmProviderSchemas } from './fcm.schema';
+import { oneSignalProviderSchema } from './one-signal.schema';
 
 export const pushProviderSchemas = {
-  [PushProviderIdEnum.FCM]: genericProviderSchemas,
-  [PushProviderIdEnum.APNS]: genericProviderSchemas,
-  [PushProviderIdEnum.EXPO]: genericProviderSchemas,
-  [PushProviderIdEnum.OneSignal]: genericProviderSchemas,
+  [PushProviderIdEnum.FCM]: fcmProviderSchemas,
+  [PushProviderIdEnum.APNS]: apnsProviderSchemas,
+  [PushProviderIdEnum.EXPO]: expoProviderSchemas,
+  [PushProviderIdEnum.OneSignal]: oneSignalProviderSchema,
   [PushProviderIdEnum.PushWebhook]: genericProviderSchemas,
   [PushProviderIdEnum.PusherBeams]: genericProviderSchemas,
   [PushProviderIdEnum.Pushpad]: genericProviderSchemas,
