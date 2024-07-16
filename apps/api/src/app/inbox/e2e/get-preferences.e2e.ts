27838295e28eb50df9ecf5bd5d71cab134f2dcb4
Biswajeet Das <biswajeetdas18@gmail.com>
2024-07-16 17:34:08 +0530

    fix: add back sub auth decorator removed during rebase

--- apps/api/src/app/inbox/e2e/get-preferences.e2e.ts
+++ apps/api/src/app/inbox/e2e/get-preferences.e2e.ts
@@ -10,45 +10,46 @@ describe('Get all preferences - /inbox/preferences (GET)', function () {
     session = new UserSession();
     subscriberRepository = new SubscriberRepository();
     await session.initialize();
-    await session.createTemplate({
-      noFeedId: true,
-    });
   });
 
-  it('should get subscriber preferences', async function () {
-    const response = await session.testAgent.get(`/inbox/preferences`);
+  it('should always get the global preferences even if workflow preferences are not present', async function () {
+    const response = await session.testAgent
+      .get('/v1/inbox/preferences')
+      .set('Authorization', `Bearer ${session.subscriberToken}`);
 
     const globalPreference = response.body.data[0];
 
-    expect(globalPreference.preference.channels.email).to.equal(true);
-    expect(globalPreference.preference.channels.in_app).to.equal(true);
+    expect(globalPreference.preferences.channels.email).to.equal(true);
+    expect(globalPreference.preferences.channels.in_app).to.equal(true);
+    expect(globalPreference.level).to.equal('global');
+    expect(response.body.data.length).to.equal(1);
+  });
+
+  it('should get both global and workflow preferences if workflow is present', async function () {
+    await session.createTemplate({
+      noFeedId: true,
+    });
+
+    const response = await session.testAgent
+      .get('/v1/inbox/preferences')
+      .set('Authorization', `Bearer ${session.subscriberToken}`);
+
+    const globalPreference = response.body.data[0];
+
+    expect(globalPreference.preferences.channels.email).to.equal(true);
+    expect(globalPreference.preferences.channels.in_app).to.equal(true);
     expect(globalPreference.level).to.equal('global');
 
     const workflowPreference = response.body.data[1];
 
-    expect(workflowPreference.preference.channels.email).to.equal(true);
-    expect(workflowPreference.preference.channels.in_app).to.equal(true);
+    expect(workflowPreference.preferences.channels.email).to.equal(true);
+    expect(workflowPreference.preferences.channels.in_app).to.equal(true);
     expect(workflowPreference.level).to.equal('template');
   });
 
-  it('should throw error when subscriber is not found', async function () {
-    const response = await session.testAgent.get(`/inbox/preferences`);
+  it('should throw error when made unauthorized call', async function () {
+    const response = await session.testAgent.get(`/v1/inbox/preferences`).set('Authorization', `Bearer InvalidToken`);
 
-    const globalPreference = response.body.data[0];
-
-    expect(globalPreference.preference.channels.email).to.equal(true);
-    expect(globalPreference.preference.channels.in_app).to.equal(true);
-    expect(globalPreference.level).to.equal('global');
-
-    await subscriberRepository.delete({
-      subscriberId: session.subscriberId,
-      _environmentId: session.environment._id,
-      _organizationId: session.organization._id,
-    });
-
-    const responseAfterDelete = await session.testAgent.get(`/inbox/preferences`);
-
-    expect(responseAfterDelete.status).to.equal(404);
-    expect(responseAfterDelete.body.message).to.equal(`Subscriber: ${session.subscriberId} not found`);
+    expect(response.status).to.equal(401);
   });
 });
--- apps/api/src/app/inbox/inbox.controller.ts
+++ apps/api/src/app/inbox/inbox.controller.ts
@@ -98,6 +98,7 @@ export class InboxController {
     return res;
   }
 
+  @UseGuards(AuthGuard('subscriberJwt'))
   @Get('/preferences')
   async getAllPreferences(
     @SubscriberSession() subscriberSession: SubscriberEntity
