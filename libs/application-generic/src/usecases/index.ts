0a4c31d14943a93bfb525d5b31fefc9ba51db729
Biswajeet Das <biswajeetdas18@gmail.com>
2024-07-17 19:16:12 +0530

    fix: remove unused export

--- libs/application-generic/src/usecases/index.ts
+++ libs/application-generic/src/usecases/index.ts
@@ -42,4 +42,3 @@ export * from './workflow';
 export * from './message-template';
 export * from './subscribers';
 export * from './execute-bridge-request';
-export * from './get-subscriber-workflow-preference';
