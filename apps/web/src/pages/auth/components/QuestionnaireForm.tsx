615e4dff6a8ae895545dcad92b785adb8da74ca3
Dima Grossman <dima@grossman.io>
2024-07-18 16:36:19 +0300

    fix: new line

--- apps/web/src/pages/auth/components/QuestionnaireForm.tsx
+++ apps/web/src/pages/auth/components/QuestionnaireForm.tsx
@@ -71,7 +71,7 @@ export function QuestionnaireForm() {
         source: 'sample-workspace',
       });
     } catch (e) {
-      // @eslint-disable-next-line no-console
+      // eslint-disable-next-line no-console
       console.error(e);
 
       captureException(e);
