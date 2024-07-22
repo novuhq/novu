ec774c09d3a3cfb2d7858584a75592804209c168
ainouzgali <ainouzgali@gmail.com>
2024-07-22 19:18:19 +0300

    test(framework): adjust tests to use payload namespace

--- packages/framework/src/client.test.ts
+++ packages/framework/src/client.test.ts
@@ -324,7 +324,7 @@ describe('Novu Client', () => {
               controlSchema: {
                 type: 'object',
                 properties: {
-                  name: { type: 'string', default: '{{name}}' },
+                  name: { type: 'string', default: '{{payload.name}}' },
                 },
                 required: [],
                 additionalProperties: false,
@@ -519,7 +519,7 @@ describe('Novu Client', () => {
 
     it('should compile default control variable', async () => {
       const bodyTemplate = `
-{% for element in elements %}
+{% for element in payload.elements %}
   {{ element }}
 {% endfor %}`;
 
@@ -538,9 +538,9 @@ describe('Novu Client', () => {
               controlSchema: {
                 type: 'object',
                 properties: {
-                  name: { type: 'string', default: '{{name}}' },
+                  name: { type: 'string', default: '{{payload.name}}' },
                   lastName: { type: 'string', default: '{{subscriber.lastName}}' },
-                  role: { type: 'string', default: '{{role}}' },
+                  role: { type: 'string', default: '{{payload.role}}' },
                   body: { type: 'string', default: bodyTemplate },
                 },
                 required: [],
