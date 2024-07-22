789bdc6b3cbb4aad07b5384ea93365766411bc3f
Dima Grossman <dima@grossman.io>
2024-07-22 11:34:36 +0300

    fix(web): banner is not show after signup (#6115)

--- apps/web/src/components/layout/components/PrivatePageLayout.tsx
+++ apps/web/src/components/layout/components/PrivatePageLayout.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { ErrorBoundary } from '@sentry/react';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
