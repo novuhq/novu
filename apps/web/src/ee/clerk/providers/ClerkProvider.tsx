dc8bcd22c0bf8addf0b1d368c8f466e67770c6df
Adam Chmara <adam.chmara1@gmail.com>
2024-07-23 17:10:05 +0200

    feat(web): normalize user emails in ee

--- apps/web/src/ee/clerk/providers/ClerkProvider.tsx
+++ apps/web/src/ee/clerk/providers/ClerkProvider.tsx
@@ -1,9 +1,12 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { CLERK_PUBLISHABLE_KEY, IS_EE_AUTH_ENABLED } from '../../../config/index';
-import { ClerkProvider as _ClerkProvider } from '@clerk/clerk-react';
+import { BrowserClerk, ClerkProvider as _ClerkProvider } from '@clerk/clerk-react';
+import { Clerk } from '@clerk/clerk-js';
 import { useColorScheme } from '@novu/design-system';
 import { dark } from '@clerk/themes';
+import { normalizeEmail } from '@novu/shared';
+import { api } from '../../../api/api.client';
 
 const ClerkModalElement = {
   modalContent: {
@@ -29,9 +32,51 @@ const localization = {
 
 const ALLOWED_REDIRECT_ORIGINS = ['http://localhost:*'];
 
+const clerk = new Clerk(CLERK_PUBLISHABLE_KEY);
+
+function getEmailFromQuery(query: string): string | null {
+  const params = new URLSearchParams(query);
+  const identifier = params.get('identifier');
+
+  return identifier ? decodeURIComponent(identifier) : null;
+}
+
+function normalizeEmailData(email: string) {
+  return api.post('/clerk/user/normalize', { email });
+}
+
 export const ClerkProvider: React.FC<PropsWithChildren<{}>> = ({ children }) => {
+  const [clerkInstance, setClerkInstance] = useState<Clerk>();
+
   const navigate = useNavigate();
   const { colorScheme } = useColorScheme();
+
+  useEffect(() => {
+    (async () => {
+      // setting Clerk onBeforeRequest to be used in the AuthProvider
+      console.log('setting Clerk onBeforeRequest to be used in the AuthProvider');
+
+      clerk.__unstable__onBeforeRequest(async (requestInit) => {
+        console.log('requestInit', requestInit);
+        const { path, method, body } = requestInit;
+        const isSignIn = path === '/client/sign_ins' && method === 'POST';
+
+        if (isSignIn) {
+          const email = getEmailFromQuery(body as string);
+          if (email && email !== normalizeEmail(email)) {
+            await normalizeEmailData(email);
+          }
+        }
+
+        return requestInit;
+      });
+
+      await clerk.load();
+      setClerkInstance(clerk);
+    })();
+  }, []);
+
+  if (!clerkInstance) return <div>Loading...</div>;
 
   if (!IS_EE_AUTH_ENABLED) {
     return <>{children}</>;
@@ -39,6 +84,7 @@ export const ClerkProvider: React.FC<PropsWithChildren<{}>> = ({ children }) => 
 
   return (
     <_ClerkProvider
+      Clerk={clerkInstance as unknown as BrowserClerk}
       routerPush={(to) => navigate(to)}
       routerReplace={(to) => navigate(to, { replace: true })}
       publishableKey={CLERK_PUBLISHABLE_KEY}
