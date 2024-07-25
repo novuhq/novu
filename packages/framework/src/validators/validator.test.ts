b853e6d9f8833818ac559de4d5a0337f51451bda
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-07-25 17:30:07 +0100

    test(client): Add test for polymorphic properties

--- packages/framework/src/validators/validator.test.ts
+++ packages/framework/src/validators/validator.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import { validateData, transformSchema } from './base.validator';
 import { ZodSchema, z } from 'zod';
-import { JsonSchema } from '../types/schema.types';
+import { FromSchema, JsonSchema } from '../types/schema.types';
 
 const schemas = ['zod', 'json'] as const;
 
@@ -40,7 +40,7 @@ describe('validators', () => {
         title: 'should remove additional properties and successfully validate',
         schemas: {
           zod: z.object({ name: z.string() }),
-          json: { type: 'object', properties: { name: { type: 'string' } } } as const,
+          json: { type: 'object', properties: { name: { type: 'string' } }, additionalProperties: false } as const,
         },
         payload: { name: 'John', age: 30 },
         result: {
@@ -103,6 +103,157 @@ describe('validators', () => {
           },
         },
       },
+      {
+        title: 'should successfully validate polymorphic properties',
+        schemas: {
+          zod: z.object({
+            elements: z.array(
+              z.discriminatedUnion('type', [
+                z.object({ type: z.literal('stringType'), stringVal: z.string() }),
+                z.object({ type: z.literal('numberType'), numVal: z.number() }),
+                z.object({ type: z.literal('booleanType'), boolVal: z.boolean() }),
+              ])
+            ),
+          }),
+          json: {
+            type: 'object',
+            properties: {
+              elements: {
+                type: 'array',
+                items: {
+                  anyOf: [
+                    {
+                      type: 'object',
+                      properties: { type: { type: 'string', const: 'stringType' }, stringVal: { type: 'string' } },
+                      additionalProperties: false,
+                      required: ['type', 'stringVal'],
+                    },
+                    {
+                      type: 'object',
+                      properties: { type: { type: 'string', const: 'numberType' }, numVal: { type: 'number' } },
+                      additionalProperties: false,
+                      required: ['type', 'numVal'],
+                    },
+                    {
+                      type: 'object',
+                      properties: { type: { type: 'string', const: 'booleanType' }, boolVal: { type: 'boolean' } },
+                      additionalProperties: false,
+                      required: ['type', 'boolVal'],
+                    },
+                  ],
+                },
+              },
+            },
+            additionalProperties: false,
+            required: ['elements'],
+          } as const,
+        },
+        payload: {
+          elements: [
+            { type: 'stringType', stringVal: '123' },
+            { type: 'numberType', numVal: 123, extra: 'shouldBeRemoved' },
+            { type: 'booleanType', boolVal: true },
+          ],
+        },
+        result: {
+          success: true,
+          data: {
+            elements: [
+              { type: 'stringType', stringVal: '123' },
+              { type: 'numberType', numVal: 123 },
+              { type: 'booleanType', boolVal: true },
+            ],
+          },
+        },
+      },
+      {
+        title: 'should return errors for invalid polymorphic properties',
+        schemas: {
+          zod: z.object({
+            elements: z.array(
+              z.discriminatedUnion('type', [
+                z.object({ type: z.literal('stringType'), stringVal: z.string() }),
+                z.object({ type: z.literal('numberType'), numVal: z.number() }),
+                z.object({ type: z.literal('booleanType'), boolVal: z.boolean() }),
+              ])
+            ),
+          }),
+          json: {
+            type: 'object',
+            properties: {
+              elements: {
+                type: 'array',
+                items: {
+                  anyOf: [
+                    {
+                      type: 'object',
+                      properties: { type: { type: 'string', const: 'stringType' }, stringVal: { type: 'string' } },
+                      additionalProperties: false,
+                      required: ['type', 'stringVal'],
+                    },
+                    {
+                      type: 'object',
+                      properties: { type: { type: 'string', const: 'numberType' }, numVal: { type: 'number' } },
+                      additionalProperties: false,
+                      required: ['type', 'numVal'],
+                    },
+                    {
+                      type: 'object',
+                      properties: { type: { type: 'string', const: 'booleanType' }, boolVal: { type: 'boolean' } },
+                      additionalProperties: false,
+                      required: ['type', 'boolVal'],
+                    },
+                  ],
+                },
+              },
+            },
+            additionalProperties: false,
+            required: ['elements'],
+          } as const,
+        },
+        payload: {
+          elements: [
+            { type: 'stringType', stringVal: '123' },
+            { type: 'numberType', numVal: '123' },
+            { type: 'booleanType', boolVal: true },
+          ],
+        },
+        result: {
+          success: false,
+          errors: {
+            zod: [{ message: 'Expected number, received string', path: '/elements/1/numVal' }],
+            /*
+             * TODO: use discriminator to get the correct error message.
+             *
+             * The `discriminator` property is only supported in OpenAPI 3.1.
+             * https://swagger.io/docs/specification/data-models/inheritance-and-polymorphism/
+             *
+             * Ajv has added limited support for the `discriminator` keyword, however because it isn't
+             * yet part of the JSON Schema standard, we can't rely on it.
+             *
+             * When using `discriminator`, the error message can be reduced to:
+             * { message: 'must be number', path: '/elements/1/numVal' },
+             *
+             * @see https://ajv.js.org/json-schema.html#discriminator
+             */
+            json: [
+              {
+                message: "must have required property 'stringVal'",
+                path: '/elements/1',
+              },
+              { message: 'must be number', path: '/elements/1/numVal' },
+              {
+                message: "must have required property 'boolVal'",
+                path: '/elements/1',
+              },
+              {
+                message: 'must match a schema in anyOf',
+                path: '/elements/1',
+              },
+            ],
+          },
+        },
+      },
     ];
 
     schemas.forEach((schema) => {
@@ -131,7 +282,7 @@ describe('validators', () => {
   describe('transformSchema', () => {
     type TransformSchemaTestCase = {
       title: string;
-      schema: {
+      schemas: {
         zod: ZodSchema;
         json: JsonSchema;
       };
@@ -140,7 +291,7 @@ describe('validators', () => {
     const testCases: TransformSchemaTestCase[] = [
       {
         title: 'should transform a simple object schema',
-        schema: {
+        schemas: {
           zod: z.object({ name: z.string(), age: z.number() }),
           json: {
             type: 'object',
@@ -158,7 +309,7 @@ describe('validators', () => {
       },
       {
         title: 'should transform a nested object schema',
-        schema: {
+        schemas: {
           zod: z.object({ name: z.string(), nested: z.object({ age: z.number() }) }),
           json: {
             type: 'object',
@@ -190,14 +341,92 @@ describe('validators', () => {
           additionalProperties: false,
         },
       },
+      {
+        title: 'should transform a polymorphic object schema',
+        schemas: {
+          zod: z.object({
+            elements: z.array(
+              z.discriminatedUnion('type', [
+                z.object({ type: z.literal('stringType'), stringVal: z.string() }),
+                z.object({ type: z.literal('numberType'), numVal: z.number() }),
+                z.object({ type: z.literal('booleanType'), boolVal: z.boolean() }),
+              ])
+            ),
+          }),
+          json: {
+            type: 'object',
+            properties: {
+              elements: {
+                type: 'array',
+                items: {
+                  anyOf: [
+                    {
+                      type: 'object',
+                      properties: { type: { type: 'string', const: 'stringType' }, stringVal: { type: 'string' } },
+                      additionalProperties: false,
+                      required: ['type', 'stringVal'],
+                    },
+                    {
+                      type: 'object',
+                      properties: { type: { type: 'string', const: 'numberType' }, numVal: { type: 'number' } },
+                      additionalProperties: false,
+                      required: ['type', 'numVal'],
+                    },
+                    {
+                      type: 'object',
+                      properties: { type: { type: 'string', const: 'booleanType' }, boolVal: { type: 'boolean' } },
+                      additionalProperties: false,
+                      required: ['type', 'boolVal'],
+                    },
+                  ],
+                },
+              },
+            },
+            additionalProperties: false,
+            required: ['elements'],
+          } as const,
+        },
+        result: {
+          type: 'object',
+          properties: {
+            elements: {
+              type: 'array',
+              items: {
+                anyOf: [
+                  {
+                    type: 'object',
+                    properties: { type: { type: 'string', const: 'stringType' }, stringVal: { type: 'string' } },
+                    additionalProperties: false,
+                    required: ['type', 'stringVal'],
+                  },
+                  {
+                    type: 'object',
+                    properties: { type: { type: 'string', const: 'numberType' }, numVal: { type: 'number' } },
+                    additionalProperties: false,
+                    required: ['type', 'numVal'],
+                  },
+                  {
+                    type: 'object',
+                    properties: { type: { type: 'string', const: 'booleanType' }, boolVal: { type: 'boolean' } },
+                    additionalProperties: false,
+                    required: ['type', 'boolVal'],
+                  },
+                ],
+              },
+            },
+          },
+          additionalProperties: false,
+          required: ['elements'],
+        },
+      },
     ];
 
     schemas.forEach((schema) => {
       return describe(`using ${schema}`, () => {
         testCases.forEach((testCase) => {
           it(testCase.title, () => {
-            const result = transformSchema(testCase.schema[schema]);
-            expect(result).deep.contains(testCase.result);
+            const result = transformSchema(testCase.schemas[schema]);
+            expect(result).toMatchInlineSnapshot(testCase.result);
           });
         });
       });
