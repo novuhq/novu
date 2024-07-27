ebc63d040ce14b657502fe5102aeb4f067142255
Joel Anton <joel@novu.co>
2024-07-26 18:30:51 -0700

    feat: Add color scheme pref to studio

--- apps/web/src/components/layout/components/LocalStudioHeader/LocalStudioHeader.tsx
+++ apps/web/src/components/layout/components/LocalStudioHeader/LocalStudioHeader.tsx
@@ -1,9 +1,10 @@
-import { Header } from '@mantine/core';
+import { Header, Tooltip } from '@mantine/core';
 import { IconButton } from '@novu/novui';
 import { css } from '@novu/novui/css';
 import { IconOutlineMenuBook } from '@novu/novui/icons';
 import { HStack } from '@novu/novui/jsx';
 import { FC } from 'react';
+import useThemeChange from '../../../../hooks/useThemeChange';
 import { useStudioWorkflowsNavigation } from '../../../../studio/hooks';
 import { HEADER_NAV_HEIGHT } from '../../constants';
 import { BridgeMenuItems } from '../v2/BridgeMenuItems';
@@ -11,6 +12,7 @@ import { BackButton } from './BackButton';
 
 export const LocalStudioHeader: FC = () => {
   const { goBack, shouldHideBackButton } = useStudioWorkflowsNavigation();
+  const { Icon, themeLabel, toggleColorScheme } = useThemeChange();
 
   return (
     <Header
@@ -27,6 +29,9 @@ export const LocalStudioHeader: FC = () => {
         <HStack gap="100">{!shouldHideBackButton && <BackButton onClick={goBack} />}</HStack>
         <HStack gap="100">
           <BridgeMenuItems />
+          <Tooltip label={themeLabel}>
+            <IconButton variant="transparent" onClick={() => toggleColorScheme()} Icon={Icon} />
+          </Tooltip>
           <IconButton
             Icon={IconOutlineMenuBook}
             title={'Go to docs'}
