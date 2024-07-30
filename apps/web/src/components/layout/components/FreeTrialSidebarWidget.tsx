3fca0f2c4a37a316566805cbc17ba4495e1b708a
Adam Chmara <adam.chmara1@gmail.com>
2024-07-30 14:25:40 +0200

    fix(web): disable billing widget btn in ee

--- apps/web/src/components/layout/components/FreeTrialSidebarWidget.tsx
+++ apps/web/src/components/layout/components/FreeTrialSidebarWidget.tsx
@@ -1,8 +1,8 @@
 import { FreeTrialSidebarWidget as Component } from '../../../ee/billing';
-import { IS_DOCKER_HOSTED } from '../../../config';
+import { IS_DOCKER_HOSTED, IS_EE_AUTH_ENABLED } from '../../../config';
 
 export const FreeTrialSidebarWidget = () => {
-  if (IS_DOCKER_HOSTED) {
+  if (IS_DOCKER_HOSTED || IS_EE_AUTH_ENABLED) {
     return null;
   }
 
