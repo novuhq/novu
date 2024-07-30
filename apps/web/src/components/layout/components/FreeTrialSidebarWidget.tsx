fc8408bc2f48a93ae1bac13287a907db01fd5088
Adam Chmara <adam.chmara1@gmail.com>
2024-07-30 15:30:29 +0200

    fix(web): dedicated billing page for upgrade button (tmp)

--- apps/web/src/components/layout/components/FreeTrialSidebarWidget.tsx
+++ apps/web/src/components/layout/components/FreeTrialSidebarWidget.tsx
@@ -2,7 +2,7 @@ import { FreeTrialSidebarWidget as Component } from '../../../ee/billing';
 import { IS_DOCKER_HOSTED, IS_EE_AUTH_ENABLED } from '../../../config';
 
 export const FreeTrialSidebarWidget = () => {
-  if (IS_DOCKER_HOSTED || IS_EE_AUTH_ENABLED) {
+  if (IS_DOCKER_HOSTED) {
     return null;
   }
 
