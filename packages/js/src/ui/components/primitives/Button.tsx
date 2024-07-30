6ec34644db05bf5c8c6581ce2cc89351a4e85517
George Desipris <73396808+desiprisg@users.noreply.github.com>
2024-07-30 14:03:54 +0300

    feat(js): Default notification component (#6163)

--- packages/js/src/ui/components/primitives/Button.tsx
+++ packages/js/src/ui/components/primitives/Button.tsx
@@ -5,21 +5,21 @@ import { AppearanceKey } from '../../context';
 import { cn, useStyle } from '../../helpers';
 
 export const buttonVariants = cva(
-  'nt-inline-flex nt-gap-4 nt-items-center nt-justify-center nt-whitespace-nowrap nt-rounded-md nt-text-sm nt-font-medium nt-ring-offset-background nt-transition-colors focus-visible:nt-outline-none focus-visible:nt-ring-2 focus-visible:nt-ring-primary nt-focus-visible:nt-ring-offset-2 disabled:nt-pointer-events-none disabled:nt-opacity-50',
+  'nt-inline-flex nt-gap-4 nt-items-center nt-justify-center nt-whitespace-nowrap nt-rounded-lg nt-text-sm nt-font-medium nt-ring-offset-background nt-transition-colors focus-visible:nt-outline-none focus-visible:nt-ring-2 focus-visible:nt-ring-primary nt-focus-visible:nt-ring-offset-2 disabled:nt-pointer-events-none disabled:nt-opacity-50',
   {
     variants: {
       variant: {
         default: 'nt-bg-primary nt-text-primary-foreground hover:nt-bg-primary-alpha-900',
         secondary: 'nt-bg-secondary nt-text-secondary-foreground hover:nt-bg-secondary-alpha-800',
-        icon: 'nt-text-secondary-foreground-alpha-600 hover:nt-bg-neutral-100',
+        icon: 'nt-text-secondary-foreground-alpha-500 hover:nt-bg-neutral-100',
         ghost: 'hover:nt-bg-neutral-100',
         unstyled: '',
       },
       size: {
         none: '',
-        default: 'nt-h-10 nt-px-4 nt-py-2',
-        sm: 'nt-h-9 nt-rounded-md nt-px-3',
-        lg: 'nt-h-11 nt-rounded-md nt-px-8',
+        default: 'nt-h-8 nt-px-3',
+        sm: 'nt-h-7 nt-rounded-md nt-px-3',
+        lg: 'nt-h-10 nt-px-8',
         icon: 'p-2',
       },
     },
