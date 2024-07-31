ecf164ddf312ed9c0259cd3cfbb164922eddb6ac
Biswajeet Das <biswajeetdas18@gmail.com>
2024-07-31 16:20:30 +0530

    chore: update

--- playground/nextjs/src/pages/render-notification/index.tsx
+++ playground/nextjs/src/pages/render-notification/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
             <div className="flex gap-2 flex-nowrap items-start self-stretch my-1 p-2 hover:bg-slate-200">
               <div className="rounded-full w-8 h-8 overflow-hidden border border-cyan-200">Avatar</div>
               <div>
-                <div className="text-xl font-bold">{notification.subject ?? 'Subject'}</div>
+                <div className="text-xl font-bold">{notification.subject || 'Subject'}</div>
                 <div>{notification.body}</div>
               </div>
             </div>
