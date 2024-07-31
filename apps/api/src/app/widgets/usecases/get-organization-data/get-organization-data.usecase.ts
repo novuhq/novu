2ae3d8d2679c346eb61e621bac39e1349bc09aff
Adam Chmara <adam.chmara1@gmail.com>
2024-07-31 17:53:53 +0200

    fix(api): omit clerk call when retrieving org in widgets (#6205)

--- apps/api/src/app/widgets/usecases/get-organization-data/get-organization-data.usecase.ts
+++ apps/api/src/app/widgets/usecases/get-organization-data/get-organization-data.usecase.ts
@@ -1,11 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { OrganizationRepository } from '@novu/dal';
+import { CommunityOrganizationRepository } from '@novu/dal';
 import { OrganizationResponseDto } from '../../dtos/organization-response.dto';
 import { GetOrganizationDataCommand } from './get-organization-data.command';
 
 @Injectable()
 export class GetOrganizationData {
-  constructor(private organizationRepository: OrganizationRepository) {}
+  constructor(private communityOrganizationRepository: CommunityOrganizationRepository) {}
 
   async execute(command: GetOrganizationDataCommand): Promise<OrganizationResponseDto> {
     const organization = await this.organizationRepository.findById(command.organizationId);
