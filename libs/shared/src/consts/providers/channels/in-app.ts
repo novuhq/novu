6aca94586a05bb5bbb716f1494ba5deba1fddc5d
Biswajeet Das <biswajeetdas18@gmail.com>
2024-08-12 18:11:22 +0530

    fix: doc reference

--- libs/shared/src/consts/providers/channels/in-app.ts
+++ libs/shared/src/consts/providers/channels/in-app.ts
@@ -12,7 +12,7 @@ export const inAppProviders: IProviderConfig[] = [
     displayName: 'Novu Inbox',
     channel: ChannelTypeEnum.IN_APP,
     credentials: novuInAppConfig,
-    docReference: `https://docs.novu.co/notification-center/introduction${UTM_CAMPAIGN_QUERY_PARAM}`,
+    docReference: `https://docs.novu.co/inbox/introduction${UTM_CAMPAIGN_QUERY_PARAM}`,
     logoFileName: { light: 'novu.png', dark: 'novu.png' },
   },
 ];
