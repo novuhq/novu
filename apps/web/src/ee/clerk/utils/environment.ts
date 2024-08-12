4a60ce47db7520aab3d838fee8c6081582d0a5f1
Adam Chmara <adam.chmara1@gmail.com>
2024-08-12 16:28:29 +0200

    refactor(shared): simplify Clerk imports

--- apps/web/src/ee/clerk/utils/environment.ts
+++ apps/web/src/ee/clerk/utils/environment.ts
@@ -1,8 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
-import {
-  UserPublicMetadata as _UserPublicMetadata,
-  OrganizationPublicMetadata as _OrganizationPublicMetadata,
-} from '@novu/shared';
+import { _UserPublicMetadata, _OrganizationPublicMetadata } from '@novu/shared';
 
 declare global {
   interface UserPublicMetadata extends _UserPublicMetadata {}
---
+++ libs/shared/src/types/clerk/types.ts
@@ -0,0 +1,20 @@
+import { ProductUseCases } from '../../dto';
+import { ApiServiceLevelEnum } from '../organization';
+import { IServicesHashes } from '../../entities/user';
+import { JobTitleEnum } from '../organization';
+
+export type _OrganizationPublicMetadata = {
+  externalOrgId?: string;
+  apiServiceLevel?: ApiServiceLevelEnum;
+  domain?: string;
+  productUseCases?: ProductUseCases;
+  language?: string[];
+};
+
+export type _UserPublicMetadata = {
+  profilePicture?: string | null;
+  showOnBoarding?: boolean;
+  showOnBoardingTour?: number;
+  servicesHashes?: IServicesHashes;
+  jobTitle?: JobTitleEnum;
+};
