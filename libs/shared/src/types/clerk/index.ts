251b2ffc0654606e462d8fdc0ec9873c116989e2
Adam Chmara <adam.chmara1@gmail.com>
2024-08-15 13:02:56 +0200

    refactor(shared): simplify Clerk imports (#6312)

--- libs/shared/src/types/clerk/index.ts
+++ libs/shared/src/types/clerk/index.ts
@@ -1,4 +1,1 @@
-export * from './clerk.organization.types';
-export * from './clerk.user.types';
-export * from './clerk.organization-membership.types';
-export * from './clerk.types';
+export * from './types';
