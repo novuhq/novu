191474b227d32767c2981f5d213c6da25723a98a
Adam Chmara <adam.chmara1@gmail.com>
2024-08-21 10:54:29 +0200

    fix: add missing imports

--- apps/api/src/app/widgets/widgets.module.ts
+++ apps/api/src/app/widgets/widgets.module.ts
@@ -6,11 +6,11 @@ import { SharedModule } from '../shared/shared.module';
 import { AuthModule } from '../auth/auth.module';
 import { SubscribersModule } from '../subscribers/subscribers.module';
 import { IntegrationModule } from '../integrations/integrations.module';
-import { CommunityOrganizationRepository } from '@novu/dal';
+import { CommunityOrganizationRepository, CommunityUserRepository } from '@novu/dal';
 
 @Module({
   imports: [SharedModule, SubscribersModule, AuthModule, IntegrationModule],
-  providers: [...USE_CASES, CommunityOrganizationRepository],
+  providers: [...USE_CASES, CommunityOrganizationRepository, CommunityUserRepository],
   exports: [...USE_CASES],
   controllers: [WidgetsController],
 })
