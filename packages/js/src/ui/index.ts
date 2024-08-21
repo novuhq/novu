5150b5533b275ee98cc420ba551d83473aebcc80
George Desipris <73396808+desiprisg@users.noreply.github.com>
2024-08-21 10:16:51 +0300

    feat(js): Include headers and tabs in separate components (#6323)

--- packages/js/src/ui/index.ts
+++ packages/js/src/ui/index.ts
@@ -1,5 +1,5 @@
-export { NovuUI } from './novuUI';
-export type { InboxProps } from './components';
-export type { BaseNovuUIOptions, NovuUIOptions } from './novuUI';
 export type { Notification } from '../notifications';
+export type { InboxPage, InboxProps } from './components';
+export { NovuUI } from './novuUI';
+export type { BaseNovuUIOptions, NovuUIOptions } from './novuUI';
 export * from './types';
