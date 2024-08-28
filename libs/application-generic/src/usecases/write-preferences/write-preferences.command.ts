ed38eab502d226a1df75de7ae9406b30f30c47da
David Söderberg <granskog_@hotmail.com>
2024-08-28 15:05:28 +0200

    feat: add write preferences usecase

---
+++ libs/application-generic/src/usecases/write-preferences/write-preferences.command.ts
@@ -0,0 +1,18 @@
+import { IsDefined, IsEnum } from 'class-validator';
+import { DiscoverWorkflowOutputPreferences } from '@novu/framework';
+import { EnvironmentCommand } from '../../commands';
+import { PreferencesActorEnum } from '@novu/dal';
+
+export class WritePreferencesCommand extends EnvironmentCommand {
+  @IsDefined()
+  readonly preferences: DiscoverWorkflowOutputPreferences;
+
+  readonly subscriberId: string;
+
+  readonly userId: string;
+
+  readonly templateId: string;
+
+  @IsEnum(PreferencesActorEnum)
+  readonly actor: PreferencesActorEnum;
+}
