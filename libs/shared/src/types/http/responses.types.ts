ffeaef0e88899de6a50f8781f4430d9dd418c5e8
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-08-29 19:24:39 +0100

    style: Remove unnecessary ESLint comments

--- libs/shared/src/types/http/responses.types.ts
+++ libs/shared/src/types/http/responses.types.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-restricted-imports, import/no-namespace
+// eslint-disable-next-line import/no-namespace
 import * as nestSwagger from '@nestjs/swagger';
 
 type NestJsExport = keyof typeof nestSwagger;
