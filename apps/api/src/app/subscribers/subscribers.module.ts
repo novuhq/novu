bf32da90d7527311a765c495060f7dec299e92b0
David Söderberg <granskog_@hotmail.com>
2024-09-02 15:07:01 +0200

    fix: more dependencies

--- apps/api/src/app/subscribers/subscribers.module.ts
+++ apps/api/src/app/subscribers/subscribers.module.ts
@@ -5,11 +5,13 @@ import { USE_CASES } from './usecases';
 import { SubscribersController } from './subscribers.controller';
 import { AuthModule } from '../auth/auth.module';
 import { WidgetsModule } from '../widgets/widgets.module';
+import { GetPreferences, UpsertPreferences } from '@novu/application-generic';
+import { PreferencesRepository } from '@novu/dal';
 
 @Module({
   imports: [SharedModule, AuthModule, TerminusModule, forwardRef(() => WidgetsModule)],
   controllers: [SubscribersController],
-  providers: [...USE_CASES],
+  providers: [...USE_CASES, GetPreferences, UpsertPreferences, PreferencesRepository],
   exports: [...USE_CASES],
 })
 export class SubscribersModule {}
