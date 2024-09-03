559f4a4e4364083085dbca07fbdaea0d605a5c44
David Söderberg <granskog_@hotmail.com>
2024-09-03 13:40:54 +0200

    fix: sort of imports

--- libs/dal/src/repositories/preferences/preferences.schema.ts
+++ libs/dal/src/repositories/preferences/preferences.schema.ts
@@ -1,7 +1,5 @@
-import mongoose from 'mongoose';
-import { Schema } from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 const mongooseDelete = require('mongoose-delete');
-
 import { schemaOptions } from '../schema-default.options';
 import { PreferencesDBModel } from './preferences.entity';
 
