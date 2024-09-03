44013241a6915d8e80ab31d807fa6258769291b1
David Söderberg <granskog_@hotmail.com>
2024-09-03 13:48:30 +0200

    fix: sort imports

--- apps/api/src/app/subscribers/subscribers.module.ts
+++ apps/api/src/app/subscribers/subscribers.module.ts
@@ -1,12 +1,12 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
-import { SharedModule } from '../shared/shared.module';
-import { USE_CASES } from './usecases';
-import { SubscribersController } from './subscribers.controller';
-import { AuthModule } from '../auth/auth.module';
-import { WidgetsModule } from '../widgets/widgets.module';
 import { GetPreferences, UpsertPreferences } from '@novu/application-generic';
 import { PreferencesRepository } from '@novu/dal';
+import { AuthModule } from '../auth/auth.module';
+import { SharedModule } from '../shared/shared.module';
+import { WidgetsModule } from '../widgets/widgets.module';
+import { SubscribersController } from './subscribers.controller';
+import { USE_CASES } from './usecases';
 
 @Module({
   imports: [SharedModule, AuthModule, TerminusModule, forwardRef(() => WidgetsModule)],
