d610b7362ff6bf4ad41063d111c9ad8b3d3505f2
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-09-04 08:25:20 +0100

    refactor(breadcrumbs): remove console.log and add breadcrumb

--- apps/ui/app/components/layout/breadcrumbs.tsx
+++ apps/ui/app/components/layout/breadcrumbs.tsx
@@ -5,7 +5,6 @@ const Separator = () => <Text c="dimmed">/</Text>;
 
 export function NavigationBreadcrumbs() {
   const matches = useMatches();
-  console.log({ matches });
 
   return (
     <>
@@ -14,8 +13,10 @@ export function NavigationBreadcrumbs() {
         <Breadcrumbs separator={<Separator />}>
           <Anchor href="/">Development</Anchor>
           {matches
+            // @ts-expect-error - need to figure out how to type this
             .filter((match) => match.handle && match.handle.breadcrumb)
             .map((match, index) => (
+              // @ts-expect-error - need to figure out how to type this
               <Anchor key={index}>{match.handle.breadcrumb(match)}</Anchor>
             ))}
         </Breadcrumbs>
--- apps/ui/app/routes/get-started/route.tsx
+++ apps/ui/app/routes/get-started/route.tsx
@@ -1,3 +1,10 @@
+import { Anchor } from '@mantine/core';
+
+// Adds a breadcrumb to the workflows route - https://remix.run/docs/en/main/guides/breadcrumbs
+export const handle = {
+  breadcrumb: () => <Anchor href="/get-started">Get Started</Anchor>,
+};
+
 export default function GetStartedRoute() {
   return <div>Get Started</div>;
 }
