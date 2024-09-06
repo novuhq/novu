b54451dceec7877373561f7cc95c6ef5e436cc78
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-09-06 19:23:28 +0100

    feat(api): add EnvironmentsBridgeController

--- apps/api/src/app/environments/environments.controller.ts
+++ apps/api/src/app/environments/environments.controller.ts
@@ -3,22 +3,15 @@ import {
   ClassSerializerInterceptor,
   Controller,
   Get,
-  NotFoundException,
   Param,
   Post,
   Put,
-  Query,
-  Req,
-  Res,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
-import { ApiAuthSchemeEnum, MemberRoleEnum, StepTypeEnum, UserSessionData } from '@novu/shared';
+import { ApiAuthSchemeEnum, MemberRoleEnum, UserSessionData } from '@novu/shared';
 import { ApiExcludeEndpoint, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { RolesGuard, Roles, decryptApiKey } from '@novu/application-generic';
-import { EnvironmentRepository, NotificationTemplateRepository } from '@novu/dal';
-import { Client, Event, workflow, Step } from '@novu/framework';
+import { RolesGuard, Roles } from '@novu/application-generic';
 import { UserSession } from '../shared/framework/user.decorator';
 import { CreateEnvironment } from './usecases/create-environment/create-environment.usecase';
 import { CreateEnvironmentCommand } from './usecases/create-environment/create-environment.command';
@@ -38,11 +31,11 @@ import { UpdateEnvironmentRequestDto } from './dtos/update-environment-request.d
 import { ApiCommonResponses, ApiResponse } from '../shared/framework/response.decorator';
 import { UserAuthentication } from '../shared/framework/swagger/api.key.security';
 import { SdkGroupName } from '../shared/framework/swagger/sdk.decorators';
-import { NovuNestjsHandler } from './novu-nestjs-handler';
 
 @ApiCommonResponses()
 @Controller('/environments')
 @UseInterceptors(ClassSerializerInterceptor)
+@UserAuthentication()
 @ApiTags('Environments')
 export class EnvironmentsController {
   constructor(
@@ -51,9 +44,7 @@ export class EnvironmentsController {
     private getApiKeysUsecase: GetApiKeys,
     private regenerateApiKeysUsecase: RegenerateApiKeys,
     private getEnvironmentUsecase: GetEnvironment,
-    private getMyEnvironmentsUsecase: GetMyEnvironments,
-    private workflowsRepository: NotificationTemplateRepository,
-    private environmentsRepository: EnvironmentRepository
+    private getMyEnvironmentsUsecase: GetMyEnvironments
   ) {}
 
   @Get('/me')
@@ -62,7 +53,6 @@ export class EnvironmentsController {
   })
   @ApiResponse(EnvironmentResponseDto)
   @ExternalApiAccessible()
-  @UserAuthentication()
   async getCurrentEnvironment(@UserSession() user: UserSessionData): Promise<EnvironmentResponseDto> {
     return await this.getEnvironmentUsecase.execute(
       GetEnvironmentCommand.create({
@@ -79,7 +69,6 @@ export class EnvironmentsController {
   })
   @ApiExcludeEndpoint()
   @ApiResponse(EnvironmentResponseDto, 201)
-  @UserAuthentication()
   async createEnvironment(
     @UserSession() user: UserSessionData,
     @Body() body: CreateEnvironmentRequestDto
@@ -99,7 +88,6 @@ export class EnvironmentsController {
   })
   @ApiResponse(EnvironmentResponseDto, 200, true)
   @ExternalApiAccessible()
-  @UserAuthentication()
   async listMyEnvironments(@UserSession() user: UserSessionData): Promise<EnvironmentResponseDto[]> {
     return await this.getMyEnvironmentsUsecase.execute(
       GetMyEnvironmentsCommand.create({
@@ -116,7 +104,6 @@ export class EnvironmentsController {
   })
   @ApiExcludeEndpoint()
   @ApiResponse(EnvironmentResponseDto)
-  @UserAuthentication()
   async updateMyEnvironment(
     @UserSession() user: UserSessionData,
     @Param('environmentId') environmentId: string,
@@ -136,89 +123,6 @@ export class EnvironmentsController {
     );
   }
 
-  @Get('/:environmentId/bridge')
-  async getHandler(@Req() req: Request, @Res() res: Response) {
-    /*
-     * TODO: remove the GET handler, it's only used right now for the bridge health check,
-     * and it's not needed for UI-defined workflows.
-     *
-     * I'm leaving it here as a reminder that we need to remove the UI health-check for Novu-managed
-     * bridge environments.
-     */
-    const novuBridgeHandler = new NovuNestjsHandler({
-      workflows: [],
-      client: new Client({ strictAuthentication: false }),
-    });
-    await novuBridgeHandler.handleRequest(req, res, 'GET');
-  }
-
-  @Post('/:environmentId/bridge')
-  async postHandler(
-    @Req() req: Request,
-    @Res() res: Response,
-    @Param('environmentId') environmentId: string,
-    @Query('workflowId') workflowId: string,
-    @Body() event: Event
-  ) {
-    // TODO: refactor this into a usecase, add caching.
-    const foundWorkflow = await this.workflowsRepository.findByTriggerIdentifier(environmentId, workflowId);
-
-    if (!foundWorkflow) {
-      throw new NotFoundException('Workflow not found');
-    }
-
-    // TODO: wrap the secret key fetching per environmentId in a usecase, including the decryption, add caching.
-    const environment = await this.environmentsRepository.findOne({ _id: environmentId });
-    if (!environment) {
-      throw new NotFoundException('Environment not found');
-    }
-    const secretKey = decryptApiKey(environment.apiKeys[0].key);
-
-    // Unfortunately we need this mapper because the `in_app` step type uses `step.inApp()` in Framework.
-    const stepFnFromStepType: Record<Exclude<StepTypeEnum, StepTypeEnum.CUSTOM | StepTypeEnum.TRIGGER>, keyof Step> = {
-      [StepTypeEnum.IN_APP]: 'inApp',
-      [StepTypeEnum.EMAIL]: 'email',
-      [StepTypeEnum.SMS]: 'sms',
-      [StepTypeEnum.CHAT]: 'chat',
-      [StepTypeEnum.PUSH]: 'push',
-      [StepTypeEnum.DIGEST]: 'digest',
-      [StepTypeEnum.DELAY]: 'delay',
-    };
-
-    const programmaticallyCreatedWorkflow = workflow(
-      foundWorkflow.name,
-      async ({ step }) => {
-        for await (const staticStep of foundWorkflow.steps) {
-          await step[stepFnFromStepType[staticStep.template!.type]](staticStep.stepId, () => event.controls, {
-            // TODO: fix the step typings, `controls` lives on template property, not step
-            controlSchema: (staticStep.template as unknown as typeof staticStep).controls?.schema,
-            /*
-             * TODO: add conditions
-             * Used to construct conditions defined with https://react-querybuilder.js.org/ or similar
-             * skip: () => !foundWorkflow.preferences.channels[staticStep.type],
-             */
-          });
-        }
-      },
-      {
-        /*
-         * TODO: these are probably not needed, given that this endpoint focuses on execution only.
-         * however we should reconsider if we decide to expose Workflow options to the `workflow` function.
-         *
-         * preferences: foundWorkflow.preferences,
-         * tags: foundWorkflow.tags,
-         */
-      }
-    );
-
-    const novuBridgeHandler = new NovuNestjsHandler({
-      workflows: [programmaticallyCreatedWorkflow],
-      client: new Client({ strictAuthentication: true, secretKey }),
-    });
-
-    await novuBridgeHandler.handleRequest(req, res, 'POST');
-  }
-
   @Get('/api-keys')
   @ApiOperation({
     summary: 'Get api keys',
@@ -226,7 +130,6 @@ export class EnvironmentsController {
   @ApiResponse(ApiKey, 200, true)
   @ExternalApiAccessible()
   @SdkGroupName('Environments.ApiKeys')
-  @UserAuthentication()
   async listOrganizationApiKeys(@UserSession() user: UserSessionData): Promise<ApiKey[]> {
     const command = GetApiKeysCommand.create({
       userId: user._id,
@@ -241,7 +144,6 @@ export class EnvironmentsController {
   @ApiResponse(ApiKey, 201, true)
   @UseGuards(RolesGuard)
   @Roles(MemberRoleEnum.ADMIN)
-  @UserAuthentication()
   async regenerateOrganizationApiKeys(@UserSession() user: UserSessionData): Promise<ApiKey[]> {
     const command = GetApiKeysCommand.create({
       userId: user._id,
