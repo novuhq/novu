8cb2ab124af069dd5e37febec85e0de55d322a68
Joel Anton <joel@novu.co>
2024-09-06 10:59:04 -0700

    feat: Add clipboard + props

--- apps/web/src/studio/components/workflows/preferences/WorkflowGeneralSettings.tsx
+++ apps/web/src/studio/components/workflows/preferences/WorkflowGeneralSettings.tsx
@@ -1,22 +1,46 @@
+import { useClipboard } from '@mantine/hooks';
 import { IconButton, Input, Textarea } from '@novu/novui';
-import { IconCopyAll } from '@novu/novui/icons';
+import { IconCheck, IconCopyAll } from '@novu/novui/icons';
 import { Stack } from '@novu/novui/jsx';
-import { FC } from 'react';
+import { ChangeEvent, FC } from 'react';
+import { WorkflowGeneralSettings } from './types';
 
-export type WorkflowGeneralSettingsProps = {};
+export type WorkflowGeneralSettingsProps = {
+  settings: WorkflowGeneralSettings;
+  updateSettings: (settings: WorkflowGeneralSettings) => void;
+  areSettingsDisabled?: boolean;
+};
 
-export const WorkflowGeneralSettings: FC<WorkflowGeneralSettingsProps> = (props) => {
+export const WorkflowGeneralSettingsForm: FC<WorkflowGeneralSettingsProps> = ({
+  settings,
+  updateSettings,
+  areSettingsDisabled,
+}) => {
+  const { copied, copy } = useClipboard();
+  const onChange =
+    (fieldKey: keyof WorkflowGeneralSettings) => (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const value = event.target.value;
+      updateSettings({ ...settings, [fieldKey]: value });
+    };
+
   return (
-    <form>
-      <Stack gap="150">
-        <Input label="Workflow name" />
-        <Input
-          label="Identifier"
-          description="Must be unique and all lowercase, using - only"
-          rightSection={<IconButton Icon={IconCopyAll} />}
-        />
-        <Textarea label="Description" maxLines={2} />
-      </Stack>
-    </form>
+    <Stack gap="150">
+      <Input label="Workflow name" value={settings.name} onChange={onChange('name')} disabled={areSettingsDisabled} />
+      <Input
+        label="Identifier"
+        description="Must be unique and all lowercase, using - only"
+        rightSection={<IconButton Icon={copied ? IconCheck : IconCopyAll} onClick={() => copy(settings.workflowId)} />}
+        value={settings.workflowId}
+        onChange={onChange('workflowId')}
+        disabled={areSettingsDisabled}
+      />
+      <Textarea
+        label="Description"
+        maxLines={2}
+        value={settings.description}
+        onChange={onChange('description')}
+        disabled={areSettingsDisabled}
+      />
+    </Stack>
   );
 };
