e819d3389196f432a177c163e37703a11d8dc984
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-09-09 17:09:04 +0100

    test(in-app.schema): add validation tests for URL and target

--- packages/framework/src/schemas/steps/channels/in-app.schema.test.ts
+++ packages/framework/src/schemas/steps/channels/in-app.schema.test.ts
@@ -41,5 +41,49 @@ describe('in-app schema', () => {
         redirect: { url: 'https://example.com/foo', target: '_blank' },
       });
     });
+
+    it('should throw an error if the url is not a valid absolute or relative url', async () => {
+      const schema = inAppChannelSchemas.output;
+
+      const data = {
+        body: 'Hello, world!',
+        redirect: {
+          url: 'foo',
+        },
+      };
+
+      const result = await validateData(schema, data);
+
+      expect(result.success).toBe(false);
+      expect(result.success === false && result.errors).toEqual([
+        {
+          message:
+            'The url field must be a relative URL starting with "/" or an absolute URL starting with "https" or "http".',
+          path: '/redirect/url',
+        },
+      ]);
+    });
+
+    it('should throw an error if the redirect target is not a valid value', async () => {
+      const schema = inAppChannelSchemas.output;
+
+      const data = {
+        body: 'Hello, world!',
+        redirect: {
+          url: '/foo',
+          target: 'foo',
+        },
+      };
+
+      const result = await validateData(schema, data);
+
+      expect(result.success).toBe(false);
+      expect(result.success === false && result.errors).toEqual([
+        {
+          message: 'The target must be one of _self, _blank, _parent, _top, or _unfencedTop.',
+          path: '/redirect/target',
+        },
+      ]);
+    });
   });
 });
