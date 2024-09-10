7a6166d9a2a6438a4a623abd217d36cc2226a5c3
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-09-10 15:24:51 +0100

    docs(validator): Add comments to validator types

--- packages/framework/src/types/validator.types.ts
+++ packages/framework/src/types/validator.types.ts
@@ -33,11 +33,23 @@ export type ValidationError = {
  */
 export type ValidateResult<T> =
   | {
+      /**
+       * `false` if the data is invalid against the schema.
+       */
       success: false;
+      /**
+       * An array of validation errors.
+       */
       errors: ValidationError[];
     }
   | {
+      /**
+       * `true` if the data is valid against the schema.
+       */
       success: true;
+      /**
+       * The data cast to the type of the schema.
+       */
       data: T;
     };
 
@@ -46,6 +58,13 @@ export type ValidateResult<T> =
  */
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export interface Validator<T_Schema extends Schema = Schema> {
+  /**
+   * Validates data against a JSON schema.
+   *
+   * @param data - The data to validate.
+   * @param schema - The schema to validate against.
+   * @returns The result of the validation.
+   */
   validate: <
     T_Unvalidated extends Record<string, unknown> = FromSchemaUnvalidated<T_Schema>,
     T_Validated extends Record<string, unknown> = FromSchema<T_Schema>,
@@ -53,6 +72,18 @@ export interface Validator<T_Schema extends Schema = Schema> {
     data: T_Unvalidated,
     schema: T_Schema
   ) => Promise<ValidateResult<T_Validated>>;
+  /**
+   * Checks if the validator can handle a given schema.
+   *
+   * @param schema - The schema to check.
+   * @returns `true` if the validator can handle the schema, `false` otherwise.
+   */
   canHandle: (schema: Schema) => schema is T_Schema;
+  /**
+   * Transforms a schema to a JSON schema.
+   *
+   * @param schema - The schema to transform.
+   * @returns The JSON schema.
+   */
   transformToJsonSchema: (schema: T_Schema) => JsonSchema;
 }
