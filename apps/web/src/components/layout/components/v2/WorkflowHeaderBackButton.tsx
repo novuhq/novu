c6989f6a5092a040760289efacdf43c83ce5f309
Gali Ainouz Baum <ainouzgali@gmail.com>
2024-09-10 15:34:39 +0300

    feat(web): add back button from workflow page to workflow list in dashboard (#6471)

--- apps/web/src/components/layout/components/v2/WorkflowHeaderBackButton.tsx
+++ apps/web/src/components/layout/components/v2/WorkflowHeaderBackButton.tsx
@@ -1,18 +1,10 @@
-import { matchPath, useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { BackButton } from '../LocalStudioHeader/BackButton';
-import { ROUTES } from '../../../../constants/routes';
-
-export const WORKFLOW_EDIT_HOME_ROUTE: ROUTES = ROUTES.WORKFLOWS_EDIT_TEMPLATEID;
-
-function isWorkflowEditRoute(pathname: string) {
-  return matchPath(WORKFLOW_EDIT_HOME_ROUTE, pathname);
-}
 
 export function WorkflowHeaderBackButton() {
-  const { pathname } = useLocation();
   const navigate = useNavigate();
   const { templateId = '' } = useParams<{ templateId: string }>();
-  const shouldHideBackButton = isWorkflowEditRoute(pathname) || !templateId;
+  const shouldHideBackButton = !templateId;
 
   return <>{!shouldHideBackButton && <BackButton onClick={() => navigate(-1)} />}</>;
 }
