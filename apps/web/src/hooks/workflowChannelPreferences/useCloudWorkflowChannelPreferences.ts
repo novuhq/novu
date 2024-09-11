57aa8cacfa0e70e82c6a1624cc4cf2fbd6f3a0e3
Joel Anton <joel@novu.co>
2024-09-10 20:51:01 -0700

    fix: If prefs not found, show defaults

--- apps/web/src/hooks/workflowChannelPreferences/useCloudWorkflowChannelPreferences.ts
+++ apps/web/src/hooks/workflowChannelPreferences/useCloudWorkflowChannelPreferences.ts
@@ -2,6 +2,8 @@ import { useQuery } from '@tanstack/react-query';
 import { WorkflowChannelPreferences } from '@novu/shared';
 import { QueryKeys } from '../../api/query.keys';
 import { useNovuAPI } from '../useNovuAPI';
+import { DEFAULT_WORKFLOW_PREFERENCES } from '../../studio/components/workflows/preferences/WorkflowSubscriptionPreferences.const';
+import { AxiosError, HttpStatusCode } from 'axios';
 
 export const useCloudWorkflowChannelPreferences = (
   workflowId: string
@@ -16,10 +18,19 @@ export const useCloudWorkflowChannelPreferences = (
     data: workflowChannelPreferences,
     isLoading,
     refetch,
-  } = useQuery<WorkflowChannelPreferences>(
-    [QueryKeys.getWorkflowChannelPreferences(workflowId)],
-    async () => (await api.getPreferences(workflowId as string))?.data
-  );
+  } = useQuery<WorkflowChannelPreferences>([QueryKeys.getWorkflowChannelPreferences(workflowId)], async () => {
+    try {
+      const result = await api.getPreferences(workflowId as string);
+      return result?.data;
+    } catch (err: unknown) {
+      if (!checkIsAxiosError(err) || err.response?.status !== HttpStatusCode.NotFound) {
+        throw err;
+      }
+
+      // if preferences aren't found (404), use default so that user can modify them to upsert properly.
+      return DEFAULT_WORKFLOW_PREFERENCES;
+    }
+  });
 
   return {
     isLoading,
@@ -27,3 +38,7 @@ export const useCloudWorkflowChannelPreferences = (
     refetch,
   };
 };
+
+function checkIsAxiosError(err: unknown): err is AxiosError {
+  return typeof err === 'object' && !!err && 'name' in err && err.name === 'AxiosError';
+}
