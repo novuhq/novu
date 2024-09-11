0ae2e2f4c8c780e8e546dae6741b09074af7efe1
GalT <39020298+tatarco@users.noreply.github.com>
2024-09-12 00:08:46 +0200

    refactor: ready for testing

--- apps/api/src/app/workflows-v2/workflow.module.ts
+++ apps/api/src/app/workflows-v2/workflow.module.ts
@@ -1,20 +1,28 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { CreateWorkflow, UpdateWorkflow } from '@novu/application-generic';
 import { SharedModule } from '../shared/shared.module';
 import { MessageTemplateModule } from '../message-template/message-template.module';
 import { ChangeModule } from '../change/change.module';
 import { AuthModule } from '../auth/auth.module';
 import { IntegrationModule } from '../integrations/integrations.module';
 import { WorkflowController } from './workflow.controller';
-import { GetWorkflowUseCase } from './usecases/get-workflow/get-workflow.usecase';
 import { UpsertWorkflowUseCase } from './usecases/upsert-workflow/upsert-workflow.usecase';
+import { GetWorkflowUseCase } from './usecases/get-workflow/get-workflow.usecase';
 import { ListWorkflowsUseCase } from './usecases/list-workflows/list-workflow.usecase';
 import { DeleteWorkflowUseCase } from './usecases/delete-workflow/delete-workflow.usecase';
 
 @Module({
-  imports: [SharedModule, MessageTemplateModule, ChangeModule, AuthModule, IntegrationModule],
+  imports: [SharedModule, MessageTemplateModule, ChangeModule, AuthModule, IntegrationModule], // Importing the module containing CreateWorkflow
   controllers: [WorkflowController],
-  providers: [GetWorkflowUseCase, UpsertWorkflowUseCase, ListWorkflowsUseCase, DeleteWorkflowUseCase],
-  exports: [],
+  providers: [
+    CreateWorkflow,
+    UpsertWorkflowUseCase,
+    UpdateWorkflow,
+    UpsertWorkflowUseCase,
+    GetWorkflowUseCase,
+    ListWorkflowsUseCase,
+    DeleteWorkflowUseCase,
+  ],
 })
 export class WorkflowV2Module implements NestModule {
   configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {}
