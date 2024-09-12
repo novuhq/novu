f873ce3346522962dbdfb78d70b3be7ac0e58a0a
Joel Anton <joel@novu.co>
2024-09-11 21:38:49 -0700

    Update index.ts

--- packages/framework/src/constants/index.ts
+++ packages/framework/src/constants/index.ts
@@ -9,4 +9,4 @@ export * from './http-status.constants';
 export * from './resource.constants';
 export * from './step.constants';
 export * from './version.constants';
-export * from './workflow-options.constants';
+export * from './workflow.constants';
