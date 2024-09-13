84dd776d304e560f816fb1ef45308faa9692b533
Adam Chmara <adam.chmara1@gmail.com>
2024-09-13 15:32:12 +0200

    fix: add v2 flag

--- apps/web/src/pages/integrations/UpdateProviderPage.tsx
+++ apps/web/src/pages/integrations/UpdateProviderPage.tsx
@@ -8,9 +8,7 @@ import { UpdateProviderSidebar as UpdateProviderSidebarOld } from './components/
 export function UpdateProviderPage() {
   const { integrationId } = useParams();
   const navigate = useNavigate();
-  let isV2Enabled = useFeatureFlag(FeatureFlagsKeysEnum.IS_V2_ENABLED);
-
-  isV2Enabled = false;
+  const isV2Enabled = useFeatureFlag(FeatureFlagsKeysEnum.IS_V2_ENABLED);
 
   const onClose = () => {
     navigate(ROUTES.INTEGRATIONS);
