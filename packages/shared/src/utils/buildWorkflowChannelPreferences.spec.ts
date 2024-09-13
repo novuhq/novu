0d05b5df8085c3bc775b370fa26e8c90fbd80032
Joel Anton <joel@novu.co>
2024-09-13 12:34:21 -0700

    feat: TDD - implement tests

--- packages/shared/src/utils/buildWorkflowChannelPreferences.spec.ts
+++ packages/shared/src/utils/buildWorkflowChannelPreferences.spec.ts
@@ -1,49 +1,105 @@
 import { buildWorkflowChannelPreferences } from './buildWorkflowChannelPreferences';
-import { IncompleteWorkflowChannelPreferences, WorkflowChannelPreferences } from '../types';
+import { ChannelPreference, IncompleteWorkflowChannelPreferences, WorkflowChannelPreferences } from '../types';
 
-const fullPreferences: WorkflowChannelPreferences = {
-  workflow: {
-    defaultValue: true,
-    readOnly: false,
-  },
+const WORKFLOW_CHANNEL_PREFERENCE_DEFAULT_VALUE = true;
+const WORKFLOW_CHANNEL_PREFERENCE_DEFAULT_READ_ONLY = false;
+
+const DEFAULT_CHANNEL_PREFERENCE: ChannelPreference = {
+  defaultValue: WORKFLOW_CHANNEL_PREFERENCE_DEFAULT_VALUE,
+  readOnly: WORKFLOW_CHANNEL_PREFERENCE_DEFAULT_READ_ONLY,
+};
+
+const testDefaultPreferences: WorkflowChannelPreferences = {
+  workflow: DEFAULT_CHANNEL_PREFERENCE,
   channels: {
-    in_app: {
-      defaultValue: true,
-      readOnly: false,
-    },
-    sms: {
-      defaultValue: true,
-      readOnly: false,
-    },
-    email: {
-      defaultValue: true,
-      readOnly: false,
-    },
-    push: {
-      defaultValue: true,
-      readOnly: false,
-    },
-    chat: {
-      defaultValue: true,
-      readOnly: false,
-    },
+    in_app: DEFAULT_CHANNEL_PREFERENCE,
+    sms: DEFAULT_CHANNEL_PREFERENCE,
+    email: DEFAULT_CHANNEL_PREFERENCE,
+    push: DEFAULT_CHANNEL_PREFERENCE,
+    chat: DEFAULT_CHANNEL_PREFERENCE,
   },
 };
 
 describe('buildWorkflowChannelPreferences', () => {
-  it('should return the input object if a complete preferences object is supplied', () => {
-    const testPreferences: IncompleteWorkflowChannelPreferences = { ...fullPreferences };
+  it('should return the defaults if input is undefined', () => {
+    const result = buildWorkflowChannelPreferences(undefined, testDefaultPreferences);
+    expect(result).toEqual(testDefaultPreferences);
+  });
 
-    const result = buildWorkflowChannelPreferences(testPreferences);
+  it('should return the input object if a complete preferences object is supplied', () => {
+    const testPreference: ChannelPreference = {
+      defaultValue: !WORKFLOW_CHANNEL_PREFERENCE_DEFAULT_VALUE,
+      readOnly: !WORKFLOW_CHANNEL_PREFERENCE_DEFAULT_READ_ONLY,
+    };
+
+    // opposite of default
+    const testPreferences: IncompleteWorkflowChannelPreferences = {
+      workflow: testPreference,
+      channels: {
+        in_app: testPreference,
+        sms: testPreference,
+        email: testPreference,
+        push: testPreference,
+        chat: testPreference,
+      },
+    };
+
+    const result = buildWorkflowChannelPreferences(testPreferences, testDefaultPreferences);
     expect(result).toEqual(testPreferences);
   });
 
   it('should populate the remainder of the object with default values', () => {
     const testPreferences: IncompleteWorkflowChannelPreferences = {
-      channels: { in_app: { readOnly: true, defaultValue: false } },
+      channels: { in_app: { readOnly: true } },
     };
 
-    const result = buildWorkflowChannelPreferences(testPreferences);
-    expect(result).toEqual(testPreferences);
+    const result = buildWorkflowChannelPreferences(testPreferences, testDefaultPreferences);
+    expect(result).toEqual({
+      ...testDefaultPreferences,
+      channels: {
+        ...testDefaultPreferences.channels,
+        in_app: { ...testDefaultPreferences.channels.in_app, ...testPreferences.channels?.in_app },
+      },
+    });
+  });
+
+  it('should use the `workflow`-level preferences to define defaults for channel-level preferences', () => {
+    const expectedDefaultValue = false;
+    const testPreferences: IncompleteWorkflowChannelPreferences = {
+      workflow: { defaultValue: expectedDefaultValue },
+    };
+
+    const result = buildWorkflowChannelPreferences(testPreferences, testDefaultPreferences);
+
+    const expectedResult: WorkflowChannelPreferences = {
+      workflow: {
+        defaultValue: expectedDefaultValue,
+        readOnly: WORKFLOW_CHANNEL_PREFERENCE_DEFAULT_READ_ONLY,
+      },
+      channels: {
+        in_app: {
+          defaultValue: expectedDefaultValue,
+          readOnly: WORKFLOW_CHANNEL_PREFERENCE_DEFAULT_READ_ONLY,
+        },
+        sms: {
+          defaultValue: expectedDefaultValue,
+          readOnly: WORKFLOW_CHANNEL_PREFERENCE_DEFAULT_READ_ONLY,
+        },
+        email: {
+          defaultValue: expectedDefaultValue,
+          readOnly: WORKFLOW_CHANNEL_PREFERENCE_DEFAULT_READ_ONLY,
+        },
+        push: {
+          defaultValue: expectedDefaultValue,
+          readOnly: WORKFLOW_CHANNEL_PREFERENCE_DEFAULT_READ_ONLY,
+        },
+        chat: {
+          defaultValue: expectedDefaultValue,
+          readOnly: WORKFLOW_CHANNEL_PREFERENCE_DEFAULT_READ_ONLY,
+        },
+      },
+    };
+
+    expect(result).toEqual(expectedResult);
   });
 });
