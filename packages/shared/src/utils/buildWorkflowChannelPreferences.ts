1f1a0e5f20e09cf807b01b944071369da55669c2
Joel Anton <joel@novu.co>
2024-09-13 11:16:23 -0700

    feat(shared): Add scaffolding of util

---
+++ packages/shared/src/utils/buildWorkflowChannelPreferences.ts
@@ -0,0 +1,38 @@
+import { WorkflowChannelPreferences } from '../types';
+
+const PREFERENCE_DEFAULT_READ_ONLY = true;
+const PREFERENCE_DEFAULT_VALUE = false;
+export const DEFAULT_WORKFLOW_PREFERENCES: WorkflowChannelPreferences = {
+  workflow: {
+    defaultValue: PREFERENCE_DEFAULT_READ_ONLY,
+    readOnly: PREFERENCE_DEFAULT_VALUE,
+  },
+  channels: {
+    in_app: {
+      defaultValue: PREFERENCE_DEFAULT_READ_ONLY,
+      readOnly: PREFERENCE_DEFAULT_VALUE,
+    },
+    sms: {
+      defaultValue: PREFERENCE_DEFAULT_READ_ONLY,
+      readOnly: PREFERENCE_DEFAULT_VALUE,
+    },
+    email: {
+      defaultValue: PREFERENCE_DEFAULT_READ_ONLY,
+      readOnly: PREFERENCE_DEFAULT_VALUE,
+    },
+    push: {
+      defaultValue: PREFERENCE_DEFAULT_READ_ONLY,
+      readOnly: PREFERENCE_DEFAULT_VALUE,
+    },
+    chat: {
+      defaultValue: PREFERENCE_DEFAULT_READ_ONLY,
+      readOnly: PREFERENCE_DEFAULT_VALUE,
+    },
+  },
+};
+
+export const buildWorkflowChannelPreferences = (
+  inputPreferences: Partial<WorkflowChannelPreferences>
+): WorkflowChannelPreferences => {
+  return DEFAULT_WORKFLOW_PREFERENCES;
+};
