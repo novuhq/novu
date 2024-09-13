9a829108a6acde514db373af184adb1ddb2c1992
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-09-13 13:34:34 +0100

    style(get-preferences): Add missing commas in comments

--- libs/application-generic/src/usecases/get-preferences/get-preferences.usecase.ts
+++ libs/application-generic/src/usecases/get-preferences/get-preferences.usecase.ts
@@ -220,13 +220,13 @@ export class GetPreferences {
       return resourcePreferences;
     }
 
-    // if the workflow should be readonly we return the resource preferences default value for workflow.
+    // if the workflow should be readonly, we return the resource preferences default value for workflow.
     if (readOnlyPreference?.workflow?.readOnly) {
       subscriberPreferences.workflow.defaultValue =
         resourcePreferences?.workflow?.defaultValue;
     }
 
-    // if the workflow channel should be readonly we return the resource preferences default value for channel.
+    // if the workflow channel should be readonly, we return the resource preferences default value for channel.
     for (const channel of Object.values(ChannelTypeEnum)) {
       if (readOnlyPreference?.channels[channel]?.readOnly) {
         subscriberPreferences.channels[channel].defaultValue =
