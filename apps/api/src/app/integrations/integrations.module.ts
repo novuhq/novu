f414e20b274bdb39f923b519fe3697d0c3792b4f
Adam Chmara <adam.chmara1@gmail.com>
2024-09-13 10:11:23 +0200

    fix(api): add missing dependency

--- apps/api/src/app/integrations/integrations.module.ts
+++ apps/api/src/app/integrations/integrations.module.ts
@@ -1,16 +1,16 @@
 import { forwardRef, Module } from '@nestjs/common';
 
-import { CompileTemplate, CreateExecutionDetails, QueuesModule } from '@novu/application-generic';
-import { JobTopicNameEnum } from '@novu/shared';
+import { CompileTemplate, CreateExecutionDetails } from '@novu/application-generic';
 import { SharedModule } from '../shared/shared.module';
 import { USE_CASES } from './usecases';
 import { IntegrationsController } from './integrations.controller';
 import { AuthModule } from '../auth/auth.module';
+import { CommunityOrganizationRepository } from '@novu/dal';
 
 @Module({
   imports: [SharedModule, forwardRef(() => AuthModule)],
   controllers: [IntegrationsController],
-  providers: [...USE_CASES, CompileTemplate, CreateExecutionDetails],
+  providers: [...USE_CASES, CompileTemplate, CreateExecutionDetails, CommunityOrganizationRepository],
   exports: [...USE_CASES],
 })
 export class IntegrationModule {}
