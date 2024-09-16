50fbfebf19b1b58eb12eebd8b164b630771492eb
George Desipris <73396808+desiprisg@users.noreply.github.com>
2024-09-16 13:05:35 +0300

    fix(js): Fix notification skeleton padding and action wrap (#6481)

--- packages/js/src/ui/components/Notification/DefaultNotification.tsx
+++ packages/js/src/ui/components/Notification/DefaultNotification.tsx
@@ -227,7 +227,7 @@ export const DefaultNotification = (props: DefaultNotificationProps) => {
         <Markdown class={style('notificationBody', 'nt-text-start')} strongAppearanceKey="notificationBody__strong">
           {props.notification.body}
         </Markdown>
-        <div class={style('notificationCustomActions', 'nt-flex nt-gap-4 nt-mt-4')}>
+        <div class={style('notificationCustomActions', 'nt-flex nt-flex-wrap nt-gap-4 nt-mt-4')}>
           <Show when={props.notification.primaryAction} keyed>
             {(primaryAction) => (
               <Button
