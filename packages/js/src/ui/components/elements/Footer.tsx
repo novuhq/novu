33c357d28efdb9fb85bd768088e2099cfa9ff1e9
Biswajeet Das <biswajeetdas18@gmail.com>
2024-09-17 21:07:05 +0530

    feat: hide branding

--- packages/js/src/ui/components/elements/Footer.tsx
+++ packages/js/src/ui/components/elements/Footer.tsx
@@ -1,11 +1,17 @@
 /* eslint-disable local-rules/no-class-without-style */
+import { Show } from 'solid-js';
+import { useInboxContext } from 'src/ui/context';
 import { Novu } from '../../icons';
 
 export const Footer = () => {
+  const { hideBranding } = useInboxContext();
+
   return (
-    <div class="nt-flex nt-shrink-0 nt-justify-center nt-items-center nt-gap-1 nt-mt-auto nt-pt-9 nt-pb-3 nt-text-foreground-alpha-200">
-      <Novu />
-      <span class="nt-text-xs">Powered by Novu</span>
-    </div>
+    <Show when={!hideBranding()}>
+      <div class="nt-flex nt-shrink-0 nt-justify-center nt-items-center nt-gap-1 nt-mt-auto nt-pt-9 nt-pb-3 nt-text-foreground-alpha-200">
+        <Novu />
+        <span class="nt-text-xs">Powered by Novu</span>
+      </div>
+    </Show>
   );
 };
