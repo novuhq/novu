399853097b813d7ab5ac3020855b3e328de84fcf
George Desipris <george.desipris.56@gmail.com>
2024-09-18 18:38:35 +0300

    feat(js): Add colorShadow variable to appearance

--- packages/js/src/ui/components/primitives/Dropdown/DropdownContent.tsx
+++ packages/js/src/ui/components/primitives/Dropdown/DropdownContent.tsx
@@ -4,7 +4,7 @@ import type { AppearanceKey } from '../../../types';
 import { Popover } from '../Popover';
 
 export const dropdownContentVariants = () =>
-  'nt-w-max nt-rounded-xl nt-p-1 nt-overflow-hidden nt-flex nt-flex-col nt-min-w-52 nt-shadow-[0_5px_20px_0_rgba(0,0,0,0.20)] nt-z-10 nt-bg-background';
+  'nt-w-max nt-rounded-xl nt-p-1 nt-overflow-hidden nt-flex nt-flex-col nt-min-w-52 nt-shadow-dropdown nt-z-10 nt-bg-background';
 
 export const DropdownContent = (props: ComponentProps<typeof Popover.Content> & { appearanceKey?: AppearanceKey }) => {
   const style = useStyle();
