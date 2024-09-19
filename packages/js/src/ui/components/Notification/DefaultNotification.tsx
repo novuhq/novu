4e099b28888cf842472cdc6a6ae16391f3f49b1e
Biswajeet Das <biswajeetdas18@gmail.com>
2024-09-19 14:27:16 +0530

    fix: hover actions position

--- packages/js/src/ui/components/Notification/DefaultNotification.tsx
+++ packages/js/src/ui/components/Notification/DefaultNotification.tsx
@@ -107,7 +107,7 @@ export const DefaultNotification = (props: DefaultNotificationProps) => {
               'notificationDefaultActions',
               `nt-transition nt-duration-100 nt-ease-out nt-gap-2 nt-flex nt-shrink-0 
               nt-opacity-0 group-hover:nt-opacity-100 nt-justify-center nt-items-center 
-              nt-absolute nt-top-0 nt-right-0  nt-bg-neutral-100 nt-p-0.5 nt-rounded nt-z-50`
+              nt-absolute nt-top-0 nt-right-0  nt-bg-neutral-100 nt-p-0.5 nt-rounded nt-z-50 nt-inset-0`
             )}
           >
             <Show when={status() !== NotificationStatus.ARCHIVED}>
