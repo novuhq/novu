756e3b60fd6a495305ed0457c81064b40827f8ad
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-09-19 13:42:35 +0100

    refactor(run-job): remove unused statelessPreferences field

--- apps/worker/src/app/workflow/usecases/run-job/run-job.usecase.ts
+++ apps/worker/src/app/workflow/usecases/run-job/run-job.usecase.ts
@@ -99,7 +99,6 @@ export class RunJob {
           events: job.digest?.events,
           job,
           tags: notification.tags || [],
-          statelessPreferences: notification.preferences,
         })
       );
 
