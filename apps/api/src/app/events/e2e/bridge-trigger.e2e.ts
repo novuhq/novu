74abbdd06f0c51e5e43ea6bf39da6ee32d3ec9ff
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-09-20 12:22:13 +0100

    test: Add test cases for workflow preference handling

--- apps/api/src/app/events/e2e/bridge-trigger.e2e.ts
+++ apps/api/src/app/events/e2e/bridge-trigger.e2e.ts
@@ -841,6 +841,83 @@ contexts.forEach((context: Context) => {
 
       expect(sentMessages.length).to.be.eq(0);
     });
+
+    it(`should deliver inApp message if workflow is disabled via workflow preferences and inApp is enabled [${context.name}]`, async () => {
+      process.env.IS_WORKFLOW_PREFERENCES_ENABLED = 'true';
+      const workflowId = `disabled-workflow-${`${context.name}-${uuidv4()}`}`;
+      const newWorkflow = workflow(
+        workflowId,
+        async ({ step }) => {
+          await step.inApp('send-in-app', () => ({ body: 'Hello there 1' }));
+        },
+        {
+          preferences: {
+            workflow: {
+              defaultValue: false,
+            },
+            channels: {
+              inApp: {
+                defaultValue: true,
+              },
+            },
+          },
+        }
+      );
+
+      await bridgeServer.start({ workflows: [newWorkflow] });
+
+      if (context.isStateful) {
+        await discoverAndSyncBridge(session, workflowsRepository, workflowId, bridgeServer);
+      }
+
+      await triggerEvent(session, workflowId, subscriber, {}, bridge);
+      await session.awaitRunningJobs();
+
+      const sentMessages = await messageRepository.find({
+        _environmentId: session.environment._id,
+        _subscriberId: subscriber._id,
+        templateIdentifier: workflowId,
+        channel: StepTypeEnum.IN_APP,
+      });
+
+      expect(sentMessages.length).to.be.eq(1);
+    });
+
+    it(`should NOT deliver inApp message if workflow is disabled via workflow preferences [${context.name}]`, async () => {
+      process.env.IS_WORKFLOW_PREFERENCES_ENABLED = 'true';
+      const workflowId = `disabled-workflow-${`${context.name}-${uuidv4()}`}`;
+      const newWorkflow = workflow(
+        workflowId,
+        async ({ step }) => {
+          await step.inApp('send-in-app', () => ({ body: 'Hello there 1' }));
+        },
+        {
+          preferences: {
+            workflow: {
+              defaultValue: false,
+            },
+          },
+        }
+      );
+
+      await bridgeServer.start({ workflows: [newWorkflow] });
+
+      if (context.isStateful) {
+        await discoverAndSyncBridge(session, workflowsRepository, workflowId, bridgeServer);
+      }
+
+      await triggerEvent(session, workflowId, subscriber, {}, bridge);
+      await session.awaitRunningJobs();
+
+      const sentMessages = await messageRepository.find({
+        _environmentId: session.environment._id,
+        _subscriberId: subscriber._id,
+        templateIdentifier: workflowId,
+        channel: StepTypeEnum.IN_APP,
+      });
+
+      expect(sentMessages.length).to.be.eq(0);
+    });
   });
 });
 
--- apps/worker/src/app/workflow/usecases/subscriber-job-bound/subscriber-job-bound.usecase.ts
+++ apps/worker/src/app/workflow/usecases/subscriber-job-bound/subscriber-job-bound.usecase.ts
@@ -7,6 +7,7 @@ import {
   EnvironmentRepository,
 } from '@novu/dal';
 import {
+  buildWorkflowChannelPreferences,
   ChannelTypeEnum,
   InAppProviderIdEnum,
   ISubscribersDefine,
@@ -136,7 +137,9 @@ export class SubscriberJobBound {
       userId,
       tenant,
       bridgeUrl: command.bridge?.url,
-      preferences: command.bridge?.workflow?.preferences,
+      ...(command.bridge?.workflow?.preferences && {
+        preferences: buildWorkflowChannelPreferences(command.bridge?.workflow?.preferences),
+      }),
     };
 
     if (actor) {
