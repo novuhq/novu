f198efc65b098e27ef22e4591388a1074ac3acf6
Paweł <pawel.tymczuk@gmail.com>
2024-09-23 16:15:34 +0200

    feat(dashboard): copy the legacy web app to the dashboard

---
+++ apps/dashboard/public/env-config.js
@@ -0,0 +1,11 @@
+window._env_ = Object.assign(
+  {
+    SKIP_PREFLIGHT_CHECK: 'true',
+    REACT_APP_ENVIRONMENT: 'dev',
+    REACT_APP_VERSION: '$npm_package_version',
+    IS_V2_ENABLED: 'true',
+    IS_V2_EXPERIENCE_ENABLED: 'true',
+  },
+  // Allow overrides of the above defaults
+  window._env_
+);
--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, './legacy') + '/[!.]*',
+          dest: './legacy',
+        },
+      ],
+    }),
+  ],
 });
