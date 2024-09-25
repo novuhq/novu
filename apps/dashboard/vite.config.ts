ff145e3a700f743f8576f0f9d9091599dba5cfb2
Paweł <pawel.tymczuk@gmail.com>
2024-09-25 18:08:57 +0200

    chore(dashboard): the pipelines and testing

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,8 +1,10 @@
 import path from 'path';
-import fs from 'fs';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { legacyHandler } from './legacy-handler';
 
 export default defineConfig({
   plugins: [
@@ -19,23 +21,7 @@ export default defineConfig({
       name: 'legacy-redirect',
       configureServer(server) {
         server.middlewares.use((req, res, next) => {
-          if (req?.url?.startsWith('/legacy/') && req.headers['accept']?.includes('text/html')) {
-            const legacyPath = path.resolve(__dirname, 'legacy', 'index.html');
-            const fileExists = fs.existsSync(legacyPath);
-
-            if (fileExists) {
-              // Serve the legacy index.html file
-              res.setHeader('Content-Type', 'text/html');
-              res.statusCode = 200;
-              fs.createReadStream(legacyPath).pipe(res);
-              return;
-            }
-
-            res.statusCode = 404;
-            res.end('Legacy index.html not found');
-            return;
-          }
-          next();
+          legacyHandler(__dirname, req, res, next);
         });
       },
     },
