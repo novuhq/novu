764e8fcc0282748ce3841b6bfbbf26ba1cf12bb4
George Desipris <73396808+desiprisg@users.noreply.github.com>
2024-09-25 10:56:30 +0300

    feat(dashboard): Add tailwind, shadcn-ui config and react router (#6559)

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
