92aae5beac772f8e19f0cea2bdabfdfe939ed4fd
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-09-25 16:30:32 +0100

    revert dashboard changes

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,27 +1,27 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
-import ErrorPage from '@/components/error-page';
-import Root from '@/routes/root';
-import { Workflows } from '@/routes/workflows';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import ErrorPage from "@/components/error-page";
+import Root from "@/routes/root";
+import { Workflows } from "@/routes/workflows";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: 'workflows',
+        path: "workflows",
         element: <Workflows />,
       },
     ],
   },
 ]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
