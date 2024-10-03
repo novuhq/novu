90b440f455dcc8bd61dfd049c51fcccddb5ba210
George Desipris <george.desipris.56@gmail.com>
2024-10-03 15:25:07 +0300

    fix(dashboard): Align css variables with design system

--- apps/dashboard/src/components/primitives/tag.tsx
+++ apps/dashboard/src/components/primitives/tag.tsx
@@ -8,7 +8,7 @@ const tagVariants = cva(
   {
     variants: {
       variant: {
-        default: 'border-transparent bg-primary/5 text-primary/50',
+        default: 'border-transparent bg-secondary-alpha-100 text-secondary-alpha-500',
         feature: 'border-transparent bg-feature/10 text-feature',
         information: 'border-transparent bg-information/10 text-information',
       },
