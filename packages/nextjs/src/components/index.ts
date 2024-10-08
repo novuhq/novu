8e4366f6d37e2929a4613b11be862ecd0108bebb
Biswajeet Das <biswajeetdas18@gmail.com>
2024-10-08 13:35:19 +0530

    fix: exports (need better way)

--- packages/nextjs/src/components/index.ts
+++ packages/nextjs/src/components/index.ts
@@ -1,8 +1,38 @@
 'use client';
 
-import * as NovuReact from '@novu/react';
+import {
+  BaseProps,
+  Bell,
+  BellProps,
+  BellRenderer,
+  DefaultInboxProps,
+  DefaultProps,
+  Inbox,
+  InboxContent,
+  InboxContentProps,
+  InboxProps,
+  InternalNovuProvider,
+  Notification,
+  NotificationProps,
+  Notifications,
+  NotificationsRenderer,
+  Preferences,
+  WithChildrenProps,
+} from '@novu/react';
 
 export * from './Inbox';
 
-const { Inbox, ...rest } = NovuReact;
-export { rest };
+export { Bell, InboxContent, InternalNovuProvider, Notification, Notifications, Preferences };
+
+export type {
+  BaseProps,
+  BellProps,
+  BellRenderer,
+  DefaultInboxProps,
+  DefaultProps,
+  InboxContentProps,
+  InboxProps,
+  NotificationProps,
+  NotificationsRenderer,
+  WithChildrenProps,
+};
