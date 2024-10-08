4e4eae87f6eaaae3bd641dbc71ff455c84bb2c10
Gosha <djabarov.george@gmail.com>
2024-10-08 18:18:35 +0300

    chore(api): remove redundant api route

--- apps/api/src/app/step-schemas/step-schemas.controller.ts
+++ apps/api/src/app/step-schemas/step-schemas.controller.ts
@@ -1,10 +1,9 @@
-import { ClassSerializerInterceptor, Controller, Get, Param, Query, UseInterceptors } from '@nestjs/common';
+import { ClassSerializerInterceptor, Controller, Get, Query, UseInterceptors } from '@nestjs/common';
 
 import { UserSessionData } from '@novu/shared';
 import { ExternalApiAccessible, UserSession } from '@novu/application-generic';
 import { StepType } from '@novu/framework';
 
-import { ApiOperation, ApiParam } from '@nestjs/swagger';
 import {
   GetStepTypeSchemaCommand,
   GetExistingStepSchemaCommand,
@@ -19,46 +18,7 @@ import { StepSchemaDto } from './dtos/step-schema.dto';
 export class StepSchemasController {
   constructor(private getStepDefaultSchemaUsecase: GetStepSchema) {}
 
-  @Get('/:stepType')
-  @ApiOperation({
-    summary: 'Get step schema',
-    description: 'Get the schema for a step type',
-  })
-  @ApiParam({
-    name: 'stepType',
-    type: String,
-    description: 'The type of step to get the schema for.',
-  })
-  @ExternalApiAccessible()
-  async getStepSchema(
-    @UserSession() user: UserSessionData,
-    @Param('stepType') stepType: StepType
-  ): Promise<StepSchemaDto> {
-    return await this.getStepDefaultSchemaUsecase.execute(
-      GetStepTypeSchemaCommand.create({
-        organizationId: user.organizationId,
-        environmentId: user.environmentId,
-        userId: user._id,
-        stepType,
-      })
-    );
-  }
-
   @Get()
-  @ApiOperation({
-    summary: 'Get step schema for a specific workflow and step',
-    description: 'Get the schema for a specific step in a workflow',
-  })
-  @ApiParam({
-    name: 'workflowId',
-    type: String,
-    description: 'The ID of the workflow.',
-  })
-  @ApiParam({
-    name: 'stepId',
-    type: String,
-    description: 'The ID of the step within the workflow.',
-  })
   @ExternalApiAccessible()
   async getWorkflowStepSchema(
     @UserSession() user: UserSessionData,
