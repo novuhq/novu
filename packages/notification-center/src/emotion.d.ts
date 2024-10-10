6f919892b72ba06968b680ea6703589b0493de0c
Sokratis Vidros <sokratis.vidros@gmail.com>
2024-10-10 17:50:38 +0300

    chore(root): Remove notification center packages
    @novu/notification-center is replaced by the <Inbox /> in @novu/react and @novu/js.

--- packages/notification-center/src/emotion.d.ts
+++
@@ -1,6 +0,0 @@
-import '@emotion/react';
-import type { MantineTheme } from '@mantine/core';
-
-declare module '@emotion/react' {
-  export interface Theme extends MantineTheme {}
-}
--- packages/notification-center/src/utils/pagination.ts
+++
@@ -1,5 +0,0 @@
-import type { IPaginatedResponse } from '@novu/shared';
-
-export const getNextPageParam = (lastPage: IPaginatedResponse) => {
-  return lastPage.hasMore ? lastPage.page + 1 : undefined;
-};
