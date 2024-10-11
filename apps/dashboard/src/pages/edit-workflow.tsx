a356ce5b196ed6f02979262a45f7f0305e3c8775
Paweł <pawel.tymczuk@gmail.com>
2024-10-11 17:06:03 +0200

    chore(dashboard): workflow editor rename file

--- apps/dashboard/src/pages/edit-workflow.tsx
+++ apps/dashboard/src/pages/edit-workflow.tsx
@@ -1,10 +1,10 @@
-import { EditWorkflow } from '@/components/edit-workflow';
+import { WorkflowEditor } from '@/components/workflow-editor';
 import { EditWorkflowLayout } from '@/components/edit-workflow-layout';
 
 export const EditWorkflowPage = () => {
   return (
     <EditWorkflowLayout headerStartItems={<h1 className="text-foreground-950">Edit Workflow</h1>}>
-      <EditWorkflow />
+      <WorkflowEditor />
     </EditWorkflowLayout>
   );
 };
