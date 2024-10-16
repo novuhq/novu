a17cfa65d288fac6f61a9c4af1b0dadede43cfd6
Biswajeet Das <biswajeetdas18@gmail.com>
2024-10-16 15:00:37 +0530

    feat: breadcrumbs

--- apps/dashboard/src/pages/edit-workflow.tsx
+++ apps/dashboard/src/pages/edit-workflow.tsx
@@ -1,10 +1,59 @@
-import { WorkflowEditor } from '@/components/workflow-editor';
 import { EditWorkflowLayout } from '@/components/edit-workflow-layout';
+import RouteFill from '@/components/icons/route-fill';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/primitives/breadcrumb';
+import { WorkflowEditor } from '@/components/workflow-editor';
+import { useEnvironment } from '@/context/environment/hooks';
+import { buildRoute, ROUTES } from '@/utils/routes';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 export const EditWorkflowPage = () => {
   return (
-    <EditWorkflowLayout headerStartItems={<h1 className="text-foreground-950">Edit Workflow</h1>}>
+    <EditWorkflowLayout headerStartItems={<StartItems />}>
       <WorkflowEditor />
     </EditWorkflowLayout>
+  );
+};
+
+const StartItems = () => {
+  const { currentEnvironment } = useEnvironment();
+  const { state } = useLocation();
+
+  const breadcrumbs = [
+    { label: currentEnvironment?.name, href: '/' },
+    {
+      label: 'Workflows',
+      href: buildRoute(ROUTES.WORKFLOWS, { environmentId: currentEnvironment?._id ?? '' }),
+    },
+  ];
+
+  return (
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          {breadcrumbs.map(({ label, href }) => (
+            <React.Fragment key={href}>
+              <BreadcrumbItem>
+                <BreadcrumbLink to={href}>{label}</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+            </React.Fragment>
+          ))}
+          <BreadcrumbItem>
+            <BreadcrumbPage>
+              <RouteFill />
+              {state?.workflowName}
+            </BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </>
   );
 };
