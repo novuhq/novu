13aa3f81c32126fdd407752386128a5011624661
Paweł Tymczuk <LetItRock@users.noreply.github.com>
2024-10-21 10:20:57 +0200

    feat(dashboard): workflow editor autosave (#6718)

--- apps/dashboard/src/components/workflow-editor/workflow-editor-provider.tsx
+++ apps/dashboard/src/components/workflow-editor/workflow-editor-provider.tsx
@@ -1,19 +1,21 @@
-import { ReactNode, useMemo, useCallback } from 'react';
+import { ReactNode, useMemo, useCallback, useRef, useLayoutEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useForm, useFieldArray } from 'react-hook-form';
 // eslint-disable-next-line
 // @ts-ignore
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
-import type { StepDto } from '@novu/shared';
+import { RiProgress1Line } from 'react-icons/ri';
 
 import { WorkflowEditorContext } from './workflow-editor-context';
 import { StepTypeEnum } from '@/utils/enums';
-import { useFetchWorkflow } from '@/hooks/use-fetch-workflow';
 import { Form } from '../primitives/form/form';
 import { buildRoute, ROUTES } from '@/utils/routes';
 import { useEnvironment } from '@/context/environment/hooks';
 import { formSchema } from './schema';
+import { useFetchWorkflow, useUpdateWorkflow, useFormAutoSave } from '@/hooks';
+import { Step } from '@/utils/types';
+import { smallToast } from '../primitives/sonner-helpers';
 
 const STEP_NAME_BY_TYPE: Record<StepTypeEnum, string> = {
   email: 'Email Step',
@@ -27,33 +29,75 @@ const STEP_NAME_BY_TYPE: Record<StepTypeEnum, string> = {
   custom: 'Custom Step',
 };
 
-const createStep = (type: StepTypeEnum): StepDto => ({
+const createStep = (type: StepTypeEnum): Step => ({
   name: STEP_NAME_BY_TYPE[type],
   type,
-  controlValues: {},
-  controls: {
-    schema: {},
-  },
 });
 
 export const WorkflowEditorProvider = ({ children }: { children: ReactNode }) => {
+  const changesSavedToastIdRef = useRef<string | number>();
   const { currentEnvironment } = useEnvironment();
   const { workflowId } = useParams<{ workflowId?: string }>();
   const navigate = useNavigate();
   const form = useForm<z.infer<typeof formSchema>>({ mode: 'onSubmit', resolver: zodResolver(formSchema) });
-  const { handleSubmit, reset } = form;
+  const { reset } = form;
   const steps = useFieldArray({
     control: form.control,
     name: 'steps',
   });
 
-  const { workflow: _workflow } = useFetchWorkflow({
+  const { workflow, error } = useFetchWorkflow({
     workflowId,
-    onSuccess: (data) => {
-      reset(data);
-    },
-    onError: () => {
+  });
+
+  useLayoutEffect(() => {
+    if (error) {
       navigate(buildRoute(ROUTES.WORKFLOWS, { environmentId: currentEnvironment?._id ?? '' }));
+    }
+
+    if (!workflow) {
+      return;
+    }
+
+    reset({ ...workflow, steps: workflow.steps.map((step) => ({ ...step })) });
+  }, [workflow, error, navigate, reset, currentEnvironment]);
+
+  const { updateWorkflow } = useUpdateWorkflow({
+    onSuccess: (data) => {
+      reset({ ...data, steps: data.steps.map((step) => ({ ...step })) });
+      if (changesSavedToastIdRef.current) {
+        return;
+      }
+
+      const id = smallToast({
+        children: (
+          <>
+            <RiProgress1Line className="size-6" />
+            <span className="text-sm">Saved</span>
+          </>
+        ),
+        options: {
+          position: 'bottom-left',
+          classNames: {
+            toast: 'ml-10',
+          },
+          onAutoClose: () => {
+            changesSavedToastIdRef.current = undefined;
+          },
+        },
+      });
+      changesSavedToastIdRef.current = id;
+    },
+  });
+
+  useFormAutoSave({
+    form,
+    onSubmit: async (data: z.infer<typeof formSchema>) => {
+      if (!workflow) {
+        return;
+      }
+
+      updateWorkflow({ id: workflow._id, workflow: { ...workflow, ...data } as any });
     },
   });
 
@@ -69,10 +113,6 @@ export const WorkflowEditorProvider = ({ children }: { children: ReactNode }) =>
     [steps]
   );
 
-  const onSubmit = async (_data: z.infer<typeof formSchema>) => {
-    // TODO: Implement submit logic
-  };
-
   const value = useMemo(
     () => ({
       addStep,
@@ -83,9 +123,7 @@ export const WorkflowEditorProvider = ({ children }: { children: ReactNode }) =>
   return (
     <WorkflowEditorContext.Provider value={value}>
       <Form {...form}>
-        <form onSubmit={handleSubmit(onSubmit)} className="h-full">
-          {children}
-        </form>
+        <form className="h-full">{children}</form>
       </Form>
     </WorkflowEditorContext.Provider>
   );
