1126866b113b63719f520478c2fae9543fa96fb4
GalT <39020298+tatarco@users.noreply.github.com>
2024-10-22 10:06:13 +0200

    fix(api): fix commandException

--- apps/api/src/app/shared/commands/commandValidationException.ts
+++ apps/api/src/app/shared/commands/commandValidationException.ts
@@ -1,7 +1,7 @@
 import { BadRequestException } from '@nestjs/common';
 
 export class CommandValidationException extends BadRequestException {
-  constructor(private mappedErrors: string[]) {
+  constructor(public mappedErrors: string[]) {
     super({ message: 'Validation failed', errors: mappedErrors });
   }
 }
