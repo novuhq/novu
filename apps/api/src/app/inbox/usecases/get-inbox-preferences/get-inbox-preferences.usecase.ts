5d99857233b5f9572efeabda37ffa03bbbecb6ea
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-10-24 17:20:48 +0200

    refactor(inbox): simplify preference retrieval logic

--- apps/api/src/app/inbox/usecases/get-inbox-preferences/get-inbox-preferences.usecase.ts
+++ apps/api/src/app/inbox/usecases/get-inbox-preferences/get-inbox-preferences.usecase.ts
@@ -1,12 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import {
   AnalyticsService,
+  GetSubscriberPreference,
+  GetSubscriberPreferenceCommand,
   GetSubscriberGlobalPreference,
   GetSubscriberGlobalPreferenceCommand,
-  GetSubscriberTemplatePreference,
-  GetSubscriberTemplatePreferenceCommand,
 } from '@novu/application-generic';
-import { NotificationTemplateRepository, SubscriberRepository } from '@novu/dal';
 import { PreferenceLevelEnum } from '@novu/shared';
 import { AnalyticsEventsEnum } from '../../utils';
 import { InboxPreference } from '../../utils/types';
@@ -15,20 +14,12 @@ import { GetInboxPreferencesCommand } from './get-inbox-preferences.command';
 @Injectable()
 export class GetInboxPreferences {
   constructor(
-    private subscriberRepository: SubscriberRepository,
-    private notificationTemplateRepository: NotificationTemplateRepository,
-    private getSubscriberTemplatePreferenceUsecase: GetSubscriberTemplatePreference,
     private getSubscriberGlobalPreference: GetSubscriberGlobalPreference,
-    private analyticsService: AnalyticsService
+    private analyticsService: AnalyticsService,
+    private getSubscriberPreference: GetSubscriberPreference
   ) {}
 
   async execute(command: GetInboxPreferencesCommand): Promise<InboxPreference[]> {
-    const subscriber = await this.subscriberRepository.findBySubscriberId(command.environmentId, command.subscriberId);
-
-    if (!subscriber) {
-      throw new NotFoundException(`Subscriber with id: ${command.subscriberId} not found`);
-    }
-
     const globalPreference = await this.getSubscriberGlobalPreference.execute(
       GetSubscriberGlobalPreferenceCommand.create({
         organizationId: command.organizationId,
@@ -42,57 +33,33 @@ export class GetInboxPreferences {
       ...globalPreference.preference,
     };
 
-    const workflowList =
-      (await this.notificationTemplateRepository.filterActive({
-        organizationId: command.organizationId,
+    const subscriberWorkflowPreferences = await this.getSubscriberPreference.execute(
+      GetSubscriberPreferenceCommand.create({
         environmentId: command.environmentId,
-        tags: command.tags,
-        critical: false,
-      })) || [];
+        subscriberId: command.subscriberId,
+        organizationId: command.organizationId,
+      })
+    );
+    const workflowPreferences = subscriberWorkflowPreferences.map((subscriberWorkflowPreference) => {
+      return {
+        ...subscriberWorkflowPreference.preference,
+        level: PreferenceLevelEnum.TEMPLATE,
+        workflow: {
+          id: subscriberWorkflowPreference.template._id,
+          identifier: subscriberWorkflowPreference.template.triggers[0].identifier,
+          name: subscriberWorkflowPreference.template.name,
+          critical: subscriberWorkflowPreference.template.critical,
+          tags: subscriberWorkflowPreference.template.tags,
+        },
+      } satisfies InboxPreference;
+    });
 
     this.analyticsService.mixpanelTrack(AnalyticsEventsEnum.FETCH_PREFERENCES, '', {
       _organization: command.organizationId,
       subscriberId: command.subscriberId,
-      workflowSize: workflowList.length,
+      workflowSize: workflowPreferences.length,
     });
 
-    const workflowPreferences = await Promise.all(
-      workflowList.map(async (workflow) => {
-        const workflowPreference = await this.getSubscriberTemplatePreferenceUsecase.execute(
-          GetSubscriberTemplatePreferenceCommand.create({
-            organizationId: command.organizationId,
-            subscriberId: command.subscriberId,
-            environmentId: command.environmentId,
-            template: workflow,
-            subscriber,
-          })
-        );
-
-        return {
-          ...workflowPreference.preference,
-          level: PreferenceLevelEnum.TEMPLATE,
-          workflow: {
-            id: workflow._id,
-            identifier: workflow.triggers[0].identifier,
-            name: workflow.name,
-            /*
-             * V1 Preferences define `critial` flag on the workflow level.
-             * V2 Preferences define `critical` flag on the template returned via Preferences.
-             * This pattern safely returns false when:
-             * 1. Workflow V1 with no critical flag set
-             * 2. Workflow V2 with no critical flag set
-             * 3. Workflow V1 with critical flag set to false
-             * 4. Workflow V2 with critical flag set to false
-             */
-            critical: workflow.critical || workflowPreference.template.critical || false,
-            tags: workflow.tags,
-          },
-        } satisfies InboxPreference;
-      })
-    );
-
-    const nonCriticalWorkflows = workflowPreferences.filter((preference) => preference.workflow.critical === false);
-
-    return [updatedGlobalPreference, ...nonCriticalWorkflows];
+    return [updatedGlobalPreference, ...workflowPreferences];
   }
 }
