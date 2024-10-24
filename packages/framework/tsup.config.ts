aba2de948f5e10876848fa9ec6208e61919652a2
Sokratis Vidros <sokratis.vidros@gmail.com>
2024-10-24 18:48:14 +0300

    fix(framework): Try with an unminified build

--- packages/framework/tsup.config.ts
+++ packages/framework/tsup.config.ts
@@ -1,16 +1,10 @@
 import { type Options, defineConfig } from 'tsup';
-import { type SupportedFrameworkName } from './src/internal';
-
-const frameworks: SupportedFrameworkName[] = ['h3', 'express', 'next', 'nuxt', 'sveltekit', 'remix', 'lambda', 'nest'];
 
 const baseConfig: Options = {
-  entry: ['src/index.ts', 'src/internal/index.ts', ...frameworks.map((framework) => `src/servers/${framework}.ts`)],
-  sourcemap: false,
+  entry: ['./src/**/*.{ts,tsx,js,jsx}', '!./src/**/*.test.{ts,tsx}'],
   clean: true,
-  treeshake: true,
   dts: true,
-  bundle: false,
-  external: ['json-schema-faker'],
+  shims: true,
 };
 
 export default defineConfig([
