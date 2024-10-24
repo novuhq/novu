f8fcc82f63ac0f44271c34a2db9023a177cceafd
Biswajeet Das <biswajeetdas18@gmail.com>
2024-10-24 13:29:21 +0530

    feat(dashboard): Nv 4479 workflow editor configure workflow drawer (#6729)

--- apps/dashboard/src/components/workflow-editor/schema.ts
+++ apps/dashboard/src/components/workflow-editor/schema.ts
@@ -1,6 +1,19 @@
 import * as z from 'zod';
 import { StepTypeEnum } from '@/utils/enums';
 
+const enabledSchema = z.object({
+  enabled: z.boolean(),
+});
+
+// Reusable schema for channels
+const channelsSchema = z.object({
+  in_app: enabledSchema,
+  sms: enabledSchema,
+  email: enabledSchema,
+  push: enabledSchema,
+  chat: enabledSchema,
+});
+
 export const formSchema = z.object({
   name: z.string(),
   workflowId: z.string(),
@@ -16,4 +29,17 @@ export const formSchema = z.object({
       })
       .passthrough()
   ),
+  preferences: z.object({
+    /**
+     * TODO: Add user schema
+     */
+    user: z.any().nullable(),
+    default: z.object({
+      all: z.object({
+        enabled: z.boolean(),
+        readOnly: z.boolean(),
+      }),
+      channels: channelsSchema,
+    }),
+  }),
 });
