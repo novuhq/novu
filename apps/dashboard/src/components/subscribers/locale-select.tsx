7236035e571ef8045733cfb01d8368c12eb80999
Biswajeet Das <biswajeetdas18@gmail.com>
2025-02-07 17:01:37 +0530

    feat(locale-select): implement virtualized rendering for locale options and update flag display

--- apps/dashboard/src/components/subscribers/locale-select.tsx
+++ apps/dashboard/src/components/subscribers/locale-select.tsx
@@ -1,8 +1,11 @@
 import { locales } from '@/utils/locales';
 import { RiEarthLine } from 'react-icons/ri';
-import { CountryFlags } from '../icons/country-flags';
+import flags from 'react-phone-number-input/flags';
+import { type Country } from 'react-phone-number-input';
+import { useVirtualizer } from '@tanstack/react-virtual';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../primitives/select';
 import TruncatedText from '../truncated-text';
+import { useMemo, useRef } from 'react';
 
 export function LocaleSelect({
   value,
@@ -20,6 +23,24 @@ export function LocaleSelect({
   required?: boolean;
   onValueChange: (val: string) => void;
 }) {
+  const currentCountryCode = value?.split('_')?.[1] as Country;
+  const CurrentFlag = currentCountryCode ? flags[currentCountryCode] : RiEarthLine;
+  const parentRef = useRef(null);
+
+  // The virtualizer
+  const rowVirtualizer = useVirtualizer({
+    count: 500,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 40,
+    overscan: 5,
+  });
+
+  const virtualItems = rowVirtualizer.getVirtualItems();
+
+  console.log({
+    virtualItems,
+    parentRef: parentRef.current,
+  });
   return (
     <Select
       value={value}
@@ -43,29 +64,51 @@ export function LocaleSelect({
           <div className="flex w-full items-center gap-2">
             {value && (
               <>
-                <div>
-                  <CountryFlags name={value.split('_')?.[1]} className="size-4" />
-                </div>
+                <div>{CurrentFlag && <CurrentFlag className="size-4" title={currentCountryCode} />}</div>
                 <TruncatedText className="text-sm">{value}</TruncatedText>
               </>
             )}
           </div>
         </SelectValue>
       </SelectTrigger>
-      <SelectContent className="p-1">
-        {locales.map((item) => (
-          <SelectItem key={item.langIso} value={item.langIso} className="px-2">
-            <div className="flex w-full items-center gap-2">
-              <div>
-                <CountryFlags name={item.alpha2} className="size-4" />
-              </div>
-              <TruncatedText className="text-sm">
-                {item.langIso} - {item.langName}
-              </TruncatedText>
+      <SelectContent className="h-64 overflow-auto p-1" ref={parentRef}>
+        <div
+          style={{
+            height: `${rowVirtualizer.getTotalSize()}px`,
+            width: '100%',
+            position: 'relative',
+          }}
+        >
+          {virtualItems.map((item) => (
+            <div>
+              <FlagItem key={item.key} index={item.index} />
             </div>
-          </SelectItem>
-        ))}
+          ))}
+        </div>
       </SelectContent>
     </Select>
   );
 }
+
+const FlagItem = ({ index }: { index: number }) => {
+  const countryCode = locales[index].alpha2;
+  const languageName = locales[index].langName;
+  const value = locales[index].langIso;
+  const CurrentFlag = countryCode ? flags[countryCode as Country] : RiEarthLine;
+  console.log({
+    countryCode,
+    languageName,
+    value,
+    CurrentFlag,
+  });
+  return (
+    <SelectItem key={value} value={value} className="px-2">
+      <div className="flex w-full items-center gap-2">
+        <div>{CurrentFlag && <CurrentFlag className="size-4" title={countryCode} />}</div>
+        <TruncatedText className="text-sm">
+          {value} - {languageName}
+        </TruncatedText>
+      </div>
+    </SelectItem>
+  );
+};
