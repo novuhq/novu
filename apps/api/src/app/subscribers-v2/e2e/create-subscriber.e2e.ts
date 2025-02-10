e78ff3d69a64cb784acb41ad94c45a648b5aa073
Biswajeet Das <biswajeetdas18@gmail.com>
2025-02-10 23:24:32 +0530

    test(subscriber): simplify assertions in create subscriber e2e test

--- apps/api/src/app/subscribers-v2/e2e/create-subscriber.e2e.ts
+++ apps/api/src/app/subscribers-v2/e2e/create-subscriber.e2e.ts
@@ -51,8 +51,8 @@ describe('Create Subscriber - /subscribers (POST) #novu-v2', () => {
     const { error, successfulBody } = await expectSdkExceptionGeneric(() =>
       novuClient.subscribers.create(payload, payload.subscriberId)
     );
-    expect(successfulBody, JSON.stringify(successfulBody)).to.be.undefined;
-    expect(error, JSON.stringify(successfulBody)).to.be.ok;
+    expect(successfulBody).to.be.undefined;
+    expect(error).to.be.ok;
     expect(error?.statusCode).to.equal(409);
   });
 });
