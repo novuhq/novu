8e112a63741be5f52a1799a1c6ef3c990448fda3
GalT <39020298+tatarco@users.noreply.github.com>
2025-02-26 20:50:34 +0100

    fix the button on trial

--- apps/dashboard/src/components/billing/plan-action-button.tsx
+++ apps/dashboard/src/components/billing/plan-action-button.tsx
@@ -64,7 +64,7 @@ export function PlanActionButton({
       {}
     );
 
-    if (indexRequested > indexActive) {
+    if (indexRequested >= indexActive) {
       return <> {'Upgrade plan'}</>;
     }
 
