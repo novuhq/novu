176353bc289c56ba0896ab325850e01c4e4cf7b3
Sokratis Vidros <sokratis.vidros@gmail.com>
2025-03-11 21:44:16 +0100

    fix(nextjs): Make next package work both with app and page router

--- packages/nextjs/src/components/Inbox.tsx
+++ packages/nextjs/src/components/Inbox.tsx
@@ -2,10 +2,21 @@
 
 import React from 'react';
 import { InboxProps, Inbox as RInbox } from '@novu/react';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/compat/router';
 
 export const Inbox = React.memo((props: InboxProps) => {
-  const router = useRouter();
+  let router = useRouter();
 
-  return <RInbox routerPush={router.push} {...props} />;
+  /*
+   * The compat version of useRouter returns null instead of throwing an error
+   * when used inside app router instead of pages router
+   * we use it to detect if the component is used inside pages or app router
+   * so we can use the correct algorithm to get the path
+   */
+
+  if (!router) {
+    router = require('next/navigation').useRouter();
+  }
+
+  return <RInbox routerPush={router!.push} {...props} />;
 });
---
+++ playground/nextjs/src/app/app-router/inbox/page.tsx
@@ -0,0 +1,7 @@
+// 'use client';
+
+// import { NovuProvider, NotificationCenter } from '@novu/nextjs';
+
+export default function InboxPage() {
+  return <h1>Hello from Inbox page</h1>;
+}
