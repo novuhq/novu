f27cdd58a38f214e4588f86a5c48fca2f3ad0726
Adam Chmara <adam.chmara1@gmail.com>
2025-03-12 10:25:40 +0100

    fix: use img url for html block preview

--- apps/dashboard/src/components/workflow-editor/steps/email/extensions/html.tsx
+++ apps/dashboard/src/components/workflow-editor/steps/email/extensions/html.tsx
@@ -6,22 +6,7 @@ export const htmlCodeBlock: BlockItem = {
   description: 'Render components from HTML',
   searchTerms: ['html', 'code', 'custom'],
   icon: <CodeXmlIcon className="mly-h-4 mly-w-4" />,
-  preview: () => (
-    <div>
-      <figure className="mly-relative mly-aspect-[2.5] mly-w-full mly-overflow-hidden mly-rounded-md mly-border mly-border-gray-200">
-        <img
-          src="/images/email-editor/html-block-thumb.png"
-          alt="New Dashboard Preview"
-          className="mly-absolute mly-w-full"
-          style={{
-            transform: 'scale(2)',
-            transformOrigin: '0 0',
-          }}
-        />
-      </figure>
-      <p className="mly-mt-2 mly-px-0.5 mly-text-gray-500">Render components from HTML</p>
-    </div>
-  ),
+  preview: '/images/email-editor/html-block-preview.png',
   command: ({ editor, range }) => {
     editor.chain().focus().deleteRange(range).setHtmlCodeBlock({ language: 'html' }).run();
   },
