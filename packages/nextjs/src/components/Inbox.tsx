626ace592f0db6a842e74d0d51405ac8ff513245
Paweł <pawel.tymczuk@gmail.com>
2025-03-12 14:01:49 +0100

    Revert "chore(nextjs): conditionally import different Inbox implementations based on router used"
    This reverts commit 38e7f0fc659d3d6f88fc2b0a744695f0497b02e2.

--- packages/nextjs/src/components/Inbox.tsx
+++ packages/nextjs/src/components/Inbox.tsx
@@ -1,13 +1,22 @@
 'use client';
 
 import React from 'react';
-import { InboxProps } from '@novu/react';
+import { InboxProps, Inbox as RInbox } from '@novu/react';
 import { useRouter } from 'next/compat/router';
-import { AppRouterInbox } from './app-router/AppRouterInbox';
-import { PagesRouterInbox } from './pages-router/PagesRouterInbox';
 
 export const Inbox = React.memo((props: InboxProps) => {
-  const router = useRouter();
+  let router = useRouter();
 
-  return router ? <PagesRouterInbox {...props} /> : <AppRouterInbox {...props} />;
+  /*
+   * The compat version of useRouter returns null instead of throwing an error
+   * when used inside app router instead of pages router
+   * we use it to detect if the component is used inside pages or app router
+   * so we can use the correct algorithm to get the path
+   */
+
+  if (!router) {
+    router = require('next/navigation').useRouter();
+  }
+
+  return <RInbox routerPush={router!.push} {...props} />;
 });
