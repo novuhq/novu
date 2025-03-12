8def01b94eaca25e09429aab7013c19d7963d1eb
Sokratis Vidros <sokratis.vidros@gmail.com>
2025-03-12 19:41:26 +0200

    Add more type safety to router resolution and an explicit error when no router is available

--- packages/nextjs/src/components/Inbox.tsx
+++ packages/nextjs/src/components/Inbox.tsx
@@ -2,11 +2,11 @@
 
 import React from 'react';
 import { InboxProps, Inbox as RInbox } from '@novu/react';
-import { useRouter } from 'next/compat/router';
+import { useRouter as usePageRouter } from 'next/compat/router';
+import type { useRouter as useAppRouter } from 'next/navigation';
 
 export const Inbox = React.memo((props: InboxProps) => {
-  let router = useRouter();
-
+  let router: ReturnType<typeof usePageRouter> | ReturnType<typeof useAppRouter> = usePageRouter();
   /*
    * The compat version of useRouter returns null instead of throwing an error
    * when used inside app router instead of pages router
@@ -15,8 +15,16 @@ export const Inbox = React.memo((props: InboxProps) => {
    */
 
   if (!router) {
-    router = require('next/navigation').useRouter();
+    // eslint-disable-next-line global-require
+    router = (require('next/navigation') as typeof import('next/navigation')).useRouter();
   }
 
-  return <RInbox routerPush={router!.push} {...props} />;
+  if (!router) {
+    throw new Error(
+      `[@novu/nextjs]: Failed to resolve Next.js Page or App Router. Pleace contact support@novu.co.
+      Meanwhile, try using @novu/react instead.`
+    );
+  }
+
+  return <RInbox routerPush={router.push} {...props} />;
 });
