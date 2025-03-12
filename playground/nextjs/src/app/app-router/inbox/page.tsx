18acc35bb9427f5f7b4d7c28c8290f11c43b3cac
Sokratis Vidros <sokratis.vidros@gmail.com>
2025-03-12 08:10:29 +0100

    WIP

--- playground/nextjs/src/app/app-router/inbox/page.tsx
+++ playground/nextjs/src/app/app-router/inbox/page.tsx
@@ -1,7 +1,11 @@
-// 'use client';
-
-// import { NovuProvider, NotificationCenter } from '@novu/nextjs';
+import { Inbox } from '@novu/nextjs';
+import { novuConfig } from '@/utils/config';
 
 export default function InboxPage() {
-  return <h1>Hello from Inbox page</h1>;
+  return (
+    <>
+      <h1>Hello from Inbox page</h1>;
+      <Inbox {...novuConfig} />
+    </>
+  );
 }
