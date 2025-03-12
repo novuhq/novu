14643255ea4e66d7d50f3850fd5e621d5334f3a7
Paweł <pawel.tymczuk@gmail.com>
2025-03-12 10:56:07 +0100

    feat(react,nextjs): inbox support nextjs app router

---
+++ packages/nextjs/src/server/index.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-restricted-imports */
+
+'use client';
+
+export type {
+  BaseProps,
+  DefaultInboxProps,
+  DefaultProps,
+  NoRendererProps,
+  NotificationRendererProps,
+  SubjectBodyRendererProps,
+  WithChildrenProps,
+  BellRenderer,
+  BodyRenderer,
+  NotificationsRenderer,
+  Notification,
+} from '@novu/react';
+
+export {
+  Inbox,
+  InboxContent,
+  Notifications,
+  Preferences,
+  Bell,
+  NovuProvider,
+  useNovu,
+  useCounts,
+  useNotifications,
+  usePreferences,
+} from '@novu/react/server';
