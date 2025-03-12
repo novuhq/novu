c585030b86be056662ed1cf33979c5077a4f920e
GalT <39020298+tatarco@users.noreply.github.com>
2025-03-12 14:36:32 +0100

    add timeout extension

--- apps/api/src/app/events/e2e/bridge-trigger.e2e.ts
+++ apps/api/src/app/events/e2e/bridge-trigger.e2e.ts
@@ -58,7 +58,8 @@ contexts.forEach((context: Context) => {
       await bridgeServer.stop();
     });
 
-    it(`should trigger the bridge workflow with sync [${context.name}]`, async () => {
+    it(`should trigger the bridge workflow with sync [${context.name}]`, async function () {
+      this.timeout(20000);
       const workflowId = `hello-world-${`${context.name}`}`;
       const newWorkflow = workflow(
         workflowId,
