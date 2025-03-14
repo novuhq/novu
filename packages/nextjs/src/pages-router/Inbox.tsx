765dbae420b45706b9bd2665d30095e22452e24e
Paweł <pawel.tymczuk@gmail.com>
2025-03-14 18:07:30 +0100

    chore(nextjs): revert inbox code for pages router

--- packages/nextjs/src/pages-router/Inbox.tsx
+++ packages/nextjs/src/pages-router/Inbox.tsx
@@ -1,17 +1,10 @@
 'use client';
 
 import { InboxProps, Inbox as RInbox } from '@novu/react';
-import { useRouter } from 'next/compat/router';
+import { useRouter } from 'next/router';
 
 export function Inbox(props: InboxProps) {
   const router = useRouter();
-
-  if (!router) {
-    throw new Error(
-      `[@novu/nextjs]: Failed to resolve Next.js Page Router. Please contact support@novu.co.
-      Meanwhile, try using @novu/react instead.`
-    );
-  }
 
   return <RInbox routerPush={router.push} {...props} />;
 }
