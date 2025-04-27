16523252336d57aac691a3378b49afe29c93e9ef
George <djabarov.george@gmail.com>
2025-04-27 11:23:40 +0300

    fix(dashboard): validate Clerk Publishable Key only when not self-hosted

--- apps/dashboard/src/config/index.ts
+++ apps/dashboard/src/config/index.ts
@@ -12,10 +12,6 @@ export const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY 
 
 export const APP_ID = import.meta.env.VITE_NOVU_APP_ID || '';
 
-if (!CLERK_PUBLISHABLE_KEY) {
-  throw new Error('Missing Clerk Publishable Key');
-}
-
 export const API_HOSTNAME = import.meta.env.VITE_API_HOSTNAME;
 
 export const IS_EU = API_HOSTNAME === 'https://eu.api.novu.co';
@@ -35,3 +31,7 @@ export const PLAIN_SUPPORT_CHAT_APP_ID = import.meta.env.VITE_PLAIN_SUPPORT_CHAT
 export const ONBOARDING_DEMO_WORKFLOW_ID = 'onboarding-demo-workflow';
 
 export const IS_SELF_HOSTED = import.meta.env.VITE_SELF_HOSTED;
+
+if (!IS_SELF_HOSTED && !CLERK_PUBLISHABLE_KEY) {
+  throw new Error('Missing Clerk Publishable Key');
+}
