efefe2db1a1aeb5f6bddbfbee9bd958437a4f6dd
George <djabarov.george@gmail.com>
2025-04-28 18:45:55 +0300

    refactor(auth): typo

--- apps/api/src/app/auth/services/system-organization.service.ts
+++ apps/api/src/app/auth/services/system-organization.service.ts
@@ -12,7 +12,7 @@ import { SwitchOrganizationCommand } from '../usecases/switch-organization/switc
 @Injectable()
 export class CommunityEditionService implements OnModuleInit {
   private readonly E11000_DUPLICATE_KEY_ERROR_CODE = 'E11000';
-  private readonly COMMUNITY_EDITION_NAME = 'Community Edition 3';
+  private readonly COMMUNITY_EDITION_NAME = 'Community Edition';
   private readonly COMMUNITY_EDITION_USER_EMAIL = 'no-reply@community.com';
 
   constructor(
