8aeab1aa40dcafd6a1a0a997c1807edbaf7c8c8b
George <djabarov.george@gmail.com>
2025-05-14 10:55:20 +0300

    fix(footer): update text color for keyless mode link in footer

--- packages/js/src/ui/components/elements/Footer.tsx
+++ packages/js/src/ui/components/elements/Footer.tsx
@@ -74,7 +74,7 @@ export const Footer = () => {
           </Show>
         </div>
         <Show when={isKeyless()}>
-          <div class="nt-z-10 nt-flex nt-items-center nt-gap-1 nt-text-xs">
+          <div class="nt-z-10 nt-flex nt-items-center nt-gap-1 nt-text-xs nt-text-secondary-foreground">
             <a
               href="https://go.novu.co/keyless?utm_campaign=footer-get_api_key"
               class="nt-underline nt-flex nt-items-center nt-gap-0.5"
