e641babbe1b16f8d7e3082eb37d4268bc1d4a6e4
George <djabarov.george@gmail.com>
2025-05-17 18:53:09 +0300

    refactor(api): replace parseLiquid with liquid engine methods in EmailOutputRendererUsecase and remove unused liquid helper

--- apps/api/src/app/shared/helpers/liquid.ts
+++
@@ -1,8 +0,0 @@
-import { createLiquidEngine } from '@novu/framework/internal';
-
-export const parseLiquid = async (value: string, variables: object): Promise<string> => {
-  const client = createLiquidEngine();
-  const template = client.parse(value);
-
-  return await client.render(template, variables);
-};
