6c5e4e38c276158053ad2147994c8f82fbc716b9
Dima Grossman <dima@grossman.io>
2025-05-18 10:28:36 +0300

    fix: auth

--- apps/api/src/app/webhooks/webhooks.controller.ts
+++ apps/api/src/app/webhooks/webhooks.controller.ts
@@ -1,19 +1,19 @@
 import { ClassSerializerInterceptor, Controller, Get, Post, UseGuards, UseInterceptors } from '@nestjs/common';
 import { ApiExcludeController, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { UserSession } from '@novu/application-generic';
-import { ProductFeatureKeyEnum, UserSessionData } from '@novu/shared';
+import { RequirePermissions, UserSession } from '@novu/application-generic';
+import { PermissionsEnum, ProductFeatureKeyEnum, UserSessionData } from '@novu/shared';
 import { GetWebhookPortalTokenUsecase } from './usecases/get-webhook-portal-token/get-webhook-portal-token.usecase';
 import { GetWebhookPortalTokenCommand } from './usecases/get-webhook-portal-token/get-webhook-portal-token.command';
 import { GetWebhookPortalTokenResponseDto } from './dtos/get-webhook-portal-token-response.dto';
-import { UserAuthentication } from '../shared/framework/swagger/api.key.security';
 import { CreateWebhookPortalUsecase } from './usecases/create-webhook-portal-token/create-webhook-portal.usecase';
 import { CreateWebhookPortalCommand } from './usecases/create-webhook-portal-token/create-webhook-portal.command';
 import { CreateWebhookPortalResponseDto } from './dtos/create-webhook-portal-token-response.dto';
 import { ProductFeature } from '../shared/decorators/product-feature.decorator';
+import { RequireAuthentication } from '../auth/framework/auth.decorator';
 
 @Controller({ path: `/webhooks`, version: '2' })
 @UseInterceptors(ClassSerializerInterceptor)
-@UserAuthentication()
+@RequireAuthentication()
 export class WebhooksController {
   constructor(
     private getWebhookPortalTokenUsecase: GetWebhookPortalTokenUsecase,
@@ -22,6 +22,7 @@ export class WebhooksController {
 
   @Get('/portal/token')
   @ProductFeature(ProductFeatureKeyEnum.WEBHOOKS)
+  @RequirePermissions(PermissionsEnum.WEBHOOK_CREATE)
   @ApiOperation({
     summary: 'Get Webhook Portal Access Token',
     description:
@@ -39,6 +40,7 @@ export class WebhooksController {
 
   @Post('/portal/token')
   @ProductFeature(ProductFeatureKeyEnum.WEBHOOKS)
+  @RequirePermissions(PermissionsEnum.WEBHOOK_CREATE)
   @ApiOperation({
     summary: 'Create Webhook Portal Access Token',
     description: 'Creates a token for accessing the webhook portal for the current environment.',
