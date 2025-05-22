21f9cdb8710162de12f884f2362b2a10514f70f5
Dima Grossman <dima@grossman.io>
2025-05-22 10:51:17 +0300

    f

--- apps/api/e2e/setup.ts
+++ apps/api/e2e/setup.ts
@@ -4,62 +4,9 @@ import chai from 'chai';
 import { Connection } from 'mongoose';
 import { DalService } from '@novu/dal';
 import { bootstrap } from '../src/bootstrap';
-import AWS from 'aws-sdk';
 
 let connection: Connection;
 const dalService = new DalService();
-
-setupS3Mock();
-
-function setupS3Mock() {
-  try {
-    const s3 = new AWS.S3({
-      endpoint: process.env.S3_LOCAL_STACK,
-      accessKeyId: 'test',
-      secretAccessKey: 'test',
-      s3ForcePathStyle: true,
-    });
-    
-    s3.config.credentials = { accessKeyId: 'test', secretAccessKey: 'test' };
-    
-  } catch (error) {
-    console.log('LocalStack S3 not available at startup, using mock implementation');
-    mockS3Service();
-  }
-}
-
-function mockS3Service() {
-  const mockS3 = {
-    putObject: sinon.stub().returns({
-      promise: sinon.stub().resolves({}),
-    }),
-    getObject: sinon.stub().returns({
-      promise: sinon.stub().resolves({
-        Body: Buffer.from('mock-data'),
-      }),
-    }),
-    listObjects: sinon.stub().returns({
-      promise: sinon.stub().resolves({
-        Contents: [],
-      }),
-    }),
-    listBuckets: sinon.stub().returns({
-      promise: sinon.stub().resolves({
-        Buckets: [],
-      }),
-    }),
-    createBucket: sinon.stub().returns({
-      promise: sinon.stub().resolves({}),
-    }),
-  };
-  
-  const originalS3 = AWS.S3;
-  AWS.S3 = function() {
-    return mockS3;
-  } as any;
-  
-  AWS.S3.prototype = originalS3.prototype;
-}
 
 async function getConnection() {
   if (!connection) {
@@ -84,22 +31,7 @@ before(async () => {
    * disable truncating for better error messages - https://www.chaijs.com/guide/styles/#configtruncatethreshold
    */
   chai.config.truncateThreshold = 0;
-  
-  try {
-    const s3 = new AWS.S3({
-      endpoint: process.env.S3_LOCAL_STACK,
-      accessKeyId: 'test',
-      secretAccessKey: 'test',
-      s3ForcePathStyle: true,
-    });
-    
-    await s3.listBuckets().promise();
-    console.log('LocalStack S3 is available');
-  } catch (error) {
-    console.log('LocalStack S3 not available in before hook, using mock implementation');
-    mockS3Service();
-  }
-  
+
   await dropDatabase();
   await testServer.create((await bootstrap()).app);
 });
