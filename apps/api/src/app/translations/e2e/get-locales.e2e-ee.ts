9c57164f0534ed80934443d84972634c2ef952c0
Dima Grossman <dima@grossman.io>
2025-05-26 13:09:21 +0300

    feat: locale translations endpoints

--- apps/api/src/app/translations/e2e/get-locales.e2e-ee.ts
+++ apps/api/src/app/translations/e2e/get-locales.e2e-ee.ts
@@ -1,7 +1,7 @@
 import { UserSession } from '@novu/testing';
 import { expect } from 'chai';
 
-describe('get locales - /translations/locales (GET) #novu-v0', async () => {
+describe('get locales - /translations/locales (GET) #novu-v2', async () => {
   let session: UserSession;
 
   before(async () => {
