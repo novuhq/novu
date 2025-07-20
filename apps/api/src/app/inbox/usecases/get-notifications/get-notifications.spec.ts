2c0276de42f81f95bab12c8d5a6e6f732d87ae40
Dima Grossman <dima@grossman.io>
2025-07-20 11:40:07 +0300

    Update get-notifications.spec.ts

--- apps/api/src/app/inbox/usecases/get-notifications/get-notifications.spec.ts
+++ apps/api/src/app/inbox/usecases/get-notifications/get-notifications.spec.ts
@@ -131,6 +131,7 @@ describe('GetNotifications', () => {
       data: command.data,
       archived: command.archived,
       snoozed: command.snoozed,
+      seen: command.seen,
     });
     expect(result.hasMore).to.be.false;
     expect(analyticsServiceMock.mixpanelTrack.calledOnce).to.be.true;
