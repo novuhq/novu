eeab8038e4a7a2016a6229acefe2b96c0862f09b
Pawan Jain <jainpawan211199@gmail.com>
2025-07-23 18:14:15 +0530

    fix: update api description

--- apps/api/src/app/events/dtos/trigger-event-request.dto.ts
+++ apps/api/src/app/events/dtos/trigger-event-request.dto.ts
@@ -227,7 +227,8 @@ export class TriggerEventRequestDto {
   to: TriggerRecipientsPayload;
 
   @ApiPropertyOptional({
-    description: 'A unique identifier for deduplication. If the same `transactionId` is sent again, the trigger is ignored. Useful to prevent duplicate notifications. The retention period depends on your billing tier.',
+    description: `A unique identifier for deduplication. If the same **transactionId** is sent again, 
+      the trigger is ignored. Useful to prevent duplicate notifications. The retention period depends on your billing tier.`,
   })
   @IsString()
   @IsOptional()
