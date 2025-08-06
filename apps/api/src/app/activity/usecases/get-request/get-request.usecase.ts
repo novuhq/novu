7105dddceac765356a85342899f99bc7eabcd840
George <djabarov.george@gmail.com>
2025-08-06 20:56:18 +0300

    refactor(api): streamline request and trace retrieval using QueryBuilder for improved readability and maintainability

--- apps/api/src/app/activity/usecases/get-request/get-request.usecase.ts
+++ apps/api/src/app/activity/usecases/get-request/get-request.usecase.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { RequestLogRepository, TraceLogRepository } from '@novu/application-generic';
+import { QueryBuilder, RequestLog, RequestLogRepository, Trace, TraceLogRepository } from '@novu/application-generic';
 import { GetRequestResponseDto, TraceResponseDto } from '../../dtos/get-request-traces.response.dto';
 import { mapRequestLogToResponseDto, mapTraceToResponseDto } from '../../shared/mappers';
 import { GetRequestCommand } from './get-request.command';
@@ -12,25 +12,29 @@ export class GetRequest {
   ) {}
 
   async execute(command: GetRequestCommand): Promise<GetRequestResponseDto> {
+    const requestQueryBuilder = new QueryBuilder<RequestLog>({
+      environmentId: command.environmentId,
+    });
+    requestQueryBuilder.whereEquals('id', command.requestId);
+    requestQueryBuilder.whereEquals('organization_id', command.organizationId);
+
     const request = await this.requestLogRepository.findOne({
-      where: [
-        { id: { operator: '=', value: command.requestId } },
-        { organization_id: { operator: '=', value: command.organizationId } },
-        { environment_id: { operator: '=', value: command.environmentId } },
-      ],
+      where: requestQueryBuilder.build(),
     });
 
     if (!request?.data) {
       throw new NotFoundException(`Request with requestId ${command.requestId} not found`);
     }
 
+    const traceQueryBuilder = new QueryBuilder<Trace>({
+      environmentId: command.environmentId,
+    });
+    traceQueryBuilder.whereEquals('entity_id', command.requestId);
+    traceQueryBuilder.whereEquals('entity_type', 'request');
+    traceQueryBuilder.whereEquals('organization_id', command.organizationId);
+
     const traceResult = await this.traceLogRepository.find({
-      where: [
-        { entity_id: { operator: '=', value: command.requestId } },
-        { entity_type: { operator: '=', value: 'request' } },
-        { environment_id: { operator: '=', value: command.environmentId } },
-        { organization_id: { operator: '=', value: command.organizationId } },
-      ],
+      where: traceQueryBuilder.build(),
       orderBy: 'created_at',
       orderDirection: 'ASC',
     });
