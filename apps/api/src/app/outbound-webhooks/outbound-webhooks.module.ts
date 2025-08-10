7ece67c976c8aedaf05ecd61e154c4ef3ac2a7d5
George <djabarov.george@gmail.com>
2025-08-10 14:10:04 +0300

    refactor(webhooks): rename WebhooksController to OutboundWebhooksController and update routes accordingly

--- apps/api/src/app/outbound-webhooks/outbound-webhooks.module.ts
+++ apps/api/src/app/outbound-webhooks/outbound-webhooks.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SendWebhookMessage, SvixProviderService } from '@novu/application-generic';
 import { SharedModule } from '../shared/shared.module';
-import { WebhooksController } from './outbound-webhooks.controller';
+import { OutboundWebhooksController } from './outbound-webhooks.controller';
 import { CreateWebhookPortalUsecase } from './usecases/create-webhook-portal-token/create-webhook-portal.usecase';
 import { GetWebhookPortalTokenUsecase } from './usecases/get-webhook-portal-token/get-webhook-portal-token.usecase';
 
 @Module({
   imports: [SharedModule],
-  controllers: [WebhooksController],
+  controllers: [OutboundWebhooksController],
   providers: [GetWebhookPortalTokenUsecase, CreateWebhookPortalUsecase, SvixProviderService, SendWebhookMessage],
   exports: [SendWebhookMessage],
 })
