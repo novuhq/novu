c2781e08720c3077e73a2b10364a81508eaf6f96
George <djabarov.george@gmail.com>
2025-08-11 14:59:11 +0300

    feat(api-service): inject outbound webhook module

--- apps/api/src/app/inbox/inbox.module.ts
+++ apps/api/src/app/inbox/inbox.module.ts
@@ -1,16 +1,33 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, ForwardReference, Module, Type } from '@nestjs/common';
 import { CommunityOrganizationRepository } from '@novu/dal';
 import { AuthModule } from '../auth/auth.module';
 import { IntegrationModule } from '../integrations/integrations.module';
 import { OrganizationModule } from '../organization/organization.module';
+import { OutboundWebhooksModule } from '../outbound-webhooks/outbound-webhooks.module';
 import { PreferencesModule } from '../preferences';
 import { SharedModule } from '../shared/shared.module';
 import { SubscribersV1Module } from '../subscribers/subscribersV1.module';
 import { InboxController } from './inbox.controller';
 import { USE_CASES } from './usecases';
 
+const enterpriseImports = (): Array<Type | DynamicModule | Promise<DynamicModule> | ForwardReference> => {
+  const imports: (Type<any> | DynamicModule | Promise<DynamicModule> | ForwardReference<any>)[] = [];
+  if (process.env.NOVU_ENTERPRISE === 'true') {
+    imports.push(OutboundWebhooksModule);
+  }
+  return imports;
+};
+
 @Module({
-  imports: [SharedModule, SubscribersV1Module, AuthModule, IntegrationModule, PreferencesModule, OrganizationModule],
+  imports: [
+    SharedModule,
+    SubscribersV1Module,
+    AuthModule,
+    IntegrationModule,
+    PreferencesModule,
+    OrganizationModule,
+    ...enterpriseImports(),
+  ],
   providers: [...USE_CASES, CommunityOrganizationRepository],
   exports: [...USE_CASES],
   controllers: [InboxController],
--- apps/api/src/app/widgets/widgets.module.ts
+++ apps/api/src/app/widgets/widgets.module.ts
@@ -1,15 +1,24 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { DynamicModule, ForwardReference, forwardRef, Module, Type } from '@nestjs/common';
 
 import { CommunityOrganizationRepository } from '@novu/dal';
 import { AuthModule } from '../auth/auth.module';
 import { IntegrationModule } from '../integrations/integrations.module';
+import { OutboundWebhooksModule } from '../outbound-webhooks/outbound-webhooks.module';
 import { SharedModule } from '../shared/shared.module';
 import { SubscribersV1Module } from '../subscribers/subscribersV1.module';
 import { USE_CASES } from './usecases';
 import { WidgetsController } from './widgets.controller';
 
+const enterpriseImports = (): Array<Type | DynamicModule | Promise<DynamicModule> | ForwardReference> => {
+  const imports: (Type<any> | DynamicModule | Promise<DynamicModule> | ForwardReference<any>)[] = [];
+  if (process.env.NOVU_ENTERPRISE === 'true') {
+    imports.push(OutboundWebhooksModule);
+  }
+  return imports;
+};
+
 @Module({
-  imports: [SharedModule, forwardRef(() => SubscribersV1Module), AuthModule, IntegrationModule],
+  imports: [SharedModule, forwardRef(() => SubscribersV1Module), AuthModule, IntegrationModule, ...enterpriseImports()],
   providers: [...USE_CASES, CommunityOrganizationRepository],
   exports: [...USE_CASES],
   controllers: [WidgetsController],
