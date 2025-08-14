c19e9f640d55f2825f00e67eccff56c862f54eb1
Paweł <pawel.tymczuk@gmail.com>
2025-08-14 17:21:39 +0200

    chore(api-service): fixed failing tests

--- apps/api/src/app/shared/utils/issues.ts
+++ apps/api/src/app/shared/utils/issues.ts
@@ -156,18 +156,20 @@ export const processControlValuesByLiquid = ({
 
       issues.controls[controlKey] = liquidTemplateIssues.invalidVariables.map((invalidVariable) => {
         const message = invalidVariable.message ? invalidVariable.message.split(' line:')[0] : '';
+        const variableName = invalidVariable.name === 'unknown' ? '{{}}' : invalidVariable.name;
+
         if ('filterMessage' in invalidVariable) {
           return {
-            message: `Filter "${invalidVariable.filterMessage}" in "${invalidVariable.name}"`,
+            message: `Filter "${invalidVariable.filterMessage}" in "${variableName}"`,
             issueType: ContentIssueEnum.INVALID_FILTER_ARG_IN_VARIABLE,
-            variableName: invalidVariable.name,
+            variableName: variableName,
           };
         }
 
         return {
-          message: `Variable "${invalidVariable.name}" ${message}`.trim(),
+          message: `Variable "${variableName}" ${message}`.trim(),
           issueType: ContentIssueEnum.ILLEGAL_VARIABLE_IN_CONTROL_VALUE,
-          variableName: invalidVariable.name,
+          variableName: variableName,
         };
       });
     } else {
