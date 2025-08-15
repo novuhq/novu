99713d0a1a423d72c5f34cc2bbbb91c957a496cd
Dima Grossman <dima@grossman.io>
2025-08-15 11:08:06 +0300

    Update command-menu.tsx

--- apps/dashboard/src/components/command-palette/command-menu.tsx
+++ apps/dashboard/src/components/command-palette/command-menu.tsx
@@ -34,6 +34,11 @@ const CommandDialog = ({
             'grid min-h-0 auto-cols-auto grid-flow-row',
             '[&>[cmdk-label]+*]:!border-t-0'
           )}
+          filter={(value, search, keywords) => {
+            const extendValue = value + ' ' + (keywords?.join(' ') || '');
+            if (extendValue.includes(search)) return 1;
+            return 0;
+          }}
         >
           {children}
         </Command>
