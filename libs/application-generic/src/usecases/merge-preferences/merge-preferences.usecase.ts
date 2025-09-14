951fe18f74569a4b74e3022dc10ddacfe2289d61
Dima Grossman <dima@grossman.io>
2025-09-14 12:26:11 +0300

    Update merge-preferences.usecase.ts

--- libs/application-generic/src/usecases/merge-preferences/merge-preferences.usecase.ts
+++ libs/application-generic/src/usecases/merge-preferences/merge-preferences.usecase.ts
@@ -1,7 +1,7 @@
 /** biome-ignore-all lint/suspicious/noImplicitAnyLet: In order to merge the preferences */
 /** biome-ignore-all lint/complexity/noStaticOnlyClass: In order to merge the preferences */
 /** biome-ignore-all lint/suspicious/noExplicitAny: In order to merge the preferences */
-import { PreferencesTypeEnum } from '@novu/shared';
+import { PreferencesTypeEnum, Schedule, WorkflowPreferences } from '@novu/shared';
 import { merge } from 'es-toolkit';
 import { GetPreferencesResponseDto } from '../get-preferences';
 import { MergePreferencesCommand } from './merge-preferences.command';
@@ -47,9 +47,8 @@ export class MergePreferences {
       [PreferencesTypeEnum.SUBSCRIBER_WORKFLOW]: command.subscriberWorkflowPreference?.preferences || null,
     };
 
-    // Manual merge for better performance - we only have max 4 items
-    let resultPreferences: any = {};
-    let resultSchedule: any = {};
+    let resultPreferences: WorkflowPreferences = {} as WorkflowPreferences;
+    let resultSchedule: Schedule = {} as Schedule;
     let resultType;
 
     for (const pref of preferencesList) {
