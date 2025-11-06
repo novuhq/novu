0d901ae4b0edded39c44a71504f2e11663fe7674
Dima Grossman <dima@grossman.io>
2025-11-06 20:11:50 +0200

    Update message.repository.ts

--- libs/dal/src/repositories/message/message.repository.ts
+++ libs/dal/src/repositories/message/message.repository.ts
@@ -878,7 +878,7 @@ export class MessageRepository extends BaseRepository<MessageDBModel, MessageEnt
       await this.update(idQuery, { $set: updatePayload });
     }
 
-    return this.find(idQuery, undefined, { limit: 200 });
+    return this.find(idQuery, undefined, { limit: 100 });
   }
 
   async updateActionStatus({
