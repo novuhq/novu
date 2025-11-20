bbeb0f9ca5eb2e086c94cc8acbab989dcfb87628
Dima Grossman <dima@grossman.io>
2025-11-20 18:23:46 +0200

    Update process-bulk-trigger.usecase.ts

--- apps/api/src/app/events/usecases/process-bulk-trigger/process-bulk-trigger.usecase.ts
+++ apps/api/src/app/events/usecases/process-bulk-trigger/process-bulk-trigger.usecase.ts
@@ -81,7 +81,7 @@ export class ProcessBulkTrigger {
       );
     };
 
-    const BATCH_SIZE = 10;
+    const BATCH_SIZE = 25;
     const results: TriggerEventResponseDto[] = [];
 
     for (let i = 0; i < command.events.length; i += BATCH_SIZE) {
