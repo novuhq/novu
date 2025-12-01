9a7431b5d8dbb121954685ab671615138e6ee84d
Adam Chmara <adam.chmara1@gmail.com>
2025-12-01 21:11:07 +0100

    refactor(api): inject module instead of usecase

--- apps/api/src/app/integrations/integrations.module.ts
+++ apps/api/src/app/integrations/integrations.module.ts
@@ -5,14 +5,10 @@ import {
   CompileTemplate,
   GetNovuProviderCredentials,
 } from '@novu/application-generic';
-import {
-  ChannelConnectionRepository,
-  ChannelEndpointRepository,
-  CommunityOrganizationRepository,
-  CommunityUserRepository,
-  ContextRepository,
-} from '@novu/dal';
+import { CommunityOrganizationRepository, CommunityUserRepository } from '@novu/dal';
 import { AuthModule } from '../auth/auth.module';
+import { ChannelConnectionsModule } from '../channel-connections/channel-connections.module';
+import { ChannelEndpointsModule } from '../channel-endpoints/channel-endpoints.module';
 import { SharedModule } from '../shared/shared.module';
 import { IntegrationsController } from './integrations.controller';
 import { USE_CASES } from './usecases';
@@ -20,17 +16,9 @@ import { USE_CASES } from './usecases';
 const PROVIDERS = [ChannelFactory, CompileTemplate, GetNovuProviderCredentials, CalculateLimitNovuIntegration];
 
 @Module({
-  imports: [SharedModule, forwardRef(() => AuthModule)],
+  imports: [SharedModule, forwardRef(() => AuthModule), ChannelConnectionsModule, ChannelEndpointsModule],
   controllers: [IntegrationsController],
-  providers: [
-    ...USE_CASES,
-    CommunityOrganizationRepository,
-    CommunityUserRepository,
-    ChannelConnectionRepository,
-    ChannelEndpointRepository,
-    ContextRepository,
-    ...PROVIDERS,
-  ],
+  providers: [...USE_CASES, CommunityOrganizationRepository, CommunityUserRepository, ...PROVIDERS],
   exports: [...USE_CASES],
 })
 export class IntegrationModule {}
