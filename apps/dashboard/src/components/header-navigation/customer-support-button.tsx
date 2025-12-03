f34552dd9f7733a061ada195d785d2f20a229d6d
George Djabarov <djabarov.george@gmail.com>
2025-12-03 15:56:57 +0200

    feat: integrate contextual help drawer feature flag and enhance customer support button functionality

--- apps/dashboard/src/components/header-navigation/customer-support-button.tsx
+++ apps/dashboard/src/components/header-navigation/customer-support-button.tsx
@@ -5,10 +5,13 @@ import { IS_SELF_HOSTED } from '../../config';
 import { openInNewTab } from '../../utils/url';
 import { HeaderButton } from './header-button';
 import { SupportDrawer } from './support-drawer';
+import { useFeatureFlag } from '@/hooks/use-feature-flag';
+import { FeatureFlagsKeysEnum } from '@novu/shared';
 
 export const CustomerSupportButton = () => {
-  usePlainChat();
   useBootIntercom();
+  const { showPlainLiveChat } = usePlainChat();
+  const isContextualHelpEnabled = useFeatureFlag(FeatureFlagsKeysEnum.IS_CONTEXTUAL_HELP_DRAWER_ENABLED);
 
   if (IS_SELF_HOSTED) {
     return (
@@ -24,7 +27,7 @@ export const CustomerSupportButton = () => {
     );
   }
 
-  return (
+  return isContextualHelpEnabled ? (
     <SupportDrawer>
       <button tabIndex={-1} className="flex items-center justify-center">
         <HeaderButton label="Help">
@@ -32,5 +35,11 @@ export const CustomerSupportButton = () => {
         </HeaderButton>
       </button>
     </SupportDrawer>
+  ) : (
+    <button tabIndex={-1} className="flex items-center justify-center" onClick={() => showPlainLiveChat()}>
+      <HeaderButton label="Help">
+        <RiQuestionFill className="text-foreground-600 size-4" />
+      </HeaderButton>
+    </button>
   );
 };
