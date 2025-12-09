b2ff92fedb4f897ba67eb5c6f30dd5c7c008b183
Paweł <pawel.tymczuk@gmail.com>
2025-12-09 13:04:00 +0100

    chore(internal-sdk): code update

--- libs/internal-sdk/src/lib/config.ts
+++ libs/internal-sdk/src/lib/config.ts
@@ -61,6 +61,6 @@ export const SDK_METADATA = {
   language: 'typescript',
   openapiDocVersion: '3.11.0',
   sdkVersion: '0.1.21',
-  genVersion: '2.772.0',
-  userAgent: 'speakeasy-sdk/typescript 0.1.21 2.772.0 3.11.0 @novu/api',
+  genVersion: '2.776.1',
+  userAgent: 'speakeasy-sdk/typescript 0.1.21 2.776.1 3.11.0 @novu/api',
 } as const;
