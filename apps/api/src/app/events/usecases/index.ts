4b04c5b426019cb996ae8d02c4a3ee82f73a4158
Dima Grossman <dima@grossman.io>
2025-12-10 19:48:50 +0200

    Update index.ts

--- apps/api/src/app/events/usecases/index.ts
+++ apps/api/src/app/events/usecases/index.ts
@@ -4,10 +4,4 @@ import { ProcessBulkTrigger } from './process-bulk-trigger';
 import { SendTestEmail } from './send-test-email';
 import { TriggerEventToAll } from './trigger-event-to-all';
 
-export const USE_CASES = [
-  CancelDelayed,
-  TriggerEventToAll,
-  ParseEventRequest,
-  ProcessBulkTrigger,
-  SendTestEmail,
-];
+export const USE_CASES = [CancelDelayed, TriggerEventToAll, ParseEventRequest, ProcessBulkTrigger, SendTestEmail];
