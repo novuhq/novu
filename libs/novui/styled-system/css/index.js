afddeb5eb1d862576d5aa3c9def77ff056a467be
George Djabarov <djabarov.george@gmail.com>
2026-01-18 11:21:43 +0200

    chore: remove redundnat code

--- libs/novui/styled-system/css/index.js
+++
@@ -1,4 +0,0 @@
-export * from './css.js';
-export * from './cx.js';
-export * from './cva.js';
-export * from './sva.js';
--- libs/novui/styled-system/jsx/factory.d.ts
+++
@@ -1,3 +0,0 @@
-/* eslint-disable */
-import type { Styled } from '../types/jsx';
-export declare const styled: Styled
