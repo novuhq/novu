1b10ce05a6774cd40317091dca13129582273879
George Djabarov <djabarov.george@gmail.com>
2026-02-01 21:25:09 +0200

    refactor(api-service): update TTL expression in traces schema to use toDateTime function

--- libs/application-generic/src/services/analytic-logs/trace-log/trace-log.schema.ts
+++ libs/application-generic/src/services/analytic-logs/trace-log/trace-log.schema.ts
@@ -80,7 +80,7 @@ const clickhouseSchemaOptions = {
   table_name: TABLE_NAME,
   engine: 'MergeTree',
   order_by: `(${ORDER_BY.join(', ')})` as any,
-  additional_options: ['PARTITION BY toYYYYMM(created_at)', `TTL ${TTL}`],
+  additional_options: ['PARTITION BY toYYYYMM(created_at)', `TTL toDateTime(${TTL})`],
 };
 
 export const traceLogSchema = new ClickhouseSchema(schemaDefinition, clickhouseSchemaOptions);
