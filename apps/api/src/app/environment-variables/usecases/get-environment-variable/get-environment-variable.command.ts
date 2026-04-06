fb8b33e211af30db4b0eaae525916c0f15f21c74
George Djabarov <djabarov.george@gmail.com>
2026-04-06 19:05:06 +0300

    refactor(api): update environment variable endpoints to use variable keys instead of IDs
    - Changed all references from variableId to variableKey in the EnvironmentVariablesController and related use cases.
    - Updated API documentation to reflect the new parameter name and description.
    - Adjusted frontend API calls and hooks to align with the new key-based approach for environment variables.

--- apps/api/src/app/environment-variables/usecases/get-environment-variable/get-environment-variable.command.ts
+++ apps/api/src/app/environment-variables/usecases/get-environment-variable/get-environment-variable.command.ts
@@ -4,5 +4,5 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class GetEnvironmentVariableCommand extends OrganizationLevelWithUserCommand {
   @IsString()
   @IsNotEmpty()
-  variableId: string;
+  variableKey: string;
 }
