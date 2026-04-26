510a71fbb4125a8b9db9b35736f8aaec0a30c21f
Dima Grossman <dima@grossman.io>
2026-04-26 13:49:45 +0300

    Update side-navigation.tsx

--- apps/dashboard/src/components/side-navigation/side-navigation.tsx
+++ apps/dashboard/src/components/side-navigation/side-navigation.tsx
@@ -1,12 +1,11 @@
 import { ApiServiceLevelEnum, FeatureFlagsKeysEnum, GetSubscriptionDto, PermissionsEnum } from '@novu/shared';
-import { ReactNode } from 'react';
+import { ReactNode, SVGProps } from 'react';
 import {
   RiBarChartBoxLine,
   RiBuildingLine,
   RiCodeSSlashLine,
   RiDatabase2Line,
   RiDiscussLine,
-  RiEarthLine,
   RiGroup2Line,
   RiKey2Line,
   RiLayout5Line,
@@ -43,6 +42,14 @@ const NavigationGroup = ({ children, label }: { children: ReactNode; label?: str
     </div>
   );
 };
+
+function MailAiLineIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
+      <path d="M20.7134 8.12811L20.4668 8.69379C20.2864 9.10792 19.7136 9.10792 19.5331 8.69379L19.2866 8.12811C18.8471 7.11947 18.0555 6.31641 17.0677 5.87708L16.308 5.53922C15.8973 5.35653 15.8973 4.75881 16.308 4.57612L17.0252 4.25714C18.0384 3.80651 18.8442 2.97373 19.2761 1.93083L19.5293 1.31953C19.7058 0.893489 20.2942 0.893489 20.4706 1.31953L20.7238 1.93083C21.1558 2.97373 21.9616 3.80651 22.9748 4.25714L23.6919 4.57612C24.1027 4.75881 24.1027 5.35653 23.6919 5.53922L22.9323 5.87708C21.9445 6.31641 21.1529 7.11947 20.7134 8.12811ZM2 4C2 3.44772 2.44772 3 3 3H14V5H4.5052L12 11.662L16.3981 7.75259L17.7269 9.24741L12 14.338L4 7.22684V19H20V11H22V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4Z" />
+    </svg>
+  );
+}
 
 type BottomNavigationProps = {
   isTrialActive?: boolean;
@@ -288,8 +295,8 @@ export const SideNavigation = () => {
                         : undefined
                     }
                   >
-                    <RiEarthLine className="size-4" />
-                    <span>Domains</span>
+                    <MailAiLineIcon className="size-4" />
+                    <span>Inbound Email</span>
                   </NavigationLink>
                 )}
                 <NavigationLink
