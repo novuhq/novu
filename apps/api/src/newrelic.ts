275e6837c061dfd3c7b5383a0c2a6b93a5542125
Himanshu Garg <garg_himanshu@outlook.com>
2026-05-05 17:00:36 +0530

    chore(api): update New Relic logging configuration to use stdout and set log level to trace

--- apps/api/src/newrelic.ts
+++ apps/api/src/newrelic.ts
@@ -41,7 +41,7 @@ exports.config = {
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level: 'info',
+    level: 'trace',
   },
   /**
    * When true, all request headers except for those listed in attributes.exclude
